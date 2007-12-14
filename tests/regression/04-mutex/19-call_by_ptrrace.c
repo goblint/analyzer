1f12c048858468f98ff22d9520b0f1a056918fdf
Vesal Vojdani <vesal.vojdani@gmail.com>
2007-12-14 15:37:47 +0000

    hmm... update tests a bit

---
+++ tests/regression/04-mutex/19-call_by_ptrrace.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+
+/**
+ * foo /migh/ call the argument function
+ */
+extern void foo(void (*)(void));
+
+int glob;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+void *t_fun(void *arg) {
+  pthread_mutex_lock(&mutex2);
+  glob=glob+1;
+  pthread_mutex_unlock(&mutex2);
+  return NULL;
+}
+
+void reset_glob(void) {
+  pthread_mutex_lock(&mutex1);
+  glob=glob+1;
+  pthread_mutex_unlock(&mutex1);
+}
+
+int main() {
+  pthread_t id;
+  pthread_create(&id, NULL, t_fun, NULL);
+  pthread_join (id, NULL);
+/*  reset_glob();*/
+  foo(reset_glob);
+  return 0;
+}
