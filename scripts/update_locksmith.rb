4e333770705312e092443baa7cf81718c9e7a784
Vesal Vojdani <vesal.vojdani@gmail.com>
2008-04-04 15:18:04 +0000

    previous

--- scripts/update_locksmith.rb
+++ scripts/update_locksmith.rb
@@ -2,23 +2,25 @@
 
 locksmith = "../others/locksmith-0.4"
 runit = "timeout 300 ./cil/bin/cilly"
-testresults = File.expand_path("tests/ls_result")
-projects    = File.expand_path("../bench")
+ls     = File.expand_path("tests/locksmith")
+bench  = File.expand_path("../bench")
+libc   = "#{ls}/lib.c"
+nlib   = "#{ls}/newlib.c"
 
 #processing the input file
 
-File.open("tests/ls.txt", "r") do |f|
+File.open("#{ls}/ls.txt", "r") do |f|
   Dir.chdir(locksmith)
   i = 0
   while line = f.gets
     fn = line.chomp
     puts "Analyzing #{fn}"
-    file = "#{projects}/pthread/#{fn}_comb.c"
-    res_n = "#{testresults}/normal/#{fn}"
-    res_f = "#{testresults}/field_insensitive/#{fn}"
-    `#{runit} #{file} experiments/newlib.c --merge --no-existentials --no-linearity 2> #{res_n}.txt`
+    file = "#{bench}/pthread/#{fn}_comb.c"
+    res_n = "#{ls}/normal/#{fn}"
+    res_f = "#{ls}/field_insensitive/#{fn}"
+    `#{runit} #{file} #{nlib} --merge --no-existentials --no-linearity 2> #{res_n}.txt`
     `grep 'Possible data race:' #{res_n}.txt > #{res_n}.short.txt`
-    `#{runit} #{file} experiments/newlib.c --merge --no-existentials --no-linearity --field-insensitive 2> #{res_f}.txt`
+    `#{runit} #{file} #{nlib} --merge --no-existentials --no-linearity --field-insensitive 2> #{res_f}.txt`
     `grep 'Possible data race: &' #{res_f}.txt > #{res_f}.short.txt`
   end
 end
