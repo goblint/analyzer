141f612f33bacbe054b24078dd09098d282c185e
Vesal Vojdani <vesal.vojdani@gmail.com>
2008-08-27 12:57:05 +0000

    update tests

--- tests/regression/02-base/06-side_effect1.c
+++ tests/regression/02-base/06-side_effect1.c
@@ -1,26 +1,24 @@
+// PARAM: --analysis base
+
 #include<pthread.h>
 #include<assert.h>
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER; 
 int glob1 = 5;
 int glob2 = 7;
-int glob3 = 7;
+int glob3 = 9;
 
 void *t_fun(void *arg) {
-  pthread_mutex_lock(&m);
-  glob3 = 7;
-  assert(glob3 == 7); // UNKNOWN
-  pthread_mutex_unlock(&m);
+  glob3 = 9;
+  assert(glob3 == 9); // UNKNOWN
   return NULL;
 }
 
 int main() {
   pthread_t id;
-  glob3 = 7;
-  assert(glob3 == 7);
+  glob3 = 9;
+  assert(glob3 == 9);
   pthread_create(&id, NULL, t_fun, NULL);
-
-  pthread_mutex_lock(&m);
 
   glob1 = 5;
   assert(glob1 == 5);
@@ -31,6 +29,5 @@ int main() {
   glob3 = 8;
   assert(glob3 == 8); // UNKNOWN
 
-  pthread_mutex_unlock(&m);
   return 0;
 }
