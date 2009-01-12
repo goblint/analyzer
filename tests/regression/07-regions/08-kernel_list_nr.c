ba665bd32241ad3eb9da832359f9967e031198a6
Vesal Vojdani <vesal.vojdani@gmail.com>
2009-01-12 14:18:50 +0000

    forgot to add

---
+++ tests/regression/07-regions/08-kernel_list_nr.c
@@ -0,0 +1,37 @@
+// PARAM: --kernel --allfuns
+#include<linux/module.h>
+#include<linux/list.h>
+#include<linux/mutex.h>
+
+struct s {
+  int datum;
+  struct list_head list;
+}; 
+
+struct list_head A, B;
+
+static DEFINE_MUTEX(A_mutex);
+static DEFINE_MUTEX(B_mutex);
+
+
+void t1() {
+  struct s *p = kmalloc(sizeof(struct s), GFP_KERNEL);
+  p->datum = 7;
+  INIT_LIST_HEAD(&p->list);
+  
+  mutex_lock(&A_mutex);
+  list_add(p, &A); 
+  p->datum++; // NORACE!
+  mutex_unlock(&A_mutex);
+}
+
+void t2 () {
+  struct s *p = kmalloc(sizeof(struct s), GFP_KERNEL);
+  p->datum = 9;
+  INIT_LIST_HEAD(&p->list);
+  
+  mutex_lock(&A_mutex);
+  list_add(p, &A);
+  p->datum++; // NORACE!
+  mutex_unlock(&A_mutex);
+}
