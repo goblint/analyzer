edd92418c884150e19a59e8c5238d4312519d8f0
Kalmer Apinis <kalmer.apinis@gmail.com>
2009-07-24 11:44:32 +0000

    test for indirect (trough global pointer) access

---
+++ tests/regression/04-mutex/37-indirect_rc.c
@@ -0,0 +1,25 @@
+#include <pthread.h>
+
+int *g1;
+int *g2;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *t_fun(void *arg) {
+  pthread_mutex_lock(&mutex);
+  *g1++; // RACE
+  pthread_mutex_lock(&mutex);
+  return NULL;
+}
+
+int main(void) {
+  pthread_t id;
+  int x;
+  g1 = g2 = &x;
+
+  pthread_create(&id, NULL, t_fun, NULL);
+
+  *g2++; //RACE
+
+  pthread_join (id, NULL);
+  return 0;
+}
