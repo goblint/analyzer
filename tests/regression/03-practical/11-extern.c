b37003fab424d5811803cbf51554fffc29f52542
Vesal Vojdani <vesal.vojdani@gmail.com>
2011-09-27 17:43:49 +0200

    Unknown externs do not invalidate static globals.

--- tests/regression/03-practical/11-extern.c
+++ tests/regression/03-practical/11-extern.c
@@ -1,11 +1,15 @@
 #include <assert.h>
 
+static int glob = 5;
 extern void mywrite(int *x);
 
 int main() {
   int i=0;
+  int j=5;
   mywrite(&i);
   assert(i == 0); // UNKNOWN!
+  assert(glob == 5);
+  assert(j == 5);
   return 0;
 }
 
