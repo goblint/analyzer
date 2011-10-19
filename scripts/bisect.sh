e5238e4fe116aab98bfee29613b9c9ae4b67e694
Vesal Vojdani <vesal.vojdani@gmail.com>
2011-10-19 17:20:19 +0200

    Restore correct benches.

--- scripts/bisect.sh
+++ scripts/bisect.sh
@@ -2,7 +2,8 @@
 ./configure
 make || exit 125
 rm goblint.json
+./scripts/update_bench.rb
 
-goblint tests/regression/01-cpa/01-expressions.c --result pretty | grep '__builtin_strncpy'
-RETVAL=$(( ! $?))
-exit $RETVAL
+#goblint tests/regression/01-cpa/01-expressions.c --result pretty | grep '__builtin_strncpy'
+#RETVAL=$(( ! $?))
+#exit $RETVAL
--- scripts/update_bench.rb
+++ scripts/update_bench.rb
@@ -2,8 +2,9 @@
 require 'fileutils' 
 
 $analyses = [
-  ["one",   ""],
-  ["two",    "--phase 0 --no mutex --phase 1 --with mutex --no-context base"],
+  ["default",   ""],
+  ["region",    "--with region"],
+  ["shape",     "--with shape"],
 ]
 maxlen = $analyses.map { |x| x[0].length }.max + 1
 
