bf585331da2db83ef68122972784363200f5739d
Kalmer Apinis <kalmer.apinis@gmail.com>
2012-07-20 22:43:43 +0200

    improved ocamlbuild and doc. generation

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ set -e
 scripts/set_version.sh
 
 TARGET=src/maingoblint
-FLAGS="-no-links -use-ocamlfind -lib xml-light -j 8"
+FLAGS="-no-links -use-ocamlfind -j 8 -no-log"
 OCAMLBUILD=ocamlbuild
 
 ocb() {
@@ -14,25 +14,31 @@ ocb() {
 
 rule() {
   case $1 in
-    clean)  rm -rf goblint goblint.byte;
-            ocb -clean
-            ;;
-    native) ocb $TARGET.native;
-            ln -sf _build/src/maingoblint.native goblint
-            ;;
-    byte)   ocb $TARGET.byte;
-            ln -sf _build/src/maingoblint.byte goblint.byte
-            ;;
-    all)    ocb $TARGET.native $TARGET.byte;
-            ln -sf _build/src/maingoblint.native goblint;
-            ln -sf _build/src/maingoblint.byte goblint.byte
-            ;;
-    doc)    ocb doclist.docdir/index.html;
-            rm -rf docs;
-            ln -sf _build/doclist.docdir/ docs;;
-    depend) echo "Not needed.";;
-    *)      echo "Unknown action $1";;
-esac; }
+    clean)   rm -rf goblint goblint.byte;
+             ocb -clean
+             ;;
+    native)  ocb $TARGET.native;
+             ln -sf _build/src/maingoblint.native goblint
+             ;;
+    debug)   ocb -tag debug $TARGET.native;
+             ln -sf _build/src/maingoblint.native goblint
+             ;;
+    profile) ocb -tag profile $TARGET.native;
+             ln -sf _build/src/maingoblint.native goblint
+             ;;
+    byte)    ocb $TARGET.byte;
+             ln -sf _build/src/maingoblint.byte goblint.byte
+             ;;
+    all)     ocb $TARGET.native $TARGET.byte;
+             ln -sf _build/src/maingoblint.native goblint;
+             ln -sf _build/src/maingoblint.byte goblint.byte
+             ;;
+    doc)     ls src/*/*.ml src/*.ml | sed 's/.*\/\(.*\)\.ml/\1/' > doclist.odocl;
+             ocb doclist.docdir/index.html;
+             ln -sf _build/doclist.docdir doc;;
+    depend)  echo "No!";;
+    *)       echo "Unknown action '$1'. Try clean, native, debug, profile, byte, or doc.";;
+  esac; }
 
 if [ $# -eq 0 ]; then
   rule all
