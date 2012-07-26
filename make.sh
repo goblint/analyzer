f945168d1832dc0fec999a89d1977ddd3010fb24
Kalmer Apinis <kalmer.apinis@gmail.com>
2012-07-26 16:34:01 +0200

    added deps so that all analyses are added to the binary

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ set -e
 # generate the version file
 scripts/set_version.sh
 
-TARGET=src/maingoblint
+TARGET=src/goblint
 FLAGS="-no-links -use-ocamlfind -j 8 -no-log"
 OCAMLBUILD=ocamlbuild
 
@@ -14,25 +14,25 @@ ocb() {
 
 rule() {
   case $1 in
-    clean)   rm -rf goblint goblint.byte;
+    clean)   rm -rf goblint goblint.byte goblint.ml;
              ocb -clean
              ;;
     opt | nat*)
              ocb $TARGET.native &&
-             mv _build/src/maingoblint.native goblint
+             mv _build/src/goblint.native goblint
              ;;
     debug)   ocb -tag debug $TARGET.native &&
-             mv _build/src/maingoblint.native goblint
+             mv _build/src/goblint.native goblint
              ;;
     profile) ocb -tag profile $TARGET.native &&
-             mv _build/src/maingoblint.native goblint
+             mv _build/src/goblint.native goblint
              ;;
     byte)    ocb $TARGET.byte &&
-             mv _build/src/maingoblint.byte goblint.byte
+             mv _build/src/goblint.byte goblint.byte
              ;;
     all)     ocb $TARGET.native $TARGET.byte &&
-             mv _build/src/maingoblint.native goblint &&
-             mv _build/src/maingoblint.byte goblint.byte
+             mv _build/src/goblint.native goblint &&
+             mv _build/src/goblint.byte goblint.byte
              ;;
     doc*)    rm -rf doc;
              ls src/*/*.ml src/*.ml | sed 's/.*\/\(.*\)\.ml/\1/' > doclist.odocl;
@@ -44,6 +44,9 @@ rule() {
     *)       echo "Unknown action '$1'. Try clean, native, debug, profile, byte, or doc.";;
   esac; }
 
+ls -1 src/analyses/*.ml | perl -pe 's/.*\/(.*)\.ml/open \u$1/g' > src/goblint.ml
+echo "open Maingoblint" >> src/goblint.ml
+
 if [ $# -eq 0 ]; then
   rule all
 else
@@ -52,3 +55,5 @@ else
     shift 
   done
 fi
+
+rm -f src/goblint.ml
