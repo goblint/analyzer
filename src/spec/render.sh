a40dfadd36c86fbcee41808d1a2c4ca38475a078
vogler <ralf.vogler@gmail.com>
2013-03-20 18:31:00 +0100

    basic parts of fileUse.ml as dot-file

--- src/spec/render.sh
+++ src/spec/render.sh
@@ -1,11 +1,14 @@
 mode=${1-"png"}
 file=${2-"file"}
 dst=graph
+viewcmd=gpicview
 mkdir -p ${dst}
 cp ${file}.dot ${dst}
 cd ${dst}
 case "$mode" in
-  png) dot -Tpng -o${file}.png ${file}.dot
+  png) dot -Tpng -o${file}.png ${file}.dot;
+       pkill ${viewcmd};
+       ${viewcmd} ${file}.png &
   ;;
   pdf) rm -f ${file}.tex;
        dot -Txdot ${file}.dot | dot2tex > ${file}.tex;
