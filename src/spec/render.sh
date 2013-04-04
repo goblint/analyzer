b177b0aa9d7415436e8b9a25b75a4d159b2b595e
Ralf Vogler <ralf.vogler@gmail.com>
2013-04-04 16:04:26 +0200

    render.sh: checks

--- src/spec/render.sh
+++ src/spec/render.sh
@@ -1,17 +1,28 @@
+# command -v ls >&- || {echo >&2 bla; exit 1;}
+function check(){
+    set -e # needed to exit script from function
+    hash $1 2>&- || (echo >&2 "$1 is needed but not installed! $2"; exit 1;)
+    set +e # do not exit shell if some command fails (default)
+}
+check dot
 mode=${1-"png"}
 file=${2-"file"}
 dst=graph
 viewcmd=gpicview
+
 mkdir -p ${dst}
 cp ${file}.dot ${dst}
 cd ${dst}
 case "$mode" in
   png) dot -Tpng -o${file}.png ${file}.dot;
+       check ${viewcmd} "Please edit viewcmd accordingly."
        pkill ${viewcmd};
        ${viewcmd} ${file}.png &
   ;;
   pdf) rm -f ${file}.tex;
+       check dot2tex
        dot -Txdot ${file}.dot | dot2tex > ${file}.tex;
+       check pdflatex
        pdflatex ${file}.tex
   ;;
 esac
