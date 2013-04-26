1e069f8071e4f2a30da820b9326ec93fbfb41e70
Ralf Vogler <ralf.vogler@gmail.com>
2013-04-26 20:26:59 +0200

    setup vagrant for local provision testing. see http://www.vagrantup.com/

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -1,25 +1,21 @@
 # This script runs the unit tests inside a Travis CI worker. See also .travis.yml
 # Inspired by http://blog.mlin.net/2013/02/testing-ocaml-projects-on-travis-ci.html
 
-export OPAM_VERSION=1.0.0
+# export OPAM_VERSION=1.0.0
+export OCAML_VERSION=4.00.1
 export OPAM_PACKAGES='ocamlfind camomile batteries cil xml-light'
 
-# install ocaml from apt
+# install non-ocaml dependencies (ocaml from here is too old)
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml
+sudo apt-get install -qq make #ruby code2html
 
-# install opam
-curl -L https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz | tar xz
-pushd opam-${OPAM_VERSION}
-./configure
-make
-sudo make install
-popd
-opam init --auto-setup
+# binary installer for opam and ocaml
+wget http://www.ocamlpro.com/pub/opam_installer.sh
+sh ./opam_installer.sh /usr/local/bin ${OCAML_VERSION} # Install the latest OPAM and OCaml
 eval `opam config -env`
 
 # install packages from opam
 opam install -q -y ${OPAM_PACKAGES}
 
 # compile
-make
+./make.sh
