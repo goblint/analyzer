d86c6fdb8d7b6656dcc080fa193bddd8793e3ea5
Ralf Vogler <ralf.vogler@gmail.com>
2013-04-26 22:08:35 +0200

    travis: don't use opam binary installer because it prompts

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -1,17 +1,28 @@
 # This script runs the unit tests inside a Travis CI worker. See also .travis.yml
 # Inspired by http://blog.mlin.net/2013/02/testing-ocaml-projects-on-travis-ci.html
 
-# export OPAM_VERSION=1.0.0
+export OPAM_VERSION=1.0.0
 export OCAML_VERSION=4.00.1
-export OPAM_PACKAGES='ocamlfind camomile batteries cil xml-light'
+export OPAM_PACKAGES='ocamlfind.1.3.3 camomile.0.8.3 batteries.2.0.0 cil.1.5.1 xml-light.2.2'
 
-# install non-ocaml dependencies (ocaml from here is too old)
+# install dependencies (but use experimental because the default repo's ocaml version is too old)
+# echo "deb http://ftp.de.debian.org/debian experimental main" >> /etc/apt/sources.list
 sudo apt-get update -qq
-sudo apt-get install -qq make #ruby code2html
+sudo apt-get install -qq ocaml curl make m4 #ruby code2html
 
-# binary installer for opam and ocaml
-wget http://www.ocamlpro.com/pub/opam_installer.sh
-sh ./opam_installer.sh /usr/local/bin ${OCAML_VERSION} # Install the latest OPAM and OCaml
+# binary installer for opam (doesn't work with travis because of promt from `opam init`)
+#wget http://www.ocamlpro.com/pub/opam_installer.sh
+#sh ./opam_installer.sh /usr/local/bin system # Install the latest OPAM using the system compiler (if any)
+
+# install opam from source
+curl -L https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz | tar xz
+pushd opam-${OPAM_VERSION}
+./configure
+make
+sudo make install
+popd
+rm -rf opam-${OPAM_VERSION} # otherwise ocamlbuild complains about hygiene violations
+opam init --auto-setup
 eval `opam config -env`
 
 # install packages from opam
