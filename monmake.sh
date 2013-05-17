7185d86789acd3b0905c5ccf0a430de15628f87f
Ralf Vogler <ralf.vogler@gmail.com>
2013-05-17 12:20:05 +0200

    monmake.sh: send error message to sublime plugin for highlighting + only recompile if ocaml source files change

--- monmake.sh
+++ monmake.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-while inotifywait -r -q -e modify src; do
+while file=$(inotifywait -r -q -e modify src); do
+    ext=${file##*.}
+    # only recompile if some ocaml source file changes
+    if [ $ext != "ml" ] && [ $ext != "mli" ] && [ $ext != "mll" ] && [ $ext != "mly" ]; then
+        continue
+    fi
 	clear
-	make
+    make
 	if [ $? -eq 0 ]; then
 		clear
 		#./test.sh file
 		./test.sh spec
 		#firefox result/file.c.html
 	else
+        # TODO find a way to save stdout to var and still print it to avoid calling make again just for the error message
+        msg=$(make)
+        # send error message to sublime plugin
+        echo -e "`pwd`\n$msg" | nc localhost 9999
 		notify-send -i stop Build failed!
 	fi
 done
