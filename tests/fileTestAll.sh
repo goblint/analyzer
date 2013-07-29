bc5cbe774c269a73e5b28a0516dc5eb165773a6c
Ralf Vogler <ralf.vogler@gmail.com>
2013-07-29 12:51:23 +0200

    rename 17-file to 18-file

--- tests/fileTestAll.sh
+++ tests/fileTestAll.sh
@@ -1,2 +1,2 @@
-for f in tests/regression/17-file/*.c; do ./goblint --sets result html --sets ana.activated[0][+] file $f; done
-./goblint --sets result html --sets ana.activated[0][+] file tests/regression/17-file/*.c
+for f in tests/regression/18-file/*.c; do ./goblint --sets result html --sets ana.activated[0][+] file $f; done
+./goblint --sets result html --sets ana.activated[0][+] file tests/regression/18-file/*.c
--- tests/regression.sh
+++ tests/regression.sh
@@ -1,4 +1,4 @@
 ana=${1-"file"}
-for f in tests/regression/17-file/*.c;
+for f in tests/regression/18-file/*.c;
 	do ./test.sh $ana $f 2>/dev/null | python2 tests/regression.py $f;
 done
