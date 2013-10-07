6092ea784afe30708d7cc5a5e36f92a6786c74cc
Ralf Vogler <ralf.vogler@gmail.com>
2013-10-08 01:28:41 +0200

    vagrant also works now
    	vagrant up
    	vagrant ssh
    	sudo su -
    	cd analyzer
    
    removed old provisioning script

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -1,39 +1,37 @@
 # This script is for either running in a Travis CI worker or the VM setup by vagrant. See .travis.yml and Vagrantfile
 # Inspired by https://github.com/lunaryorn/flycheck
 
-ppa () {
-    for ppa in "$@"; do
-        sudo apt-add-repository -y "$ppa"
-    done
-}
-apt_update () {
+# setup base system and clone goblint if not running in travis-ci
+if test -e "make.sh"; then # travis-ci
+    echo "already in repository"
+    # USER=`whoami`
+else # vagrant
+    apt () {
+        sudo apt-get install -yy --fix-missing "$@"
+    }
+    # update repositories to prevent errors caused by missing packages
     sudo apt-get update -qq
-}
-apt () {
-    sudo apt-get install -yy --fix-missing "$@"
-}
+    apt python-software-properties # needed for ppa
+    apt make m4  # needed for compiling ocamlfind
+    apt patch    # needed for compiling xml-light
+    apt autoconf # needed for compiling cil
+    apt git      # needed for cloning goblint source
 
-# Silence debconf
-export DEBIAN_FRONTEND='noninteractive'
-
-# Update repositories to prevent errors caused by missing packages
-apt_update
-
-# Needed for ppa
-apt python-software-properties
-
-# Bring in the necessary PPAs and 3rd party repositories
-# ocaml in standard Ubuntu repo is too old and compiling with opam takes too long
-apt make m4  # needed for compiling ocamlfind
-apt patch    # needed for compiling xml-light
-apt autoconf # needed for compiling cil
-apt git      # needed for cloning goblint source
+    # USER=vagrant # provisioning is done as root, but ssh login is 'vagrant'
+    cd /root # just do everything as root and later use 'sudo su -' for ssh
+    if test ! -e "analyzer"; then # ignore if source already exists
+        git clone https://github.com/vogler/analyzer.git
+        # chown -hR $USER:$USER analyzer # make ssh user the owner
+    fi
+    pushd analyzer
+fi
 
 
-# http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
+# install ocaml and friends, see http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
 OPAM_DEPENDS="ocamlfind camomile batteries xml-light cil"
-   
-case "$OCAML_VERSION,$OPAM_VERSION" in
+
+# use default versions if none are set in environment
+case "${OCAML_VERSION:=4.01.0},${OPAM_VERSION:=1.0.0}" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
 3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
 4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
@@ -42,23 +40,15 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
-   
+
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
-opam init 
+opam init
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 
-
-# clone goblint (if not running in travis-ci)
-if test -e "make.sh"; then
-    echo "already in repository"
-else
-    git clone https://github.com/vogler/analyzer.git
-    pushd analyzer
-fi
 
 # compile
 ./make.sh
