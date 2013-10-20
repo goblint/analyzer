759010f3a8d03259a1cd2da7b34540b75b812c70
Ralf Vogler <ralf.vogler@gmail.com>
2013-10-20 23:57:06 +0200

    TODO: state of global pointer does not get updated

---
+++ tests/regression/18-file/29-alias-global.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+FILE* fp;
+FILE* myfopen(char* f){
+  	fp = fopen(f, "a");
+  	return fp;
+}
+
+int main(){
+	FILE *fp1;
+	FILE *fp2;
+	fp1 = myfopen("test1.txt");
+	fp2 = myfopen("test2.txt");
+	fprintf(fp1, "Testing...\n");  
+	fclose(fp1);
+	fprintf(fp2, "Testing...\n");  
+	fclose(fp2);
+}
+
+// All ok!
