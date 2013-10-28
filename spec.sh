dd8203d381a09ddcba713a78c36c7d4c66a3607f
Ralf Vogler <ralf.vogler@gmail.com>
2013-10-28 23:11:44 +0100

    show number of passed tests adjust path to spec

--- spec.sh
+++ spec.sh
@@ -1,7 +1,7 @@
 # print all states the parser goes through
 #export OCAMLRUNPARAM='p'
 bin=src/mainspec.native
-spec=${1-"src/spec/file.spec"}
+spec=${1-"tests/regression/18-file/file.spec"}
 ocamlbuild -yaccflag -v -X webapp -no-links -use-ocamlfind $bin \
     && (./_build/$bin $spec \
         || (echo "$spec failed, running interactive now...";
--- tests/regression.sh
+++ tests/regression.sh
@@ -1,7 +1,10 @@
 ana=${1-"file"}
 r=0
+n=0
+c=0
 for f in tests/regression/18-file/*.c; do
-	./test.sh $ana $f 2>/dev/null | python2 tests/regression.py $f || r=1
-	# r=$? && $r
+	./test.sh $ana $f 2>/dev/null | python2 tests/regression.py $f && ((c++)) || r=1
+	((n++))
 done
+echo "passed $c/$n tests"
 exit $r
