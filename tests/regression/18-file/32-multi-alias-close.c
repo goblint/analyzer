ea400ae97601cca753004a1ad50ed1be6c9b6d39
Ralf Vogler <ralf.vogler@gmail.com>
2013-11-03 19:45:10 +0100

    new tests for may alias

---
+++ tests/regression/18-file/32-multi-alias-close.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+  	FILE *fp1;
+	fp1 = fopen("test1.txt", "a");
+	fprintf(fp1, "Testing...\n");
+
+	FILE *fp2;
+	fp2 = fopen("test2.txt", "a");
+	fprintf(fp2, "Testing...\n");
+
+  	FILE *fp;
+  	int b;
+    if(b){
+      fp = fp1;
+    }else{
+      fp = fp2;
+    }
+
+  	fclose(fp);
+	fclose(fp1); // WARN: MAYBE closeing already closed file handle fp1
+	fclose(fp2); // WARN: MAYBE closeing already closed file handle fp2
+}
---
+++ tests/regression/18-file/33-multi-alias-open.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+  	FILE *fp1;
+	fp1 = fopen("test1.txt", "a"); // WARN: MAYBE file is never closed
+
+	FILE *fp2;
+	fp2 = fopen("test2.txt", "r"); // WARN: MAYBE file is never closed
+	
+  	FILE *fp;
+  	int b;
+    if(b){
+      fp = fp1;
+    }else{
+      fp = fp2;
+    }
+
+  	fprintf(fp, "Testing...\n"); // WARN: MAYBE writing to read-only file handle fp
+
+  	fclose(fp);
+} // WARN: MAYBE unclosed files: fp1, fp2
