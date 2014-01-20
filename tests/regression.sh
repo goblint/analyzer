8a4a54114cc1199e5e06e3557977187366e24c30
Ralf Vogler <ralf.vogler@gmail.com>
2014-01-20 15:07:32 +0100

    test for mutex locks disable debug output when doing regression tests

--- tests/regression.sh
+++ tests/regression.sh
@@ -1,10 +1,13 @@
 ana=${1-"file"}
+debug_tmp=$debug
+debug=false # temporarily disable debug output
 n=0
 c=0
 for f in tests/regression/18-file/*.c; do
 	./test.sh $ana $f 2>/dev/null | python2 tests/regression.py $f && ((c++))
 	((n++))
 done
+debug=$debug_tmp
 msg="passed $c/$n tests"
 echo $msg
 if [ $c -eq $n ]; then
