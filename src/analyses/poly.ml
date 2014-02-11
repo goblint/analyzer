754124d678369ae80b772faec0ca51cd4c9a17a9
Kalmer Apinis <kalmer.apinis@gmail.com>
2014-02-11 18:05:23 +0100

    interprocedural convex polyhedra

--- src/analyses/poly.ml
+++ src/analyses/poly.ml
@@ -12,64 +12,62 @@ struct
   include Analyses.DefaultSpec
 
   let name = "poly"
-  
+
   module D = D
   module G = Lattice.Unit
-  module C = Lattice.Unit
+  module C = D
   
-  let val_of () = D.top ()
-  let context _ = ()
+  let val_of x = x
+  let context x = x
   
   let otherstate _ = D.top ()       
   let exitstate  _ = D.top ()      
   let startstate _ = D.top ()       
-
-  let enter      ctx r f args = [ctx.local,ctx.local]  
-  let combine    ctx r fe f args d = D.top ()   
+  
+  let enter ctx r f args = 
+    let f = Cilfacade.getdec f in
+    let newnames = List.map (fun x -> x.vname) f.sformals @
+                   List.map (fun x -> x.vname^"'") f.sformals in
+    let newd = D.add_vars ctx.local newnames in
+    List.iter2 (fun v e -> D.assign_var_with newd    v.vname e) f.sformals args;
+    List.iter  (fun v   -> D.assign_var_eq_with newd v.vname (v.vname^"'")) f.sformals;
+    D.forget_all_but_with newd newnames;
+    [ctx.local, newd]  
+  
+  
+  let combine ctx r fe f args d = 
+    let f = Cilfacade.getdec f in
+    match r with
+      | Some (Var v, NoOffset) ->
+        let nd = D.forget_all ctx.local [v.vname] in
+        let nd' = D.add_vars d (List.map (fun x -> Var.to_string x) (D.get_vars ctx.local)) in
+        D.substitute_var_eq_with nd' "#ret" v.vname;
+        List.iter2 (fun v e -> D.substitute_var_with nd' (v.vname^"'") e) f.sformals args;
+        D.forget_all_with nd' ("#ret" :: List.map (fun x -> x.vname^"'") f.sformals);
+        A.unify Man.mgr nd nd'
+      | _ -> D.top ()
+      
   let special    ctx r f args = D.top () 
   let branch     ctx e  b = ctx.local
-
-  open Texpr1 
-
-  let rec exptoexpr env = 
-    function 
-    | Lval (Var v,NoOffset) ->
-        Var (Var.of_string v.vname)
-    | Const (CInt64 (i,_,_)) -> 
-        Cst (Coeff.s_of_int (Int64.to_int i))
-    | UnOp  (Neg ,e,_) -> 
-        Unop (Neg,exptoexpr env e,Int,Near)
-    | BinOp (PlusA,e1,e2,_) ->
-        Binop (Add,exptoexpr env e1,exptoexpr env e2,Int,Near)
-    | BinOp (MinusA,e1,e2,_) ->
-        Binop (Sub,exptoexpr env e1,exptoexpr env e2,Int,Near)
-    | BinOp (Mult,e1,e2,_) ->
-        Binop (Mul,exptoexpr env e1,exptoexpr env e2,Int,Near)
-    | BinOp (Div,e1,e2,_) ->
-        Binop (Div,exptoexpr env e1,exptoexpr env e2,Int,Near)
-    | BinOp (Mod,e1,e2,_) ->
-        Binop (Mod,exptoexpr env e1,exptoexpr env e2,Int,Near)
-    | _ -> Cst (Coeff.s_of_int 666)
-
-  let exptotexpr1 env x =
-    Texpr1.of_expr env (exptoexpr env x) 
     
-
   let return ctx e f = 
-    let vars = f.slocals in
-    let vars = Array.of_enum (List.enum (List.map (fun v -> Var.of_string v.vname) vars)) in
-    A.forget_array Man.mgr ctx.local vars false
+    match e with 
+      | Some e -> 
+          let nd = D.add_vars ctx.local ["#ret"] in
+          D.assign_var_with nd "#ret" e;
+          let vars = List.map (fun x -> x.vname) (f.slocals @ f.sformals) in
+          D.forget_all_with nd vars;
+          nd
+      | None -> D.top ()
+  
   let body ctx f = 
-    let vars = f.slocals in
-    let vars = Array.of_enum (List.enum (List.map (fun v -> Var.of_string v.vname) vars)) in
-    let env = Environment.make vars [||] in
-    A.top Man.mgr env
+    let vars = List.map (fun x -> x.vname) f.slocals in
+    D.add_vars ctx.local vars
+    
   let assign ctx (lv:lval) e = 
     match lv with
-      | Var v, NoOffset -> 
-        A.assign_texpr Man.mgr ctx.local (Var.of_string (v.vname)) 
-              (exptotexpr1 ctx.local.env (Cil.constFold false e)) None
-      | _ -> ctx.local
+      | Var v, NoOffset -> D.assign_var ctx.local v.vname e
+      | _ -> D.top ()
 end
 
 let _ = 
--- src/cdomains/apronDomain.ml
+++ src/cdomains/apronDomain.ml
@@ -17,9 +17,8 @@ end
 
 module A = Abstract1
 
-module D : Lattice.S with type t = Man.mt A.t = 
+module D = 
 struct
-
   type t = Man.mt A.t
 
   let name () = "APRON numerical abstract domain"
@@ -40,13 +39,99 @@ struct
   let hash (x:t) = Hashtbl.hash x
   let compare (x:t) y = Pervasives.compare x y
   let isSimple x = false
-  let printXml f x = Printf.fprintf f "..."
+  let printXml f (x:t) = Printf.fprintf f "..."
   let short n x = 
     A.print Legacy.Format.str_formatter x;
     Legacy.Format.flush_str_formatter ()
-  let toXML_f s x = Xml.Element ("node",[],[])
+  let toXML_f s (x:t) = Xml.Element ("node",[],[])
   let toXML = toXML_f short
-  let pretty_f s () x = text (s 10 x)
+  let pretty_f s () (x:t) = text (s 10 x)
   let pretty = pretty_f short
   let pretty_diff () (x,y) = text "pretty_diff"
+  
+  open Texpr1 
+
+  let rec exptoexpr env = 
+    function 
+    | Lval (Var v,NoOffset) ->
+        Var (Var.of_string v.vname)
+    | Const (CInt64 (i,_,_)) -> 
+        Cst (Coeff.s_of_int (Int64.to_int i))
+    | UnOp  (Neg ,e,_) -> 
+        Unop (Neg,exptoexpr env e,Int,Near)
+    | BinOp (PlusA,e1,e2,_) ->
+        Binop (Add,exptoexpr env e1,exptoexpr env e2,Int,Near)
+    | BinOp (MinusA,e1,e2,_) ->
+        Binop (Sub,exptoexpr env e1,exptoexpr env e2,Int,Near)
+    | BinOp (Mult,e1,e2,_) ->
+        Binop (Mul,exptoexpr env e1,exptoexpr env e2,Int,Near)
+    | BinOp (Div,e1,e2,_) ->
+        Binop (Div,exptoexpr env e1,exptoexpr env e2,Int,Near)
+    | BinOp (Mod,e1,e2,_) ->
+        Binop (Mod,exptoexpr env e1,exptoexpr env e2,Int,Near)
+    | _ -> raise (Invalid_argument "exptotexpr1")
+
+  let exptotexpr1 env x =
+    Texpr1.of_expr env (exptoexpr env x) 
+
+  let assign_var_eq_with d v v' =
+    A.assign_texpr_with Man.mgr d (Var.of_string v) 
+          (Texpr1.of_expr (A.env d) (Var (Var.of_string v'))) None
+
+  let substitute_var_eq_with d v v' =
+    A.substitute_texpr_with Man.mgr d (Var.of_string v) 
+          (Texpr1.of_expr (A.env d) (Var (Var.of_string v'))) None
+          
+          
+  let assign_var_with d v e =
+    begin try
+      A.assign_texpr_with Man.mgr d (Var.of_string v) 
+            (exptotexpr1 (A.env d) (Cil.constFold false e)) None
+    with Invalid_argument "exptotexpr1" -> 
+      A.forget_array_with Man.mgr d [|Var.of_string v|] false
+    end
+
+  let assign_var d v e =
+    let newd = A.copy Man.mgr d in
+    assign_var_with newd v e;
+    newd
+
+  let substitute_var_with d v e =
+    begin try
+      A.substitute_texpr_with Man.mgr d (Var.of_string v) 
+            (exptotexpr1 (A.env d) (Cil.constFold false e)) None
+    with Invalid_argument "exptotexpr1" -> 
+      A.forget_array_with Man.mgr d [|Var.of_string v|] false
+    end
+  
+  let add_vars_with newd vars =
+    let vars = Array.of_enum (List.enum (List.map (fun v -> Var.of_string v) vars)) in
+    let newenv = Environment.add (A.env newd) vars [||] in
+    A.change_environment_with Man.mgr newd newenv false
+
+  let add_vars d vars =
+    let newd = A.copy Man.mgr d in
+    add_vars_with newd vars;
+    newd
+    
+  let forget_all_but_with d xs = 
+    let all_vars = Array.enum (fst (Environment.vars (A.env d))) in
+    let vars = Array.of_enum (Enum.filter (fun x -> not (List.mem (Var.to_string x) xs)) all_vars) in
+    let env = Environment.remove (A.env d) vars in
+    A.change_environment_with Man.mgr d env false
+
+  let forget_all_with d xs = 
+    let vars = Array.of_enum (List.enum (List.map (fun v -> Var.of_string v) xs)) in
+    let env = Environment.remove (A.env d) vars in
+    A.change_environment_with Man.mgr d env false
+    
+  let forget_all d vars =
+    let newd = A.copy Man.mgr d in
+    forget_all_with newd vars;
+    newd
+    
+  let get_vars d = List.of_enum (Array.enum (fst (Environment.vars (A.env d))))
+    
+  let copy = A.copy Man.mgr 
+  
 end
