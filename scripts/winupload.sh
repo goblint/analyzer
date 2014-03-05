434f99eea8e6d24342e1aab2abe70dc90a57065e
Ralf Vogler <ralf.vogler@gmail.com>
2014-03-05 22:47:15 +0100

    run winupload.sh only under cygwin

--- scripts/winupload.sh
+++ scripts/winupload.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+if [ "$(expr substr $(uname -s) 1 6)" != "CYGWIN" ];then
+    echo This script must be run using cygwin!
+    exit 1
+fi
 git pull
 ./make.sh all
-scp goblint www2.informatik.tu-muenchen.de:/srv/www/htdocs.goblint/upload_here/goblint.exe
-scp goblint.byte www2.informatik.tu-muenchen.de:/srv/www/htdocs.goblint/upload_here/goblint.byte.exe
-ssh www2.informatik.tu-muenchen.de 'cd /srv/www/htdocs.goblint && sudo -u vesal ./doitall.sh'
+
+# optional argument: username for server (might be different)
+user=${1-`whoami`}
+host=$user@www2.informatik.tu-muenchen.de
+scp goblint goblint.byte $host:/srv/www/htdocs.goblint/upload_here/
+ssh $host 'cd /srv/www/htdocs.goblint && sudo -u vesal ./doitall.sh'
