0f1b1399545bb4d548abafcc34d336447fb6c5f8
Ralf Vogler <ralf.vogler@gmail.com>
2014-12-18 15:25:56 +0100

    quote URL containing # see https://github.com/kerneis/cil/issues/18#issuecomment-67484959

--- make.sh
+++ make.sh
@@ -70,7 +70,7 @@ rule() {
              eval `opam config env`
              opam install ocamlfind batteries xml-light
              # opam's cil is too old
-             opam pin -y add cil https://github.com/kerneis/cil.git#develop
+             opam pin -y add cil "https://github.com/kerneis/cil.git#develop"
              ;;
     bench)   git submodule update --init --recursive bench;;
     *)       echo "Unknown action '$1'. Try clean, opt, debug, profile, byte, or doc.";;
