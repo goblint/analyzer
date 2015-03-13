dc6d5d6d5dacb151b675c1096a194a52df692d46
Ralf Vogler <ralf.vogler@gmail.com>
2015-03-13 14:02:10 +0100

    use gcc cpp on OS X automatically, so one doesn't have to edit config.ml by hand after `make clean` also, fix shellcheck warnings

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -1,26 +1,33 @@
-#/bin/bash
-VERSION=`git describe --tags --dirty 2> /dev/null | sed s/^v//`
-CILVERSION=`git --git-dir=../cil/.git describe --tags 2> /dev/null | sed s/^cil-//`
+#!/bin/bash
+VERSION=$(git describe --tags --dirty 2> /dev/null | sed s/^v//)
+CILVERSION=$(git --git-dir=../cil/.git describe --tags 2> /dev/null | sed s/^cil-//)
 
 if [ ! -f src/version.ml ]; then
-  echo "let goblint = \"unknown\"" > src/version.ml
-  echo "let cil = \"unknown\"" >> src/version.ml
+  {
+    echo "let goblint = \"unknown\""
+    echo "let cil = \"unknown\""
+  } >> src/version.ml
 fi
 
 if [ ! -f src/config.ml ]; then
-  echo "let tracing = false" > src/config.ml
-  echo "let tracking = false" >> src/config.ml
-  echo "let track_n = 2" >> src/config.ml
-  echo "let experimental = false" >> src/config.ml
-  echo "let cpp = \"cpp\"" >> src/config.ml
+  cpp="cpp"
+  # if we are on OS X and cpp is the Apple LLVM version, we should look for some Homebrew gcc cpp-4.9 (or other version)
+  case $OSTYPE in darwin*) if compgen -c | grep -q "^cpp-"; then cpp=$(compgen -c | grep "^cpp-"); fi;; esac
+  {
+    echo "let tracing = false"
+    echo "let tracking = false"
+    echo "let track_n = 2"
+    echo "let experimental = false"
+    echo "let cpp = \"$cpp\""
+  } >> src/config.ml
 fi
 
-if [ $VERSION ]; then
+if [ "$VERSION" ]; then
   grep -q "goblint = \"$VERSION\"" src/version.ml 2> /dev/null ||
     (sed "s/goblint = .*/goblint = \"$VERSION\"/" src/version.ml > src/version.tmp && mv src/version.tmp src/version.ml)
 fi
 
-if [ $CILVERSION ]; then
+if [ "$CILVERSION" ]; then
   grep -q "cil = \"$CILVERSION\"" src/version.ml 2> /dev/null ||
     (sed "s/cil = .*/cil = \"$CILVERSION\"/" src/version.ml > src/version.tmp && mv src/version.tmp src/version.ml)
 fi
