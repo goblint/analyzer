e5e84fdd6c3884fac86fdaffc2a71e3aeb80bf1d
Ralf Vogler <ralf.vogler@gmail.com>
2016-03-02 14:05:14 +0100

    script for running goblint on a regression test with its options (`PARAM: ...`)

---
+++ pgoblint.sh
@@ -0,0 +1,12 @@
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 group-nr test-nr [extra options]"
+    exit 1
+fi
+file=tests/regression/$1*/$2*.c
+if [ ! -e $file ]; then
+  echo "No file found!"
+  exit 1
+fi
+params=`grep -oP "PARAM: \K.*" $file`
+echo "$file: $params"
+./goblint --enable colors --enable dbg.debug --enable dbg.verbose --enable dbg.showtemps $params ${@:3} $file
