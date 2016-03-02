39eb9aaa5cdfb03b51565083447fdf2ac818e680
Ralf Vogler <ralf.vogler@gmail.com>
2016-03-02 18:14:16 +0100

    shell quoting is magic

--- pgoblint.sh
+++ pgoblint.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
 if [ $# -lt 2 ]; then
     echo "Usage: $0 group-nr test-nr [extra options]"
     exit 1
 fi
-file=tests/regression/$1*/$2*.c
+file="tests/regression/$1*/$2*.c"
 if [ ! -e $file ]; then
   echo "No file found!"
   exit 1
 fi
-params=`grep -oP "PARAM: \K.*" $file`
-echo "$file: $params"
-./goblint --enable colors --enable dbg.debug --enable dbg.verbose --enable dbg.showtemps $params ${@:3} $file
+params="`grep -oP "PARAM: \K.*" $file`"
+cmd="./goblint --enable colors --enable dbg.debug --enable dbg.verbose --enable dbg.showtemps $params ${@:3} $file"
+echo "$cmd"
+eval $cmd
