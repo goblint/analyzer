f8d5c51797639f441f3d7cb506ccf4f17a0f13d3
Isabel Max <mail@isabelmax.de>
2016-04-07 16:51:29 +0200

    fixed joins of equations

--- src/cdomains/equation.ml
+++ src/cdomains/equation.ml
@@ -26,7 +26,7 @@ sig
   val filter_equations_for_useful_keys: (store * t) -> t
   val get_equation_of_keys_and_offset: key -> (key option * float option) -> float option -> equation option
   val fold: ('a -> equation -> 'a) -> 'a -> t -> 'a
-  val join_equations: t -> t -> t
+  val join_equations: store -> t -> t -> t
   val map_keys: (key -> key) -> t -> t
   val meet_equations: t -> t -> t
   val meet_with_new_equation: store * t -> store * t
@@ -63,6 +63,7 @@ struct
          (match (IntDomain.IntDomTuple.to_int new_value), (IntDomain.IntDomTuple.to_int value_in_store) with
           | Some x, Some y -> (Int64.add x y)
           | _ -> 0L)) in
+    Pervasives.print_endline ("sum values: " ^ (Pervasives.string_of_float sum_values));
     if Key.compare key_in_store new_key > 0 then ((new_key, 1.0), (key_in_store, 1.0), (-. sum_values))
     else ((key_in_store, 1.0), (new_key, 1.0), (-. sum_values))
 
@@ -103,14 +104,26 @@ struct
 
   let solve_equation_for_key ((key_to_solve_for, const1), (key2, const2), const) store =
     let key1_int_dom_tuple, key1_variable_name, key1_is_local = (Domain.to_int_val (Store.find key_to_solve_for store)) in
+
     Pervasives.print_endline ("key1_variable_name: " ^ key1_variable_name);
-    let key2_int_dom_tuple, _, _ = (Domain.to_int_val (Store.find key2 store)) in
+    Pervasives.print_string ("key1 domain value:" );
+    Pretty.fprint Pervasives.stdout 0 (Domain.pretty () (Store.find key_to_solve_for store));
+    Pervasives.print_string ("key1 int value:" );
+    Pretty.fprint Pervasives.stdout 0 (IntDomain.IntDomTuple.pretty () key1_int_dom_tuple);
+    let key2_int_dom_tuple, key2_variable_name, _ = (Domain.to_int_val (Store.find key2 store)) in
+    Pervasives.print_string "\n";
+    Pervasives.print_endline ("key2_variable_name: " ^ key2_variable_name);
+    Pervasives.print_string ("key2 domain value:" );
+    Pretty.fprint Pervasives.stdout 0 (Domain.pretty () (Store.find key2 store));
+    Pervasives.print_string ("key2 int value:" );
+    Pretty.fprint Pervasives.stdout 0 (IntDomain.IntDomTuple.pretty () key2_int_dom_tuple);
+    Pervasives.print_string "\n";
     Domain.of_int_val (IntDomain.IntDomTuple.meet key1_int_dom_tuple (IntDomain.IntDomTuple.div (IntDomain.IntDomTuple.sub (IntDomain.IntDomTuple.mul (IntDomain.IntDomTuple.neg (IntDomain.IntDomTuple.of_int (Int64.of_float const2))) key2_int_dom_tuple) (IntDomain.IntDomTuple.of_int (Int64.of_float const))) (IntDomain.IntDomTuple.of_int (Int64.of_float const1)))) key1_variable_name key1_is_local
 
   let filter_equations_for_useful_keys filter (store, equations) =
     filter(
       fun ((key1, const1), (key2, const2), const) ->
-        not(Domain.is_top (Store.find key1 store)) && not(Domain.is_top (Store.find key1 store))
+        not(Domain.is_top (Store.find key1 store)) && not(Domain.is_top (Store.find key2 store))
     ) equations
 
   let meet_with_new_equation filter fold (store, equations) =
@@ -124,32 +137,32 @@ struct
       ) store equations in
     (store, equations)
 
-    let not_meet_with_new_equation fold (store, equations) =
-      let get_excl_list_of_int_abstract_value value =
-        match (IntDomain.IntDomTuple.to_int value) with | Some int64 -> [int64] | _ -> []
-      in
-      let get_new_val_for_key key store not_val_for_other_key =
-        let not_val_int_dom_tuple_other_key, not_val_variable_name_tuple_other_key, not_val_is_local_tuple_other_key =
-          Domain.to_int_val not_val_for_other_key in
-        Domain.meet (Store.find key store)(
-          Domain.of_int_val (
-            IntDomain.IntDomTuple.of_excl_list (
-              get_excl_list_of_int_abstract_value not_val_int_dom_tuple_other_key
-            )
-          ) not_val_variable_name_tuple_other_key not_val_is_local_tuple_other_key
-        )
-      in
-      let store = fold (
-          fun store ((key1, const1), (key2, const2), const) ->
-            let not_val_for_key1 =  solve_equation_for_key ((key1, const1), (key2, const2), const) store in
-            let not_val_for_key2 =  solve_equation_for_key ((key2, const2), (key1, const1), const) store in
-            let new_val_for_key1 = get_new_val_for_key key1 store not_val_for_key2 in
-            let store = if (Domain.is_bot new_val_for_key1) then store else Store.add key1 new_val_for_key1 store in
-            let new_val_for_key2 = get_new_val_for_key key2 store not_val_for_key1 in
-            if (Domain.is_bot new_val_for_key1) || (Domain.is_bot new_val_for_key2) then (Store.bot ())
-            else Store.add key2 new_val_for_key2 store
+  let not_meet_with_new_equation fold (store, equations) =
+    let get_excl_list_of_int_abstract_value value =
+      match (IntDomain.IntDomTuple.to_int value) with | Some int64 -> [int64] | _ -> []
+    in
+    let get_new_val_for_key key store not_val_for_other_key =
+      let not_val_int_dom_tuple_other_key, not_val_variable_name_tuple_other_key, not_val_is_local_tuple_other_key =
+        Domain.to_int_val not_val_for_other_key in
+      Domain.meet (Store.find key store)(
+        Domain.of_int_val (
+          IntDomain.IntDomTuple.of_excl_list (
+            get_excl_list_of_int_abstract_value not_val_int_dom_tuple_other_key
+          )
+        ) not_val_variable_name_tuple_other_key not_val_is_local_tuple_other_key
+      )
+    in
+    let store = fold (
+        fun store ((key1, const1), (key2, const2), const) ->
+          let not_val_for_key1 =  solve_equation_for_key ((key1, const1), (key2, const2), const) store in
+          let not_val_for_key2 =  solve_equation_for_key ((key2, const2), (key1, const1), const) store in
+          let new_val_for_key1 = get_new_val_for_key key1 store not_val_for_key2 in
+          let store = if (Domain.is_bot new_val_for_key1) then store else Store.add key1 new_val_for_key1 store in
+          let new_val_for_key2 = get_new_val_for_key key2 store not_val_for_key1 in
+          if (Domain.is_bot new_val_for_key1) || (Domain.is_bot new_val_for_key2) then (Store.bot ())
+          else Store.add key2 new_val_for_key2 store
         ) store equations in
-      (store, equations)
+    (store, equations)
 
   let change_keys_in_equations map old_key new_key equations =
     map (fun (((key1, const1), (key2, const2), const3)) ->
@@ -174,6 +187,31 @@ struct
       else
         Some (new_equation rkey offs lkey (-.1.0) const)
     | _ -> None
+
+  let remove_invalid_equations fold add empty_equations store equations =
+    fold (fun new_equations equation ->
+        match equation with
+          ((key1,const1),(key2,const2),const) ->
+          let val_of_key1_in_store = Store.find key1 store in
+          let val_key1_after_equation = solve_equation_for_key ((key1, const1), (key2, const2), const) store in
+          let val_of_key2_in_store = Store.find key2 store in
+          let val_key2_after_equation = solve_equation_for_key ((key2, const2), (key1, const1), const) store in
+          if (Domain.equal val_key1_after_equation val_of_key1_in_store) && (Domain.equal val_of_key2_in_store val_key2_after_equation) then
+            add new_equations equation
+          else
+            let int_val_of_key1_in_store, _, _ = (Domain.to_int_val val_of_key1_in_store) in
+            let int_val_of_key2_in_store, _, _ = (Domain.to_int_val val_of_key2_in_store) in
+            Pervasives.print_endline ("key1 int val in store: ");
+            Pretty.fprint Pervasives.stdout 0 (Domain.pretty () val_of_key1_in_store);
+            Pervasives.print_endline ("key2 int val in store: ");
+            Pretty.fprint Pervasives.stdout 0 (Domain.pretty () val_of_key2_in_store);
+            let real_int value = match IntDomain.IntDomTuple.to_int value with | Some x -> true | _ -> false in
+            if real_int int_val_of_key1_in_store && real_int int_val_of_key2_in_store then
+              add new_equations (build_new_equation (key1, int_val_of_key1_in_store) (key2, int_val_of_key2_in_store))
+            else
+              new_equations
+      ) empty_equations equations
+
 end
 
 module EquationList (Key: MapDomain.Groupable) (Domain: Domain_TransformableFromIntDomTupleT) : Signature
@@ -224,16 +262,8 @@ struct
   let equations_leq equation_list1 equation_list2 =
     List.fold_left2 (fun leq eq1 eq2 -> leq && Equation.compare_equation eq1 eq2 < 0) false equation_list1 equation_list2
 
-  let rec join_equations eqlist1 eqlist2 =
-    match eqlist1, eqlist2 with
-    | [], x | x, [] -> x
-    | x::xs, y::ys -> (
-        match Equation.compare_equation x y with
-        | 0 -> x :: join_equations xs ys
-        | 1 -> y :: join_equations eqlist1 ys
-        | -1 -> x :: join_equations xs eqlist2
-        | _ -> join_equations xs ys
-      )
+  let meet_with_new_equation = Equation.meet_with_new_equation filter fold
+  let not_meet_with_new_equation = Equation.not_meet_with_new_equation fold
 
   let rec meet_equations eqlist1 eqlist2 =
     match eqlist1, eqlist2 with
@@ -245,10 +275,24 @@ struct
         | -1 -> meet_equations xs eqlist2
         | _ -> meet_equations  xs ys
       )
+
+  let rec join_equations store eqlist1 eqlist2 =
+    let result =
+      match eqlist1, eqlist2 with
+      | [], x | x, [] -> x
+      | x::xs, y::ys -> (
+          match Equation.compare_equation x y with
+          | 0 -> x :: join_equations store xs ys
+          | 1 -> y :: join_equations store eqlist1 ys
+          | -1 -> x :: join_equations store xs eqlist2
+          | _ -> join_equations store xs ys
+        )
+    in
+    let result = Equation.remove_invalid_equations fold (fun equation_list equation -> List.append [equation] equation_list)[] store result in
+    result
+
   let remove_equations_with_key = Equation.remove_equations_with_key filter
   let filter_equations_for_useful_keys = Equation.filter_equations_for_useful_keys filter
-  let meet_with_new_equation = Equation.meet_with_new_equation filter fold
-  let not_meet_with_new_equation = Equation.not_meet_with_new_equation fold
   let change_keys_in_equations = Equation.change_keys_in_equations List.map
 end
 
@@ -325,8 +369,12 @@ struct
       (fun key equation leq -> leq && Equation.compare_equation equation (EquationMap.find key equation_map2) < 1)
       equation_map1 false
 
-  let join_equations eqmap1 eqmap2 =
-    EquationMap.fold EquationMap.add eqmap2 eqmap1
+  let meet_with_new_equation = Equation.meet_with_new_equation filter fold
+  let join_equations store eqmap1 eqmap2 =
+    let result = EquationMap.fold EquationMap.add eqmap2 eqmap1
+    in
+    Equation.remove_invalid_equations fold (fun new_equations equation -> match equation with ((key1, _), (key2, _),_) -> EquationMap.add (key1, key2) equation new_equations) EquationMap.empty store  result
+
 
   let rec meet_equations eqmap1 eqmap2 =
     EquationMap.fold (
@@ -341,7 +389,6 @@ struct
 
   let remove_equations_with_key = Equation.remove_equations_with_key filter
   let filter_equations_for_useful_keys = Equation.filter_equations_for_useful_keys filter
-  let meet_with_new_equation = Equation.meet_with_new_equation filter fold
   let not_meet_with_new_equation = Equation.not_meet_with_new_equation fold
   let change_keys_in_equations = Equation.change_keys_in_equations EquationMap.map
 end
