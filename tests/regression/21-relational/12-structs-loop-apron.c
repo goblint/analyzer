e5b1776ee4dfa99882a49858a3daf5e76b7a0da7
Isabel Max <mail@isabelmax.de>
2016-04-14 15:12:27 +0200

    Added tests for apron structs loop

---
+++ tests/regression/21-relational/12-structs-loop-apron.c
@@ -0,0 +1,25 @@
+// PARAM: --set ana.activated "['base']" --set ana.int.interval true --set ana.int.trier true --set ana.int.queries true --sets solver slr3 --set ana.int.queries true --set ana.structs.relational true --set ana.structs.relational_to_analyze "['S']" --set ana.structs.apron true --set ana.structs.equations false
+typedef struct  {
+    int i;
+    int k;
+    
+} S;
+
+S some_function(){
+    S xx;
+    xx.i = 5;
+    S z;
+    z.i = 5;
+    for (z.i = 5; z.i > 0; z.i--) {
+        xx.i = xx.i - 1;
+    }
+    return xx;
+}
+
+int main(){
+    S a;
+    a.i = 5;
+    a.k = 7;
+    a = some_function();
+    return a.i;
+}
