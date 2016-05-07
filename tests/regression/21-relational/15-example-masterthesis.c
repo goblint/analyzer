cab7be8f2e1b3beea42608ea01def688c037af96
Isabel Max <mail@isabelmax.de>
2016-05-07 14:39:47 +0200

    Changed the brackets of the out prints of the relational int domain with equations

--- tests/regression/21-relational/15-example-masterthesis.c
+++ tests/regression/21-relational/15-example-masterthesis.c
@@ -1,12 +1,10 @@
 // PARAM: --set ana.activated "['base']"  --set ana.int.interval true --set ana.int.trier false --set ana.int.relational true --set ana.int.equations true
-#include<stdio.h>
-#include<assert.h>
-
 
 int main () {
-    int a = 1,b = 2,c = 3;
+    int a = 1;
+    int b = 2;
+    int c = 3;
     int z;
-    
     if (z != 0){
         a = 8;
         b = 9;
@@ -15,7 +13,6 @@ int main () {
         b = 8;
     }
     c = a + b;
-    
     return 0;
 }
 
