0e5fe51a36444dd166ebce8a92e52bd88c8dcf7b
Isabel Max <mail@isabelmax.de>
2016-05-16 15:30:30 +0200

    Adjusted test and added assert statements

--- tests/regression/21-relational/02-structs-loop.c
+++ tests/regression/21-relational/02-structs-loop.c
@@ -1,4 +1,5 @@
-// PARAM: --set ana.activated "['base']" --set ana.int.interval true --set ana.int.trier true --set ana.int.queries true --sets solver slr3 --set ana.int.queries true --set ana.structs.relational true --set ana.structs.relational_to_analyze "['S']"
+// PARAM: --set ana.activated "['base']" --set ana.int.interval false --set ana.int.trier true --set ana.int.relational false --set ana.int.queries true --set ana.structs.relational true --set ana.structs.relational_to_analyze "['S']" --set ana.structs.equations true
+
 typedef struct  {
     int i;
     int k;
@@ -10,9 +11,11 @@ S some_function(){
     xx.i = 5;
     S z;
     z.i = 5;
+    assert (xx.i == 5);
     for (z.i = 5; z.i > 0; z.i--) {
         xx.i = xx.i - 1;
     }
+    assert (xx.i == 0); //NOWARN
     return xx;
 }
 
@@ -21,5 +24,6 @@ int main(){
     a.i = 5;
     a.k = 7;
     a = some_function();
+    assert (a.i == 0);
     return a.i;
 }
