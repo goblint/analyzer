e244e1687d7b64afc82e0246ed338d09ff09d7bd
Ralf Vogler <ralf.vogler@gmail.com>
2016-07-18 16:10:24 +0200

    generate `config.ml` before changing it

--- scripts/trace_on.sh
+++ scripts/trace_on.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+scripts/set_version.sh
 grep -q 'tracing = false' src/config.ml && \
   sed 's/tracing = false/tracing = true/' src/config.ml > src/config.tmp && mv src/config.tmp src/config.ml
 make
--- scripts/track_off.sh
+++ scripts/track_off.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+scripts/set_version.sh
 grep -q 'tracking = true' src/config.ml && \
   sed 's/tracking = true/tracking = false/' src/config.ml > src/config.tmp && mv src/config.tmp src/config.ml
 make
