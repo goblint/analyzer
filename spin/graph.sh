2f498af0f7272b575f85ab8c2e3e9bd671c362a5
Ralf Vogler <ralf.vogler@gmail.com>
2017-04-12 14:17:24 +0200

    open svg in spin/graph.sh

--- spin/graph.sh
+++ spin/graph.sh
@@ -1,4 +1,6 @@
 spin -a ${1-"airbus.pml"}
 cc -o pan pan.c
-./pan -D | dot | gvpack -array_u >! pan.dot
-zgrv pan.dot
+rm -f pan.dot pan.svg
+./pan -D | dot | gvpack -array_u > pan.dot
+dot -Tsvg pan.dot > pan.svg
+open -a Safari pan.svg
