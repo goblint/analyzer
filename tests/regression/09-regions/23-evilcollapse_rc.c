f33f4b067df8e5ac4a1313113b5a9a471ad91b8b
Ralf Vogler <ralf.vogler@gmail.com>
2017-08-24 15:51:40 +0200

    simplified failing test for wpoint solver

--- tests/regression/09-regions/23-evilcollapse_rc.c
+++ tests/regression/09-regions/23-evilcollapse_rc.c
@@ -1,5 +1,4 @@
-// PARAM: --set ana.activated[+] "'var_eq'"  --set ana.activated[+] "'symb_locks'"  --set ana.activated[+] "'region'"  --set exp.region-offsets true
-#include<pthread.h>
+// PARAM: --set ana.activated[+] "'symb_locks'" --set ana.activated[+] "'region'" --set exp.region-offsets true --sets solver wpoint
 #include<stdlib.h>
 
 struct list_head {
@@ -7,31 +6,20 @@ struct list_head {
   struct list_head *prev ;
 };
 
-struct s {
-  int datum ;
-  struct list_head list ;
-};
+struct list_head c[10] ;
 
-struct cache {
-  struct list_head slot[10] ;
-  pthread_mutex_t slots_mutex[10] ;
-};
-
-struct cache c  ;
-
-static inline void INIT_LIST_HEAD(struct list_head *list) {
+static void INIT_LIST_HEAD(struct list_head *list) {
   list->next = list;
   list->prev = list;
 }
 
-struct s *new(int x) {
-  struct s *p = malloc(sizeof(struct s));
-  p->datum = x;
-  INIT_LIST_HEAD(&p->list);
+struct list_head *new(int x) {
+  struct list_head *p = malloc(sizeof(struct list_head));
+  INIT_LIST_HEAD(p);
   return p;
 }
 
-static inline void list_add(struct list_head *new, struct list_head *head) {
+static void list_add(struct list_head *new, struct list_head *head) {
   struct list_head *next = head->next;
   next->prev = new;
   new->next = next;
@@ -39,49 +27,27 @@ static inline void list_add(struct list_head *new, struct list_head *head) {
   head->next = new;
 }
 
-inline static struct list_head *lookup (int d) {
+static struct list_head *lookup (int d) {
   int hvalue;
   struct list_head *p;
-  p = c.slot[hvalue].next;
+  p = c[hvalue].next;
   return p;
-}
-
-void *f(void *arg) { 
-  struct s *pos ;
-  int j;
-  struct list_head  const  *p ;
-  struct list_head  const  *q ;
-
-  while (j < 10) {
-    pthread_mutex_lock(&c.slots_mutex[j]);
-    p = c.slot[j].next;
-    pos = (struct s *)((char *)p - (size_t)(& ((struct s *)0)->list));
-
-    while (& pos->list != & c.slot[j]) {
-      pos->datum++; // RACE!
-      q = pos->list.next;
-      pos = (struct s *)((char *)q - (size_t)(& ((struct s *)0)->list));
-    }
-
-    pthread_mutex_unlock(&c.slots_mutex[j]);
-    j ++;
-  }
-  return 0;
 }
 
 int main() {
   struct list_head *p1, *p2;
-  pthread_t t1, t2;
   for (int i = 0; i < 10; i++) {
-    INIT_LIST_HEAD(&c.slot[i]);
-    pthread_mutex_init(&c.slots_mutex[i], NULL);
-    for (int j = 0; j < 30; j++) list_add(&new(j*i)->list, &c.slot[i]);
+    INIT_LIST_HEAD(&c[i]);
+    for (int j = 0; j < 3; j++) {
+      struct list_head *arg1, *arg2;
+      arg1 = new(j*i);
+      arg2 = &c[i];
+      list_add(arg1, arg2);
+    }
   }
   p1 = lookup(1);
   p2 = lookup(2);
   p1->next = p2->next;
   // per-element scheme no longer safe.
-  pthread_create(&t1, NULL, f, NULL);
-  pthread_create(&t2, NULL, f, NULL);
   return 0;
 }
