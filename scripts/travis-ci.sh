0be5743ad948ade590eea23a6457f19d501e0857
Ralf Vogler <ralf.vogler@gmail.com>
2017-11-30 11:12:36 +0100

    travis: "Could not symlink include/c++"

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -5,6 +5,7 @@
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
     brew install ocaml opam gcc
+    brew link --overwrite gcc
 else
     if test -e "make.sh"; then # travis-ci
         echo "already in repository"
