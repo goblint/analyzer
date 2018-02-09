c30b0c6085edf4e674c03d10091c2b392d7f1a21
Simmo Saan <simmo.saan@gmail.com>
2018-02-09 13:03:02 +0200

    Add domain name to test names

--- src/maindomaintest.ml
+++ src/maindomaintest.ml
@@ -2,10 +2,15 @@ open QCheck
 
 module DomainProperties (D: Lattice.S) =
 struct
+  (* Wrapper for Test.make: prepends domain name to required name *)
+  let make ?count ?long_factor ?max_gen ?max_fail ?small ~name arb law =
+    let full_name = D.name () ^ " " ^ name in
+    Test.make ?count ?long_factor ?max_gen ?max_fail ?small ~name:full_name arb law
+
   let arb = D.arbitrary ()
 
-  let leq_refl = Test.make ~name:"leq refl" (arb) (fun x -> D.leq x x)
-  let leq_trans = Test.make ~name:"leq trans" (triple arb arb arb) (fun (a, b, c) -> (D.leq a b && D.leq b c) ==> D.leq a c)
+  let leq_refl = make ~name:"leq refl" (arb) (fun x -> D.leq x x)
+  let leq_trans = make ~name:"leq trans" (triple arb arb arb) (fun (a, b, c) -> (D.leq a b && D.leq b c) ==> D.leq a c)
 
   let tests = [
     leq_refl;
