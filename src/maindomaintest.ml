eeecae4ab6bea5d5509c984b6018f3128573c71f
Simmo Saan <simmo.saan@gmail.com>
2018-02-09 13:24:31 +0200

    Run domaintest over a list of domains

--- src/maindomaintest.ml
+++ src/maindomaintest.ml
@@ -23,8 +23,18 @@ struct
   ]
 end
 
-module DP = DomainProperties (IntDomain.Flattened)
-let testsuite = DP.tests
+let domains: (module Lattice.S) list = [
+  (module IntDomain.Integers);
+  (module IntDomain.Flattened)
+]
+
+let testsuite =
+  List.map (fun d ->
+      let module D = (val d: Lattice.S) in
+      let module DP = DomainProperties (D) in
+      DP.tests)
+    domains
+  |> List.flatten
 
 let () =
   QCheck_runner.run_tests_main ~argv:Sys.argv testsuite
