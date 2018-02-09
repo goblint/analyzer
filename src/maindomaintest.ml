ba40908f238652abd5c089eae068935d617f999a
Simmo Saan <simmo.saan@gmail.com>
2018-02-09 12:50:03 +0200

    Extract domain properties into a functor

--- src/maindomaintest.ml
+++ src/maindomaintest.ml
@@ -1,11 +1,20 @@
 open QCheck
 
-module D = IntDomain.Flattened
-let arb = D.arbitrary ()
-let testsuite = [
-  Test.make ~name:"leq refl" (arb) (fun x -> D.leq x x);
-  Test.make ~name:"leq trans" (triple arb arb arb) (fun (a, b, c) -> (D.leq a b && D.leq b c) ==> D.leq a c)
-]
+module DomainProperties (D: Lattice.S) =
+struct
+  let arb = D.arbitrary ()
+
+  let leq_refl = Test.make ~name:"leq refl" (arb) (fun x -> D.leq x x)
+  let leq_trans = Test.make ~name:"leq trans" (triple arb arb arb) (fun (a, b, c) -> (D.leq a b && D.leq b c) ==> D.leq a c)
+
+  let tests = [
+    leq_refl;
+    leq_trans
+  ]
+end
+
+module DP = DomainProperties (IntDomain.Flattened)
+let testsuite = DP.tests
 
 let () =
   QCheck_runner.run_tests_main ~argv:Sys.argv testsuite
