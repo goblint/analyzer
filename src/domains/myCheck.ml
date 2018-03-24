2b696eca2dd99cbc00457ea24847f9ed4a9259e7
Simmo Saan <simmo.saan@gmail.com>
2018-03-24 15:13:59 +0200

    Add shrinker to sequence

--- src/domains/myCheck.ml
+++ src/domains/myCheck.ml
@@ -10,13 +10,28 @@ struct
     List.fold_right f gens (return [])
 end
 
+module Shrink =
+struct
+  let sequence (shrinks: 'a Shrink.t list) (xs: 'a list) yield = (* https://github.com/c-cube/qcheck/blob/e2c27723bbffd85b992355f91e2e2ba7dcd04f43/src/QCheck.ml#L380-L387 *)
+    let ss = Array.of_list shrinks in
+    let ys = Array.of_list xs in
+    for i = 0 to Array.length ys - 1 do
+      ss.(i) ys.(i) (fun x ->
+          let zs = Array.copy ys in
+          zs.(i) <- x;
+          yield (Array.to_list zs)
+        )
+    done
+end
+
 module Arbitrary =
 struct
   let int64: int64 arbitrary = int64 (* S TODO: custom int64 arbitrary with shrinker *)
 
   let sequence (arbs: 'a arbitrary list): 'a list arbitrary =
     let gens = List.map gen arbs in
-    make (Gen.sequence gens)
+    let shrinks = List.map shrink arbs in
+    make ~shrink:(Shrink.sequence shrinks) (Gen.sequence gens)
 
   let varinfo: Cil.varinfo arbitrary = QCheck.always (Cil.makeGlobalVar "arbVar" Cil.voidPtrType) (* S TODO: how to generate this *)
 end
