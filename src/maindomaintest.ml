eb94f9d3fd8e0e61eb59608d5c6f0eef71e366f7
Simmo Saan <simmo.saan@gmail.com>
2018-04-18 16:33:55 +0300

    Test IntDomain abstraction validity

--- src/maindomaintest.ml
+++ src/maindomaintest.ml
@@ -9,6 +9,19 @@ let domains: (module Lattice.S) list = [
   (module IntDomain.CircInterval);
   (module IntDomain.Trier);
   (module IntDomain.Enums);
+  (module IntDomain.IntDomTuple);
+  (module IntDomainProperties.IntegerSet)
+]
+
+let intDomains: (module IntDomain.S) list = [
+  (module IntDomain.Integers);
+  (module IntDomain.Flattened);
+  (module IntDomain.Lifted);
+  (module IntDomain.Interval32);
+  (module IntDomain.Booleans);
+  (module IntDomain.CircInterval);
+  (module IntDomain.Trier);
+  (module IntDomain.Enums);
   (module IntDomain.IntDomTuple)
 ]
 
@@ -19,6 +32,13 @@ let testsuite =
       DP.tests)
     domains
   |> List.flatten
+let intTestsuite =
+  List.map (fun d ->
+      let module D = (val d: IntDomain.S) in
+      let module DP = IntDomainProperties.Valid (D) in
+      DP.tests)
+    intDomains
+  |> List.flatten
 
 let () =
-  QCheck_runner.run_tests_main ~argv:Sys.argv testsuite
+  QCheck_runner.run_tests_main ~argv:Sys.argv (List.append testsuite intTestsuite)
