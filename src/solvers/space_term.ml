b69dab17935773b121c85df1a2fec8c9d8926141
Ralf Vogler <ralf.vogler@gmail.com>
2018-05-14 17:34:41 +0200

    cleanup topdown

--- src/solvers/space_term.ml
+++ src/solvers/space_term.ml
@@ -37,7 +37,10 @@ module WP =
         if tracing then trace "sol2" "destabilize %a on %i\n" S.Var.pretty_trace x (S.Var.line_nr x);
         let w = HM.find_default infl x VS.empty in
         HM.replace infl x VS.empty;
-        VS.iter (fun y -> HM.remove stable y; if not (HM.mem called y) then destabilize y) w
+        VS.iter (fun y ->
+          HM.remove stable y;
+          (* if tracing then trace "sol2" "destabilize %a on %i\n" S.Var.pretty_trace y (S.Var.line_nr y); *)
+          if not (HM.mem called y) then destabilize y) w
       and solve x phase =
         if tracing then trace "sol2" "solve %a on %i, called: %b, stable: %b\n" S.Var.pretty_trace x (S.Var.line_nr x) (HM.mem called x) (HM.mem stable x);
         if not (HM.mem called x || HM.mem stable x) then (
@@ -49,7 +52,7 @@ module WP =
           if tracing then trace "sol" "Var: %a\n" S.Var.pretty_trace x ;
           if tracing then trace "sol" "Contrib:%a\n" S.Dom.pretty tmp;
           HM.remove called x;
-          let tmp = match phase with Widen -> S.Dom.widen old (S.Dom.join old tmp) | Narrow -> S.Dom.narrow old tmp in
+          let tmp = if S.system x <> None then match phase with Widen -> S.Dom.widen old (S.Dom.join old tmp) | Narrow -> S.Dom.narrow old tmp else tmp in
           if not (S.Dom.equal old tmp) then (
             (* if tracing then if is_side x then trace "sol2" "solve side: old = %a, tmp = %a, widen = %a\n" S.Dom.pretty old S.Dom.pretty tmp S.Dom.pretty (S.Dom.widen old (S.Dom.join old tmp)); *)
             update_var_event x old tmp;
@@ -60,7 +63,7 @@ module WP =
             (solve[@tailcall]) x phase;
           ) else if not (HM.mem stable x) then (
             (solve[@tailcall]) x Widen;
-          ) else if phase = Widen then (
+          ) else if phase = Widen && S.system x <> None then (
             HM.remove stable x;
             (solve[@tailcall]) x Narrow;
           );
@@ -90,10 +93,9 @@ module WP =
         if HM.mem rho y then add_infl y x;
         tmp
       and side y d =
-        if tracing then trace "sol2" "side to %a on %i (wpx: %b) ## value: %a\n" S.Var.pretty_trace y (S.Var.line_nr y) (HM.mem rho y) S.Dom.pretty d;
         let old = try HM.find rho' y with Not_found -> S.Dom.bot () in
         if not (S.Dom.leq d old) then (
-          HM.replace rho' y (S.Dom.join old d);
+          HM.replace rho' y (S.Dom.widen old d);
           HM.remove stable y;
           init y;
           solve y Widen;
@@ -195,6 +197,7 @@ module WP =
 
       HM.clear stable;
       HM.clear infl  ;
+      HM.clear rho'  ;
 
       rho
 
