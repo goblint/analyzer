c6445634ba9f548889e851bc895cc948d0fae669
Ralf Vogler <ralf.vogler@gmail.com>
2018-05-24 15:20:07 +0200

    try afl-fuzz for IntDomain

---
+++ src/afl.ml
@@ -0,0 +1,33 @@
+(* afl-fuzz -i input -o output ./a.out *)
+open BatteriesExceptionless
+
+let test (module X : Lattice.S) n =
+  let read () =
+    try match read_line () with
+      | "bot" -> bot ()
+      | "top" -> top ()
+      | x -> Int64.of_string x |> of_int
+    with _ -> exit 0
+  in
+  let x = read () in
+  let y = read () in
+
+  assert (leq (bot ()) x);
+  assert (leq x (top ()));
+
+  assert (leq x (join x y));
+  assert (leq y (join x y));
+
+  assert (leq (meet x y) x);
+  assert (leq (meet x y) y);
+
+  assert (leq y (widen x y));
+  assert (leq (narrow x y) x);
+
+  assert (leq (join x y) (widen x y));
+  assert (leq (narrow x y) (meet x y));
+
+  assert (not (leq x y && leq y x) || equal x y)
+
+let () =
+  test (module IntDomain.Interval32)
