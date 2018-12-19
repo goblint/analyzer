8e67ab7b1274f21909c78eb8673f9986be97b7d4
Michael Schwarz <michael.schwarz93@gmail.com>
2018-12-19 11:27:44 +0100

    Change simple fragmented to keep track of an additional value (will be expression later)

--- src/cdomains/arrayDomain.ml
+++ src/cdomains/arrayDomain.ml
@@ -41,11 +41,12 @@ end
 module TrivialFragmented (Val: Lattice.S) (Idx: Lattice.S): S with type value = Val.t and type idx = Idx.t =
 struct
   let name () = "trivial arrays"
-  include Lattice.Prod3 (Val) (Val) (Val)
+  module Base = Lattice.Prod3 (Val) (Val) (Val)
+  include Lattice.ProdSimple(Lattice.Unit) (Base)
   type idx = Idx.t
   type value = Val.t
 
-  let short w (xl, xm, xr) = "Array: (" ^ Val.short (w - 7) xl ^ "," ^ Val.short (w - 7) xm ^ "," ^ Val.short (w - 7) xr ^ ")" (* TODO w-7 needs to be replaced here *)
+  let short w (_,(xl, xm, xr)) = "Array: (" ^ Val.short (w - 7) xl ^ "," ^ Val.short (w - 7) xm ^ "," ^ Val.short (w - 7) xr ^ ")" (* TODO w-7 needs to be replaced here *)
   let pretty () x = text "Array: " ++ pretty_f short () x
   let pretty_diff () (x,y) = dprintf "%s: %a not leq %a" (name ()) pretty x pretty y
   let toXML m = toXML_f short m
@@ -53,33 +54,33 @@ struct
   (* For set&get we later need to distinguish between must & may euality to see *)
   (* decide whether to apply a least upper bound or not *)
 
-  let get (xl, xm, xr) i =    (* This is currently under the assumption that we *)
+  let get (e, (xl, xm, xr)) i =    (* This is currently under the assumption that we *)
     if i == Idx.bot () then xm     (* always get exact integers here *)
     else if i < Idx.bot () then xl
     else xr
 
-  let set (xl, xm, xr) i a =         (* Also under the assumption that we always get *)
-    if i == Idx.bot () then (xl, a, xr)   (* exact integers as the indices *)
-    else if i < Idx.bot () then (Val.join xl a, xm, xr)
-    else (xl, xm, Val.join xr a)
+  let set (e, (xl, xm, xr)) i a =         (* Also under the assumption that we always get *)
+    if i == Idx.bot () then (e, (xl, a, xr))   (* exact integers as the indices *)
+    else if i < Idx.bot () then (e, (Val.join xl a, xm, xr))
+    else (e, (xl, xm, Val.join xr a))
 
-  let make i v = (Val.top(), v, Val.top())    (* We need to see whether we need to modify the bottom element from the Prod3 domain here *)
+  let make i v = (Lattice.Unit.top(), (Val.top(), v, Val.top()))    (* We need to see whether we need to modify the bottom element from the Prod3 domain here *)
   let length _ = None
 
-  let move (xl, xm, xr) (i:int) =     (* Under the assumption that we always get exact information about how much it moved *)
+  let move (e, (xl, xm, xr)) (i:int) =     (* Under the assumption that we always get exact information about how much it moved *)
     match i with
-    | 0   -> (xl, xm, xr)
-    | 1   -> (Val.join xl xm, xr, xr) (* moved one to the right *)
-    | -1  -> (xl, xl, Val.join xm xr) (* moved one to the left *)
+    | 0   -> (e, (xl, xm, xr))
+    | 1   -> (e, (Val.join xl xm, xr, xr)) (* moved one to the right *)
+    | -1  -> (e, (xl, xl, Val.join xm xr)) (* moved one to the left *)
     | _ when i > 1
-      -> (Val.join (Val.join xl xm) xr, xr, xr) (* moved more than one to the right *)
+      -> (e, (Val.join (Val.join xl xm) xr, xr, xr)) (* moved more than one to the right *)
     | _ when i < -1
-      -> (xl, xl, Val.join (Val.join xl xm) xr) (* moved more than one to the left *)
+      -> (e, (xl, xl, Val.join (Val.join xl xm) xr)) (* moved more than one to the left *)
     | _ -> top()
 
   let set_inplace = set
   let copy a = a
-  let printXml f (xl, xm, xr) = BatPrintf.fprintf f "<value>\n<map>\n<key>Any</key>\n%a\n</map>\n</value>\n" Val.printXml xl
+  let printXml f (e, (xl, xm, xr)) = BatPrintf.fprintf f "<value>\n<map>\n<key>Any</key>\n%a\n</map>\n</value>\n" Val.printXml xl
 end
 
 
