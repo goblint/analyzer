41712395007fd5cd9e32551eeab1e4bf9fad1eae
Michael Schwarz <michael.schwarz93@gmail.com>
2019-01-23 10:21:07 +0100

    Example

--- toy_examples_michael/simple_fixpoint_not_reached.c
+++ toy_examples_michael/simple_fixpoint_not_reached.c
@@ -1,14 +1,24 @@
+#include <stdlib.h>
 int main(void) {
   int array1[10000000];
-  int* ptr = &array1;
+  int array2[10000000];
+  int* ptr;
 
+  if(rand()) {
+    ptr = &array1;
+  }
+  else {
+    ptr = &array2;
+  }
+
+/**
   *ptr = 5;
   ptr++;
   *ptr = 5;
 
 
-  int val = *ptr;
+  int val = *ptr+345;
   
   int b = 8;
-  array1[7] = 17;
+  array1[7] = 17; **/
 }
