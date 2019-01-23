36e5dcb3f0f8d83aa9b23009c6a87b91a2b684cd
Julian Erhard <julian.erhard@tum.de>
2019-01-23 13:29:38 +0100

    VersionLookup: Add create_map

--- src/util/versionLookup.ml
+++ src/util/versionLookup.ml
@@ -9,6 +9,17 @@ let updateMap (oldFile: Cil.file) (newFile: Cil.file) (newCommitID: commitID) (h
     List.iter (fun (fundec: fundec) ->  Hashtbl.replace ht fundec.svar.vname (fundec.svar, newCommitID)) assocList;
     ht
 
+
+let create_map (new_file: Cil.file) (commit: commitID) =
+    let add_to_hashtbl tbl (global: Cil.global) =
+        match global with
+            | Cil.GFun (fund, loc) ->
+              Hashtbl.replace tbl fund.svar.vname (fund, commit) 
+            | other -> ()
+    in
+    let tbl = Hashtbl.create 1000 in
+    Cil.iterGlobals new_file (add_to_hashtbl tbl)
+
 (* load the old cil.file, load the corresponding map, update the map, return it*)
 let restoreMap (folder: string) (commit: commitID) (newFile: Cil.file)= 
     let commitFolder = Filename.concat folder commit in
