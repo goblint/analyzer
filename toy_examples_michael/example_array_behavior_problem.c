f264f6c4f46e8642d5a3de69d0200fd45c475679
Michael Schwarz <michael.schwarz93@gmail.com>
2019-03-20 10:38:31 +0100

    Add example showing an array can never be partitioned according to an expression pointing into multiple arrays

--- toy_examples_michael/example_array_behavior_problem.c
+++ toy_examples_michael/example_array_behavior_problem.c
@@ -3,8 +3,43 @@ int global_array[50];
 
 int main(void) {
   some_func();
-  
+  pretty_awesome();
+
   int x = global_array[5];
+}
+
+void pretty_awesome() {
+  int top;
+
+  int arr1[42];
+  int arr2[42];
+  int *ptr;
+
+  for(int i = 0; i < 42; i++) {
+    arr1[i] = 4;
+    arr2[i] = 4;
+  }
+
+  ptr = &arr1[7];
+
+  if(top) {
+    ptr = &arr2[7];
+  }
+
+  *ptr = 9;
+  
+  // Case ptr = &arr1[7]
+  //    arr1 -> (ptr-arr1, ([4,4], [9,9],[4,4]))
+  //    arr2 -> (-,[4,4])
+
+  // Case ptr = &arr2[7]
+  //    arr1 -> (-, [4,4])
+  //    arr2 -> (ptr-arr2, ([4,4], [9,9],[4,4]))
+
+  // LUB:
+  //    arr1 -> (-, [4,9])
+  //    arr2 -> (-, [4,9])
+  int x = arr1[7];
 }
 
 void some_func(void) {
