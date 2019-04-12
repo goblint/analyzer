fc38f2a4f1c911249987403d08e651a39470dc18
Michael Schwarz <michael.schwarz93@gmail.com>
2019-04-12 09:35:18 +0200

    cleanup expRelation

--- src/analyses/expRelation.ml
+++ src/analyses/expRelation.ml
@@ -1,6 +1,6 @@
-(** An analysis specification to answer questions about how two expressions relate to each other. *)
-(** Currently this works purely statically on the expressions, but at a later stage, this analysis *)
-(** can be improved to perform more involved things *)
+(** An analysis specification to answer questions about how two expressions relate to each other.   *)
+(** Currently this works purely syntactically on the expressions, and only for =_{must}.            *)
+(** Does not keep state, this is only formulated as an analysis to integrate well into framework    *)
 open Prelude.Ana
 open Analyses
 
@@ -16,7 +16,7 @@ struct
   let rec canonize (e:exp) =
     match e with
       | BinOp (MinusA, BinOp(PlusA, e1, e2, typ1), e3, typ2)  when typ1 = typ2 -> (* (e1+e2)-e3 --> (e1-e3)+e2 *)
-        begin                                                                      (* where + is arithmetic +   *)
+        begin                                                                     (* where + is arithmetic +   *)
           let ce1 = canonize e1 in
           let ce2 = canonize e2 in
           let ce3 = canonize e3 in
@@ -29,41 +29,30 @@ struct
           let ce3 = canonize e3 in
           BinOp(MinusA, BinOp(MinusA, ce1, ce2, typ1), ce3, typ2)
         end
-      | BinOp (MinusPP, BinOp(PlusPI, e1, e2, typ1), e3, typ2) -> (*                                                    *)
-        begin                                                     (*          MinusPP                     PlusA         *)
-          let ce1 = canonize e1 in                                (*         /      \      =>            /     \        *)
-          let ce2 = canonize e2 in                                (*     PlusPI      \              MinusPP     \       *)
-          let ce3 = canonize e3 in                                (*    /   \         \            /      \      \      *) 
-          BinOp(PlusA, BinOp(MinusPP, ce1, ce3, typ2), ce2, typ2) (*  ptr    i     array1        ptr    array1    i     *)
+      | BinOp (MinusPP, BinOp(PlusPI, e1, e2, typ1), e3, typ2) -> (*                                                  *)
+        begin                                                     (*          MinusPP                     PlusA       *)
+          let ce1 = canonize e1 in                                (*         /      \      =>            /     \      *)
+          let ce2 = canonize e2 in                                (*     PlusPI      \              MinusPP     \     *)
+          let ce3 = canonize e3 in                                (*    /   \         \            /      \      \    *) 
+          BinOp(PlusA, BinOp(MinusPP, ce1, ce3, typ2), ce2, typ2) (*  ptr    i     array1        ptr    array1    i   *)
         end
-      | BinOp (MinusPP, BinOp(MinusPI, e1, e2, typ1), e3, typ2) -> (*                                                    *)
-        begin                                                      (*          MinusPP                     MinusA        *)
-          let ce1 = canonize e1 in                                 (*         /      \      =>            /     \        *)
-          let ce2 = canonize e2 in                                 (*     MinusPI     \              MinusPP     \       *)
-          let ce3 = canonize e3 in                                 (*    /   \         \            /      \      \      *) 
-          BinOp(MinusA, BinOp(MinusPP, ce1, ce3, typ2), ce2, typ2) (*  ptr    i     array1        ptr    array1    i     *)
+      | BinOp (MinusPP, BinOp(MinusPI, e1, e2, typ1), e3, typ2) -> (*                                                 *)
+        begin                                                      (*          MinusPP                     MinusA     *)
+          let ce1 = canonize e1 in                                 (*         /      \      =>            /     \     *)
+          let ce2 = canonize e2 in                                 (*     MinusPI     \              MinusPP     \    *)
+          let ce3 = canonize e3 in                                 (*    /   \         \            /      \      \   *) 
+          BinOp(MinusA, BinOp(MinusPP, ce1, ce3, typ2), ce2, typ2) (*  ptr    i     array1        ptr    array1    i  *)
         end
       | x -> x
 
   let query ctx (q:Queries.t) : Queries.Result.t =
   match q with
   | Queries.MustBeEqual (e1, e2) ->
-      begin
-        (* Printf.printf "---------------------->   comparing %s and %s \n" (ExpDomain.short 20 (`Lifted e1)) (ExpDomain.short 20 (`Lifted e2)); *)
-        if Expcompare.compareExp (canonize e1) (canonize e2) then
-          `Bool (true)
-        else
-          Queries.Result.top()
-      end
-  | Queries.MayBeEqual (e1, e2) ->
     begin
-      (* TODO *)
-      Queries.Result.top ()
-    end
-  | Queries.MayBeLess (e1, e2) ->
-    begin
-      (* TODO *)
-      Queries.Result.top ()
+      if Expcompare.compareExp (canonize e1) (canonize e2) then
+        `Bool (true)
+      else
+        Queries.Result.top()
     end
   | _ -> Queries.Result.top ()
 
