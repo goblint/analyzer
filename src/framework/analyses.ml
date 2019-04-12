4c3495e1a5b850637371d2ae5d88231cf781c4bc
Julian Erhard <julian.erhard@tum.de>
2019-04-12 22:17:47 +0200

    Use proper hash function on unknowns

--- src/framework/analyses.ml
+++ src/framework/analyses.ml
@@ -101,11 +101,10 @@ struct
     | (MyCFG.FunctionEntry f,_) -> 3
 
   let hashmul x y = if x=0 then y else if y=0 then x else x*y
-  let hash x = 1
-(*     match x with
+  let hash x = match x with
     | (MyCFG.Statement     s,d) -> hashmul (LD.hash d) (s.sid*17)
     | (MyCFG.Function      f,d) -> hashmul (LD.hash d) (f.vid*19)
-    | (MyCFG.FunctionEntry f,d) -> hashmul (LD.hash d) (f.vid*23) *)
+    | (MyCFG.FunctionEntry f,d) -> hashmul (LD.hash d) (f.vid*23)
 
   let equal (n1,d1) (n2,d2) = MyCFG.Node.equal n1 n2 && LD.equal d1 d2
 
