d58e53f04b114048f0cd14e0c1af9278bcb4e2a3
Simmo Saan <simmo.saan@gmail.com>
2019-06-25 17:03:30 +0300

    Add file sorting to SV-COMP set runner

--- sv-comp-run.py
+++ sv-comp-run.py
@@ -22,35 +22,40 @@ set_filename = sys.argv[1]
 with open(set_filename) as set_file:
     stats = collections.defaultdict(int)
 
+    code_filenames = []
     for pattern in set_file:
         pattern = pattern.strip()
         pattern = os.path.join(os.path.dirname(set_filename), pattern)
         if pattern:
             for code_filename in glob.iglob(pattern):
-                print(f"{code_filename}: ", end="", flush=True)
-                expected = extract_bool(r"_(false|true)-unreach-call", code_filename)
+                code_filenames.append(code_filename)
 
-                p = subprocess.run(f"~/Desktop/sv-comp/goblint/goblint --enable ana.sv-comp --enable dbg.debug {code_filename}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
-                actual = extract_bool(r"__VERIFIER_error unreach2: (false|true)", p.stdout)
+    for code_filename in sorted(code_filenames):
+        print(f"{code_filename}: ", end="", flush=True)
+        expected = extract_bool(r"_(false|true)-unreach-call", code_filename)
 
-                missing_funcs = False
-                for m in re.finditer(r"Function definition missing for (__VERIFIER_\S+)", p.stdout):
-                    missing_funcs = True
-                    print(f"MISSING FUNC {m.group(1)}")
+        # p = subprocess.run(f"~/Desktop/sv-comp/goblint/goblint --enable ana.sv-comp --enable dbg.debug {code_filename}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
+        p = subprocess.run(f"~/Desktop/sv-comp/goblint/goblint --enable ana.sv-comp --enable dbg.debug --set ana.activated[+] \"'var_eq'\" --enable ana.int.interval {code_filename}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
+        actual = extract_bool(r"__VERIFIER_error unreach2: (false|true)", p.stdout)
 
-                if missing_funcs:
-                    sys.exit(0)
+        missing_funcs = False
+        for m in re.finditer(r"Function definition missing for (__VERIFIER_\S+)", p.stdout):
+            missing_funcs = True
+            print(f"MISSING FUNC {m.group(1)}")
 
-                text = None
-                if expected is None or actual is None:
-                    text = f"NONE expected {expected}, actual {actual}"
-                elif actual == expected:
-                    text = f"CORRECT {expected}"
-                else:
-                    text = f"INCORRECT expected {expected}, actual {actual}"
+        if missing_funcs:
+            sys.exit(0)
 
-                print(text)
-                stats[text] += 1
+        text = None
+        if expected is None or actual is None:
+            text = f"NONE expected {expected}, actual {actual}"
+        elif actual == expected:
+            text = f"CORRECT {expected}"
+        else:
+            text = f"INCORRECT expected {expected}, actual {actual}"
+
+        print(text)
+        stats[text] += 1
 
     print("-" * 80)
     for text, count in stats.items():
