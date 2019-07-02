0596f3652e9afa52e5d9517626f01df70a4c2c90
Simmo Saan <simmo.saan@gmail.com>
2019-07-02 17:29:41 +0300

    Extract GraphML writing into separate module

--- src/framework/witness.ml
+++ src/framework/witness.ml
@@ -29,6 +29,52 @@ let find_loop_heads (module Cfg:CfgBidir) (file:Cil.file): unit NH.t =
 
   loop_heads
 
+
+module type GraphMlWriter =
+sig
+  type t
+
+  val start: out_channel -> t
+  val write_metadata: t -> string -> string -> unit
+  val write_node: t -> string -> (string * string) list -> unit
+  val write_edge: t -> string -> string -> (string * string) list -> unit
+  val stop: t -> unit
+end
+
+module XmlGraphMlWriter: GraphMlWriter =
+struct
+  type t =
+    {
+      out: out_channel;
+      mutable children: Xml.xml list
+    }
+
+  let start out = { out; children = [] }
+
+  let write_child g xml = g.children <- xml :: g.children
+
+  let xml_data key value = Xml.Element ("data", [("key", key)], [Xml.PCData value])
+  let xml_datas = List.map (fun (key, value) -> xml_data key value)
+  let write_metadata g key value = write_child g (xml_data key value)
+
+  let xml_node id datas =
+    Xml.Element ("node", [("id", id)], xml_datas datas)
+  let write_node g id datas = write_child g (xml_node id datas)
+
+  let xml_edge source target datas =
+    Xml.Element ("node", [("source", source); ("target", target)], xml_datas datas)
+  let write_edge g source target datas = write_child g (xml_edge source target datas)
+
+  let stop g =
+    let xml =
+      Xml.Element ("graphml", [], [
+          Xml.Element ("graph", [("edgedefault", "directed")], List.rev g.children)
+        ])
+    in
+    output_string g.out (Xml.to_string_fmt xml);
+    flush g.out
+end
+
 let write_file filename (module Cfg:CfgBidir) (file:Cil.file) entrystates (invariant:node -> Invariant.t) (is_live:node -> bool): unit =
   let (main_entry_nodes, other_entry_nodes) =
     entrystates
@@ -54,17 +100,27 @@ let write_file filename (module Cfg:CfgBidir) (file:Cil.file) entrystates (invar
     | FunctionEntry f -> Printf.sprintf "fun%d%s" f.vid f.vname
   in
 
-  let xml_data key value = Xml.Element ("data", [("key", key)], [Xml.PCData value]) in
-  let xml_node ~entry node =
-    Xml.Element ("node", [("id", node_name node)], List.concat [
+  let module GML = XmlGraphMlWriter in
+  let out = open_out filename in
+  let g = GML.start out in
+
+  GML.write_metadata g "witness-type" "correctness_witness";
+  GML.write_metadata g "sourcecodelang" "C";
+  GML.write_metadata g "producer" "Goblint";
+  GML.write_metadata g "specification" "CHECK( init(main()), LTL(G ! call(__VERIFIER_error())) )";
+  GML.write_metadata g "programfile" (getLoc main_entry).file;
+
+  let write_node ~entry node =
+    GML.write_node g (node_name node) (List.concat [
         begin if entry then
-            [xml_data "entry" "true"]
+            [("entry", "true")]
           else
             []
         end;
         begin match node, invariant node with
           | Statement _, Some i ->
-            [xml_data "invariant" i; xml_data "invariant.scope" (getFun node).svar.vname]
+            [("invariant", i);
+             ("invariant.scope", (getFun node).svar.vname)]
           | _ ->
             (* ignore entry and return invariants, variables of wrong scopes *)
             (* TODO: don't? fix scopes? *)
@@ -72,60 +128,56 @@ let write_file filename (module Cfg:CfgBidir) (file:Cil.file) entrystates (invar
         end;
         begin match node with
           | Statement s ->
-            [xml_data "sourcecode" (Pretty.sprint 80 (Basetype.CilStmt.pretty () s))] (* TODO: sourcecode not official? especially on node? *)
+            [("sourcecode", Pretty.sprint 80 (Basetype.CilStmt.pretty () s))] (* TODO: sourcecode not official? especially on node? *)
 
           (* violation actually only allowed in violation witness *)
           (* maybe should appear on from_node of entry edge instead *)
           | FunctionEntry f when f.vname = "__VERIFIER_error" ->
-            [xml_data "violation" "true"]
+            [("violation", "true")]
           | _ -> []
         end
       ])
   in
-  let xml_edge from_node ((loc, edge):Cil.location * edge) to_node =
-    Xml.Element ("edge", [("source", node_name from_node); ("target", node_name to_node)], List.concat [
+  let write_edge from_node ((loc, edge):Cil.location * edge) to_node =
+    GML.write_edge g (node_name from_node) (node_name to_node) (List.concat [
         begin if loc.line <> -1 then
-            [xml_data "startline" (string_of_int loc.line); xml_data "endline" (string_of_int loc.line)]
+            [("startline", string_of_int loc.line);
+             ("endline", string_of_int loc.line)]
           else
             []
         end;
         begin if NH.mem loop_heads to_node then
-            [xml_data "enterLoopHead" "true"]
+            [("enterLoopHead", "true")]
           else
             []
         end;
         begin match from_node, to_node with
           | _, FunctionEntry f ->
-            [xml_data "enterFunction" f.vname]
+            [("enterFunction", f.vname)]
           | Function f, _ ->
-            [xml_data "returnFromFunction" f.vname]
+            [("returnFromFunction", f.vname)]
           | _, _ -> []
         end;
         begin match edge with
           (* control actually only allowed in violation witness *)
           | Test (_, b) ->
-            [xml_data "control" ("condition-" ^ string_of_bool b)]
+            [("control", "condition-" ^ string_of_bool b)]
           (* enter and return on other side of nodes,
              more correct loc (startline) but had some scope problem? *)
           | Entry f ->
-            [xml_data "enterFunction2" f.svar.vname]
+            [("enterFunction2", f.svar.vname)]
           | Ret (_, f) ->
-            [xml_data "returnFromFunction2" f.svar.vname]
+            [("returnFromFunction2", f.svar.vname)]
           | _ -> []
         end
       ])
-  in
-
-  let graph_children = ref [] in
-  let add_graph_child xml =
-    graph_children := xml :: !graph_children
   in
 
   let added_nodes = NH.create 100 in
   let add_node ?(entry=false) node =
     if not (NH.mem added_nodes node) then begin
       NH.add added_nodes node ();
-      add_graph_child (xml_node ~entry node)
+      write_node ~entry node
     end
   in
 
@@ -137,13 +189,13 @@ let write_file filename (module Cfg:CfgBidir) (file:Cil.file) entrystates (invar
       let entry_node = FunctionEntry f in
       let return_node = Function f in
       iter_node entry_node;
-      add_graph_child (xml_edge from_node (loc, edge) entry_node);
+      write_edge from_node (loc, edge) entry_node;
       if NH.mem added_nodes return_node then
-        add_graph_child (xml_edge return_node (loc, edge) to_node)
+        write_edge return_node (loc, edge) to_node
       else
         () (* return node missing, function never returns *)
     | _ ->
-      add_graph_child (xml_edge from_node (loc, edge) to_node)
+      write_edge from_node (loc, edge) to_node
   and add_edges from_node locedges to_node =
     List.iter (fun locedge -> add_edge from_node locedge to_node) locedges
   and iter_node node =
@@ -167,18 +219,5 @@ let write_file filename (module Cfg:CfgBidir) (file:Cil.file) entrystates (invar
   add_node ~entry:true main_entry;
   iter_node main_entry;
 
-  let xml =
-    Xml.Element ("graphml", [], [
-        Xml.Element ("graph", [("edgedefault", "directed")], List.append [
-            xml_data "witness-type" "correctness_witness";
-            xml_data "sourcecodelang" "C";
-            xml_data "producer" "Goblint";
-            xml_data "specification" "CHECK( init(main()), LTL(G ! call(__VERIFIER_error())) )";
-            xml_data "programfile" (getLoc main_entry).file
-          ] (List.rev !graph_children))
-      ])
-  in
-  let out = open_out filename in
-  Printf.fprintf out "%s" (Xml.to_string_fmt xml);
-  flush out;
+  GML.stop g;
   close_out_noerr out
