5f4155be9d845498c8fedb89c3e11c7559e46908
Simmo Saan <simmo.saan@gmail.com>
2019-09-04 14:40:49 +0300

    Add comment about CIL and its temporary variables

--- src/domains/invariantCil.ml
+++ src/domains/invariantCil.ml
@@ -2,6 +2,7 @@ open Cil
 
 (* TODO: detect temporaries created by Cil? *)
 (* let var_is_tmp {vdescrpure} = not vdescrpure (* doesn't exclude tmp___0 *) *)
+(* TODO: instead check if vdescr is nonempty? (doesn't cover all cases, e.g. ternary temporary) *)
 let tmp_var_regexp = Str.regexp "^\\(tmp\\(___[0-9]+\\)?\\|cond\\|RETURN\\)$"
 let var_is_tmp {vname} = Str.string_match tmp_var_regexp vname 0
 let rec exp_contains_tmp = function
