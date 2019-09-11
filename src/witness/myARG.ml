bdeb29feec852144c1530e3f4341d54e209f53d6
Simmo Saan <simmo.saan@gmail.com>
2019-09-11 14:52:43 +0300

    Avoid generating unnecessary ternary for logic uncil

--- src/witness/myARG.ml
+++ src/witness/myARG.ml
@@ -230,8 +230,12 @@ struct
   open Cil
 
   let ternary e_cond e_true e_false =
-    (* CIL has no exp for ternary at all..., this string constant is just decorative *)
-    Const (CStr (Pretty.sprint 1000 (Pretty.dprintf "%a ? %a : %a" dn_exp e_cond dn_exp e_true dn_exp e_false)))
+    if e_true = Cil.one && e_false = Cil.zero then
+      (* avoid unnecessary ternary *)
+      e_cond
+    else
+      (* CIL has no exp for ternary at all..., this string constant is just decorative *)
+      Const (CStr (Pretty.sprint 1000 (Pretty.dprintf "%a ? %a : %a" dn_exp e_cond dn_exp e_true dn_exp e_false)))
 
   let rec next_opt' n = match n with
     | Statement {skind=If (_, _, _, loc)} when GobConfig.get_bool "exp.uncilwitness" ->
