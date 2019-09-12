8e98b77c67bfb02e8b42ba59faef7ff2278fe8e9
Michael Schwarz <michael.schwarz93@gmail.com>
2019-09-12 08:29:03 +0200

    Fix typo

--- src/util/defaults.ml
+++ src/util/defaults.ml
@@ -176,7 +176,7 @@ let _ = ()
       ; reg Experimental "exp.fast_global_inits" "false" "Only generate 'a[0] = 0' for a zero-initialized array a[n]. This is only sound for our flat array domain! TODO change this once we use others!"
       ; reg Experimental "exp.uninit-ptr-safe"   "false" "Assume that uninitizalized stack-allocated pointers may only point to variables not in the program or null."
       ; reg Experimental "exp.ptr-arith-safe"    "false" "Assume that pointer arithmetic only yields safe addresses."
-      ; reg Experimental "exp.partition-arrays"  "false" "Employ the paritioning array domain. When this is on, make sure to enable the expRelation analysis as well."
+      ; reg Experimental "exp.partition-arrays"  "false" "Employ the partitioning array domain. When this is on, make sure to enable the expRelation analysis as well."
 
 (* {4 category [Debugging]} *)
 let _ = ()
