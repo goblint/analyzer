2d5c13e255485212f8a489d6e2f742405f122884
Ralf Vogler <ralf.vogler@gmail.com>
2019-10-01 19:31:45 +0200

    allow to call script on individual .yml file

--- sv-comp-run.py
+++ sv-comp-run.py
@@ -28,67 +28,72 @@ def extract_bool(p, s):
 
 stats = collections.defaultdict(int)
 try:
-    set_filename = sys.argv[1]
-    with open(set_filename) as set_file:
-        task_filenames = []
-        for pattern in set_file:
-            pattern = pattern.strip()
-            if pattern:
-                pattern = os.path.join(os.path.dirname(set_filename), pattern)
-                for task_filename in glob.iglob(pattern):
-                    task_filenames.append(task_filename)
+    inp = sys.argv[1]
+    task_filenames = []
+    if inp.endswith('.yml'):
+        task_filenames.append(inp)
+    elif inp.endswith('.set'):
+        with open(inp) as set_file:
+            for pattern in set_file:
+                pattern = pattern.strip()
+                if pattern:
+                    pattern = os.path.join(os.path.dirname(inp), pattern)
+                    for task_filename in glob.iglob(pattern):
+                        task_filenames.append(task_filename)
+    else:
+        print('Call script on .yml or .set file') # TODO also allow .c to just call goblint on with above options?
 
-        for task_filename in sorted(task_filenames):
-            print(f"{task_filename}: ", end="", flush=True)
+    for task_filename in sorted(task_filenames):
+        print(f"{task_filename}: ", end="", flush=True)
 
-            if task_filename.endswith(".yml"):
-                with open(task_filename) as task_file:
-                    y = yaml.safe_load(task_file)
+        if task_filename.endswith(".yml"):
+            with open(task_filename) as task_file:
+                y = yaml.safe_load(task_file)
 
-                    code_filename = y["input_files"]
-                    code_filename = os.path.join(os.path.dirname(task_filename), code_filename)
+                code_filename = y["input_files"]
+                code_filename = os.path.join(os.path.dirname(task_filename), code_filename)
 
-                    expected = None
-                    for y_property in y["properties"]:
-                        if y_property["property_file"] == "../properties/unreach-call.prp":
-                            expected = y_property["expected_verdict"]
-            else:
-                code_filename = task_filename
-                expected = extract_bool(r"_(false|true)-unreach-call", task_filename)
+                expected = None
+                for y_property in y["properties"]:
+                    if y_property["property_file"] == "../properties/unreach-call.prp":
+                        expected = y_property["expected_verdict"]
+        else:
+            code_filename = task_filename
+            expected = extract_bool(r"_(false|true)-unreach-call", task_filename)
 
-            if OVERVIEW:
-                actual = None
-            else:
-                try:
-                    p = subprocess.run(shlex.split(GOBLINT_COMMAND.format(code_filename=code_filename)), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8", timeout=TIMEOUT)
-                    if "Fatal error: exception " in p.stdout:
-                        print(p.stdout)
-                        exit(1)
-                    actual = extract_bool(r"SV-COMP \(unreach-call\): (false|true)", p.stdout)
-                except subprocess.TimeoutExpired:
-                    actual = "timeout"
+        if OVERVIEW:
+            actual = None
+        else:
+            try:
+                p = subprocess.run(shlex.split(GOBLINT_COMMAND.format(code_filename=code_filename)), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8", timeout=TIMEOUT)
+                if "Fatal error: exception " in p.stdout:
+                    print(p.stdout)
+                    exit(1)
+                actual = extract_bool(r"SV-COMP \(unreach-call\): (false|true)", p.stdout)
+            except subprocess.TimeoutExpired:
+                actual = "timeout"
 
-            if "p" in locals(): # sometimes on timeout p is declared, sometimes isn't
-                missing_funcs = False
-                for m in re.finditer(r"Function definition missing for (__VERIFIER_\S+)", p.stdout):
-                    missing_funcs = True
-                    print(f"MISSING FUNC {m.group(1)}")
+        if "p" in locals(): # sometimes on timeout p is declared, sometimes isn't
+            missing_funcs = False
+            for m in re.finditer(r"Function definition missing for (__VERIFIER_\S+)", p.stdout):
+                missing_funcs = True
+                print(f"MISSING FUNC {m.group(1)}")
 
-                if missing_funcs:
-                    sys.exit(1)
+            if missing_funcs:
+                sys.exit(1)
 
-            text = None
-            if expected is None or actual is None:
-                text = f"NONE expected {expected}, actual {actual}"
-            elif actual == expected:
-                text = f"CORRECT {expected}"
-            elif actual == "timeout":
-                text = f"UNKNOWN expected {expected}, actual {actual}"
-            else:
-                text = f"INCORRECT expected {expected}, actual {actual}"
+        text = None
+        if expected is None or actual is None:
+            text = f"NONE expected {expected}, actual {actual}"
+        elif actual == expected:
+            text = f"CORRECT {expected}"
+        elif actual == "timeout":
+            text = f"UNKNOWN expected {expected}, actual {actual}"
+        else:
+            text = f"INCORRECT expected {expected}, actual {actual}"
 
-            print(text)
-            stats[text] += 1
+        print(text)
+        stats[text] += 1
 
 except KeyboardInterrupt:
     pass
