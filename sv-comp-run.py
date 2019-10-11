6afdaac0eb87fb620f7bb123593c2cc0555fe9e3
Ralf Vogler <ralf.vogler@gmail.com>
2019-10-11 15:24:18 +0200

    sort stats, explain points

--- sv-comp-run.py
+++ sv-comp-run.py
@@ -30,6 +30,7 @@ def extract_bool(p, s):
 
 stats = collections.defaultdict(int)
 total_time = 0
+points = 0
 try:
     inp = sys.argv[1]
     task_filenames = []
@@ -65,7 +66,7 @@ try:
             expected = extract_bool(r"_(false|true)-unreach-call", task_filename)
 
         if OVERVIEW:
-            actual = None
+            result = None
             task_time = None
         else:
             start_time = timer()
@@ -74,9 +75,9 @@ try:
                 if "Fatal error: exception " in p.stdout:
                     print(p.stdout)
                     exit(1)
-                actual = extract_bool(r"SV-COMP \(unreach-call\): (false|true)", p.stdout)
+                result = extract_bool(r"SV-COMP \(unreach-call\): (false|true)", p.stdout)
             except subprocess.TimeoutExpired:
-                actual = "timeout"
+                result = "timeout"
             finally:
                 end_time = timer()
                 task_time = end_time - start_time
@@ -93,15 +94,15 @@ try:
 
         text = None
         if expected is None:
-            text = f"NONE {actual}, no expected_verdict for unreach-call"
-        elif actual == expected:
+            text = f"NONE {result}, no expected_verdict for unreach-call"
+        elif result == expected:
             text = f"CORRECT {expected}"
-            points += 2 if actual else 1
-        elif actual == "timeout" or actual is None:
-            text = f"UNKNOWN {actual}, expected {expected}"
+            points += 2 if result else 1
+        elif result == "timeout" or result is None:
+            text = f"UNKNOWN {result}, expected {expected}"
         else:
-            text = f"INCORRECT {actual}, expected {expected}"
-            points -= 32 if actual else 16
+            text = f"INCORRECT {result}, expected {expected}"
+            points -= 32 if result else 16
 
         time_text = f" ({task_time:.2f} s)" if task_time is not None else ""
         print(text + time_text)
@@ -114,11 +115,13 @@ finally:
     print("-" * 80)
     print("For each .yml we compare with expected_verdict for 'c/properties/unreach-call.prp':")
     print("CHECK( init(main()), LTL(G ! call(__VERIFIER_error())) )")
-    print("-> 'expected True' means for all paths __VERIFIER_error is never called (__VERIFIER_assert always true) ")
-    print("-> 'expected False' means there exists a path where  __VERIFIER_error could be called (__VERIFIER_assert can be false)")
+    print("-> True means for all paths __VERIFIER_error is never called (__VERIFIER_assert always true) ")
+    print("-> False means there exists a path where __VERIFIER_error could be called (__VERIFIER_assert can be false)")
+    print("We will often report False for may-reach because we have no must-reach, just must-not-reach (bot).")
     print("-" * 80)
-    for text, count in stats.items():
+    for text, count in sorted(stats.items()):
         print(f"{text}: {count}")
     print("-" * 80)
-    print(f"total time: {total_time:.2f} s")
+    print("points: CORRECT{ True: 2, False: 1 }, INCORRECT{ True: -32, False: -16 }")
     print(f"total points: {points} (assuming witnesses are correct)")
+    print(f"total time: {total_time:.2f} s")
