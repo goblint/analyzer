219a0572d64468f6fe04da3a10d1118785000cff
Michael Schwarz <michael.schwarz93@gmail.com>
2019-10-17 10:57:16 +0200

    More examples

--- tests/regression/25-vla/04-passing_ptr_to_array.c
+++ tests/regression/25-vla/04-passing_ptr_to_array.c
@@ -2,7 +2,47 @@
 
 void foo(int (*a)[40]){
     int x = (*(a + 29))[7];
-    assert(x == 23); //UNKNOWN
+    assert(x == 23); //FAIL
+
+    int y = (*(a + 7))[13];
+    assert(y == 23);
+
+    assert(a[7][13] == 23);
+}
+
+void foo2(int n,int (*a)[n]){
+    int x = (*(a + 29))[7];
+    assert(x == 23); //FAIL
+
+    int y = (*(a + 7))[13];
+    assert(y == 23);
+
+    assert(a[7][13] == 23);
+}
+
+void foo3(int n,int a[][n]){
+    int x = (*(a + 29))[7];
+    assert(x == 23); //FAIL
+
+    int y = (*(a + 7))[13];
+    assert(y == 23);
+
+    assert(a[7][13] == 23);
+}
+
+void foo4(int n,int a[n][n]){
+    int x = (*(a + 29))[7];
+    assert(x == 23); //FAIL
+
+    int y = (*(a + 7))[13];
+    assert(y == 23);
+
+    assert(a[7][13] == 23);
+}
+
+void foo5(int n, int m, int a[n][m]){
+    int x = (*(a + 29))[7];
+    assert(x == 23); //FAIL
 
     int y = (*(a + 7))[13];
     assert(y == 23);
@@ -14,7 +54,18 @@ int main(void)
 {
     int n =40;
     int b[n][n];
+
+    for(int i=0;i < 40; i++) {
+        for(int j=0; j<40;j++) {
+            b[i][j] = 0;
+        }
+    }
+
     b[7][13] = 23;
 
     foo(b);
+    foo2(40,b);
+    foo3(40,b);
+    foo4(40,b);
+    foo5(40,40,b);
 }
