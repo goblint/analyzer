13d56bef75a5ce26ef28a2a07dd75470b347c2a7
Michael Schwarz <michael.schwarz93@gmail.com>
2020-03-09 10:58:32 +0100

    Fix {track,trace}_{on,off}.sh to work with dune
    Was complaining about some files being present, and others not (see blow). Run `make gen` to solve this issue.
    
    Error: Some of the targets of this fallback rule are present in the source
    tree, and some are not. This is not allowed. Either none of the targets must
    be present in the source tree, either they must all be.
    
    The following targets are present:
    - src/config.ml
    - src/version.ml
    
    The following targets are not:
    - src/goblint.ml
    Makefile:4: recipe for target 'native' failed
    make: *** [native] Error 1

--- scripts/trace_off.sh
+++ scripts/trace_off.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+make gen
 scripts/set_version.sh
 grep -q 'tracing = true' src/config.ml && \
   sed 's/tracing = true/tracing = false/' src/config.ml > src/config.tmp && mv src/config.tmp src/config.ml
