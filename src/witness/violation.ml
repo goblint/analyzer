ba51b1267fd60fca6a4a44cd410011d5772cb0bd
Simmo Saan <simmo.saan@gmail.com>
2020-04-07 15:52:26 +0300

    Implement BFS-based find_path in Violation

--- src/witness/violation.ml
+++ src/witness/violation.ml
@@ -32,8 +32,25 @@ exception Found
 let find_path (module Arg:ViolationArg) =
   let module NHT = BatHashtbl.Make (Arg.Node) in
 
+  let rec trace_path next_nodes node2 =
+    if NHT.mem next_nodes node2 then begin
+      (* ignore (Pretty.printf "PATH: %s\n" (Arg.Node.to_string node2)); *)
+      let (edge, next_node) = NHT.find next_nodes node2 in
+      (* ignore (Pretty.printf "  %a\n" MyCFG.pretty_edge edge); *)
+      (node2, edge, next_node) :: trace_path next_nodes next_node
+    end
+    else
+      []
+  in
+
+  let print_path path =
+    List.iter (fun (n1, e, n2) ->
+        ignore (Pretty.printf "  %s =[%a]=> %s\n" (Arg.Node.to_string n1) MyCFG.pretty_edge e (Arg.Node.to_string n2))
+      ) path
+  in
+
   let find_path node =
-    (* TODO: non-crap find_path *)
+    (* TODO: delete crap find_path *)
     let next_nodes = NHT.create 100 in
 
     let itered_nodes = NHT.create 100 in
@@ -51,24 +68,42 @@ let find_path (module Arg:ViolationArg) =
       end
     in
 
-    let rec trace_path node2 =
-      if not (Arg.Node.equal node node2) then begin
-        (* ignore (Pretty.printf "PATH: %s\n" (Arg.Node.to_string node2)); *)
-        let (edge, next_node) = NHT.find next_nodes node2 in
-        (* ignore (Pretty.printf "  %a\n" MyCFG.pretty_edge edge); *)
-        (node2, edge, next_node) :: trace_path next_node
-      end
-      else
-        []
-    in
-
     try iter_node node with
     | Found ->
-      let path = trace_path Arg.main_entry in
-      List.iter (fun (n1, e, n2) ->
-          ignore (Pretty.printf "  %s =[%a]=> %s\n" (Arg.Node.to_string n1) MyCFG.pretty_edge e (Arg.Node.to_string n2))
-        ) path
+      let path = trace_path next_nodes Arg.main_entry in
+      print_path path
   in
 
-  find_path (List.hd Arg.violations);
+  let find_path2 nodes =
+    let next_nodes = NHT.create 100 in
+
+    let itered_nodes = NHT.create 100 in
+    let rec bfs curs nexts = match curs with
+      | node :: curs' ->
+        if Arg.Node.equal node Arg.main_entry then
+          raise Found
+        else if not (NHT.mem itered_nodes node) then begin
+          NHT.replace itered_nodes node ();
+          List.iter (fun (edge, prev_node) ->
+              NHT.replace next_nodes prev_node (edge, node)
+            ) (Arg.prev node);
+          bfs curs' (List.map snd (Arg.prev node) @ nexts)
+        end
+        else
+          bfs curs' nexts
+      | [] ->
+        match nexts with
+        | [] -> ()
+        | _ -> bfs nexts []
+    in
+
+    try bfs nodes [] with
+    | Found ->
+      let path = trace_path next_nodes Arg.main_entry in
+      print_path path
+  in
+
+  (* find_path (List.hd Arg.violations);
+  print_endline (String.make 80 '='); *)
+  find_path2 Arg.violations;
   ()
