55432483136f23387199b16c80e1e33bdc6c533e
Julian Erhard <julian.erhard@tum.de>
2020-08-12 15:34:36 +0200

    Ouput name of source file when incomparable abstract values are encnountered.

--- src/cdomains/valueDomain.ml
+++ src/cdomains/valueDomain.ml
@@ -359,7 +359,7 @@ struct
 
   let warn_type op x y =
     if GobConfig.get_bool "dbg.verbose" then
-      ignore @@ printf "warn_type %s: incomparable abstr. values %s and %s at line %i: %a and %a\n" op (tag_name x) (tag_name y) !Tracing.current_loc.line pretty x pretty y
+      ignore @@ printf "warn_type %s: incomparable abstr. values %s and %s in %s at line %i: %a and %a\n" op (tag_name x) (tag_name y) !Tracing.current_loc.file !Tracing.current_loc.line pretty x pretty y
 
   let leq x y =
     match (x,y) with
