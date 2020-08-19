4f6141ca94a2992e0e2d98275b0b4f5790a01761
Michael Schwarz <michael.schwarz93@gmail.com>
2020-08-19 14:01:50 +0200

    DefExc: Operation with one argument bottom yields bottom
    This was only like this for lift2_inj but not for lift2 and lift2_special. This behavior is consistent with happens for Interval32, and fixes an issue in one of the larger programs where invariant has a problem with the top () of the wrong ikind

--- src/cdomains/intDomain.ml
+++ src/cdomains/intDomain.ml
@@ -756,12 +756,13 @@ struct
       complain xr yr;
       (try `Definite (f x y,xr) with | Division_by_zero -> top ())
     (* We don't bother with exclusion sets: *)
-    | `Excluded _, _ -> top ()
-    | _, `Excluded _ -> top ()
-    (* The good case: *)
-    | `Definite x, `Definite y -> (try `Definite (f x y) with | Division_by_zero -> top ())
-    (* If any one of them is bottom, we return top *)
-    | _ -> top ()
+    | `Excluded (_, xr), `Definite(_, yr)
+    | `Definite (_, xr), `Excluded(_, yr)
+    | `Excluded (_, xr), `Excluded(_, yr) ->
+      check_identical_range xr yr;
+      `Excluded(S.empty (), xr)
+    (* If any one of them is bottom, we return bottom *)
+    | _ -> `Bot
 
   (* For the shift operations, CIL does not cast the right argument to the type of the left argument,    *)
   (* so we should not warn about operations on different types here. The result has the type of the left *)
@@ -774,12 +775,8 @@ struct
     | `Definite (_, xr), `Excluded(_, yr)
     | `Excluded (_, xr), `Excluded(_, yr) ->
       `Excluded(S.empty (), xr)
-    (* If any one of them is bottom, we return top *)
-    | `Bot, `Excluded(_, xr)
-    | `Bot, `Definite(_, xr)
-    | `Excluded(_ , xr), `Bot
-    | `Definite(_, xr), `Bot -> top ()
-    | `Bot, `Bot -> top ()
+    (* If any one of them is bottom, we return bottom *)
+    | _ -> `Bot
 
   (* Default behaviour for binary operators that are injective in either
    * argument, so that Exclusion Sets can be used: *)
