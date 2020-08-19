0bcce88f7302c6a45d1e85859e65a8626ebe1dba
Ralf Vogler <ralf.vogler@gmail.com>
2020-08-19 11:28:12 +0200

    DefExc.cast_to: Excluded: use range of target type (more precise)
    This failed before, but now somehow works.

--- src/cdomains/intDomain.ml
+++ src/cdomains/intDomain.ml
@@ -634,7 +634,7 @@ struct
   let cast_to t = function
     | `Excluded (s,r) ->
       let r' = size t in (* target range *)
-      `Excluded (if R.leq r r' then S.map (Integers.cast_to t) s, r else S.empty (), r') (* TODO can we do better here? *)
+      `Excluded (if R.leq r r' then S.map (Integers.cast_to t) s, r' else S.empty (), r') (* TODO can we do better here? *)
     | `Definite x -> (try `Definite (Integers.cast_to t x) with Size.Not_in_int64 -> top_of t)
     | `Bot -> `Bot
 
