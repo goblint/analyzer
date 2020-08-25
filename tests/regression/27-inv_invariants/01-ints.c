a06b095baf918331017f43176ed52f528e7ec7a2
Michael Schwarz <michael.schwarz93@gmail.com>
2020-08-25 08:19:12 +0200

    Invariant: Add tests in 27/01 back with correct assertions

--- tests/regression/27-inv_invariants/01-ints.c
+++ tests/regression/27-inv_invariants/01-ints.c
@@ -12,12 +12,14 @@ int main() {
     assert(x == 2);
   if (5-x == 3 && x+y == x*3)
     assert(x == 2 && y == 4);
-  // if (x == 3 && y/x == 2)
-  //   assert(y == 6);
+  if (x == 3 && y/x == 2)
+    // y could for example also be 7
+    assert(y == 6);  // UNKNOWN!
   if (y/x == 2 && x == 3)
     assert(x == 3); // TODO y == 6
-  // if (2+(3-x)*4/5 == 6 && 2*y >= x+4)
-  //   assert(x == -2 && y >= 1);
+  if (2+(3-x)*4/5 == 6 && 2*y >= x+4)
+    // x could also be -3
+    assert(x == -2 && y >= 1); //UNKNOWN!
   if (x > 1 && x < 5 && x % 2 == 1)
     assert(x != 2); // [2,4] -> [3,4] TODO x % 2 == 1
 
@@ -32,20 +34,19 @@ int main() {
     assert(xl == 2);
   if (5-xl == 3 && xl+yl == xl*3)
     assert(xl == 2 && yl == 4);
-  // if (xl == 3 && yl/xl == 2)
-  //   assert(yl == 6);
+  if (xl == 3 && yl/xl == 2)
+    // yl could for example also be 7
+    assert(yl == 6); // UNKNOWN!
   if (yl/xl == 2 && xl == 3)
     assert(xl == 3); // TODO yl == 6
-  if (2+(3-xl)*4/5 == 6 && 2*yl >= xl+4) {
-
-  }
-    // UNKNOWN due to Interval32 not being able to represent long
-    // assert(xl == -2 && yl >= 1);
+  if (2+(3-xl)*4/5 == 6 && 2*yl >= xl+4)
+    // xl could also be -3
+    assert(xl == -2 && yl >= 1); //UNKNOWN!
   if (xl > 1 && xl < 5 && xl % 2 == 1) {
-
-  }
     // UNKNOWN due to Interval32 not being able to represent long
     // assert(xl != 2); // [2,4] -> [3,4] TODO x % 2 == 1
+  }
+
 
   short xs, ys, zs;
   if (xs+1 == 2) {
@@ -58,13 +59,16 @@ int main() {
     assert(xs == 2);
   if (5-xs == 3 && xs+ys == xs*3)
     assert(xs == 2 && ys == 4);
-  // if (xs == 3 && ys/xs == 2)
-  //   assert(ys == 6);
+  if (xs == 3 && ys/xs == 2)
+    // ys could for example also be 7
+    assert(ys == 6); // UNKNOWN!
   if (ys/xs == 2 && xs == 3)
     assert(xs == 3); // TODO yl == 6
-  // if (2+(3-xs)*4/5 == 6 && 2*ys >= xs+4) {
-  //   assert(xs == -2 && ys >= 1);
-  // }
+  if (2+(3-xs)*4/5 == 6 && 2*ys >= xs+4) {
+    // xs could also be -3
+    assert(xs == -2 && ys >= 1); //UNKNOWN!
+  }
   if (xs > 1 && xs < 5 && xs % 2 == 1) {
     assert(xs != 2);
-  }}
+  }
+}
