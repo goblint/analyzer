2d1a0f5ecf442ace18345975535f48f8aea4429f
Michael Schwarz <michael.schwarz93@gmail.com>
2020-09-08 19:57:45 +0200

    Move enum test to proper place

--- tests/regression/01-cpa/35-enum.c
+++
@@ -1,7 +0,0 @@
-// PARAM: --disable ana.int.interval --disable ana.int.def_exc --enable ana.int.enums
-void main(){
-    int n = 1;
-    for (; n; n++) { // fixed point not reached here
-	}
-	return;
-}
