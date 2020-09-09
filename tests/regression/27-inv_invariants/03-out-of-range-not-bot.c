094d0c8770800e5954fba2c9a602d71fad4154fc
Julian Erhard <julian.erhard@tum.de>
2020-09-09 15:00:33 +0200

    Indentation

--- tests/regression/27-inv_invariants/03-out-of-range-not-bot.c
+++ tests/regression/27-inv_invariants/03-out-of-range-not-bot.c
@@ -1,7 +1,7 @@
 // PARAM: --enable ana.int.def_exc --disable ana.int.interval
 #include<assert.h>
 // Some unsigned long constant that is larger than 2^63-1
-#define SIZE_MAX	18446744073709551615UL
+#define SIZE_MAX 18446744073709551615UL
 
 int foo(){
     int top;
