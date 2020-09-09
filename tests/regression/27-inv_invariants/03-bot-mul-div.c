bba9480ec79676c3ed783adee7c7964d2ce9aa19
Julian Erhard <julian.erhard@tum.de>
2020-09-09 15:16:51 +0200

    Cleanup test.

--- tests/regression/27-inv_invariants/03-bot-mul-div.c
+++ tests/regression/27-inv_invariants/03-bot-mul-div.c
@@ -1,10 +1,10 @@
 // PARAM: --enable ana.int.def_exc --disable ana.int.interval
 #include<assert.h>
-
-int foo(){
-    int top;
-    int a = 3;
-    if (top < -1 / (8 * sizeof(int))){ // parts of the expression errorneously evaluate to bottom
+#include<stdio.h>
+int main(){
+    unsigned long top;
+    printf("value to reach true branch: %ld\n", (-1 / (8 * sizeof(int))));
+    if (top == (-1 / (8 * sizeof(int)))){ // parts of the expression evaluate to bottom and make Goblint crash
         assert(1);
     }
     return 0;
