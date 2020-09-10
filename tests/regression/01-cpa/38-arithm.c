b0dd9068b841452bab3bde5ea44c5f375bb66980
Julian Erhard <julian.erhard@tum.de>
2020-09-10 14:47:12 +0200

    Add testcase showing multiplication (w/o 0) is not injective.
    This shows a problem in the treatment of operations:
        (Excluded (s, r)) op (Definite y)
    
    Excluded(s op y, _) is in general not a correct result value here.
    (Where s op y := {x op y | x in s})
    This is because there might be an z in gamma(Excluded (s, r)) such that
        z op y (mod 2^32)
    is in (s op y).
    
    Some overflow checking is needed.

---
+++ tests/regression/01-cpa/38-arithm.c
@@ -0,0 +1,18 @@
+// PARAM: --enable ana.int.def_exc --disable ana.int.interval
+#include<stdio.h>
+#include<assert.h>
+// 2 ^ 30
+#define MULT 1073741824
+
+int main(){
+    unsigned int top;
+    unsigned int result;
+    // top = 7;
+    if(top != 3){
+        result = top * MULT;
+        // if top == 7 then we have (2 + 1) * 2^30 == (4 + 2 + 1) * 2^30  (mod 2^32)
+        assert(result != 3221225472); // UNKNOWN
+        printf("%u\n", result);
+    }
+    return result;
+}
