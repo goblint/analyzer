4de21f22d6d2f1b5880bde9cbfb788f21d2b1bb0
superbr4in <15275491+superbr4in@users.noreply.github.com>
2020-09-29 21:39:09 +0200

    Clean up and fix sorting of multi-file locations

--- src/transform/expressionEvaluation.ml
+++ src/transform/expressionEvaluation.ml
@@ -1,38 +1,24 @@
-let transformation_name = "expeval"
+open Batteries
 
-module Transformation : Transform.S =
+let transformation_identifier = "expeval"
+let transformation_query_file_name_identifier = "trans." ^ transformation_identifier ^ ".query_file_name"
+
+module ExpEval : Transform.S =
   struct
 
-    type query =
-      {
-        kind : SyntacticalAnalyzer.JsonParser.kind;
-        target : SyntacticalAnalyzer.JsonParser.target;
-        find : SyntacticalAnalyzer.JsonParser.find;
-        structure : (SyntacticalAnalyzer.JsonParser.structure [@default None_s]);
-        limitation : (SyntacticalAnalyzer.JsonParser.constr [@default None_c]);
-        expression : string;
-        mode :
-          [
-          | `Must
-          | `May
-          ];
-      }
-      [@@deriving yojson]
+    let (~?) exception_function =
+      try Some (exception_function ()) with
+      | _ -> None
 
     class evaluator (file : Cil.file) (ask : Cil.location -> Queries.t -> Queries.Result.t) =
+      object (self)
 
-      let global_functions =
-        file.globals
-          |> List.filter_map (function Cil.GFun (f, _) -> Some f | _ -> None)
-      in
-      let global_variables =
-        file.globals
-          |> List.filter_map (function Cil.GVar (v, _, _) -> Some (v.vname, Cil.Fv v) | _ -> None)
-      in
-
-      let function_table, statement_table =
-        let function_statements =
-          global_functions
+        val global_variables =
+          file.globals
+            |> List.filter_map (function Cil.GVar (v, _, _) -> Some (v.vname, Cil.Fv v) | _ -> None)
+        val statements =
+          file.globals
+            |> List.filter_map (function Cil.GFun (f, _) -> Some f | _ -> None)
             (* Take all statements *)
             |> List.map (fun (f : Cil.fundec) -> f.sallstmts |> List.map (fun s -> f, s))
             |> List.flatten
@@ -40,107 +26,67 @@ module Transformation : Transform.S =
             |> List.map (fun (f, (s : Cil.stmt)) -> (Cil.get_stmtLoc s.skind, f, s))
             (* Filter artificial ones by impossible location *)
             |> List.filter (fun ((l : Cil.location), _, _) -> l.line >= 0)
-        in
-        function_statements
-          |> List.fold_left (fun functions (l, f, _) -> Hashtbl.add functions l f; functions) (Hashtbl.create 0),
-        function_statements
-          |> List.fold_left
-            begin
-              fun statements (l, _, s) ->
-                begin
-                  match Hashtbl.find_opt statements l with
-                  | Some ss -> Hashtbl.replace statements l (s::ss)
-                  | None -> Hashtbl.add statements l [s]
-                end;
-                statements
-            end
-            (Hashtbl.create 0)
-      in
-
-      object (self)
-
-        val mutable debug_info_1 : bool = false
-        val mutable debug_info_2 : (Cil.stmt * (Cil.location * Cil.stmt) option) option = None
-        val mutable debug_info_3 : bool option = None
-
-        method get_debug_info_1 () = debug_info_1
-        method get_debug_info_2 () = debug_info_2
-        method get_debug_info_3 () = debug_info_3
+            (* Create hash table *)
+            |> List.fold_left (fun ss (l, f, s) -> Hashtbl.add ss l (f, s); ss) (Hashtbl.create 0)
 
         method evaluate location expression_string =
-          debug_info_1 <- false;
-          debug_info_2 <- None;
-          debug_info_3 <- None;
-          match self#get_query location expression_string with
-          | Some query ->
-              begin
-                match self#get_value location query with
-                | Some value_before ->
-                    begin
-                      match
-                        self#get_successors location
-                          |> List.find_map (fun succeeding_location -> self#get_value succeeding_location query)
-                      with
-                      | Some value_after -> value_after
-                      | None -> value_before
-                    end
-                | None -> Some false
-              end
-          | None -> Some false
-
-        method private get_query location expression_string =
+          (* Compute the available local variables *)
           let local_variables =
-            match Hashtbl.find_opt function_table location with
-            | Some function_declaration ->
-                function_declaration.slocals
-                  |> List.map (fun (v : Cil.varinfo) -> v.vname, Cil.Fv v)
+            match Hashtbl.find_option statements location with
+            | Some (function_definition, _) -> function_definition.slocals |> List.map (fun (v : Cil.varinfo) -> v.vname, Cil.Fv v)
             | None -> []
           in
-          let parse_result =
-            try Some (Formatcil.cExp expression_string (local_variables @ global_variables))
-            with _ -> None
-          in
-          match parse_result with
+          (* Compute evaluation result *)
+          match ~? (fun () -> Formatcil.cExp expression_string (local_variables @ global_variables)) with
+          | None ->
+              Some false
           | Some expression ->
-              debug_info_1 <- true;
-              Some (Queries.EvalInt expression)
-          | None ->
-              debug_info_1 <- false;
-              None
-        method private get_successors location =
-          let statement =
-            Hashtbl.find statement_table location
-              |> List.hd (* TODO *)
-          in
-          statement.succs
-            |> List.map (fun (succeeding_statement : Cil.stmt) -> Cil.get_stmtLoc succeeding_statement.skind)
-        method private get_value location query =
-          let ask_result =
-            try Some (ask location query)
-            with Not_found -> None
-          in
-          match ask_result with
-          | Some `Bot ->
-              debug_info_3 <- None;
-              None
-          | Some `Int value ->
-              debug_info_3 <- Some true;
-              Some (Some (value <> Int64.zero))
-          | Some `Top ->
-              debug_info_3 <- Some false;
-              Some None
+              match self#try_ask location expression with
+              | None ->
+                  Some false
+              | Some value_before ->
+                  (* Use the last (TODO -> find_all) matching statement *)
+                  let _, statement = Hashtbl.find statements location in
+                  (* Use the first (TODO) reachable successor *)
+                  let successor_evaluation =
+                    statement.succs
+                      |> List.find_map_opt (fun (s : Cil.stmt) -> self#try_ask (Cil.get_stmtLoc s.skind) expression)
+                  in
+                  match successor_evaluation with
+                  | Some value_after ->
+                      value_after
+                  | None ->
+                      value_before
+
+        method private try_ask location expression =
+          match ~? (fun () -> ask location (Queries.EvalInt expression)) with
+            (* Evaluable: Definite *)
+          | Some `Int value -> Some (Some (value <> Int64.zero))
+            (* Evaluable: Inconclusive *)
+          | Some `Top -> Some None
+            (* Inapplicable: Unreachable *)
+          | Some `Bot -> None
+            (* Inapplicable: Unlisted *)
+          | None -> None
+            (* Unexpected result *)
           | Some _ -> raise Exit
-          | None ->
-              debug_info_3 <- None;
-              None
 
       end
 
-    let padding = 5
+    type query =
+      {
 
-    let string_of_int_padded value =
-      let value_string = string_of_int value in
-      (String.make (padding - String.length value_string) ' ') ^ value_string
+        kind : SyntacticalAnalyzer.JsonParser.kind;
+        target : SyntacticalAnalyzer.JsonParser.target;
+        find : SyntacticalAnalyzer.JsonParser.find;
+        structure : (SyntacticalAnalyzer.JsonParser.structure [@default None_s]);
+        limitation : (SyntacticalAnalyzer.JsonParser.constr [@default None_c]);
+
+        expression : string;
+        mode : [ `Must | `May ];
+
+      } [@@deriving yojson]
+
     let string_of_location (location : Cil.location) =
       location.file ^ ":" ^ (location.line |> string_of_int) ^ " [" ^ (location.byte |> string_of_int) ^ "]"
     let string_of_statement statement =
@@ -152,17 +98,21 @@ module Transformation : Transform.S =
         |> List.map String.trim
         |> List.fold_left (^) ""
 
-    let parse_query () =
-      let query_file_name = GobConfig.get_string ("trans." ^ transformation_name ^ ".query_file_name") in
-      match Yojson.Safe.from_file query_file_name |> query_of_yojson with
-      | Ok parsed_query -> parsed_query
-      | Error message ->
-          print_endline ("Parsing error: " ^ message);
-          raise Exit
+    let location_file_compare (location_1 : Cil.location) (location_2 : Cil.location) = compare location_1.file location_2.file
+    let location_byte_compare (location_1 : Cil.location) (location_2 : Cil.location) = compare location_1.byte location_2.byte
 
     let transform (ask : Cil.location -> Queries.t -> Queries.Result.t) (file : Cil.file) =
+      (* Create an evaluator *)
       let evaluator = new evaluator file ask in
-      let query = parse_query () in
+      (* Syntactic query *)
+      let query_file_name = GobConfig.get_string transformation_query_file_name_identifier in
+      let query =
+        match Yojson.Safe.from_file query_file_name |> query_of_yojson with
+        | Ok parsed_query -> parsed_query
+        | Error message ->
+            prerr_endline ("Parsing error: " ^ message);
+            raise Exit
+      in
       let query_syntactic : SyntacticalAnalyzer.JsonParser.query =
         {
           sel = [];
@@ -173,68 +123,33 @@ module Transformation : Transform.S =
           lim = query.limitation;
         }
       in
-      SyntacticalAnalyzer.QueryMapping.map_query query_syntactic file
-        (* Use only location results *)
-        |> List.map (fun (_, l, _, _) -> l)
-        (* Sort locations *)
-        |> List.sort_uniq (fun (l_1 : Cil.location) (l_2 : Cil.location) -> compare l_1.byte l_2.byte)
-        (* Evaluate *)
-        |> List.iter
-          begin
-            fun location ->
-              begin
-                match evaluator#evaluate location query.expression with
-                | Some value ->
-                    if value then
-                      print_endline (location |> string_of_location)
-                | None ->
-                    begin
-                      match query.mode with
-                      | `Must -> ()
-                      | `May -> print_endline ((location |> string_of_location) ^ " (Possibly)")
-                    end
-              end;
-              print_endline ">>>";
-              print_endline ("\"" ^ location.file ^ "\":");
-              print_string ((location.line |> string_of_int_padded) ^ " | ");
-              begin
-                match evaluator#get_debug_info_2 () with
-                | Some (statement, successor) ->
-                    print_endline (statement |> string_of_statement);
-                    begin
-                      match successor with
-                      | Some (succeeding_location, succeeding_statement) ->
-                          if succeeding_location.line < 1 then
-                            print_string (String.make padding ' ')
-                          else
-                            print_string (succeeding_location.line |> string_of_int_padded);
-                          print_endline (" | " ^ (succeeding_statement |> string_of_statement))
-                      | None ->
-                          print_endline ((String.make padding ' ') ^ " | (No succeeding statement)")
-                    end
-                | None ->
-                    print_endline "(No statement)"
-              end;
-              if evaluator#get_debug_info_1 () then
-                print_endline "Successfully parsed expression"
-              else
-                print_endline "Failed to parse expression";
-              print_endline
-                begin
-                  match evaluator#get_debug_info_3 () with
-                  | Some value ->
-                      if value then
-                        "Definite"
-                      else
-                        "Unknown"
-                  | None ->
-                      "Not reachable"
-                end;
-              print_endline "<<<";
-              print_newline ()
-          end
+      let locations =
+        SyntacticalAnalyzer.QueryMapping.map_query query_syntactic file
+          (* Use only locations *)
+          |> List.map (fun (_, l, _, _) -> l)
+          (* Group by source files *)
+          |> List.group location_file_compare
+          (* Sort and remove duplicates *)
+          |> List.map (fun ls -> List.sort_uniq location_byte_compare ls)
+          (* Ungroup *)
+          |> List.flatten
+      in
+      (* Semantic queries *)
+      let debug = GobConfig.get_bool "dbg.verbose" in
+      let evaluate location =
+        match evaluator#evaluate location query.expression with
+        | Some value ->
+            if value then
+              print_endline (location |> string_of_location)
+            else if debug then
+              print_endline ((location |> string_of_location) ^ " x")
+        | None ->
+            if query.mode = `May || debug then
+              print_endline ((location |> string_of_location) ^ " ?")
+      in
+      List.iter evaluate locations
 
   end
 
 let _ =
-  Transform.register transformation_name (module Transformation)
+  Transform.register transformation_identifier (module ExpEval)
