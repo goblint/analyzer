697f9feeb78a8e9912c94fdf8515a0417a75505c
Michael Schwarz <michael.schwarz93@gmail.com>
2020-10-02 13:33:09 +0200

    Remove pre-defined CFGs and always load them from JSON

--- src/framework/arinc_cfg.ml
+++ src/framework/arinc_cfg.ml
@@ -74,74 +74,6 @@ let mkEdge cfgF cfgB fromNode edge toNode =
   in
   addCfg  toNode (edge, fromNode); ()
 
-let example_extracted () =
-  let cfgF = H.create 113 in
-  let cfgB = H.create 113 in
-  let mkEdge = mkEdge cfgF cfgB in
-
-  for i = 0 to 7 do
-    mkEdge (PCCombined ([0; i])) (0, StartComputation 10) (PCCombined [13; i]);
-    mkEdge (PCCombined ([13; i])) (0, FinishComputation) (PCCombined [1; i]);
-
-    mkEdge (PCCombined ([1; i])) (0, PeriodicWait) (PCCombined [2; i]);
-    mkEdge (PCCombined ([2; i])) (0, WaitingForPeriod) (PCCombined [3; i]);
-    mkEdge (PCCombined ([3; i])) (0, SetEvent 0) (PCCombined [4; i]);
-
-    mkEdge (PCCombined ([4; i])) (0, StartComputation 20) (PCCombined [14; i]);
-    mkEdge (PCCombined ([14; i])) (0, FinishComputation) (PCCombined [5; i]);
-
-    mkEdge (PCCombined ([5; i])) (0, ResumeTask 1) (PCCombined [6; i]);
-    mkEdge (PCCombined ([6; i])) (0, TimedWait 20) (PCCombined[12;i]);
-    mkEdge (PCCombined ([12; i])) (0, WaitingForEndWait 20) (PCCombined[16;i]);
-    mkEdge (PCCombined ([6; i])) (0, WaitEvent 1) (PCCombined [7; i]);
-    mkEdge (PCCombined ([7; i])) (0, ResetEvent 1) (PCCombined [8; i]);
-
-    mkEdge (PCCombined ([16; i])) (0, NOP) (PCCombined[9;i]);
-
-    mkEdge (PCCombined ([8; i])) (0, StartComputation 42) (PCCombined [15; i]);
-    mkEdge (PCCombined ([15; i])) (0, FinishComputation) (PCCombined [9; i]);
-
-    mkEdge (PCCombined ([9; i])) (0, SuspendTask 1) (PCCombined [10; i]);
-    mkEdge (PCCombined ([10; i])) (0, PeriodicWait) (PCCombined [11; i]);
-    mkEdge (PCCombined ([11; i])) (0, WaitingForPeriod) (PCCombined [4; i]);
-
-  done;
-  for i = 0 to 17 do
-    mkEdge (PCCombined ([i; 0])) (1, SuspendTask 1) (PCCombined [i; 1]);
-    mkEdge (PCCombined ([i; 1])) (1, WaitSemaphore 0) (PCCombined [i; 2]);
-
-    mkEdge (PCCombined ([i; 2])) (1, StartComputation 40) (PCCombined [i; 6]);
-    mkEdge (PCCombined ([i; 6])) (1, FinishComputation) (PCCombined [i; 3]);
-
-    mkEdge (PCCombined ([i; 3])) (1, SignalSemaphore 0) (PCCombined [i; 4]);
-    mkEdge (PCCombined ([i; 4])) (1, SetEvent 1) (PCCombined [i; 5]);
-    mkEdge (PCCombined ([i; 5])) (1, NOP) (PCCombined [i; 1]);
-  done;
-  (* Printf.printf "!!!!!!!Edge count %i\n" (List.length (H.find_all cfgB (PCCombined ([9; 4])))); *)
-  (* H.iter (fun n (e,t) -> match n,t with PCCombined [a;b], PCCombined[c;d] -> Printf.printf "[%i,%i] -> [%i,%i]\n" a b c d;) cfgF; *)
-  H.find_all cfgF, H.find_all cfgB
-
-let minimal_problematic () =
-  let cfgF = H.create 113 in
-  let cfgB = H.create 113 in
-  let mkEdge = mkEdge cfgF cfgB in
-
-  for i = 0 to 2 do
-    mkEdge (PCCombined ([0; i])) (0, TimedWait 20) (PCCombined[1;i]);
-    mkEdge (PCCombined ([1; i])) (0, WaitingForEndWait 20) (PCCombined[2;i]);
-    mkEdge (PCCombined ([2; i])) (0, PeriodicWait) (PCCombined [3; i]);
-    mkEdge (PCCombined ([3; i])) (0, WaitingForPeriod) (PCCombined [0; i]);
-
-  done;
-  for i = 0 to 3 do
-    mkEdge (PCCombined ([i; 0])) (1, StartComputation 40) (PCCombined [i; 1]);
-    mkEdge (PCCombined ([i; 1])) (1, FinishComputation) (PCCombined [i; 2]);
-    mkEdge (PCCombined ([i; 2])) (1, NOP) (PCCombined [i; 0]);
-  done;
-  (* Printf.printf "!!!!!!!Edge count %i\n" (List.length (H.find_all cfgB (PCCombined([9; 4])))); *)
-  (* H.iter (fun n (e,t) -> match n,t with PCCombined[a;b], PC[c;d] -> Printf.printf "[%i,%i] -> [%i,%i]\n" a b c d;) cfgF; *)
-  H.find_all cfgF, H.find_all cfgB
-
 let create_from (a:arinc_task_cfg) (b:arinc_task_cfg) =
   let cfgF = H.create 113 in
   let cfgB = H.create 113 in
@@ -170,14 +102,16 @@ let create_from (a:arinc_task_cfg) (b:arinc_task_cfg) =
   done;
   H.find_all cfgF, H.find_all cfgB
 
-let get_cfg i =
-  match i with
-  | 0 -> example_extracted ()
-  | 1 ->
-    let file = Yojson.Safe.from_file (List.nth !Goblintutil.jsonFiles 0) in
-    let tasks = match arinc_tasks_of_yojson file  with
-      | Error e -> failwith "invalid input"
-      | Ok b -> b
-    in
-    create_from (List.nth tasks 0) (List.nth tasks 1)
-  | _ -> failwith ("Selected unknown CFG " ^ string_of_int(i))
+let print_to_json () =
+  let tasks = [] in
+  let yo = arinc_tasks_to_yojson tasks in
+  Yojson.Safe.to_file "extracted.json"  yo;
+  failwith "over and out"
+
+let get_cfg =
+  let file = Yojson.Safe.from_file (List.nth !Goblintutil.jsonFiles 0) in
+  let tasks = match arinc_tasks_of_yojson file  with
+    | Error e -> failwith "invalid input"
+    | Ok b -> b
+  in
+  create_from (List.nth tasks 0) (List.nth tasks 1)
--- tests/arinc_wcet/arinc_wcet_regtest.sh
+++ tests/arinc_wcet/arinc_wcet_regtest.sh
@@ -1,4 +1,4 @@
-if ./goblint --set arinc_cfg_id 0 | grep -q 'deadline'; then
+if ./goblint --set arinc_cfg_id 0 --sets result fast_xml tests/arinc_wcet/extracted.json | grep -q 'deadline'; then
    echo "Test 0 failed, deadline violated"
 fi
 if ./goblint --set arinc_cfg_id 1 --sets result fast_xml tests/arinc_wcet/minimal_problematic.json | grep -q 'deadline'; then
