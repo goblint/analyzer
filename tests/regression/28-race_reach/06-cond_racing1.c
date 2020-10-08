51b6be1a66eb1665a2ab7350d4d7e00bada56b26
Vesal Vojdani <vesal.vojdani@gmail.com>
2020-10-08 15:56:38 +0300

    Races as Reachability.

---
+++ tests/regression/28-race_reach/06-cond_racing1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<assert.h>
+
+#include <pthread.h>
+#include "racemacros.h"
+
+int global = 0;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+void *t_fun(void *arg) {
+  pthread_mutex_lock(&mutex1);
+  access(global);
+  pthread_mutex_unlock(&mutex1);
+  return NULL;
+}
+
+int main() {
+  int i;
+  pthread_t id;
+
+  // Create the thread
+  pthread_create(&id, NULL, t_fun, NULL);
+  
+  printf("Do the work? ");
+  if (i) pthread_mutex_lock(&mutex2);
+
+  printf("Now we do the work..\n");
+  if (i) assert_racefree(global);  // UNKNOWN
+  
+  printf("Work is completed...");
+  if (i) pthread_mutex_unlock(&mutex2);
+
+  pthread_join (id, NULL);
+  return 0;
+}
+
+
+
+
