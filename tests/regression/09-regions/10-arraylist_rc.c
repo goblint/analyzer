eb301108c8781b5cf04770df6bae3f404d0f192f
Simmo Saan <simmo.saan@gmail.com>
2020-10-16 14:29:13 +0300

    Add explicit mutex initialization where missing to SV-COMP data race tests

--- tests/regression/09-regions/10-arraylist_rc.c
+++ tests/regression/09-regions/10-arraylist_rc.c
@@ -40,6 +40,9 @@ void *t_fun(void *arg) {
 }
 
 int main () {
+  for (int i = 0; i < 10; i++)
+    pthread_mutex_init(&mutex[i], NULL);
+
   int j = __VERIFIER_nondet_int();
   assume_abort_if_not(0 <= j && j < 10);
   struct s *p;
