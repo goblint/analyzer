50a72094ff13b87b3d4b55d4c58a36c0e70c4913
Simmo Saan <simmo.saan@gmail.com>
2020-10-19 14:04:52 +0300

    Add three container_of related unsoundness tests from sv-benchmarks

--- scripts/regression2sv-benchmarks.py
+++ scripts/regression2sv-benchmarks.py
@@ -32,6 +32,10 @@ EXCLUDE_TASKS = [
     "09-regions_35-list2_rc-offsets-thread", # duplicate of 09/03
     "10-synch_17-glob_fld_nr", # duplicate of 05/08
     "19-spec_02-mutex_rc", # duplicate of 04/01
+
+    "29-svcomp_01-race-2_3b-container_of", # duplicate sv-benchmarks
+    "29-svcomp_01-race-2_4b-container_of", # duplicate sv-benchmarks
+    "29-svcomp_01-race-2_5b-container_of", # duplicate sv-benchmarks
 ]
 
 target_root = Path(sys.argv[1])
