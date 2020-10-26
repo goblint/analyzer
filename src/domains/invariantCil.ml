fe86d3922ece27ffe110f18c4dc1d9881e0a2695
Simmo Saan <simmo.saan@gmail.com>
2020-10-26 12:56:19 +0200

    Reimplement InvariantCil.var_is_tmp using var_find_original_name
    This is more accurate than regex-based heuristic check.

--- src/domains/invariantCil.ml
+++ src/domains/invariantCil.ml
@@ -19,7 +19,8 @@ let var_find_original_name vi = VM.find_opt vi (Lazy.force var_original_names)
 (* let var_is_tmp {vdescrpure} = not vdescrpure (* doesn't exclude tmp___0 *) *)
 (* TODO: instead check if vdescr is nonempty? (doesn't cover all cases, e.g. ternary temporary) *)
 let tmp_var_regexp = Str.regexp "^\\(tmp\\(___[0-9]+\\)?\\|cond\\|RETURN\\)$"
-let var_is_tmp {vname; _} = Str.string_match tmp_var_regexp vname 0
+(* let var_is_tmp {vname; _} = Str.string_match tmp_var_regexp vname 0 *)
+let var_is_tmp vi = Option.is_none (var_find_original_name vi)
 let rec exp_contains_tmp = function
   | Lval (Var vi, _) -> var_is_tmp vi
   | UnOp (_, e, _) -> exp_contains_tmp e
