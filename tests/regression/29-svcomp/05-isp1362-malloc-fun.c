64250edc41c909efa8f7bde8aeff1317438e0b88
Simmo Saan <simmo.saan@gmail.com>
2020-10-27 13:55:51 +0200

    Add two failing regression tests for field on non-compound errors (#119)
    Both extracted from ldv-consumption/32_7a_cilled_linux-3.8-rc1-32_7a-drivers--usb--host--isp1362-hcd.ko-ldv_main0_sequence_infinite_withcheck_stateful.cil.out.i from sv-benchmarks.

---
+++ tests/regression/29-svcomp/05-isp1362-malloc-fun.c
@@ -0,0 +1,45 @@
+// SKIP
+typedef unsigned long __kernel_ulong_t;
+typedef __kernel_ulong_t __kernel_size_t;
+typedef __kernel_size_t size_t;
+typedef unsigned long long u64;
+typedef u64 phys_addr_t;
+typedef phys_addr_t resource_size_t;
+
+
+struct usb_hcd {
+  resource_size_t rsrc_start ;
+};
+
+struct resource {
+  resource_size_t start ;
+};
+
+
+void *ldv_malloc(size_t size)
+{
+  return malloc(size);
+}
+
+struct usb_hcd *usb_create_hcd() {
+  return ldv_malloc(sizeof(struct usb_hcd));
+
+  // normal malloc is fine
+  // return malloc(sizeof(struct usb_hcd));
+}
+
+static int isp1362_probe()
+{
+  struct usb_hcd *hcd ;
+  struct resource *data ;
+
+  hcd = usb_create_hcd();
+  hcd->rsrc_start = data->start;
+  return 0;
+}
+
+int main(void)
+{
+  isp1362_probe();
+  return 0;
+}
---
+++ tests/regression/29-svcomp/06-isp1362-widen-context.c
@@ -0,0 +1,57 @@
+// SKIP PARAM: --enable exp.widen-context
+typedef unsigned long __kernel_ulong_t;
+typedef __kernel_ulong_t __kernel_size_t;
+typedef __kernel_size_t size_t;
+typedef unsigned long long u64;
+typedef u64 phys_addr_t;
+typedef phys_addr_t resource_size_t;
+
+
+struct usb_hcd {
+  resource_size_t rsrc_start ;
+  unsigned long hcd_priv[0U] ;
+};
+
+struct isp1362_hcd {
+  void *data_reg ;
+};
+
+struct resource {
+  resource_size_t start ;
+};
+
+
+void *ldv_malloc(size_t size)
+{
+  return malloc(size);
+}
+
+struct usb_hcd *usb_create_hcd() {
+  return ldv_malloc(sizeof(struct usb_hcd));
+}
+
+struct isp1362_hcd *hcd_to_isp1362_hcd(struct usb_hcd *hcd )
+{
+  return ((struct isp1362_hcd *)(& hcd->hcd_priv));
+}
+
+static int isp1362_probe()
+{
+  struct usb_hcd *hcd ;
+  struct isp1362_hcd *isp1362_hcd ;
+  struct resource *data ;
+  void *data_reg ;
+
+  hcd = usb_create_hcd();
+  hcd->rsrc_start = data->start;
+
+  isp1362_hcd = hcd_to_isp1362_hcd(hcd);
+  isp1362_hcd->data_reg = data_reg;
+  return 0;
+}
+
+int main(void)
+{
+  isp1362_probe();
+  return 0;
+}
