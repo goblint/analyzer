2c787fa60143ce56210070f5aeb41480ca1be613
Simmo Saan <simmo.saan@gmail.com>
2020-10-29 16:56:20 +0200

    Add skipped regression tests for malloc wrapper function precision (issue #123)

---
+++ tests/regression/29-svcomp/08-malloc_fun2.c
@@ -0,0 +1,30 @@
+// SKIP
+// Copied & modified from 02/35.
+#include <stdlib.h>
+#include <assert.h>
+
+void *myalloc(size_t n) {
+  return malloc(n);
+}
+
+void *myalloc2(size_t n) {
+  return myalloc(n);
+}
+
+int main() {
+  int *x = myalloc2(sizeof(int));
+  int *y = myalloc2(sizeof(int));
+  int *p;
+
+  *x = 0;
+  *y = 1;
+
+  assert(*x == 0);
+  assert(*y == 1);
+
+  p = x; x = y; y = p;
+  assert(*x == 1);
+  assert(*y == 0);
+
+  return 0;
+}
