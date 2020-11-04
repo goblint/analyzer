938b137bf0d12a999ae823e3e33ab5825810cf26
Julian Erhard <julian.erhard@tum.de>
2020-11-04 11:23:26 +0100

    Add assertion about result of multiplication to test

--- tests/regression/01-cpa/10-posneg.c
+++ tests/regression/01-cpa/10-posneg.c
@@ -15,7 +15,9 @@ int main() {
   assert(i != 6);
   i = k - 1;
   assert(i != 4);
-  i = k * 2;
+  i = k * 3;       // multiplication with odd numbers is injective
+  assert(i != 15);
+  i = k * 2;       // multiplication with even numbers is not-injective
   assert(i != 10); // UNKNOWN! k could be -2147483643;
   i = k / 2;
   assert(i != 2); // UNKNOWN! k could be 4
