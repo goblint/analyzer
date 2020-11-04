3800179caee2678c5855a06119cb184829b673f0
Julian Erhard <julian.erhard@tum.de>
2020-11-04 11:24:06 +0100

    Fix typo

--- tests/regression/01-cpa/10-posneg.c
+++ tests/regression/01-cpa/10-posneg.c
@@ -17,7 +17,7 @@ int main() {
   assert(i != 4);
   i = k * 3;       // multiplication with odd numbers is injective
   assert(i != 15);
-  i = k * 2;       // multiplication with even numbers is not-injective
+  i = k * 2;       // multiplication with even numbers is not injective
   assert(i != 10); // UNKNOWN! k could be -2147483643;
   i = k / 2;
   assert(i != 2); // UNKNOWN! k could be 4
