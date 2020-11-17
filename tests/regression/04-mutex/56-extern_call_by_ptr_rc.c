dce6cca0fa0c6ef05a203c96588906a399c007ba
Simmo Saan <simmo.saan@gmail.com>
2020-11-17 11:52:22 +0200

    Add regression test for extern function calling/spawning function pointer argument
    04/19 previously used custom_libc although that was actually unnecessary.

---
+++ tests/regression/04-mutex/56-extern_call_by_ptr_rc.c
@@ -0,0 +1,30 @@
+#include<pthread.h>
+
+extern void foo(int (*callback)());
+
+
+int glob;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+void *t_fun(void *arg) {
+  pthread_mutex_lock(&mutex2);
+  glob=glob+1; // RACE!
+  pthread_mutex_unlock(&mutex2);
+  return NULL;
+}
+
+int bar() {
+  pthread_mutex_lock(&mutex1);
+  glob=glob+1; // RACE!
+  pthread_mutex_unlock(&mutex1);
+  return 0;
+}
+
+int main() {
+  pthread_t id;
+  pthread_create(&id, NULL, t_fun, NULL);
+  foo(bar);
+  pthread_join (id, NULL);
+  return 0;
+}
