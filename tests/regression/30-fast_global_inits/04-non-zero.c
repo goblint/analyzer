aa382cf0c4b6207625647aa0da9e89910d30a989
Michael Schwarz <michael.schwarz93@gmail.com>
2020-11-19 11:15:24 +0100

    fast_global_init: Add test checking we know something

--- tests/regression/30-fast_global_inits/04-non-zero.c
+++ tests/regression/30-fast_global_inits/04-non-zero.c
@@ -9,17 +9,19 @@ int main(void) {
   assert(global_array[2] == 3);  //UNKNOWN
   assert(global_array[3] == 42); //UNKNOWN
   assert(global_array[3] == 11); //UNKNOWN
+  assert(global_array[1] == -1); //FAIL
 
   assert(global_array_multi[0][0] == 9);  //UNKNOWN
   assert(global_array_multi[0][1] == 0);  //UNKNOWN
   assert(global_array_multi[0][2] == 3);  //UNKNOWN
   assert(global_array_multi[0][3] == 42); //UNKNOWN
   assert(global_array_multi[0][3] == 11); //UNKNOWN
-
+  assert(global_array_multi[0][1] == -1); //FAIL
 
   assert(global_array_multi[1][0] == 9);  //UNKNOWN
   assert(global_array_multi[1][1] == 0);  //UNKNOWN
   assert(global_array_multi[1][2] == 3);  //UNKNOWN
   assert(global_array_multi[1][3] == 42); //UNKNOWN
   assert(global_array_multi[1][3] == 11); //UNKNOWN
+  assert(global_array_multi[1][1] == -1); //FAIL
 }
