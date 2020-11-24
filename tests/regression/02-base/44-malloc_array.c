72d3d27dd2b4b15bba30f0b7a56a1255ee09fbdc
Ivana Zuzic <ivana.zuzic@tum.de>
2020-11-24 17:55:33 +0100

    Mark the comment in a proper C style

--- tests/regression/02-base/44-malloc_array.c
+++ tests/regression/02-base/44-malloc_array.c
@@ -8,7 +8,7 @@ int main(void) {
     r[3] = 2;
 
     assert(r[4] == 2); 
-    (* Here we only test our implementation. Concretely, accessing the uninitialised r[4] is undefined behavior.
+    /* Here we only test our implementation. Concretely, accessing the uninitialised r[4] is undefined behavior.
     In our implementation we keep the whole memory allocated by malloc as one Blob and the whole Blob contains 2 after it was assigned to r[3].
-    This is more useful than keeping the Blob unknown. *)
+    This is more useful than keeping the Blob unknown. */
 }
