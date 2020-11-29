1ff24e863383c90ae72d33cb875db5de79c9af12
Simmo Saan <simmo.saan@gmail.com>
2020-11-29 18:16:08 +0200

    Use function location for pseudo return node so it's present in HTML output

--- src/framework/myCFG.ml
+++ src/framework/myCFG.ml
@@ -216,7 +216,7 @@ let createCFG (file: file) =
         (* Return node to be used for infinite loop connection to end of function
          * lazy, so it's only added when actually needed *)
         let pseudo_return = lazy (
-          let newst = mkStmt (Return (None, locUnknown)) in
+          let newst = mkStmt (Return (None, loc)) in
           let start_id = 10_000_000_000 in (* TODO get max_sid? *)
           let sid = Hashtbl.hash loc in (* Need pure sid instead of Cil.new_sid for incremental, similar to vid in Goblintutil.create_var. We only add one return stmt per loop, so the location hash should be unique. *)
           newst.sid <- if sid < start_id then sid + start_id else sid;
