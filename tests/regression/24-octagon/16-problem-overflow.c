43d80a2319a192dc5384ca76ee0564d88c68dd2d
Michael Schwarz <michael.schwarz93@gmail.com>
2021-01-20 15:23:55 +0100

    Add problem overflow

---
+++ tests/regression/24-octagon/16-problem-overflow.c
@@ -0,0 +1,12 @@
+// PARAM: --sets solver td3 --set ana.activated "['base','threadid','threadflag','octagon','mallocWrapper']"
+// Example from https://github.com/sosy-lab/sv-benchmarks/blob/master/c/bitvector-loops/overflow_1-2.c
+
+int main(void) {
+  unsigned int x = 10;
+
+  while (x >= 10) {
+    x += 2;
+  }
+
+  assert(1);
+}
