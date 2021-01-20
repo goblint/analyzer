a2d271b13efb2f4ed94572feb4ae5b5f01bb74cd
Michael Schwarz <michael.schwarz93@gmail.com>
2021-01-20 15:10:57 +0100

    Add further problematic example

---
+++ tests/regression/24-octagon/14-problem-rec.c
@@ -0,0 +1,12 @@
+// PARAM: --sets solver td3 --set ana.activated "['base','threadid','threadflag','octagon','mallocWrapper']"
+// Example from https://github.com/sosy-lab/sv-benchmarks/blob/master/c/recursive-simple/afterrec-1.c
+void f(int n) {
+  if (n<3) return;
+  n--;
+  f(n);
+  assert(1);
+}
+
+int main(void) {
+  f(4);
+}
