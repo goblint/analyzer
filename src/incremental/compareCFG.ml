5cf625748015de120703162ac92970df48647ecd
stilscher <66023521+stilscher@users.noreply.github.com>
2021-01-20 17:10:52 +0100

    refine comparison functions for cfg elements

--- src/incremental/compareCFG.ml
+++ src/incremental/compareCFG.ml
@@ -1,6 +1,7 @@
 open MyCFG
 open Queue
 open Cil
+open CompareAST
 
 module StdS = Set.Make (
   struct
@@ -18,32 +19,47 @@ module DiffS = Set.Make (
 
 let waitingList : (node * node) t = Queue.create ()
 
+(* in contrast to the original eq_varinfo in CompareAST, this method also ignores location, vid, vreferenced, vdescr, vdescrpure, vinline, vaddrof *)
+let eq_varinfo' (a: varinfo) (b: varinfo) = a.vname = b.vname && eq_typ a.vtype b.vtype && eq_list eq_attribute a.vattr b.vattr &&
+                                           a.vstorage = b.vstorage && a.vglob = b.vglob
+
+let eq_instr' (a: instr) (b: instr) = match a, b with
+  | Set (lv1, exp1, _l1), Set (lv2, exp2, _l2) -> eq_lval lv1 lv2 && eq_exp exp1 exp2
+  | Call (Some lv1, f1, args1, _l1), Call (Some lv2, f2, args2, _l2) -> eq_lval lv1 lv2 && eq_exp f1 f2 && eq_list eq_exp args1 args2
+  | Call (None, f1, args1, _l1), Call (None, f2, args2, _l2) -> eq_exp f1 f2 && eq_list eq_exp args1 args2
+  | Asm (attr1, tmp1, ci1, dj1, rk1, l1), Asm (attr2, tmp2, ci2, dj2, rk2, l2) ->
+      eq_list String.equal tmp1 tmp2 && eq_list(fun (x1,y1,z1) (x2,y2,z2)-> x1 = x2 && y1 = y2
+      && eq_lval z1 z2) ci1 ci2 && eq_list(fun (x1,y1,z1) (x2,y2,z2)-> x1 = x2 && y1 = y2
+      && eq_exp z1 z2) dj1 dj2 && eq_list String.equal rk1 rk2(* ignore attributes and locations *)
+  | VarDecl (v1, _l1), VarDecl (v2, _l2) -> eq_varinfo' v1 v2
+  | _, _ -> false
+
 (* in contrast to the similar method eq_stmtkind in CompareAST,
 this method does not compare the inner body, that is sub blocks,
 of if and switch statements *)
-let rec eq_stmtkind' ((a, af): stmtkind * fundec) ((b, bf): stmtkind * fundec) =
-  let eq_block' = fun x y -> CompareAST.eq_block (x, af) (y, bf) in
+let eq_stmtkind' ((a, af): stmtkind * fundec) ((b, bf): stmtkind * fundec) =
+  let eq_block' = fun x y -> eq_block (x, af) (y, bf) in
   match a, b with
-  | Instr is1, Instr is2 -> CompareAST.eq_list CompareAST.eq_instr is1 is2
-  | Return (Some exp1, _l1), Return (Some exp2, _l2) -> CompareAST.eq_exp exp1 exp2
+  | Instr is1, Instr is2 -> eq_list eq_instr' is1 is2
+  | Return (Some exp1, _l1), Return (Some exp2, _l2) -> eq_exp exp1 exp2
   | Return (None, _l1), Return (None, _l2) -> true
   | Return _, Return _ -> false
-  | Goto (st1, _l1), Goto (st2, _l2) -> CompareAST.eq_stmt_with_location (!st1, af) (!st2, bf)
+  | Goto (st1, _l1), Goto (st2, _l2) -> eq_stmt_with_location (!st1, af) (!st2, bf)
   | Break _, Break _ -> true
   | Continue _, Continue _ -> true
   | If (exp1, then1, else1, _l1), If (exp2, then2, else2, _l2) ->
-      CompareAST.eq_exp exp1 exp2 (* && eq_block' then1 then2 && eq_block' else1 else2 *)
+      eq_exp exp1 exp2 (* && eq_block' then1 then2 && eq_block' else1 else2 *)
   | Switch (exp1, block1, stmts1, _l1), Switch (exp2, block2, stmts2, _l2) ->
-      CompareAST.eq_exp exp1 exp2 (* && eq_block' block1 block2 && CompareAST.eq_list (fun a b -> eq_stmt (a,af) (b,bf)) stmts1 stmts2 *)
-  | Loop (block1, _l1, _con1, _br1), Loop (block2, _l2, _con2, _br2) -> eq_block' block1 block2
+      eq_exp exp1 exp2 (* && eq_block' block1 block2 && eq_list (fun a b -> eq_stmt (a,af) (b,bf)) stmts1 stmts2 *)
+  | Loop (block1, _l1, _con1, _br1), Loop (block2, _l2, _con2, _br2) -> true (* eq_block' block1 block2 *)
   | Block block1, Block block2 -> eq_block' block1 block2
-  | TryFinally (tryBlock1, finallyBlock1, _l1), TryFinally (tryBlock2, finallyBlock2, _l2) ->
-      eq_block' tryBlock1 tryBlock2 && eq_block' finallyBlock1 finallyBlock2
-  | TryExcept (tryBlock1, exn1, exceptBlock1, _l1), TryExcept (tryBlock2, exn2, exceptBlock2, _l2) ->
-      eq_block' tryBlock1 tryBlock2 && eq_block' exceptBlock1 exceptBlock2
+  | TryFinally (tryBlock1, finallyBlock1, _l1), TryFinally (tryBlock2, finallyBlock2, _l2) -> assert false
+      (* eq_block' tryBlock1 tryBlock2 && eq_block' finallyBlock1 finallyBlock2 *)
+  | TryExcept (tryBlock1, exn1, exceptBlock1, _l1), TryExcept (tryBlock2, exn2, exceptBlock2, _l2) -> assert false
+      (* eq_block' tryBlock1 tryBlock2 && eq_block' exceptBlock1 exceptBlock2 *)
   | _, _ -> false
 
-and eq_stmt' ((a, af): stmt * fundec) ((b, bf): stmt * fundec) =
+let eq_stmt' ((a, af): stmt * fundec) ((b, bf): stmt * fundec) =
   (* catch Invalid Argument exception which is thrown by List.combine if the label lists are of different length *)
   try List.for_all (fun (x,y) -> CompareAST.eq_label x y) (List.combine a.labels b.labels)
     && eq_stmtkind' (a.skind, af) (b.skind, bf)
@@ -52,8 +68,8 @@ and eq_stmt' ((a, af): stmt * fundec) ((b, bf): stmt * fundec) =
 let eq_node (x, fun1) (y, fun2) =
   match x,y with
   | Statement s1, Statement s2 -> eq_stmt' (s1, fun1) (s2, fun2)
-  | Function f1, Function f2 -> CompareAST.eq_varinfo f1.svar f2.svar
-  | FunctionEntry f1, FunctionEntry f2 -> CompareAST.eq_varinfo f1.svar f2.svar
+  | Function f1, Function f2 -> eq_varinfo' f1.svar f2.svar
+  | FunctionEntry f1, FunctionEntry f2 -> eq_varinfo' f1.svar f2.svar
   | _ -> false
 
 let eq_edge x y = match x, y with
@@ -61,19 +77,19 @@ let eq_edge x y = match x, y with
   | Proc (None,f1,ars1), Proc (None,f2,ars2) -> CompareAST.eq_exp f1 f2 && CompareAST.eq_list CompareAST.eq_exp ars1 ars2
   | Proc (Some r1,f1,ars1), Proc (Some r2,f2,ars2) ->
       CompareAST.eq_lval r1 r2 && CompareAST.eq_exp f1 f2 && CompareAST.eq_list CompareAST.eq_exp ars1 ars2
-  | Entry f1, Entry f2 -> CompareAST.eq_varinfo f1.svar f2.svar
-  | Ret (None,fd1), Ret (None,fd2) -> CompareAST.eq_varinfo fd1.svar fd2.svar
-  | Ret (Some r1,fd1), Ret (Some r2,fd2) -> CompareAST.eq_exp r1 r2 && CompareAST.eq_varinfo fd1.svar fd2.svar
+  | Entry f1, Entry f2 -> eq_varinfo' f1.svar f2.svar
+  | Ret (None,fd1), Ret (None,fd2) -> eq_varinfo' fd1.svar fd2.svar
+  | Ret (Some r1,fd1), Ret (Some r2,fd2) -> CompareAST.eq_exp r1 r2 && eq_varinfo' fd1.svar fd2.svar
   | Test (p1,b1), Test (p2,b2) -> CompareAST.eq_exp p1 p2 && b1 = b2
   | ASM _, ASM _ -> false
   | Skip, Skip -> true
-  | VDecl v1, VDecl v2 -> CompareAST.eq_varinfo v1 v2
+  | VDecl v1, VDecl v2 -> eq_varinfo' v1 v2
   | SelfLoop, SelfLoop -> true
   | _ -> false
 
 (* The order of the edges in the list is relevant. Therefor compare
 them one to one without sorting first*)
-let eq_edge_list xs ys = CompareAST.eq_list eq_edge xs ys
+let eq_edge_list xs ys = eq_list eq_edge xs ys
 
 let to_edge_list ls = List.map (fun (loc, edge) -> edge) ls
 
