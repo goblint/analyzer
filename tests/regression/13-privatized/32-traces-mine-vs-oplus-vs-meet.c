1d994f5f73d40805acf695197fcb8363e4f0301e
Simmo Saan <simmo.saan@gmail.com>
2021-01-29 13:22:40 +0200

    Simplify 13/32 to single global

--- tests/regression/13-privatized/32-traces-mine-vs-oplus-vs-meet.c
+++ tests/regression/13-privatized/32-traces-mine-vs-oplus-vs-meet.c
@@ -1,25 +1,20 @@
 #include <pthread.h>
 #include <assert.h>
 
-int g1 = 0;
-int g2 = 0;
+int g = 0;
 pthread_mutex_t A = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t B = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t C = PTHREAD_MUTEX_INITIALIZER;
 
 void *t_fun(void *arg) {
   pthread_mutex_lock(&A);
+  pthread_mutex_lock(&C);
   pthread_mutex_lock(&B);
-  g1 = 5;
+  g = 5;
   pthread_mutex_unlock(&B);
   pthread_mutex_lock(&B);
-  g1 = 0;
+  g = 0;
   pthread_mutex_unlock(&B);
-  pthread_mutex_lock(&C);
-  g2 = 5;
-  pthread_mutex_unlock(&C);
-  pthread_mutex_lock(&C);
-  g2 = 0;
   pthread_mutex_unlock(&C);
   pthread_mutex_unlock(&A);
   return NULL;
@@ -34,15 +29,15 @@ int main(void) {
 
   pthread_mutex_lock(&A);
   pthread_mutex_lock(&B);
-  assert(g1 == 0);
+  assert(g == 0);
   pthread_mutex_unlock(&B);
   pthread_mutex_unlock(&A);
 
+  pthread_mutex_lock(&B);
   pthread_mutex_lock(&C);
-  pthread_mutex_lock(&A);
-  assert(g2 == 0);
-  pthread_mutex_unlock(&A);
+  assert(g == 0);
   pthread_mutex_unlock(&C);
+  pthread_mutex_unlock(&B);
 
   pthread_join(id, NULL);
   return 0;
