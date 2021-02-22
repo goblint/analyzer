83c1f5a968774b56a42c329ca430a0658a64f5de
Julian Erhard <julian.erhard@tum.de>
2021-02-22 19:34:10 +0100

    Add test

---
+++ tests/regression/32-library-analysis/03-malloc-int-not-distinct.c
@@ -0,0 +1,17 @@
+//PARAM: --enable allfuns --enable ana.library  --set ana.activated "['base','mallocWrapperTypeBased']"
+#include <stdlib.h>
+
+
+int main(){
+    int *x = malloc(sizeof(int));
+    int *y = malloc(sizeof(int));
+
+    int z = 1;
+    if(x==y){
+        z = 0;
+    }
+
+    assert(z == 1); // Unknown!
+
+    return 2;
+}
