677ae1155c04ef0c6b1271fcc57bac57a8e9bf43
Julian Erhard <julian.erhard@tum.de>
2021-02-23 11:24:17 +0100

    Fix typo

--- tests/regression/32-library-analysis/04-int-args.c
+++ tests/regression/32-library-analysis/04-int-args.c
@@ -2,8 +2,40 @@
 
 int f(int *x, int *y){
     int z = 1;
-    if(x == y){
+    if(x == y){ // Pointers of same type might be equal or not
         z = 0;
     }
     assert(z == 1); //UNKNOWN!
+    return 0;
+}
+
+struct data
+{
+    int *x;
+    int *y;
+};
+
+
+int g(struct data d, int *y){
+    int z = 1;
+    if(d.x == y){
+        z = 0;
+    }
+    assert(z == 1); //UNKNOWN!
+    if(d.x == d.y){
+        z = 0;
+    }
+    assert(z == 1); //UNKNOWN!
+    return 0;
+}
+
+
+int h(int *x){
+    int *y = malloc(sizeof(int));
+    int z = 1;
+    if(x == y){ // memory allocated within the function h should be detected different to memory reachable from arguments
+        z = 0;
+    }
+    assert(z == 1);
+    return 0;
 }
