4e6d5c343429d2f9f2403931012282949e49b7c0
Julian Erhard <julian.erhard@tum.de>
2021-02-23 17:49:28 +0100

    Add test

---
+++ tests/regression/32-library-analysis/05-calls.c
@@ -0,0 +1,32 @@
+//PARAM: --enable allfuns --enable ana.library  --set ana.activated "['base','mallocWrapperTypeBased']"
+#include <stdio.h>
+#include <assert.h>
+
+int *foo(int *x){
+    return x;
+}
+
+int *bar(int *x){
+    foo(x);
+    return x;
+}
+
+int *fooBar(int *x){
+    foo(x);
+    bar(x);
+    return x;
+}
+
+int main(){
+    int i = 3;
+    foor(&i);
+    bar(&i);
+    int *p;
+    p = foo(&i);
+    int z = 1;
+    if(p == &i){
+        z = 0;
+    }
+    assert(z == 0);
+    return 0;
+}
