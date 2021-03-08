8fe0c091bbcc6b2ec849b6ee787ba2a483db0075
Ivana Zuzic <ivana.zuzic@tum.de>
2021-03-08 02:20:01 +0100

    Possible to assert numbers as booleans

--- tests/regression/32-loop-termination/01-simple.c
+++ tests/regression/32-loop-termination/01-simple.c
@@ -4,16 +4,16 @@ int main(void) {
     int x, y, r;
     x = 5;
     assert(x == 3); // FAIL
-    assert(x == 5); 
+    assert(x == 5); // SUCCESS
     y = 3;
     r = x + y;
     while (x != y) {
-        assert(r > 0); 
+        assert(r > 0); // UNKNOWN // base analysis doens't know this but octApron does
         if (y > x) 
             y = y - x;
         else 
             x = x - y;
-        assert(r > x + y);
+        assert(r > x + y); // UNKNOWN // base analysis doens't know this but octApron does
         r = x + y; 
     }
     return 0;
