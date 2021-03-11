8b2c7cb4e75c7088245497a757a3aced5e501571
Simmo Saan <simmo.saan@gmail.com>
2021-03-11 12:55:47 +0200

    Add traces examples

---
+++ tests/regression/13-privatized/43-traces-mine1.c
@@ -0,0 +1,30 @@
+// PARAM: --enable ana.int.interval
+#include <pthread.h>
+#include <assert.h>
+
+int g = 3; // matches one expected precise read
+pthread_mutex_t A = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t B = PTHREAD_MUTEX_INITIALIZER;
+
+void *t_fun(void *arg) {
+  pthread_mutex_lock(&A);
+  g = 1;
+  pthread_mutex_lock(&B);
+  g = 2;
+  pthread_mutex_unlock(&A);
+  g = 3;
+  pthread_mutex_unlock(&B);
+  return NULL;
+}
+
+int main(void) {
+  pthread_t id;
+  pthread_create(&id, NULL, t_fun, NULL);
+
+  pthread_mutex_lock(&A);
+  assert(g >= 2);
+  pthread_mutex_lock(&B);
+  assert(g >= 2);
+  assert(g == 3);
+  return 0;
+}
