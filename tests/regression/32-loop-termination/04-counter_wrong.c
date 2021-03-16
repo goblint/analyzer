656d24bb779990aba94e47b08da899ccb78ebb2a
Ivana Zuzic <ivana.zuzic@tum.de>
2021-03-16 13:25:01 +0100

    Change an incorrect filename extension

---
+++ tests/regression/32-loop-termination/04-counter_wrong.c
@@ -0,0 +1,9 @@
+// PARAM: --set ana.activated[+] "'octApron'" --enable dbg.debug --enable ana.int.interval 
+
+int main() {
+    int i = 0;
+    while (i > 0) {
+        i++;
+    }
+    return 0;
+}
