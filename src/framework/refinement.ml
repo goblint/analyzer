98b349818ccbb26fefc8b40c381c76596255dcde
Simmo Saan <simmo.saan@gmail.com>
2021-03-23 17:56:59 +0200

    Document analysis restarting for refinement purposes

---
+++ src/framework/refinement.ml
@@ -0,0 +1,11 @@
+(** Restarts the analysis from scratch in Control.
+    Its raiser is expected to have modified modified some global state to do
+    a more precise analysis next time.
+    For example:
+    1. Add information to global variable that is used in transfer functions.
+    2. Activate more analyses (see Witness).
+    3. Make more analyses path-sensitive (see Witness).
+    4. Register (and activate) new analyses dynamically (see Witness). *)
+exception RestartAnalysis
+
+(* TODO: less crude means of refinement based on incrementality and reusing parts of solution? *)
