44c12929dfb23a53803dfd95b59d39e152a7c2af
Simmo Saan <simmo.saan@gmail.com>
2021-03-23 12:18:47 +0200

    Add aggregation to privPrecCompare

--- src/privPrecCompare.ml
+++ src/privPrecCompare.ml
@@ -5,18 +5,7 @@ open Ana
 
 module VD = BaseDomain.VD
 (* TODO: share with PrecisionDumpPriv *)
-module LVH =
-struct
-  include Hashtbl.Make (Printable.Prod (Basetype.ProgLines) (Basetype.Variables))
-
-  let iter2 f h1 h2 =
-    ignore (
-      merge (fun k v1 v2 ->
-          f k v1 v2;
-          None
-        ) h1 h2
-    )
-end
+module LVH = Hashtbl.Make (Printable.Prod (Basetype.ProgLines) (Basetype.Variables))
 
 let load filename =
   let f = open_in_bin filename in
@@ -24,27 +13,67 @@ let load filename =
   close_in_noerr f;
   lvh
 
+module Comparison =
+struct
+  type t =
+    | Equal
+    | MorePrecise
+    | LessPrecise
+    | Incomparable
+
+  let aggregate_same c1 c2 = match c1, c2 with
+    | Incomparable, _
+    | _, Incomparable
+    | MorePrecise, LessPrecise
+    | LessPrecise, MorePrecise ->
+      Incomparable
+    | Equal, c
+    | c, Equal ->
+      c
+    | MorePrecise, MorePrecise ->
+      MorePrecise
+    | LessPrecise, LessPrecise ->
+      LessPrecise
+
+  let to_string_infix = function
+    | Equal -> "equal to"
+    | MorePrecise -> "more precise than"
+    | LessPrecise -> "less precise than"
+    | Incomparable -> "incomparable to"
+end
+
+
 let compare_dumps filename1 filename2 =
+  (* TODO: don't load multiple times *)
   let lvh1 = load filename1 in
   let lvh2 = load filename2 in
-  LVH.iter2 (fun (l, x) v1 v2 ->
+  let lvh = LVH.merge (fun k v1 v2 -> Some (v1, v2)) lvh1 lvh2 in
+  let compared = LVH.map (fun (l, x) (v1, v2) ->
       let v1 = v1 |? VD.bot () in
       let v2 = v2 |? VD.bot () in
-      let diff_msg =
-        match VD.leq v1 v2, VD.leq v2 v1 with
-        | true, true -> None
-        | true, false -> Some (Pretty.dprintf "%s lt %s (%a lt %a)" filename1 filename2 VD.pretty v1 VD.pretty v2)
-        | false, true -> Some (Pretty.dprintf "%s gt %s (%a gt %a)" filename1 filename2 VD.pretty v1 VD.pretty v2)
-        | false, false -> Some (Pretty.dprintf "%s incmp %s (%a incmp %a)" filename1 filename2 VD.pretty v1 VD.pretty v2)
+      let c = match VD.leq v1 v2, VD.leq v2 v1 with
+        | true, true -> Comparison.Equal
+        | true, false -> Comparison.MorePrecise
+        | false, true -> Comparison.LessPrecise
+        | false, false -> Comparison.Incomparable
       in
-      match diff_msg with
-      | Some diff_msg ->
-        ignore (Pretty.printf "%a %a: %t\n" d_loc l d_varinfo x (fun () -> diff_msg))
-      | None -> ()
-    ) lvh1 lvh2
+      let msg = Pretty.dprintf "%s %s %s (%a %s %a)" filename1 (Comparison.to_string_infix c) filename2 VD.pretty v1 (Comparison.to_string_infix c) VD.pretty v2 in
+      (c, msg)
+    ) lvh
+  in
+  LVH.iter (fun (l, x) (c, msg) ->
+      match c with
+      | Comparison.Equal -> ()
+      | _ ->
+        ignore (Pretty.printf "%a %a: %t\n" d_loc l d_varinfo x (fun () -> msg))
+    ) compared;
+  let c = LVH.fold (fun _ (c, _) acc -> Comparison.aggregate_same c acc) compared Comparison.Equal in
+  let msg = Pretty.dprintf "%s %s %s" filename1 (Comparison.to_string_infix c) filename2 in
+  (c, msg)
 
 let () =
   let filenames = List.tl (Array.to_list Sys.argv) in
   List.cartesian_product filenames filenames
   |> List.filter (fun (filename1, filename2) -> String.compare filename1 filename2 < 0)
-  |> List.iter (uncurry compare_dumps)
+  |> List.map (uncurry compare_dumps)
+  |> List.iter (fun (_, msg) -> ignore (Pretty.printf "%t\n" (fun () -> msg)))
