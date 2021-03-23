90fdea58dca2f4ea240ba6cfa54f65aee08b748c
Ivana Zuzic <ivana.zuzic@tum.de>
2021-03-24 00:23:54 +0100

    Correcting tests

--- tests/regression/32-loop-termination/04-counter_wrong.c
+++ tests/regression/32-loop-termination/04-counter_wrong.c
@@ -1,7 +1,7 @@
 // PARAM: --set ana.activated[+] "'octApron'" --enable dbg.debug --enable ana.int.interval 
 
 int main() {
-    int i = 0;
+    int i = 1;
     while (i > 0) {
         i++;
     }
--- tests/regression/32-loop-termination/08-counter_for.c
+++ tests/regression/32-loop-termination/08-counter_for.c
@@ -1,28 +1,9 @@
 // PARAM: --enable dbg.debug --enable ana.int.interval
 
 int main() {
-    int t1 = 0, t2 = 0;
     int i;
-    for (i = 0; i < 5; i++) {
-        t1++;
-        t2--;
-    }
-    assert(t1 == i); //SUCCESS!
-
-    t1 = 5;
-    t2 = 5;
-    for (i = 5; i > 0; i--) {
-        t1++;
-        t2--;
-    }
-    assert(t2 == i); //SUCCESS! 
-
-    t1 = 0;
-    t2 = 0;
-    for (i = 0; i < 5; i--) {
-        t1++;
-        t2--;
-    }
-    assert(t1 == i); //FAIL!
+    for (i = 0; i < 5; i++);
+    for (i = 5; i > 0; i--);
+    for (i = 0; i < 5; i--);
     return 0;
 }
