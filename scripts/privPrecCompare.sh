d6ef7ced22fbde8eda4e74cbe9caf60f730caf20
Simmo Saan <simmo.saan@gmail.com>
2021-04-05 18:12:47 +0300

    Fix arguments with spaces in privPrecCompare

--- scripts/privPrecCompare.sh
+++ scripts/privPrecCompare.sh
@@ -10,7 +10,7 @@ mkdir -p $OUTDIR
 for PRIV in "${PRIVS[@]}"; do
     echo $PRIV
     PRIVDUMP="$OUTDIR/$PRIV"
-    ./goblint --sets exp.privatization $PRIV --sets exp.priv-prec-dump $PRIVDUMP $*
+    ./goblint --sets exp.privatization $PRIV --sets exp.priv-prec-dump $PRIVDUMP "$@"
 done
 
 PRIVDUMPS=("${PRIVS[*]/#/$OUTDIR/}") # why [*] here?
