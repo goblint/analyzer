81a2828bfae8b57acafc6c4e69f0fc1f4840f524
Michael Schwarz <michael.schwarz93@gmail.com>
2021-04-07 14:06:52 +0200

    Add failing example

---
+++ tests/regression/31-ikind-aware-ints/15-strange.c
@@ -0,0 +1,7 @@
+//PARAM: --disable ana.int.interval --disable ana.int.def_exc --enable ana.int.enums
+int main (int argc, char* argv[])
+{
+    // This used to cause an excpetion because of incompatible ikinds
+    signed char f2 = 7;
+    signed char l_1857 = ((0xFFBD4A17L && f2) | f2);
+}
