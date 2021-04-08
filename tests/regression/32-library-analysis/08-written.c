8c4d9cddfb1657e1918be3a971de11200b3ebec1
Julian Erhard <julian.erhard@tum.de>
2021-04-08 18:43:40 +0200

    Add some test case for writtenLvals

---
+++ tests/regression/32-library-analysis/08-written.c
@@ -0,0 +1,35 @@
+//PARAM: --enable allfuns --enable ana.library  --set ana.activated "['base','mallocWrapperTypeBased','writtenLvals']"
+
+typedef struct foo{
+    long *ptr;
+    long x;
+} bar;
+
+
+#include <stdlib.h>
+#include <assert.h>
+int main(){
+    int *p = malloc(sizeof(int));
+    int *p2 = malloc(sizeof(int));
+
+    bar *p3 = malloc(sizeof(bar));
+
+    *p = 2;
+    *p2 = 3;
+    (*p3).x = 3;
+    *(p3->ptr) = 2;
+
+    int z = 1;
+    int *ptr = &z;
+    *ptr = 2;
+
+    if(p == p2){
+        z = 0;
+    } else {
+        z = 32;
+    }
+    // p and p2 should be represented by the same non-definite memory-block representation.
+    // Therefore, the following assert should be unknown:
+    assert(z == 0); // UNKNOWN
+    return 0;
+}
