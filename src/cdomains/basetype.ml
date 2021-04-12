b5520e5797564629e6393ba78e7e882a6ab19a8a
Simmo Saan <simmo.saan@gmail.com>
2021-04-12 16:10:38 +0300

    Remove unnecessary quotes from RawBools printing

--- src/cdomains/basetype.ml
+++ src/cdomains/basetype.ml
@@ -177,7 +177,7 @@ struct
   let hash (x:t) = Hashtbl.hash x
   let equal (x:t) (y:t) = x=y
   let isSimple _ = true
-  let short _ (x:t) =  if x then "\" true \"" else "\" false \""
+  let short _ (x:t) =  if x then "true" else "false"
   let pretty_f sf () x = text (if x then "true" else "false")
   let pretty () x = text (short () x)
   let name () = "raw bools"
