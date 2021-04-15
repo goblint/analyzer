4a03dcf36934f0d222783ca43941ffd51e269060
Julian Erhard <julian.erhard@tum.de>
2021-04-15 11:29:42 +0200

    Add simple show case test case

---
+++ tests/regression/34-showcase/01-interval.c
@@ -0,0 +1,28 @@
+//PARAM: --enable ana.int.interval
+#include <stdio.h>
+
+int f(int x, int y){
+    int sum = x + y;
+    return sum * sum;
+}
+
+int main(){
+    int x = 0;
+    int y = 4;
+    int *py = &y;
+    int fac = 1;
+
+    while(y != 0){
+        fac = fac * y;
+        *py = *py - 1;
+    }
+
+    printf("fac 100: %d\n", fac);
+
+    int res = f(3, 5);
+
+    assert(res == 64);
+    printf("result of f: %d\n", res);
+
+    return res;
+}
