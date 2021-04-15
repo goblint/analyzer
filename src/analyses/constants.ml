b5cdf5c3056f8d86def7dea6d11434094ab03472
Michael Schwarz <michael.schwarz93@gmail.com>
2021-04-15 18:03:06 +0200

    Didactical Toy Analysis (#190)
    Add simple toy analysis that does constant propagation (extremely poorly ;))
    
    Co-authored-by: Julian Erhard <julian.erhard@tum.de>

---
+++ src/analyses/constants.ml
@@ -0,0 +1,105 @@
+
+open Prelude.Ana
+open Analyses
+
+(** An analysis specification for didactic purposes.
+ It only considers definite values of local variables.
+ We do not pass information interprocedurally. *)
+module Spec : Analyses.Spec =
+struct
+  include Analyses.DefaultSpec
+
+  let name () = "constants"
+
+  module I = IntDomain.Flattened
+
+  (* Map of (local int) variables to flat integers *)
+  module D = MapDomain.MapBot (Basetype.Variables) (I)
+  (* No information about globals*)
+  module G = Lattice.Unit
+  (* No contexts*)
+  module C = Lattice.Unit
+
+  let val_of () = D.bot ()
+  let context _ = ()
+
+  let is_integer_var (v: varinfo) =
+    match v.vtype with
+      | TInt _ -> true
+      | _ -> false
+
+  let get_local = function
+    | Var v, NoOffset when is_integer_var v && not (v.vglob || v.vaddrof) -> Some v (* local integer variable whose address is never taken *)
+    | _, _ -> None
+
+  (** Evaluates expressions *)
+  let rec eval (state : D.t) (e: exp) =
+    match e with
+    | Const c -> (match c with
+      | CInt64 (i,_,_) -> I.of_int i
+      | _ -> I.top ()
+      )
+    | Lval lv -> (match get_local lv with
+      | Some v -> D.find v state
+      | _ -> I.top ()
+      )
+    | BinOp (PlusA, e1, e2, t) -> (
+      let v1 = eval state e1 in
+      let v2 = eval state e2 in
+      I.add v1 v2
+    )
+    | _ -> I.top ()
+
+  (* transfer functions *)
+  let assign ctx (lval:lval) (rval:exp) : D.t =
+    match get_local lval with
+        | Some loc -> D.add loc (eval ctx.local rval) ctx.local
+        | None -> ctx.local
+
+  let branch ctx (exp:exp) (tv:bool) : D.t =
+    let v = eval ctx.local exp in
+    match I.to_bool v with
+      | Some b when b <> tv -> raise Deadcode (* if the expression evaluates to not tv, the tv branch is not reachable *)
+      | _ -> ctx.local
+
+  let body ctx (f:fundec) : D.t =
+    (* Initialize locals to top *)
+    List.fold (fun m l -> D.add l (I.top ()) m) ctx.local f.slocals
+
+  let return ctx (exp:exp option) (f:fundec) : D.t =
+    (* Do nothing, as we are not interested in return values for now. *)
+    ctx.local
+
+  let enter ctx (lval: lval option) (fv:varinfo) (args:exp list) : (D.t * D.t) list =
+    let f = Cilfacade.getdec fv in
+    (* Set the formal int arguments to top *)
+    let callee_state = List.fold (fun m l -> D.add l (I.top ()) m) (D.bot ()) f.sformals in
+    [(ctx.local, callee_state)]
+
+  let set_local_int_lval_top (state: D.t) (lval: lval option) =
+    match lval with
+      | Some lv ->
+        (match get_local lv with
+          | Some local -> D.add local (I.top ()) state
+          | _ -> state
+        )
+      |_ -> state
+
+  let combine ctx (lval:lval option) fexp (f:varinfo) (args:exp list) fc (au:D.t) : D.t =
+    (* If we have a function call with assignment
+        x = f (e1, ... , ek)
+      with a local int variable x on the left, we set it to top *)
+    set_local_int_lval_top ctx.local lval
+
+  let special ctx (lval: lval option) (f:varinfo) (arglist:exp list) : D.t =
+    (* When calling a special function, and assign the result to some local int variable, we also set it to top. *)
+    set_local_int_lval_top ctx.local lval
+
+  let startstate v = D.bot ()
+  let threadenter ctx lval f args = D.top ()
+  let threadspawn ctx lval f args fctx = D.bot ()
+  let exitstate  v = D.top ()
+end
+
+let _ =
+  MCP.register_analysis (module Spec : Spec)
---
+++ tests/regression/27-inv_invariants/08-bor-inv.c
@@ -0,0 +1,26 @@
+#include<assert.h>
+
+int main(){
+
+
+    int x = 2;
+    int y = 3;
+    int z = 0;
+    int n = 0;
+    int nn = 0;
+    int top;
+
+    if( x + y == 4){
+        z = 3;
+    }
+    assert(z == 0);
+
+    // if( ((n - nn) == 3) ==  (x+top == 3) * (y+ top == 3)){
+    //     z = 1;
+    // } else {
+    //     z = 3;
+    // }
+
+    // assert(z == 1);
+    return 0;
+}
