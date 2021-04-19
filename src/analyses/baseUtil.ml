3130b9119028b0b0a082bfcf3ff2029e789a5754
Simmo Saan <simmo.saan@gmail.com>
2021-04-19 16:11:30 +0300

    Fix unsoundness when thread escape analysis is disabled

--- src/analyses/baseUtil.ml
+++ src/analyses/baseUtil.ml
@@ -3,7 +3,7 @@ open GobConfig
 module Q = Queries
 
 let is_global (a: Q.ask) (v: varinfo): bool =
-  v.vglob || match a (Q.MayEscape v) with `MayBool tv -> tv | _ -> false
+  v.vglob || ThreadEscape.has_escaped a v
 
 let is_static (v:varinfo): bool = v.vstorage = Static
 
