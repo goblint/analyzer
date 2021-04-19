1cee7911a16bb3a689f664f99a481f9d89732ba3
Simmo Saan <simmo.saan@gmail.com>
2021-04-19 16:21:26 +0300

    Enable escape analysis in remaining regression tests

--- tests/regression/22-partitioned_arrays/06-interprocedural.c
+++ tests/regression/22-partitioned_arrays/06-interprocedural.c
@@ -1,4 +1,4 @@
-// PARAM: --sets solver td3 --enable ana.int.interval --enable exp.partition-arrays.enabled  --set ana.activated "['base','threadid','threadflag','expRelation','mallocWrapper']"
+// PARAM: --sets solver td3 --enable ana.int.interval --enable exp.partition-arrays.enabled  --set ana.activated "['base','threadid','threadflag','escape','expRelation','mallocWrapper']"
 int main(void) {
   example1();
   example2();
--- tests/regression/23-partitioned_arrays_last/05-adapted_from_01_09_array.c
+++ tests/regression/23-partitioned_arrays_last/05-adapted_from_01_09_array.c
@@ -1,4 +1,4 @@
-// PARAM: --sets solver td3 --enable ana.int.interval --enable exp.partition-arrays.enabled  --sets exp.partition-arrays.keep-expr "last" --set ana.activated "['base','threadid','threadflag','expRelation','mallocWrapper']"
+// PARAM: --sets solver td3 --enable ana.int.interval --enable exp.partition-arrays.enabled  --sets exp.partition-arrays.keep-expr "last" --set ana.activated "['base','threadid','threadflag','escape','expRelation','mallocWrapper']"
 #include<stdio.h>
 #include<assert.h>
 
