2eee28e5ca2e994cb3abf596d56ef77706bcdd9e
Simmo Saan <simmo.saan@gmail.com>
2021-04-19 16:03:09 +0300

    Use structural equality for vstorage consistently

--- src/analyses/baseUtil.ml
+++ src/analyses/baseUtil.ml
@@ -5,7 +5,7 @@ module Q = Queries
 let is_global (a: Q.ask) (v: varinfo): bool =
   v.vglob || match a (Q.MayEscape v) with `MayBool tv -> tv | _ -> false
 
-let is_static (v:varinfo): bool = v.vstorage == Static
+let is_static (v:varinfo): bool = v.vstorage = Static
 
 let is_always_unknown variable = variable.vstorage = Extern || Ciltools.is_volatile_tp variable.vtype
 
