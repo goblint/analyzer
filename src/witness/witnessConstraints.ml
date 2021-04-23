13ab39b4bc1da17d3c10841cb6444d7195ef6d90
Simmo Saan <simmo.saan@gmail.com>
2021-04-23 12:45:32 +0300

    Fix PathSensitive3.Dom.pretty_diff based on PathSensitive2.D.pretty_diff fix

--- src/witness/witnessConstraints.ml
+++ src/witness/witnessConstraints.ml
@@ -192,11 +192,12 @@ struct
         try
           let p t tr = not (mem t tr s2) in
           let (evil, evilr) = choose' (filter' p s1) in
-          let (other, otherr) = choose' s2 in
-          (* dprintf "%s has a problem with %a not leq %a because %a" (name ())
-             Spec.D.pretty evil Spec.D.pretty other
-             Spec.D.pretty_diff (evil,other) *)
-          Spec.D.pretty_diff () (evil,other)
+          let evilr' = R.choose evilr in
+          dprintf "%a -> %a:\n" Spec.D.pretty evil VIEB.pretty evilr'
+          ++
+          fold' (fun other otherr acc ->
+              (dprintf "not leq %a because %a\nand not mem %a because %a\n" Spec.D.pretty other Spec.D.pretty_diff (evil, other) R.pretty otherr R.pretty_diff (R.singleton evilr', otherr)) ++ acc
+            ) s2 nil
         with _ ->
           dprintf "choose failed b/c of empty set s1: %d s2: %d"
           (cardinal s1)
