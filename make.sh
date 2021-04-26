23e80889b4e4992494d594104aacf5f331dfa01e
Ivana Zuzic <ivana.zuzic@tum.de>
2021-04-26 14:24:37 +0200

    Mention libmpfr-dev in make setup, it's for Apron

--- make.sh
+++ make.sh
@@ -95,7 +95,7 @@ rule() {
     ;; deps)
       opam update; opam install -y . --deps-only --locked --unlock-base
     ;; setup)
-      echo "Make sure you have the following installed: opam >= 2.0.0, git, patch, m4, autoconf, libgmp-dev"
+      echo "Make sure you have the following installed: opam >= 2.0.0, git, patch, m4, autoconf, libgmp-dev, libmpfr-dev"
       echo "For the --html output you also need: javac, ant, dot (graphviz)"
       echo "For running the regression tests you also need: ruby, gem, curl"
       echo "For reference see ./Dockerfile or ./scripts/travis-ci.sh"
