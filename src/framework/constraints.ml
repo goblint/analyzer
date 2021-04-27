310b98db935bc1659d6e989e6951154cd022517c
Simmo Saan <simmo.saan@gmail.com>
2021-04-27 18:35:31 +0300

    Remove unnecessary topname module for PathSensitive2

--- src/framework/constraints.ml
+++ src/framework/constraints.ml
@@ -948,8 +948,6 @@ module GlobSolverFromIneqSolver (Sol:GenericIneqBoxSolver)
         (l', g')
     end
 
-(* TODO: remove unnecessary *)
-module N = struct let topname = "Top" end
 
 (** Add path sensitivity to a analysis *)
 module PathSensitive2 (Spec:Spec)
