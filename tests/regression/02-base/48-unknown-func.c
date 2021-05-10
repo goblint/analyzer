5d4caae22325f378f745f5f1ae2009bd781cc0bc
Julian Erhard <julian.erhard@tum.de>
2021-05-10 10:30:58 +0200

    Add test where memory is not invalidated correctly.

---
+++ tests/regression/02-base/48-unknown-func.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef struct list {
+    int val;
+    struct list *next;
+} list_t;
+
+// void mutate_list(list_t n){
+//     list_t *next = n.next;
+//     next->val = 42;
+// }
+
+int main(){
+    list_t first;
+    list_t second;
+
+    first.next = &second;
+    first.val = 1;
+    second.next = NULL;
+    second.val = 2;
+
+    // When passing a struct to an unknown function, reachable memory should be invalidated
+    mutate_list(first);
+    assert(second.val == 2); //UNKNOWN!
+
+    // Passing a pointer to the struct here.
+    mutate_list2(&first);
+    assert(second.val == 2); //UNKNOWN!
+}
