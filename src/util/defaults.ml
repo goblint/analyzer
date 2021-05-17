cbbc4e055eb8104363b81cc001de32f6cbabb6b3
wherekonshade <wherekonshade@web.de>
2021-05-17 17:35:12 +0200

    default flag added

--- src/util/defaults.ml
+++ src/util/defaults.ml
@@ -92,6 +92,7 @@ let _ = ()
       ; reg Std "load_run"        "''"           "Load a saved run. See save_run."
       ; reg Std "compare_runs"    "[]"           "Load these saved runs and compare the results. Note that currently only two runs can be compared!"
       ; reg Std "warn"            "'post'"       "Output warnings: 'post'. Output warnings after solving. Best results. 'never': Do not produce warnings, 'early'. For debugging. Outputs warnings already while solving (may lead to spurious warnings/asserts that would disappear after narrowing)."
+      ; reg Std "arrayoob"        "false"        "Array out of bounds check"
 
 (* {4 category [Analyses]} *)
 let _ = ()
