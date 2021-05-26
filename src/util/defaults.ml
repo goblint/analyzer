48dd0aad8d126e6d6ada61689b1c2934b1ca51c1
Ivana Zuzic <ivana.zuzic@tum.de>
2021-05-26 17:16:57 +0200

    Add explanation for octagon_vars

--- src/util/defaults.ml
+++ src/util/defaults.ml
@@ -87,7 +87,7 @@ let _ = ()
       ; reg Std "save_run"        "''"           "Save the result of the solver, the current configuration and meta-data about the run to this directory (if set). The data can then be loaded (without solving again) to do post-processing like generating output in a different format or comparing results."
       ; reg Std "load_run"        "''"           "Load a saved run. See save_run."
       ; reg Std "compare_runs"    "[]"           "Load these saved runs and compare the results. Note that currently only two runs can be compared!"
-      ; reg Std "octagon_vars"    "[]"           "Variables tracked by the octagon"
+      ; reg Std "octagon_vars"    "[]"           "Variables tracked by the octagon. Empty list means all are included."
 
 (* {4 category [Analyses]} *)
 let _ = ()
