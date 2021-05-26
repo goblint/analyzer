07c1d086d7fa8dd206a378c2f894df51b035d018
Simmo Saan <simmo.saan@gmail.com>
2021-05-26 15:08:50 +0300

    Extract CommonPriv

--- src/analyses/basePriv.ml
+++ src/analyses/basePriv.ml
@@ -10,6 +10,8 @@ module VD     = BaseDomain.VD
 module CPA    = BaseDomain.CPA
 module BaseComponents = BaseDomain.BaseComponents
 
+open CommonPriv
+
 
 module type S =
 sig
@@ -654,37 +656,6 @@ struct
   let threadenter = startstate_threadenter startstate
 end
 
-module Locksets =
-struct
-  module Lock = LockDomain.Addr
-
-  module Lockset =
-  struct
-    include Printable.Std (* To make it Groupable *)
-    include SetDomain.ToppedSet (Lock) (struct let topname = "All locks" end)
-    let disjoint s t = is_empty (inter s t)
-  end
-
-  let rec conv_offset = function
-    | `NoOffset -> `NoOffset
-    | `Field (f, o) -> `Field (f, conv_offset o)
-    (* TODO: better indices handling *)
-    | `Index (_, o) -> `Index (IdxDom.top (), conv_offset o)
-
-  let current_lockset (ask: Q.ask): Lockset.t =
-    (* TODO: remove this global_init workaround *)
-    if !GU.global_initialization then
-      Lockset.empty ()
-    else
-      let ls = ask.f Queries.CurrentLockset in
-      Q.LS.fold (fun (var, offs) acc ->
-          Lockset.add (Lock.from_var_offset (var, conv_offset offs)) acc
-        ) ls (Lockset.empty ())
-
-  (* TODO: reversed SetDomain.Hoare *)
-  module MinLocksets = HoareDomain.Set_LiftTop (Lattice.Reverse (Lockset)) (struct let topname = "All locksets" end) (* reverse Lockset because Hoare keeps maximal, but we need minimal *)
-end
-
 module AbstractLockCenteredGBase (WeakRange: Lattice.S) (SyncRange: Lattice.S) =
 struct
   open Locksets
@@ -1141,29 +1112,6 @@ struct
   (* weak: G -> (S:2^M -> (W:2^M -> D)) *)
   (* sync: M -> (S:2^M -> (W:2^M -> (G -> D))) *)
   include AbstractLockCenteredGBase (GWeakW) (GSyncW)
-end
-
-module WriteCenteredD =
-struct
-  open Locksets
-
-  module W =
-  struct
-    include MapDomain.MapBot_LiftTop (Basetype.Variables) (MinLocksets)
-    let name () = "W"
-  end
-
-  module P =
-  struct
-    (* Note different Map order! *)
-    (* MapTop because default value in P must be top of MinLocksets,
-       as opposed to bottom in W. *)
-    include MapDomain.MapTop_LiftBot (Basetype.Variables) (MinLocksets)
-    let name () = "P"
-
-    (* TODO: change MinLocksets.exists/top instead? *)
-    let find x p = find_opt x p |? MinLocksets.singleton (Lockset.empty ()) (* ensure exists has something to check for thread returns *)
-  end
 end
 
 (** Write-Centered Reading. *)
