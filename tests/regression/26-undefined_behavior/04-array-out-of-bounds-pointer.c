9c6b5b7052736ed9daf563a105933cd065e68997
wherekonshade <wherekonshade@web.de>
2021-05-29 18:25:18 +0200

    arrayoob pointer test added

---
+++ tests/regression/26-undefined_behavior/04-array-out-of-bounds-pointer.c
@@ -0,0 +1,16 @@
+//Pointer to arrays: out of bounds access
+#include <stdio.h>
+int main( )
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int * ptr = arr;
+
+    ptr[6] = 10; //WARN
+    ptr[-1] = 10; //WARN
+    for (int i = 0; i < 10; ++i)
+    {
+        ptr[i] = 5; //WARN
+    }
+    return 0;
+}
+
