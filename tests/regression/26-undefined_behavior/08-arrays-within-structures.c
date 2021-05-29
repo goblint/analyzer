e369b22078047b4fdbabc867f27ead025f2a537e
wherekonshade <wherekonshade@web.de>
2021-05-29 20:15:32 +0200

    yet another 26/08 fix

--- tests/regression/26-undefined_behavior/08-arrays-within-structures.c
+++ tests/regression/26-undefined_behavior/08-arrays-within-structures.c
@@ -1,29 +1,27 @@
 // PARAM: --set ana.activated "['base','threadid','threadflag','escape','mallocWrapper']" --set dbg.debug true --enable ana.arrayoob
-//Arrays within structures. Source of sample struct:  https://codeforwin.org/2018/07/how-to-declare-initialize-and-access-array-of-structure.html
+// Arrays within structures. Source of sample struct:
+// https://codeforwin.org/2018/07/how-to-declare-initialize-and-access-array-of-structure.html
 #include <stdio.h>
-int main()
-{
-    struct student 
-{
-    char  name[100];
-    int   roll;
+int main() {
+  struct student {
+    char name[100];
+    int roll;
     float marks;
-};
-// Structure array declaration
-struct student stu[100] = 
-{
-    { "vandah", 12, 89.5f },
-    { "edin", 15, 98.0f },
-    { "david",  17, 90.0f },
-};
-    
-    stu[6].name = "test"; //WARN
-    stu[-1].roll = 10; //WARN
-    stu[20].marks = 89.5f;
-    for (int i = 0; i < 10; ++i)
-    {
-        stu[i].roll = 5; //WARN
-    }
-    return 0;
+  };
+  // Structure array declaration
+  struct student stu[] = {
+      {"vandah", 12, 89.5f},
+      {"edin", 15, 98.0f},
+      {"david", 17, 90.0f},
+  };  
+  stu[6].name = "thisisareallylongstringjkdsfjbkslagjbfdghfdnvbfsöajgfklgjfgjlfdsjhhgjgfbldfhghlfdgjöjsadfjhsagjflghfkdsgjhsgblkjsadgfkjabgjlsagdhlshagljhdasghsadgjh"; // WARN
+  stu[-1].roll = 10;    // WARN
+  stu[20].marks = 89.5f; //WARN
+  for (int i = 0; i < 3; ++i) {
+    stu[i].roll = 5;
+  }
+  for (int i = 0; i < 10; ++i) {
+    stu[i].roll = 5;
+  }
+  return 0;
 }
-
