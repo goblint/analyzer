3d3c95df6d6e983fd1ef983652b8ed88c54929b6
wherekonshade <wherekonshade@web.de>
2021-05-29 18:43:36 +0200

    pointer pointing to arrays of different sizes

---
+++ tests/regression/26-undefined_behavior/07-pointer-to-arrays-of-different-sizes.c
@@ -0,0 +1,24 @@
+//Pointer pointing to arrays of different sizes
+#include <stdio.h>
+int main( )
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int arr2[] = {1, 2, 3};
+    int * ptr = arr;
+
+    ptr[6] = 10; //WARN
+    ptr[-1] = 10; //WARN
+    for (int i = 0; i < 10; ++i)
+    {
+        ptr[i] = 5; //WARN
+    }
+    int * ptr = arr2;
+    ptr[3] = 10; //WARN
+    ptr[-1] = 10; //WARN
+    for (int i = 0; i < 5; ++i)
+    {
+        ptr[i] = 5; //WARN
+    }
+    return 0;
+}
+
