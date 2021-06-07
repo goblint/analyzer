2139e822e6b8f9c03c606969cce885e16b16855b
Simmo Saan <simmo.saan@gmail.com>
2021-06-07 18:03:09 +0300

    Add 36/16

---
+++ tests/regression/36-octapron/16-traces-unprot2.c
@@ -0,0 +1,23 @@
+// PARAM: --sets ana.activated[+] octApron
+#include <pthread.h>
+#include <assert.h>
+
+int g;
+
+void *t_fun(void *arg) {
+  return NULL;
+}
+
+int main(void) {
+  int x, y, r;
+
+  pthread_t id;
+  pthread_create(&id, NULL, t_fun, NULL);
+
+  g = r;
+
+  x = g;
+  y = g;
+  assert(x == y); // TODO (like 13/66)
+  return 0;
+}
