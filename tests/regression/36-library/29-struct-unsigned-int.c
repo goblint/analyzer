57efd542aaf92d22922a61e566b677c027abdda5
Julian Erhard <julian.erhard@tum.de>
2021-06-08 14:08:03 +0200

    Add test case for issue with ikinds in struct in the library analysis

---
+++ tests/regression/36-library/29-struct-unsigned-int.c
@@ -0,0 +1,16 @@
+//PARAM: --enable ana.library --sets ana.activated[-] threadid  --sets ana.activated[+] mallocWrapperTypeBased  --sets ana.activated[+] typecasts --sets ana.activated[-] mallocWrapper --sets ana.activated[+] writtenLvals --sets ana.activated[+] varArgs
+
+
+struct bar {
+   unsigned int x;
+   unsigned int y;
+};
+
+
+int foo(struct bar* b){
+    if(!b->x){ // Used to crash here, because of wrong ikind that was used for fields in b
+        return 0;
+    }
+
+    return 1;
+}
