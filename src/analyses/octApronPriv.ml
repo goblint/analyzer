25a10a6ef1f6edb5870581111b2c4ab770831f28
Simmo Saan <simmo.saan@gmail.com>
2021-06-10 13:30:48 +0300

    Add broken protection-based octApron read_global not restricting local enough

--- src/analyses/octApronPriv.ml
+++ src/analyses/octApronPriv.ml
@@ -147,12 +147,13 @@ struct
         let g_unprot_var = var_unprot g in
         let oct_unprot = AD.add_vars_int oct [g_unprot_var] in
         let oct_unprot = AD.assign_var' oct_unprot x_var g_unprot_var in
-        let oct_unprot' = AD.join oct_local oct_unprot in
+        (* let oct_unprot' = AD.join oct_local oct_unprot in
         (* unlock *)
         let oct_unprot' = AD.remove_vars oct_unprot' [g_unprot_var; g_local_var] in
         (* add, assign from, remove is not equivalent to forget if g#unprot already existed and had some relations *)
         (* TODO: why removing g_unprot_var? *)
-        oct_unprot'
+        oct_unprot' *)
+        AD.join oct_local oct_unprot
       )
       else (
         let g_prot_var = var_prot g in
@@ -161,6 +162,7 @@ struct
         AD.join oct_local oct_prot
       )
     in
+    let oct_local' = restrict_local oct_local' p (W.empty ()) in (* TODO: this even breaks 36/12 by immediately forgetting g#in = g#prot by removing g#prot, which hasn't ben locally written *)
     let oct_local' = AD.meet oct_local' (getg (global_varinfo ())) in
     {st with oct = oct_local'}
 
