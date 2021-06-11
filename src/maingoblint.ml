847352e74a1f388f5a23074dd9bd16c47f85c547
Simmo Saan <simmo.saan@gmail.com>
2021-06-11 11:54:00 +0300

    Fix full exception backtraces getting lost when re-raising (closes #247)

--- src/maingoblint.ml
+++ src/maingoblint.ml
@@ -316,14 +316,15 @@ let do_analyze change_info merged_AST =
           print_endline @@ "Activated transformations for phase " ^ string_of_int p ^ ": " ^ at
         );
         try Control.analyze change_info ast funs
-        with x ->
+        with e ->
+          let backtrace = Printexc.get_raw_backtrace () in (* capture backtrace immediately, otherwise the following loses it (internal exception usage without raise_notrace?) *)
           let loc = !Tracing.current_loc in
           Messages.print_msg "{RED}About to crash!" loc;
           (* trigger Generic.SolverStats...print_stats *)
           Goblintutil.(self_signal (signal_of_string (get_string "dbg.solver-signal")));
           do_stats ();
           print_newline ();
-          raise x
+          Printexc.raise_with_backtrace e backtrace (* re-raise with captured inner backtrace *)
           (* Cilfacade.current_file := ast'; *)
       in
       (* old style is ana.activated = [phase_1, ...] with phase_i = [ana_1, ...]
