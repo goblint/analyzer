d8f2c31d0c78f5e22cfd7eb6a96e1de2ea5f6ca8
wherekonshade <wherekonshade@web.de>
2021-06-16 19:15:11 +0200

    Use-after-free added

---
+++ tests/regression/26-undefined_behavior/13-Use_after_free.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int *a = malloc(sizeof(int));
+  if (a != NULL) {
+    *a = 1;
+    int *b = a;
+    free(b); // detects the free even when the pointer is aliased to b
+    return *a; // use after ‘free’ of ‘a’
+  }
+  return 0;
+}
