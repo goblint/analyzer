7b2274370812a17e4e26ecea5d9c00c8757a8a92
Simmo Saan <simmo.saan@gmail.com>
2021-07-02 18:54:11 +0300

    Fix 02/52
    Thread creation is by otherfun.

--- tests/regression/02-base/52-otherfun-special.c
+++ tests/regression/02-base/52-otherfun-special.c
@@ -6,9 +6,6 @@ extern void* magic(void* arg);
 int g;
 
 int main() {
-  pthread_t id;
-  pthread_create(&id, NULL, magic, NULL);
-
   assert(g == 0); // UNKNOWN! (magic may invalidate)
   return 0;
 }
