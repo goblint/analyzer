84609af688030582f072b0c55aeeeecd2b9dec63
Simmo Saan <simmo.saan@gmail.com>
2021-07-07 17:07:41 +0300

    Disable CIL CFG printing

--- src/framework/myCFG.ml
+++ src/framework/myCFG.ml
@@ -220,7 +220,7 @@ let createCFG (file: file) =
       | GFun (fd,loc) ->
         if Messages.tracing then Messages.trace "cfg" "Looking at the function %s.\n" fd.svar.vname;
 
-        Cfg.printCfgFilename ("cilcfg." ^ fd.svar.vname ^ ".dot") fd;
+        (* Cfg.printCfgFilename ("cilcfg." ^ fd.svar.vname ^ ".dot") fd; *)
         (* Walk through the parameters and pre-process them a bit... *)
         do_the_params fd;
         (* Find the first statement in the function *)
