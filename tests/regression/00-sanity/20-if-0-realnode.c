eac4633021b42ae7230207f79b6b6c0012bd2563
Simmo Saan <simmo.saan@gmail.com>
2021-07-08 11:23:08 +0300

    Add regression tests for if (0)

---
+++ tests/regression/00-sanity/20-if-0-realnode.c
@@ -0,0 +1,19 @@
+#include <assert.h>
+
+void stuff() {
+
+}
+
+int main() {
+    // MyCFG shouldn't ignore loops with exp 0 because they may have else branches
+    bar:
+    if (0) {
+        foo: // label prevents CIL from optimizing away this branch
+        stuff(); // something non-empty
+    }
+    else {
+        goto bar; // direct realnode to If via goto
+        stuff(); // something non-empty
+    }
+    return 0;
+}
