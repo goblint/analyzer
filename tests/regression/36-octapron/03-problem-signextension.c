1cd4359abc8a5aa78adfea8066eb09a7f7edb7e2
Simmo Saan <simmo.saan@gmail.com>
2021-07-12 16:02:51 +0300

    Update octApron unsoundness tests (issue #248)

--- tests/regression/36-octapron/03-problem-signextension.c
+++ tests/regression/36-octapron/03-problem-signextension.c
@@ -1,7 +1,8 @@
-// SKIP PARAM: --sets solver td3 --set ana.activated "['base','threadid','threadflag','mallocWrapper']"
+// SKIP PARAM: --sets solver td3 --set ana.activated "['base','threadid','threadflag','mallocWrapper','octApron']"
 // Example from https://github.com/sosy-lab/sv-benchmarks/blob/master/c/bitvector-regression/signextension-1.c
-// TODO: make this work for octApron by handling variable casts
-#include "stdio.h"
+
+#include <assert.h>
+
 int main() {
 
   unsigned short int allbits = -1;
@@ -11,16 +12,9 @@ int main() {
   int signedtosigned = signedallbits;
   unsigned int signedtounsigned = signedallbits;
 
-
-  printf ("unsignedtosigned: %d\n", unsignedtosigned);
-  printf ("unsignedtounsigned: %u\n", unsignedtounsigned);
-  printf ("signedtosigned: %d\n", signedtosigned);
-  printf ("signedtounsigned: %u\n", signedtounsigned);
-
-
   if (unsignedtosigned == 65535 && unsignedtounsigned == 65535
       && signedtosigned == -1 && signedtounsigned == 4294967295) {
-    assert(1);
+    assert(1); // reachable
   }
 
   return (0);
