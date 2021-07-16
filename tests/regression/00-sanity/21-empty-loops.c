130e764d61e4de8002e9725471f556f6ebaa98b8
Simmo Saan <simmo.saan@gmail.com>
2021-07-16 09:59:41 +0300

    Change 00/21 to make all loops live

--- tests/regression/00-sanity/21-empty-loops.c
+++ tests/regression/00-sanity/21-empty-loops.c
@@ -1,11 +1,28 @@
 int main()
 {
-  f_empty_goto_loop();
-  f_empty_while_loop();
-  f_empty_goto_loop_suffix();
-  f_empty_while_loop_suffix();
-  f_nonempty_goto_loop();
-  f_nonempty_while_loop();
+  // non-deterministically make all variants live
+  int r;
+  switch (r)
+  {
+    case 0:
+      f_empty_goto_loop();
+      break;
+    case 1:
+      f_empty_while_loop();
+      break;
+    case 2:
+      f_empty_goto_loop_suffix();
+      break;
+    case 3:
+      f_empty_while_loop_suffix();
+      break;
+    case 4:
+      f_nonempty_goto_loop();
+      break;
+    case 5:
+      f_nonempty_while_loop();
+      break;
+  }
 
   return 0;
 }
