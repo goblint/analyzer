1f85df5c35e675f9805131d3a74d5e1f4244498d
Simmo Saan <simmo.saan@gmail.com>
2021-07-20 14:20:27 +0300

    Extract MyCFG.edge to Edge module

--- src/framework/myCFG.ml
+++ src/framework/myCFG.ml
@@ -19,68 +19,8 @@ type node = Node.t =
 let pretty_node = Node.pretty
 let pretty_short_node = Node.pretty_short
 
+include Edge (* TODO: remove include *)
 
-type asm_out = (string option * string * CilType.Lval.t) list [@@deriving to_yojson]
-type asm_in  = (string option * string * CilType.Exp.t ) list [@@deriving to_yojson]
-
-type edge =
-  | Assign of CilType.Lval.t * CilType.Exp.t
-  (** Assignments lval = exp *)
-  | Proc of CilType.Lval.t option * CilType.Exp.t * CilType.Exp.t list
-  (** Function calls of the form lva = fexp (e1, e2, ...) *)
-  | Entry of CilType.Fundec.t
-  (** Entry edge that relates function declaration to function body. You can use
-    * this to initialize the local variables. *)
-  | Ret of CilType.Exp.t option * CilType.Fundec.t
-  (** Return edge is between the return statement, which may optionally contain
-    * a return value, and the function. The result of the call is then
-    * transferred to the function node! *)
-  | Test of CilType.Exp.t * bool
-  (** The true-branch or false-branch of a conditional exp *)
-  | ASM of string list * asm_out * asm_in
-  (** Inline assembly statements, and the annotations for output and input
-    * variables. *)
-  | VDecl of CilType.Varinfo.t
-  (** VDecl edge for the variable in varinfo. Whether such an edge is there for all
-    * local variables or only when it is not possible to pull the declaration up, is
-    * determined by alwaysGenerateVarDecl in cabs2cil.ml in CIL. One case in which a VDecl
-    * is always there is for VLA. If there is a VDecl edge, it is where the declaration originally
-    * appeared *)
-  | Skip
-  (** This is here for historical reasons. I never use Skip edges! *)
-  | SelfLoop
-  (** This for interrupt edges.! *)
-[@@deriving to_yojson]
-
-
-let pretty_edge () = function
-  | Assign (lv,rv) -> dprintf "Assign '%a = %a' " d_lval lv d_exp rv
-  | Proc (None  ,f,ars) -> dprintf "Proc '%a(%a)'" d_exp f (d_list ", " d_exp) ars
-  | Proc (Some r,f,ars) -> dprintf "Proc '%a = %a(%a)'" d_lval r d_exp f (d_list ", " d_exp) ars
-  | Entry f -> dprintf "Entry %s" f.svar.vname
-  | Ret (None,fd) -> dprintf "Ret (None, %s)" fd.svar.vname
-  | Ret (Some r,fd) -> dprintf "Ret (Some %a, %s)" d_exp r fd.svar.vname
-  | Test (p,b) -> dprintf "Test (%a,%b)" d_exp p b
-  | ASM _ -> text "ASM ..."
-  | Skip -> text "Skip"
-  | VDecl v -> dprintf "VDecl '%a %s;'" d_type v.vtype v.vname
-  | SelfLoop -> text "SelfLoop"
-
-let rec pretty_edges () = function
-  | [] -> Pretty.dprintf ""
-  | [_,x] -> pretty_edge () x
-  | (_,x)::xs -> Pretty.dprintf "%a; %a" pretty_edge x pretty_edges xs
-
-let pretty_edge_kind () = function
-  | Assign (lv,rv) -> dprintf "Assign"
-  | Proc (_  ,f,ars) -> dprintf "Proc"
-  | Entry f -> dprintf "Entry %s" f.svar.vname
-  | Ret (r,fd) -> dprintf "Ret"
-  | Test (p,b) -> dprintf "Test"
-  | ASM _ -> text "ASM"
-  | Skip -> text "Skip"
-  | VDecl _ -> text "VDecl"
-  | SelfLoop -> text "SelfLoop"
 
 type cfg = node -> ((location * edge) list * node) list
 
