cff154901736807bb3ae3ee78427620c20734cb1
Simmo Saan <simmo.saan@gmail.com>
2021-07-20 13:55:04 +0300

    Derive node eq & ord

--- src/framework/node.ml
+++ src/framework/node.ml
@@ -11,7 +11,7 @@ type node =
   (** *)
   | Function of CilType.Fundec.t
   (** The variable information associated with the function declaration. *)
-[@@deriving to_yojson]
+[@@deriving eq, ord, to_yojson]
 
 let write_cfgs : ((node -> bool) -> unit) ref = ref (fun _ -> ())
 
@@ -25,23 +25,6 @@ let pretty_short_node () = function
   | Statement s -> text "Statement @ " ++ d_loc () (get_stmtLoc s.skind)
   | Function f -> text "Function " ++ text f.svar.vname
   | FunctionEntry f -> text "FunctionEntry " ++ text f.svar.vname
-
-let compare_node n1 n2 =
-  match n1, n2 with
-  | FunctionEntry f, FunctionEntry g -> compare f.svar.vid g.svar.vid
-  | _                    , FunctionEntry g -> -1
-  | FunctionEntry g, _                     -> 1
-  | Statement _, Function _  -> -1
-  | Function  _, Statement _ -> 1
-  | Statement s, Statement l -> compare s.sid l.sid
-  | Function  f, Function g  -> compare f.svar.vid g.svar.vid
-
-let equal_node x y =
-  match x,y with
-  | Statement s1, Statement s2 -> CilType.Stmt.equal s1 s2
-  | Function f1, Function f2 -> CilType.Fundec.equal f1 f2
-  | FunctionEntry f1, FunctionEntry f2 -> CilType.Fundec.equal f1 f2
-  | _ -> false
 
 module Node :
 sig
