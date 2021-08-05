4e6858d98c1c167f6c8512db5c2eb59ed296fd68
Ralf Vogler <ralf.vogler@gmail.com>
2021-08-05 10:25:44 +0200

    link in comment that long long is just 64 bit

--- includes/arm64.h
+++ includes/arm64.h
@@ -4,6 +4,6 @@
 // which uses __uint128_t which is somehow not defined before.
 
 #if defined(__arm64__)
-  // typedef unsigned __int128 uint128_t; // https://stackoverflow.com/a/34588884 TODO __int128 exists in CIL but fails; see https://github.com/goblint/cil/issues/41
-  typedef unsigned long long __uint128_t; // TODO wrong! use the above once fixed. This the definition for __uint64_t
+  // typedef unsigned __int128 uint128_t; // https://stackoverflow.com/a/34588884 __int128 exists in gcc and in CIL but fails there; see https://github.com/goblint/cil/issues/41
+  typedef unsigned long long __uint128_t; // TODO Use the above once fixed! This is wrong since long long is just 64 bit. See https://github.com/goblint/cil/issues/41#issuecomment-893266843
 #endif
