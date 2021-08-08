7af05bdfc05e9eb4b7fb0c5af08550b5be9c33a2
Michael Schwarz <michael.schwarz93@gmail.com>
2021-08-08 15:52:32 +0200

    Have normal contexts in octApron

--- src/analyses/apron/octApron.apron.ml
+++ src/analyses/apron/octApron.apron.ml
@@ -21,7 +21,7 @@ struct
   let should_join = Priv.should_join
 
   let val_of x = x
-  let context x = if GobConfig.get_bool "exp.full-context" then x else D.bot ()
+  let context x = if GobConfig.get_bool "exp.addr-context" || GobConfig.get_bool "exp.no-int-context" then D.bot () else x
 
 
   let exitstate  _ = { oct = AD.bot (); priv = Priv.startstate () }
