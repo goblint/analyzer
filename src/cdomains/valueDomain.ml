2c51d90e856beb5fcfa33237717f729f9649d1fd
Simmo Saan <simmo.saan@gmail.com>
2021-08-17 15:11:05 +0300

    Add special case for joining int and thread in ValueDomain

--- src/cdomains/valueDomain.ml
+++ src/cdomains/valueDomain.ml
@@ -447,6 +447,7 @@ struct
     | (`List x, `List y) -> Lists.leq x y
     | (`Blob x, `Blob y) -> Blobs.leq x y
     | (`Thread x, `Thread y) -> Threads.leq x y
+    | (`Int x, `Thread y) -> true
     | _ -> warn_type "leq" x y; false
 
   let rec join x y =
@@ -472,6 +473,9 @@ struct
     | `Blob (x,s,o), y
     | y, `Blob (x,s,o) -> `Blob (join (x:t) y, s, o)
     | (`Thread x, `Thread y) -> `Thread (Threads.join x y)
+    | (`Int x, `Thread y)
+    | (`Thread y, `Int x) ->
+      `Thread y (* TODO: ignores int! *)
     | _ ->
       warn_type "join" x y;
       `Top
@@ -501,6 +505,9 @@ struct
     | y, `Blob (x,s,o) ->
       `Blob (join (x:t) y, s, o)
     | (`Thread x, `Thread y) -> `Thread (Threads.join x y)
+    | (`Int x, `Thread y)
+    | (`Thread y, `Int x) ->
+      `Thread y (* TODO: ignores int! *)
     | _ ->
       warn_type "join" x y;
       `Top
@@ -527,6 +534,9 @@ struct
     | (`List x, `List y) -> `List (Lists.widen x y) (* `List can not contain array -> normal widen  *)
     | (`Blob x, `Blob y) -> `Blob (Blobs.widen x y) (* `Blob can not contain array -> normal widen  *)
     | (`Thread x, `Thread y) -> `Thread (Threads.widen x y)
+    | (`Int x, `Thread y)
+    | (`Thread y, `Int x) ->
+      `Thread y (* TODO: ignores int! *)
     | _ ->
       warn_type "widen" x y;
       `Top
@@ -552,6 +562,7 @@ struct
     | (`List x, `List y) -> Lists.leq x y (* `List can not contain array -> normal leq  *)
     | (`Blob x, `Blob y) -> Blobs.leq x y (* `Blob can not contain array -> normal leq  *)
     | (`Thread x, `Thread y) -> Threads.leq x y
+    | (`Int x, `Thread y) -> true
     | _ -> warn_type "leq" x y; false
 
   let rec meet x y =
@@ -570,6 +581,9 @@ struct
     | (`List x, `List y) -> `List (Lists.meet x y)
     | (`Blob x, `Blob y) -> `Blob (Blobs.meet x y)
     | (`Thread x, `Thread y) -> `Thread (Threads.meet x y)
+    | (`Int x, `Thread y)
+    | (`Thread y, `Int x) ->
+      `Int x (* TODO: ignores thread! *)
     | _ ->
       warn_type "meet" x y;
       `Bot
@@ -595,6 +609,9 @@ struct
     | (`List x, `List y) -> `List (Lists.widen x y)
     | (`Blob x, `Blob y) -> `Blob (Blobs.widen x y)
     | (`Thread x, `Thread y) -> `Thread (Threads.widen x y)
+    | (`Int x, `Thread y)
+    | (`Thread y, `Int x) ->
+      `Thread y (* TODO: ignores int! *)
     | _ ->
       warn_type "widen" x y;
       `Top
@@ -611,6 +628,9 @@ struct
     | (`List x, `List y) -> `List (Lists.narrow x y)
     | (`Blob x, `Blob y) -> `Blob (Blobs.narrow x y)
     | (`Thread x, `Thread y) -> `Thread (Threads.narrow x y)
+    | (`Int x, `Thread y)
+    | (`Thread y, `Int x) ->
+      `Int x (* TODO: ignores thread! *)
     | x, `Top | `Top, x -> x
     | x, `Bot | `Bot, x -> `Bot
     | _ ->
