d05a076b61d4e3cfc8382b6eba3aa0588cccd0e2
Simmo Saan <simmo.saan@gmail.com>
2021-08-17 18:55:48 +0300

    Add thread IDs with creation history list

--- src/cdomains/threadIdDomain.ml
+++ src/cdomains/threadIdDomain.ml
@@ -12,7 +12,7 @@ sig
 end
 
 (** Type to represent an abstract thread ID. *)
-module FunLoc: S =
+module FunLoc: S with type t = varinfo * location option =
 struct
   module M = Printable.Prod (CilType.Varinfo) (Printable.Option (CilType.Location) (struct let name = "no location" end))
   include M
@@ -40,6 +40,23 @@ struct
     | _ -> false
 end
 
+module FunLocHistory: S =
+struct
+  module M = Printable.Liszt (FunLoc)
+  include M
+
+  let start_thread v = [(v, None)]
+  let spawn_thread current l v = (v, Some l) :: current (* reversed storage is more efficient *)
+
+  let to_varinfo: t -> varinfo =
+    let module RichVarinfoM = RichVarinfo.Make (M) in
+    RichVarinfoM.map ~name:show ~size:113
+
+  let is_main = function
+    | [fl] when FunLoc.is_main fl -> true
+    | _ -> false
+end
+
 module ThreadLiftNames = struct
   let bot_name = "Bot Threads"
   let top_name = "Top Threads"
@@ -50,5 +67,5 @@ struct
   let name () = "Thread"
 end
 
-module Thread = FunLoc (* TODO: make dynamically switchable? *)
+module Thread = FunLocHistory (* TODO: make dynamically switchable? *)
 module ThreadLifted = Lift (Thread)
