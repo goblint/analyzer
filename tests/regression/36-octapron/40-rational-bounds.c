40213327904c72b7f3ef7bf367c1ab343afe7d62
Simmo Saan <simmo.saan@gmail.com>
2021-08-18 12:09:02 +0300

    Add rational bounds rounding to octApron (issue #324)

--- tests/regression/36-octapron/40-rational-bounds.c
+++ tests/regression/36-octapron/40-rational-bounds.c
@@ -3,7 +3,12 @@
 
 void main() {
   int x;
-  if (x <= 10) {
-    assert((x / 3) <= 3);
+  {
+//   if (1 <= x) {
+//   if (-10 <= x) {
+    if (x <= 10) {
+      assert((x / 3) <= 4);
+      assert((x / 3) <= 3); // TODO: why does apron think the upper bound is 13/3 if no (positive) lower bound is known?
+    }
   }
 }
