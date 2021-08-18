c66d1322f44575107fd2b0d42760ab0c73946659
Simmo Saan <simmo.saan@gmail.com>
2021-08-18 16:54:36 +0300

    Fix octApron tests to not contain overflows

--- tests/regression/36-octapron/22-traces-write-centered-vs-meet-mutex.c
+++ tests/regression/36-octapron/22-traces-write-centered-vs-meet-mutex.c
@@ -11,40 +11,43 @@ pthread_mutex_t C = PTHREAD_MUTEX_INITIALIZER;
 
 void *t_fun(void *arg) {
   int x, y, z;
-  pthread_mutex_lock(&C);
-  pthread_mutex_lock(&A);
-  x = g;
-  y = h;
-  assert(y <= x);
-  pthread_mutex_lock(&B);
-  assert(x == y); // TODO (mutex-meet succeeds, write unknown)
-  pthread_mutex_unlock(&B);
-  i = x + 31;
-  z = i;
-  assert(z >= x); // TODO (write succeeds, mutex-meet unknown)
-  pthread_mutex_unlock(&A);
-  pthread_mutex_unlock(&C);
+  if (x < 1000) { // avoid overflow
+    pthread_mutex_lock(&C);
+    pthread_mutex_lock(&A);
+    x = g;
+    y = h;
+    assert(y <= x);
+    pthread_mutex_lock(&B);
+    assert(x == y); // TODO (mutex-meet succeeds, write unknown)
+    pthread_mutex_unlock(&B);
+    i = x + 31;
+    z = i;
+    assert(z >= x); // TODO (write succeeds, mutex-meet unknown)
+    pthread_mutex_unlock(&A);
+    pthread_mutex_unlock(&C);
+  }
   return NULL;
 }
 
 int main(void) {
   int x; // rand
+  if (x > -1000) { // avoid underflow
+    pthread_t id;
+    pthread_create(&id, NULL, t_fun, NULL);
 
-  pthread_t id;
-  pthread_create(&id, NULL, t_fun, NULL);
-
-  pthread_mutex_lock(&B);
-  pthread_mutex_lock(&A);
-  i = 11;
-  g = x;
-  h = x - 17;
-  pthread_mutex_unlock(&A);
-  pthread_mutex_lock(&A);
-  h = x;
-  pthread_mutex_unlock(&A);
-  pthread_mutex_unlock(&B);
-  pthread_mutex_lock(&C);
-  i = 3;
-  pthread_mutex_unlock(&C);
+    pthread_mutex_lock(&B);
+    pthread_mutex_lock(&A);
+    i = 11;
+    g = x;
+    h = x - 17;
+    pthread_mutex_unlock(&A);
+    pthread_mutex_lock(&A);
+    h = x;
+    pthread_mutex_unlock(&A);
+    pthread_mutex_unlock(&B);
+    pthread_mutex_lock(&C);
+    i = 3;
+    pthread_mutex_unlock(&C);
+  }
   return 0;
 }
--- tests/regression/36-octapron/28-combine-forget-fun-lval-imprecise.c
+++ tests/regression/36-octapron/28-combine-forget-fun-lval-imprecise.c
@@ -7,30 +7,31 @@ int f(int x) {
 
 int main(void) {
   int y, z;
+  if (y < 1000) { // avoid overflow
+    z = y;
+    y = f(y);
 
-  z = y;
-  y = f(y);
+    // OLD:
+    // local is: y == z
+    // fun is: #ret == x' + 1
+    // fun args subst (x' -> y) is: #ret == y + 1
+    // local forget y is: top
+    // fun forget y is: top
+    // fun subst (#ret -> y) is: top
+    // unify is: top
 
-  // OLD:
-  // local is: y == z
-  // fun is: #ret == x' + 1
-  // fun args subst (x' -> y) is: #ret == y + 1
-  // local forget y is: top
-  // fun forget y is: top
-  // fun subst (#ret -> y) is: top
-  // unify is: top
+    // WANT/NEW:
+    // local is: y == z
+    // fun is: #ret == x' + 1
+    // fun args subst (x' -> y) is: #ret == y + 1
+    // unify is: y == z && #ret == y + 1 (&& #ret == z + 1)
+    // assign (y = #ret) is: (internally)
+      // 1. y == z && #ret == y + 1 && y#new == #ret (&& #ret == z + 1 && y#new == z + 1)
+      // 2. y#new == #ret && #ret == z + 1 (&& y#new == z + 1)
+      // 3. y == #ret && #ret == z + 1 (&& y == z + 1)
+    // forget #ret is: y == z + 1
 
-  // WANT/NEW:
-  // local is: y == z
-  // fun is: #ret == x' + 1
-  // fun args subst (x' -> y) is: #ret == y + 1
-  // unify is: y == z && #ret == y + 1 (&& #ret == z + 1)
-  // assign (y = #ret) is: (internally)
-    // 1. y == z && #ret == y + 1 && y#new == #ret (&& #ret == z + 1 && y#new == z + 1)
-    // 2. y#new == #ret && #ret == z + 1 (&& y#new == z + 1)
-    // 3. y == #ret && #ret == z + 1 (&& y == z + 1)
-  // forget #ret is: y == z + 1
-
-  assert(y == z + 1);
+    assert(y == z + 1);
+  }
   return 0;
 }
