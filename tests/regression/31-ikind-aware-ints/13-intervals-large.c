13e48ec2df49c90416926c86d393844e15a5c41b
Simmo Saan <simmo.saan@gmail.com>
2021-08-20 13:49:33 +0300

    Fix comments about int wrap around

--- tests/regression/31-ikind-aware-ints/13-intervals-large.c
+++ tests/regression/31-ikind-aware-ints/13-intervals-large.c
@@ -12,7 +12,6 @@ int main(){
     assert(a);
 
     unsigned long long y = x + 4;
-    // Unsigned overflow -- The following assertion should succeed, but is unknown for now
     assert(y == 3);
 
     // maximum value for long long
