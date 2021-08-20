22c37104ef5857b865966c2becb23e95b89c7c45
Simmo Saan <simmo.saan@gmail.com>
2021-08-20 15:14:50 +0300

    Add thread uniqueness test relying on thread ID history

---
+++ tests/regression/39-threadid/01-threadid_history_unique.c
@@ -0,0 +1,17 @@
+// PARAM: --sets ana.activated[-] thread
+#include <pthread.h>
+#include <stdio.h>
+
+int myglobal;
+
+void *t_fun(void *arg) {
+  myglobal=40; // NORACE
+  return NULL;
+}
+
+int main(void) {
+  pthread_t id;
+  pthread_create(&id, NULL, t_fun, NULL);
+  pthread_join (id, NULL);
+  return 0;
+}
