85dcc516540142d40f6a0fcb45028dc70780821c
Simmo Saan <simmo.saan@gmail.com>
2021-08-25 10:55:54 +0300

    Update TODO comment about reachable from thread

--- src/analyses/base.ml
+++ src/analyses/base.ml
@@ -375,7 +375,7 @@ struct
     | `List e -> reachable_from_value ask gs st (`Address (ValueDomain.Lists.entry_rand e)) t description
     | `Struct s -> ValueDomain.Structs.fold (fun k v acc -> AD.join (reachable_from_value ask gs st v t description) acc) s empty
     | `Int _ -> empty
-    | `Thread _ -> empty (* TODO: is this right? *)
+    | `Thread _ -> empty (* thread IDs are abstract and nothing known can be reached from them *)
 
   (* Get the list of addresses accessable immediately from a given address, thus
    * all pointers within a structure should be considered, but we don't follow
