2206b64f12dd77bcbef8b6aff6552c33f7dcc304
Simmo Saan <simmo.saan@gmail.com>
2021-08-25 14:05:21 +0300

    Rename octApron to apron in tests

---
+++ tests/regression/36-apron/01-octagon_simple.c
@@ -0,0 +1,22 @@
+// SKIP PARAM: --sets solver td3 --enable ana.int.interval  --enable exp.partition-arrays.enabled  --set ana.activated "['base','threadid','threadflag','expRelation','mallocWrapper','apron']" --sets exp.privatization none --sets exp.apron.privatization dummy
+// Example from https://www-apr.lip6.fr/~mine/publi/article-mine-HOSC06.pdf
+void main(void) {
+  int X = 0;
+  int N = rand();
+  if(N < 0) { N = 0; }
+
+  while(X < N) {
+    X++;
+  }
+
+  assert(X-N == 0);
+  assert(X == N);
+
+  if(X == N) {
+    N = 8;
+  } else {
+    // is dead code but if that is detected or not depends on what we do in branch
+    // currenlty we can't detect this
+    N = 42;
+  }
+}
---
+++ tests/regression/36-apron/61-branched.c
@@ -0,0 +1,24 @@
+// SKIP PARAM: --sets ana.activated[+] apron --enable ana.int.interval
+#include<pthread.h>
+int global = 0;
+
+void *t_fun(void *arg)
+{
+    global = 5;
+    assert(1);
+}
+
+int main(void)
+{
+    pthread_t t;
+    int i;
+
+    if(i < 1) {
+        pthread_create(&t, ((void *)0), t_fun, ((void *)0));
+        assert(global == 0); //UNKNOWN!
+        i++;
+    }
+
+    assert(global == 0); //UNKNOWN!
+    return 0;
+}
