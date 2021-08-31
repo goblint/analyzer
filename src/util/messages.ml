064cd0a406f82048607f0cb1880cb0b9c39037dd
Simmo Saan <simmo.saan@gmail.com>
2021-08-31 15:07:42 +0300

    Move Format ANSI color tags to GobFormat

--- src/util/messages.ml
+++ src/util/messages.ml
@@ -158,26 +158,7 @@ let print ?(out= !warn_out) (m: Message.t) =
     | Success -> "green"
   in
   let ppf = Format.formatter_of_out_channel out in
-  (* TODO: extract this elsewhere *)
-  let stag_functions = Format.pp_get_formatter_stag_functions ppf () in
-  let stag_functions' = {stag_functions with
-      mark_open_stag = (function
-        (* TODO: support all colors like colorize *)
-        | Format.String_tag "red" -> Format.sprintf "\027[%sm" "0;31"
-        | Format.String_tag "yellow" -> Format.sprintf "\027[%sm" "0;33"
-        | Format.String_tag "blue" -> Format.sprintf "\027[%sm" "0;34"
-        | Format.String_tag "white" -> Format.sprintf "\027[%sm" "0;37"
-        | Format.String_tag "green" -> Format.sprintf "\027[%sm" "0;32"
-        | Format.String_tag "violet" -> Format.sprintf "\027[%sm" "0;35"
-        | Format.String_tag s -> Format.sprintf "{%s}" s
-        | _ -> "");
-      mark_close_stag = (function
-        | Format.String_tag _ -> "\027[0m"
-        | _ -> "");
-    }
-  in
-  Format.pp_set_formatter_stag_functions ppf stag_functions';
-  Format.pp_set_mark_tags ppf true;
+  GobFormat.pp_set_ansi_color_tags ppf;
   let pp_prefix = Format.dprintf "@{<%s>[%s]%s@}" severity_stag (Severity.show m.severity) (Tags.show m.tags) in
   let pp_piece ppf piece =
     Format.fprintf ppf "@{<%s>%s@} @{<violet>(%s)@}" severity_stag (Piece.show piece) (CilType.Location.show piece.print_loc)
