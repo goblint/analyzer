cbb66b72d185d99d31ce9371822d7cbd151aee7e
Simmo Saan <simmo.saan@gmail.com>
2021-09-10 16:40:52 +0300

    Fix indentation in 50-juliet/06

--- tests/regression/50-juliet/06-CWE191_Integer_Underflow__03.c
+++ tests/regression/50-juliet/06-CWE191_Integer_Underflow__03.c
@@ -6,12 +6,12 @@ void main()
     char data;
     for (int i = 0; i < 100; i++)
     {
-    	// value of data is pseudo-random
-    	data = rand();
-    	if(data < 0) // avoid potential overflow
-    	{
-	    char result = data * 2; // TODO WARN: potential underflow
-	    printf("%hhd\n", result);
-    	}
+        // value of data is pseudo-random
+        data = rand();
+        if(data < 0) // avoid potential overflow
+        {
+            char result = data * 2; // TODO WARN: potential underflow
+            printf("%hhd\n", result);
+        }
     }
 }
