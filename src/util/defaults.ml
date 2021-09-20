c5252d368b8acef7cc5835e259d2dd652c722c17
Simmo Saan <simmo.saan@gmail.com>
2021-09-20 11:17:25 +0300

    Add missing warn.analyzer option

--- src/util/defaults.ml
+++ src/util/defaults.ml
@@ -243,6 +243,7 @@ let _ = ()
       ; reg Warnings "warn.cast"            "true"  "Cast (Type_mismatch(bug) warnings"
       ; reg Warnings "warn.race"            "true"  "Race warnings"
       ; reg Warnings "warn.array"           "true"  "Array (Out_of_bounds of int*int) warnings"
+      ; reg Warnings "warn.analyzer"        "true"  "Analyzer messages"
       ; reg Warnings "warn.unknown"         "true"  "Unknown (of string) warnings"
       ; reg Warnings "warn.error"           "true"  "Error severity messages"
       ; reg Warnings "warn.warning"         "true"  "Warning severity messages"
