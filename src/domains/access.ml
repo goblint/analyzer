ba468f4ff479cafa16b6ebe13d19d0dcf58d278f
Simmo Saan <simmo.saan@gmail.com>
2021-09-20 12:01:25 +0300

    Use race category for old group warnings

--- src/domains/access.ml
+++ src/domains/access.ml
@@ -510,7 +510,7 @@ let print_races_oldscool () =
         sprint 80 (dprintf "Datarace at %a" d_memo (ty,lv))
     in
     if not safe || allglobs then
-      Messages.warn_group_old groupname xs
+      Messages.msg_group_race_old groupname xs
   in
   let f ty = LvalOptHash.iter (h ty) in
   TypeHash.iter f accs
