82f6dfcf3afb844998e7db1362a4783e56bec301
Simmo Saan <simmo.saan@gmail.com>
2021-09-20 12:55:41 +0300

    Add CWE to dead branch warnings

--- src/framework/control.ml
+++ src/framework/control.ml
@@ -131,8 +131,8 @@ struct
     let report tv (loc, dead) =
       if Deadcode.Locmap.mem dead_locations loc then
         match dead, Deadcode.Locmap.find_option Deadcode.dead_branches_cond loc with
-        | true, Some exp -> M.warn ~loc ~category:Deadcode "the %s branch over expression '%a' is dead" (str tv) d_exp exp
-        | true, None     -> M.warn ~loc ~category:Deadcode "an %s branch is dead" (str tv)
+        | true, Some exp -> M.warn ~loc ~category:Deadcode ~tags:[CWE (if tv then 570 else 571)] "the %s branch over expression '%a' is dead" (str tv) d_exp exp
+        | true, None     -> M.warn ~loc ~category:Deadcode ~tags:[CWE (if tv then 570 else 571)] "an %s branch is dead" (str tv)
         | _ -> ()
     in
     if get_bool "dbg.print_dead_code" then (
