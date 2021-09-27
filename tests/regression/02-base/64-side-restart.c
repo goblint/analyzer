75503af6f30e487ea08206e65bbc19f07d20f599
Simmo Saan <simmo.saan@gmail.com>
2021-09-27 14:07:41 +0300

    Add side restart example

---
+++ tests/regression/02-base/64-side-restart.c
@@ -0,0 +1,26 @@
+// PARAM: --enable ana.int.interval
+#include <pthread.h>
+#include <assert.h>
+
+int g;
+
+void* t_fun(void *arg) {
+    int x = g;
+    assert(x <= 8);
+    return NULL;
+}
+
+int main() {
+    pthread_t id;
+    pthread_create(&id, NULL, t_fun, NULL);
+
+    int i = 0;
+    int j;
+
+    for (j = 1; j < 10; j++) {
+        for (i = 0; i < j; i++) {
+            g = i;
+        }
+    }
+    return 0;
+}
