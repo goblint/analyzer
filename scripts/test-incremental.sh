e8919b845e2c2f5b6a792f0e249b8c4c00435424
Simmo Saan <simmo.saan@gmail.com>
2021-09-29 11:11:34 +0300

    Add incremental test script

---
+++ scripts/test-incremental.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+test=$1
+
+base=./tests/incremental/
+source=$base$test.c
+conf=$base$test.json
+patch=$base$test.patch
+
+args="--enable dbg.debug --enable printstats -v"
+
+./goblint --conf $conf $args --enable incremental.save $source &> $base$test.before.log
+
+patch -b $source $patch
+
+./goblint --conf $conf $args --enable incremental.load $source &> $base$test.after.log
+
+patch -b -R $source $patch
