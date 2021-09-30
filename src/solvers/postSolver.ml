89a19fd50b18883a1f0cc6f7ee3305e9ef615535
Simmo Saan <simmo.saan@gmail.com>
2021-09-30 15:56:02 +0300

    Implement top-down verify in PostSolver (issue #367)

--- src/solvers/postSolver.ml
+++ src/solvers/postSolver.ml
@@ -1,5 +1,6 @@
 open Prelude
 open Analyses
+open GobConfig
 
 module Post (S: EqConstrSys) (VH: Hash.H with type key = S.v) =
 struct
@@ -7,32 +8,59 @@ struct
     (* TODO: reachability/verify should do something with xs as well? *)
     ignore (Pretty.printf "Post solver\n");
 
+    let should_verify = get_bool "verify" in
+    Goblintutil.in_verifying_stage := true;
+    (if should_verify then Goblintutil.verified := Some true);
+
+    let complain_constraint x ~lhs ~rhs =
+      Goblintutil.verified := Some false;
+      ignore (Pretty.printf "Fixpoint not reached at %a\n @[Solver computed:\n%a\nRight-Hand-Side:\n%a\nDifference: %a\n@]" S.Var.pretty_trace x S.Dom.pretty lhs S.Dom.pretty rhs S.Dom.pretty_diff (rhs, lhs))
+    in
+    let complain_side x y ~lhs ~rhs =
+      Goblintutil.verified := Some false;
+      ignore (Pretty.printf "Fixpoint not reached at %a\nOrigin: %a\n @[Solver computed:\n%a\nSide-effect:\n%a\nDifference: %a\n@]" S.Var.pretty_trace y S.Var.pretty_trace x S.Dom.pretty lhs S.Dom.pretty rhs S.Dom.pretty_diff (rhs, lhs))
+    in
+
     let reachability xs =
       let reachable = VH.create (VH.length vh) in
       let rec one_var x =
         if not (VH.mem reachable x) then (
           VH.replace reachable x ();
-          Option.may one_constraint (S.system x)
+          Option.may (one_constraint x) (S.system x)
         )
-      and one_constraint f =
-        let get x =
-          one_var x;
-          try VH.find vh x with Not_found -> S.Dom.bot ()
+      and one_constraint x f =
+        let get y =
+          one_var y;
+          try VH.find vh y with Not_found -> S.Dom.bot ()
         in
-        let set x _ =
-          one_var x
+        let set y d =
+          if should_verify then (
+            let y_lhs = try VH.find vh y with Not_found -> S.Dom.bot () in
+            if not (S.Dom.leq d y_lhs) then
+              complain_side x y ~lhs:y_lhs ~rhs:d
+          );
+          (* check before recursing *)
+          one_var y
         in
-        ignore (f get set)
+        let rhs = f get set in
+        if should_verify then (
+          let lhs = try VH.find vh x with Not_found -> S.Dom.bot () in
+          if not (S.Dom.leq rhs lhs) then
+            complain_constraint x ~lhs ~rhs
+        )
       in
       List.iter one_var xs;
       (* TODO: expose VH.filteri_inplace *)
 
+      (* TODO: should_remove option *)
       VH.iter (fun x _ ->
           if not (VH.mem reachable x) then
             VH.remove vh x
         ) vh
     in
     reachability vs;
+
+    Goblintutil.in_verifying_stage := false
 
   (* TODO: add (combined) timing *)
 end
