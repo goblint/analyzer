615638f47d00cf9d3676ff79c468a524e49af8c2
Simmo Saan <simmo.saan@gmail.com>
2021-09-30 16:29:04 +0300

    Remove unused Hash.S

--- src/util/hash.ml
+++ src/util/hash.ml
@@ -1,23 +1,5 @@
 (** Minimal signature for hashtables. *)
 
-module type S =
-sig
-  type key
-  type 'a t
-  val create: int -> 'a -> 'a t
-  val clear: 'a t -> unit
-  val copy: 'a t -> 'a t
-  val add: 'a t -> key -> 'a -> unit
-  val remove: 'a t -> key -> unit
-  val find: 'a t -> key -> 'a
-  val find_all: 'a t -> key -> 'a list
-  val replace : 'a t -> key -> 'a -> unit
-  val mem : 'a t -> key -> bool
-  val iter: (key -> 'a -> unit) -> 'a t -> unit
-  val fold: (key -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
-  val length: 'a t -> int
-end
-
 module type H =
 sig
   type key
