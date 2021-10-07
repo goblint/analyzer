1595e1c7caba4218083b0a75eafacdb2b88b5b7f
stilscher <66023521+stilscher@users.noreply.github.com>
2021-10-07 14:36:48 +0200

    add test example for not overwriting start states of start functions

---
+++ tests/incremental/01-sideeffects.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+
+int g = 1;
+
+void f() {
+    g = 2;
+}
+
+int main() {
+    f();
+    assert(g == 2); // unknown! before, unknown! after
+    assert(g == 1); // unknown! before, unknown! after (when wrongly overriding the start state of start functions this did succeed in the incremental run)
+    return 0;
+}
