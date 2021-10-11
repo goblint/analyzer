c9a815524972009c1655f44bbe1a796a7f6f1257
Julian Erhard <julian.erhard@tum.de>
2021-10-11 15:41:25 +0200

    Add relift for Printable.Either, Prod3, Liszt, LiftBot, LiftTop

--- src/domains/printable.ml
+++ src/domains/printable.ml
@@ -222,7 +222,7 @@ struct
 
   let relift x = match x with
     | `Bot |`Top -> x
-    | `Lifted v -> `Lifted (relift v)
+    | `Lifted v -> `Lifted (Base.relift v)
 
   let arbitrary () =
     let open QCheck.Iter in
@@ -266,6 +266,10 @@ struct
   let to_yojson = function
     | `Left x -> `Assoc [ Base1.name (), Base1.to_yojson x ]
     | `Right x -> `Assoc [ Base2.name (), Base2.to_yojson x ]
+
+  let relift = function
+    | `Left x -> `Left (Base1.relift x)
+    | `Right x -> `Right (Base2.relift x)
 end
 
 module Option (Base: S) (N: Name) =
@@ -296,6 +300,8 @@ struct
   let to_yojson = function
     | None -> `String N.name
     | Some x -> Base.to_yojson x
+
+  let relift = Option.map Base.relift
 end
 
 module Lift2 (Base1: S) (Base2: S) (N: LiftingNames) =
@@ -428,6 +434,7 @@ struct
 
   let name () = Base1.name () ^ " * " ^ Base2.name () ^ " * " ^ Base3.name ()
 
+  let relift (x,y,z) = (Base1.relift x, Base2.relift y, Base3.relift z)
   let invariant c (x, y, z) = Invariant.(Base1.invariant c x && Base2.invariant c y && Base3.invariant c z)
   let arbitrary () = QCheck.triple (Base1.arbitrary ()) (Base2.arbitrary ()) (Base3.arbitrary ())
 end
@@ -443,6 +450,8 @@ struct
     "[" ^ (String.concat ", " elems) ^ "]"
 
   let pretty () x = text (show x)
+
+  let relift x = List.map Base.relift x
 
   let name () = Base.name () ^ " list"
   let printXml f xs =
@@ -506,6 +515,10 @@ struct
   let to_yojson = function
     | `Bot -> `String "⊥"
     | `Lifted n -> Base.to_yojson n
+
+  let relift = function
+    | `Bot -> `Bot
+    | `Lifted n -> `Lifted (Base.relift n)
 end
 
 module LiftTop (Base : S) =
@@ -538,6 +551,10 @@ struct
   let to_yojson = function
     | `Top -> `String "⊤"
     | `Lifted n -> Base.to_yojson n
+
+  let relift = function
+    | `Top -> `Top
+    | `Lifted n -> `Lifted (Base.relift n)
 
   let arbitrary () =
     let open QCheck.Iter in
