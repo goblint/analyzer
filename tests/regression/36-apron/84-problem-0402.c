b5a13b65fb04d1db176456626bf117ae64a680c9
Michael Schwarz <michael.schwarz93@gmail.com>
2021-10-12 18:28:15 +0200

    Add problem

---
+++ tests/regression/36-apron/84-problem-0402.c
@@ -0,0 +1,23 @@
+// SKIP PARAM: --set ana.activated[+] apron --sets exp.apron.privatization mutex-meet-tid --set ana.activated[-] threadJoins
+#include <pthread.h>
+#include <stdio.h>
+
+int myglobal;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+void *t_fun(void *arg) {
+  pthread_mutex_lock(&mutex1);
+  myglobal=myglobal+1; // NORACE
+  pthread_mutex_unlock(&mutex1);
+  return NULL;
+}
+
+int main(void) {
+  pthread_t id;
+  pthread_create(&id, NULL, t_fun, NULL);
+  pthread_mutex_lock(&mutex1);
+  myglobal=myglobal+1; // NORACE
+  pthread_mutex_unlock(&mutex1);
+  return 0;
+}
