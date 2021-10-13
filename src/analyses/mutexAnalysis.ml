48e0cd65584f24e46e36e993ca5d109f927620f5
Simmo Saan <simmo.saan@gmail.com>
2021-10-13 13:06:50 +0300

    Remove race access logic from mutex analysis

--- src/analyses/mutexAnalysis.ml
+++ src/analyses/mutexAnalysis.ml
@@ -1,4 +1,4 @@
-(** Data race analysis. *)
+(** Mutex analysis. *)
 
 module M = Messages
 module GU = Goblintutil
@@ -28,7 +28,7 @@ sig
   val check_fun: ?write:bool -> Lockset.t -> G.t
 end
 
-(** Data race analyzer without base --- this is the new standard *)
+(** Mutex analyzer without base --- this is the new standard *)
 module MakeSpec (P: SpecParam) =
 struct
   include Analyses.DefaultSpec
@@ -60,6 +60,7 @@ struct
     (* TODO: better indices handling *)
     | `Index (_, o) -> `Index (MyCFG.unknown_exp, conv_offset_inv o)
 
+  (* TODO: unused *)
   let rec conv_const_offset x =
     match x with
     | NoOffset    -> `NoOffset
@@ -67,6 +68,7 @@ struct
     | Index (_,o) -> `Index (ValueDomain.IndexDomain.top (), conv_const_offset o)
     | Field (f,o) -> `Field (f, conv_const_offset o)
 
+  (* TODO: unused *)
   let rec replace_elem (v,o) q ex =
     match ex with
     | AddrOf  (Mem e,_) when Basetype.CilExp.equal e q ->v, Offs.from_offset (conv_offset o)
@@ -136,81 +138,9 @@ struct
       end
     | _ -> Lockset.top ()
 
+  (* TODO: unused *)
   let arinc_analysis_activated = ref false
 
-  let do_access (ctx: (D.t, G.t, C.t) ctx) (w:bool) (reach:bool) (conf:int) (e:exp) =
-    let open Queries in
-    let part_access ctx (e:exp) (vo:varinfo option) (w: bool) =
-      (*privatization*)
-      begin match vo with
-        | Some v ->
-          if not (Lockset.is_bot ctx.local) then
-            let ls = Lockset.filter snd ctx.local in
-            let el = P.effect_fun ~write:w ls in
-            ctx.sideg v el
-        | None -> M.warn "Write to unknown address: privatization is unsound."
-      end;
-
-      (*partitions & locks*)
-      ctx.ask (PartAccess {exp=e; var_opt=vo; write=w})
-    in
-    let add_access conf vo oo =
-      let (po,pd) = part_access ctx e vo w in
-      Access.add e w conf vo oo (po,pd)
-    in
-    let add_access_struct conf ci =
-      let (po,pd) = part_access ctx e None w in
-      Access.add_struct e w conf (`Struct (ci,`NoOffset)) None (po,pd)
-    in
-    let has_escaped g = ctx.ask (Queries.MayEscape g) in
-    (* The following function adds accesses to the lval-set ls
-       -- this is the common case if we have a sound points-to set. *)
-    let on_lvals ls includes_uk =
-      let ls = LS.filter (fun (g,_) -> g.vglob || has_escaped g) ls in
-      let conf = if reach then conf - 20 else conf in
-      let conf = if includes_uk then conf - 10 else conf in
-      let f (var, offs) =
-        let coffs = Lval.CilLval.to_ciloffs offs in
-        if CilType.Varinfo.equal var dummyFunDec.svar then
-          add_access conf None (Some coffs)
-        else
-          add_access conf (Some var) (Some coffs)
-      in
-      LS.iter f ls
-    in
-    let reach_or_mpt = if reach then ReachableFrom e else MayPointTo e in
-    match ctx.ask reach_or_mpt with
-    | ls when not (LS.is_top ls) && not (Queries.LS.mem (dummyFunDec.svar,`NoOffset) ls) ->
-      (* the case where the points-to set is non top and does not contain unknown values *)
-      on_lvals ls false
-    | ls when not (LS.is_top ls) ->
-      (* the case where the points-to set is non top and contains unknown values *)
-      let includes_uk = ref false in
-      (* now we need to access all fields that might be pointed to: is this correct? *)
-      begin match ctx.ask (ReachableUkTypes e) with
-        | ts when Queries.TS.is_top ts ->
-          includes_uk := true
-        | ts ->
-          if Queries.TS.is_empty ts = false then
-            includes_uk := true;
-          let f = function
-            | TComp (ci, _) ->
-              add_access_struct (conf - 50) ci
-            | _ -> ()
-          in
-          Queries.TS.iter f ts
-      end;
-      on_lvals ls !includes_uk
-    | _ ->
-      add_access (conf - 60) None None
-
-  let access_one_top ?(force=false) ctx write reach exp =
-    (* ignore (Pretty.printf "access_one_top %b %b %a:\n" write reach d_exp exp); *)
-    if force || ThreadFlag.is_multi (Analyses.ask_of_ctx ctx) then (
-      let conf = 110 in
-      if reach || write then do_access ctx write reach conf exp;
-      Access.distribute_access_exp (do_access ctx) false false conf exp;
-    )
 
   (** We just lift start state, global and dependency functions: *)
   let startstate v = Lockset.empty ()
@@ -267,22 +197,12 @@ struct
   (** Transfer functions: *)
 
   let assign ctx lval rval : D.t =
-    (* ignore global inits *)
-    if !GU.global_initialization then ctx.local else begin
-      access_one_top ctx true  false (AddrOf lval);
-      access_one_top ctx false false rval;
-      ctx.local
-    end
+    ctx.local
 
   let branch ctx exp tv : D.t =
-    access_one_top ctx false false exp;
     ctx.local
 
   let return ctx exp fundec : D.t =
-    begin match exp with
-      | Some exp -> access_one_top ctx false false exp
-      | None -> ()
-    end;
     (* deprecated but still valid SV-COMP convention for atomic block *)
     if get_bool "ana.sv-comp.functions" && String.starts_with fundec.svar.vname "__VERIFIER_atomic_" then (
       ctx.emit (Events.Unlock verifier_atomic);
@@ -377,37 +297,16 @@ struct
         ) ms;
       raise Deadcode (* splits cover all cases *)
     | _, x ->
-      let arg_acc act =
-        match LF.get_threadsafe_inv_ac x with
-        | Some fnc -> (fnc act arglist)
-        | _ -> arglist
-      in
-      List.iter (access_one_top ctx false true) (arg_acc `Read);
-      List.iter (access_one_top ctx true  true ) (arg_acc `Write);
-      (match lv with
-      | Some x -> access_one_top ctx true false (AddrOf x)
-      | None -> ());
       ctx.local
 
   let enter ctx lv f args : (D.t * D.t) list =
     [(ctx.local,ctx.local)]
 
   let combine ctx lv fexp f args fc al =
-    access_one_top ctx false false fexp;
-    begin match lv with
-      | None      -> ()
-      | Some lval -> access_one_top ctx true false (AddrOf lval)
-    end;
-    List.iter (access_one_top ctx false false) args;
     al
 
 
   let threadspawn ctx lval f args fctx =
-    (* must explicitly access thread ID lval because special to pthread_create doesn't if singlethreaded before *)
-    begin match lval with
-    | None -> ()
-    | Some lval -> access_one_top ~force:true ctx true false (AddrOf lval) (* must force because otherwise doesn't if singlethreaded before *)
-    end;
     ctx.local
 
   let init marshal =
