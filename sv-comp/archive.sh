0d8edfc95ad31aae2c030cf2aa78394c4d1d80bb
Simmo Saan <simmo.saan@gmail.com>
2021-10-14 18:22:25 +0300

    Update conf name in sv-comp/archive.sh

--- sv-comp/archive.sh
+++ sv-comp/archive.sh
@@ -10,7 +10,7 @@ rm goblint/sv-comp/goblint.zip
 
 zip goblint/sv-comp/goblint.zip \
     goblint/goblint \
-    goblint/conf/svcomp21.json \
+    goblint/conf/svcomp22.json \
     goblint/includes/sv-comp.c \
     goblint/README.md \
     goblint/LICENSE
