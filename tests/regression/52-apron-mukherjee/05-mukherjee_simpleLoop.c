691be385371d514564d7e3a4c6b56960bef51354
Michael Schwarz <michael.schwarz93@gmail.com>
2021-11-04 16:09:01 +0100

    52/05 assume no ov

--- tests/regression/52-apron-mukherjee/05-mukherjee_simpleLoop.c
+++ tests/regression/52-apron-mukherjee/05-mukherjee_simpleLoop.c
@@ -1,4 +1,4 @@
-// SKIP PARAM: --set ana.activated[+] apron --set ana.path_sens[+] threadflag --set ana.activated[+] threadJoins
+// SKIP PARAM: --set ana.activated[+] apron --set ana.path_sens[+] threadflag --set ana.activated[+] threadJoins --set sem.int.signed_overflow assume_none
 
 #include <pthread.h>
 #include "assert.h"
