9270d5b74378a31c992a7cc5c77cfcb127dcdd8f
Julian Erhard <julian.erhard@tum.de>
2021-11-05 14:54:10 +0100

    Move PrePrivPrecCompare.ComparePrec to PrecCompare.MakeDump

--- src/privPrecCompare.ml
+++ src/privPrecCompare.ml
@@ -1,6 +1,6 @@
 (* Utility program to compare precision of results of the base analysis with different privatizations *)
 (* The result files that can be compared here may be created by passing an output file name in "exp.priv-prec-dump" to Goblint *)
-module A = PrePrivPrecCompare.ComparePrec (PrivPrecCompareUtil.Util)
+module A = PrecCompare.MakeDump (PrivPrecCompareUtil.Util)
 
 let () =
   A.main ()
