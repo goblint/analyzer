81de0f64d2f99580dd7403ee0f6c1d6c7526840f
Ivana Zuzic <ivana.zuzic@tum.de>
2021-11-05 17:18:24 +0100

    Temporary folder for tests

---
+++ tests/regression/98-callgraph/01-many_functions.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<assert.h>
+
+int x = 0;
+
+int g(int a) {
+  return a * a;
+}
+
+int f() {
+  return g(x) + 2;
+}
+
+void callme(void)  {
+  x = 5;
+  int y = f() + x;
+  assert(y != 32);
+}
+
+void callfun(void (*fun)()) {
+  fun();
+  return;
+}
+
+int main() {
+  callfun(& callme);
+  assert(x == 5);
+  int z = g(3);
+  return 0;
+}
