f8352f7b1bf3f7f960267f06adb41ad9b4f93f99
Ivana Zuzic <ivana.zuzic@tum.de>
2021-11-09 17:15:35 +0100

    Fixing indents, removing comments

--- src/util/callGraph.ml
+++ src/util/callGraph.ml
@@ -15,7 +15,6 @@ struct
     CGNH.clear callers
 
   let enter ctx lv f args =
-    (*let () = Printf.printf "<<<<ENTER IN CAllGRAPH>>>>\n" in*) 
     let r = S.enter ctx lv f args in
     begin
       try
@@ -28,17 +27,9 @@ struct
       with Ctx_failure _ ->
         ()
     end;
-    
-    (*CGNH.iter (fun callee callers' ->
-        Printf.printf "<<<<CALLEE: %d >>>>\n" (CGNode.hash callee);
-        CGNS.iter (fun caller ->
-            Printf.printf "<<<<CALLER :D :D :D: %d >>>>\n" (CGNode.hash caller)
-          ) callers'
-      ) callers;*)
     r
 
   let write_dot () =
-    (*let () = Printf.printf "<<<<WRITE IN CAllGRAPH>>>>\n" in *)
     let out = open_out "callgraph.dot" in
     Printf.fprintf out "digraph callgraph {\n";
 
@@ -46,7 +37,6 @@ struct
     let write_node cgnode =
       if not (CGNH.mem written_nodes cgnode) then (
         CGNH.replace written_nodes cgnode ();
-        (*let () = Printf.printf "<<<<NEW node IN CAllGRAPH: %d has a label %s(%d)>>>>\n" (CGNode.hash cgnode) ((fst cgnode).svar.vname) (S.C.hash (snd cgnode)) in*) 
         Printf.fprintf out "\t%d [label=\"%s(%d)\"];\n" (CGNode.hash cgnode) ((fst cgnode).svar.vname) (S.C.hash (snd cgnode))
       )
     in
@@ -58,21 +48,19 @@ struct
 
     CGNH.iter (fun callee callers' ->
         CGNS.iter (fun caller ->
-            (*let () = Printf.printf "<<<<NEW edge IN CAllGRAPH: %d -> %d>>>>\n" (CGNode.hash caller) (CGNode.hash callee) in *)
             Printf.fprintf out "\t%d -> %d;\n" (CGNode.hash caller) (CGNode.hash callee)
           ) callers'
       ) callers;
 
     Printf.fprintf out "}\n";
-    (*let () = List.iter (fun (a, b) -> let text:string = CGNode.show a in Printf.printf "<<<<WRITTEN NODE: %s>>>>\n" text) (CGNH.to_list written_nodes) in *)
 
     close_out_noerr out
 
   let update_config fundec context =
     let performed_update = 
-        match Hashtbl.find_opt PrecisionUtil.function_config fundec with
-        | Some p -> p != (true, true, true, true)
-        | None -> true in
+      match Hashtbl.find_opt PrecisionUtil.function_config fundec with
+      | Some p -> p != (true, true, true, true)
+      | None -> true in
     Hashtbl.replace PrecisionUtil.function_config fundec (true, true, true, true);
     PrecisionUtil.changed_configs := performed_update;
     Printf.printf "<<<< Should do something about function %s >>>>\n" (CilType.Fundec.show fundec)
