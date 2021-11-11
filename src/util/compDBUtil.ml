bba530fa460045cd4f40868fe9611401949fed0e
stilscher <66023521+stilscher@users.noreply.github.com>
2021-11-11 17:59:46 +0100

    simplify json, fix regexes

--- src/util/compDBUtil.ml
+++ src/util/compDBUtil.ml
@@ -3,14 +3,11 @@ open MakefileUtil
 
 let comp_db_filename = "compile_commands.json"
 
-type execute = Command of string | Arguments of string list [@@deriving yojson]
-
+(* TODO command objects might also have the optional field "output" or the field "arguments" (either "command" or "arguments" is required) *)
 type command_object = {
   directory   : string;
   file : string;
-  execute : execute;
-  argument : string option;
-  output : string option;
+  command : string;
 } [@@deriving yojson]
 
 type comp_db = command_object list [@@deriving yojson]
@@ -26,21 +23,18 @@ let preprocess (path: string) : string list =
     remove_comb_files path;
     let comp_file = Filename.concat path comp_db_filename in
     let command_objs = compDB comp_file in
-    let exec_command_obj o =
-      match o.execute with
-      | Command c ->
-        let outfile = (Filename.basename o.file) ^ ".i" in
-        let regex_out = Str.regexp "-o \\([a-zA-Z0-9_/.]\\)" in
-        let regex_gcc = Str.regexp "\\([a-zA-Z0-9_/]\\)" in
-        (* TODO use given compiler or gcc version specified in config file? *)
-        let preprocess_command = Str.replace_first regex_gcc "\\1 -E" c |> Str.replace_first regex_out ("-o " ^ outfile) in
-        let (exit_code, output) = MakefileUtil.exec_command ~path preprocess_command in
-        print_string output;
-        (* fail if command execution failed *)
-        if exit_code <> WEXITED 0 then
-          failwith ("Failed preprocessing file " ^ o.file ^ ": " ^ (string_of_process_status exit_code) ^ ".");
-        print_endline ("Preprocessed file: " ^ outfile);
-        outfile
-      | _ -> failwith "Processing compilation database: currently only objects with commands are being supported." in
+    let exec_command_obj obj =
+      let outfile = (Filename.basename obj.file) ^ ".i" in
+      let regex_out = Str.regexp "-o \\([a-zA-Z0-9_/.-]+\\) " in
+      let regex_gcc = Str.regexp "^\\(\\([a-zA-Z0-9_/.-]+\\) \\)" in
+      (* TODO use given compiler or gcc version specified in config file? *)
+      let preprocess_command = Str.replace_first regex_gcc "\\1 -E " obj.command |> Str.replace_first regex_out ("-o " ^ outfile ^ " ") in
+      let (exit_code, output) = MakefileUtil.exec_command ~path preprocess_command in
+      print_string output;
+      (* fail if command execution failed *)
+      if exit_code <> WEXITED 0 then
+        failwith ("Failed preprocessing file " ^ obj.file ^ ": " ^ (string_of_process_status exit_code) ^ ".");
+      print_endline ("Preprocessed file: " ^ outfile);
+      outfile in
     List.fold_right (fun o acc -> (exec_command_obj o) :: acc) command_objs []
   ) else failwith "Failed while preprocessing: Path does not exist."
