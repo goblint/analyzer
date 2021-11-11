bf5eeab884fdffbd48cca6d672ed1c916f3593c8
Simmo Saan <simmo.saan@gmail.com>
2021-11-11 17:40:30 +0200

    Try hacky bear compilation database

--- src/maingoblint.ml
+++ src/maingoblint.ml
@@ -290,10 +290,17 @@ let preprocess_files () =
     let i = ref 0 in
     convert_each (fun entry ->
         let file = entry |> member "file" |> to_string in
-        let command = entry |> member "command" |> to_string in
+        (* let command = entry |> member "command" |> to_string in *)
+        let arguments = entry |> member "arguments" |> convert_each to_string in
         let o_re = Str.regexp "-o +[^ ]+" in
         let file' = Printf.sprintf "%d.i" !i in
-        let command' = Str.replace_first o_re (cppflags ^ " -E -o " ^ file') command in
+        (* let command' = Str.replace_first o_re (cppflags ^ " -E -o " ^ file') command in *)
+
+        let (o_i, _) = List.findi (fun i e -> e = "-o") arguments in
+        let (arguments_init, _ :: _ :: arguments_tl) = List.split_at o_i arguments in
+        let arguments' = arguments_init @ "-I" :: List.hd custom_include_dirs :: "-E" :: "-o" :: file' :: arguments_tl in (* TODO: custom includes, cppflags *)
+        let command' = Filename.quote_command (List.hd arguments') (List.tl arguments') in
+
         Printf.printf "CD: %s: %s\n" file command';
         ignore (Sys.command command');
         incr i;
