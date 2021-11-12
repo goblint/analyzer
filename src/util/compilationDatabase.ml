fe3c7c53b3612c9ef93d32de3500d18d4a208e4b
Simmo Saan <simmo.saan@gmail.com>
2021-11-12 12:45:50 +0200

    Rewrite Simmo's compilation database to use yojson

--- src/util/compilationDatabase.ml
+++ src/util/compilationDatabase.ml
@@ -16,24 +16,25 @@ let parse_file filename =
   Result.get_ok (of_yojson (Yojson.Safe.from_file filename))
 
 let preprocess ~include_args filename =
-  let cd = Yojson.Safe.from_file filename in
-  let open Yojson.Safe.Util in
-  let i = ref 0 in
-  convert_each (fun entry ->
-      let file = entry |> member "file" |> to_string in
-      (* let command = entry |> member "command" |> to_string in *)
-      let arguments = entry |> member "arguments" |> convert_each to_string in
-      let o_re = Str.regexp "-o +[^ ]+" in (* TODO: include args *)
-      let file' = Printf.sprintf "%d.i" !i in
-      (* let command' = Str.replace_first o_re (cppflags ^ " -E -o " ^ file') command in *)
-
-      let (o_i, _) = List.findi (fun i e -> e = "-o") arguments in
-      let (arguments_init, _ :: _ :: arguments_tl) = List.split_at o_i arguments in
-      let arguments' = arguments_init @ include_args @ "-E" :: "-o" :: file' :: arguments_tl in (* TODO: custom includes, cppflags *)
-      let command' = Filename.quote_command (List.hd arguments') (List.tl arguments') in
-
-      Printf.printf "CD: %s: %s\n" file command';
-      ignore (Sys.command command');
-      incr i;
-      file'
-    ) cd
+  parse_file filename
+  |> List.mapi (fun i obj ->
+      let file = obj.file in
+      let preprocessed_file = Printf.sprintf "%d.i" i in
+      let preprocess_command = match obj.command, obj.arguments with
+        | Some command, None ->
+          let o_re = Str.regexp "-o +[^ ]+" in (* TODO: include args *)
+          Str.replace_first o_re (String.join " " include_args ^ " -E -o " ^ preprocessed_file) command
+        | None, Some arguments ->
+          let (o_i, _) = List.findi (fun i e -> e = "-o") arguments in
+          let (arguments_init, _ :: _ :: arguments_tl) = List.split_at o_i arguments in
+          let arguments' = arguments_init @ include_args @ "-E" :: "-o" :: preprocessed_file :: arguments_tl in (* TODO: custom includes, cppflags *)
+          Filename.quote_command (List.hd arguments') (List.tl arguments')
+        | Some _, Some _ ->
+          failwith "CompilationDatabase.preprocess: both command and arguments specified for " ^ file
+        | None, None ->
+          failwith "CompilationDatabase.preprocess: neither command nor arguments specified for " ^ file
+      in
+      Printf.printf "CD: %s: %s\n" file preprocess_command;
+      ignore (Sys.command preprocess_command);
+      preprocessed_file
+    )
