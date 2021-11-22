da1d394f8a925e81a47fc633aad653aafbe3a30e
Simmo Saan <simmo.saan@gmail.com>
2021-11-22 13:56:11 +0200

    Fix solver unittest ArithmeticOnIntegerBot

--- unittest/solver/solverTest.ml
+++ unittest/solver/solverTest.ml
@@ -36,13 +36,13 @@ module ConstrSys = struct
     1. x := g
     2. y := 8
     3. z := y
-    4. z := z + 1 [also g := 42]
+    4. w := w + 1 [also g := 42; _ := z]
     *)
   let system : LVar.t -> ((LVar.t -> D.t) -> (LVar.t -> D.t -> unit) -> (GVar.t -> G.t) -> (GVar.t -> G.t -> unit) -> D.t) option = function
     | "x" -> Some (fun loc _ glob gside -> glob "g")
     | "y" -> Some (fun loc _ glob gside -> (ignore (loc "x"); Int.of_int (Z.of_int64 8L)))
     | "z" -> Some (fun loc _ glob gside -> (ignore (loc "y"); loc "y"))
-    | "w" -> Some (fun loc _ glob gside -> (gside "g" (Int.of_int (Z.of_int64 42L)); ignore (loc "z"); Int.add (loc "w") (Int.of_int (Z.of_int64 1L))))
+    | "w" -> Some (fun loc _ glob gside -> (gside "g" (Int.of_int (Z.of_int64 42L)); ignore (loc "z"); try Int.add (loc "w") (Int.of_int (Z.of_int64 1L)) with IntDomain.ArithmeticOnIntegerBot _ -> Int.top ()))
     | _   -> None
 end
 
@@ -64,7 +64,7 @@ let test1 () =
   assert_equal ~printer:id "42" (Int.show (LH.find sol "x"));
   assert_equal ~printer:id "8"  (Int.show (LH.find sol "y"));
   assert_equal ~printer:id "8"  (Int.show (LH.find sol "z"));
-  assert_equal ~printer:id "Unknown int" (Int.show (LH.find sol "w"))
+  assert_equal ~printer:id "Unknown int([-63,63])" (Int.show (LH.find sol "w"))
 
 let test () = "solverTest" >:::
   [ "system1" >:: test1 ]
