d475c4e74198879473c0267f7abe49282535d32a
Vesal Vojdani <vesal.vojdani@gmail.com>
2021-11-24 19:36:01 +0200

    [skip ci] Fix to own mixup of flags.

--- scripts/trace_on.sh
+++ scripts/trace_on.sh
@@ -2,7 +2,7 @@
 
 echo "You can also change profile to \"trace\" in ./dune-workspace directly!"
 
-if grep -qi '(profile .*)' dune-workspace; then
+if grep -qs '(profile .*)' dune-workspace; then
   sed -i.bak 's/(profile .*)/(profile trace)/' dune-workspace && rm dune-workspace.bak
 else
   echo "(lang dune 2.8)" >> dune-workspace
