e5d63c856d8284cd9fb4a85a33408333ba28a6eb
Michael Schwarz <michael.schwarz93@gmail.com>
2021-11-24 13:18:54 +0100

    Make normal sides default, sides-pp only as an option

--- tests/regression/36-apron/85-problem-0405.c
+++ tests/regression/36-apron/85-problem-0405.c
@@ -1,4 +1,4 @@
-// SKIP PARAM: --set ana.activated[+] apron --set ana.path_sens[+] threadflag --set ana.activated[-] threadJoins
+// SKIP PARAM: --set ana.activated[+] apron --set ana.path_sens[+] threadflag --set ana.activated[-] threadJoins --set exp.solver.td3.side_widen sides-pp
 #include <pthread.h>
 #include <stdio.h>
 
