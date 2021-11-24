81a22a366fddd151c5f39dbd5dd87a8d211a0b29
Simmo Saan <simmo.saan@gmail.com>
2021-11-24 16:13:55 +0200

    Use dune rules to determine cpp

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -12,26 +12,9 @@ fi
 if [ ! -f src/config.ml ]; then
   {
     echo "let tracing = false"
-    echo "let cpp = \"cpp\""
     echo "let mach = \"$(uname -m)\""
   } >> src/config.ml
 fi
-
-# if we are on OS X and cpp is the Apple LLVM version, we should look for some Homebrew cpp-8 (or other version)
-case $OSTYPE in
-  darwin*)
-    if [[ $(compgen -c cpp-) ]]; then
-      cpp=$(compgen -c cpp- | head -n1)
-    else
-      cpp="cpp"
-      echo "Warning! GNU cpp not found! Goblint does not work with clang."
-      echo "Please install gcc using homebrew."
-    fi
-    grep -q "cpp = \"$cpp\"" src/config.ml 2> /dev/null ||
-      (sed "s@cpp = .*@cpp = \"$cpp\"@" src/config.ml > src/config.tmp && mv src/config.tmp src/config.ml &&
-        echo "Set cpp command to \"$cpp\".")
-    ;;
-esac
 
 if [ "$VERSION" ]; then
   grep -q "goblint = \"$VERSION\"" src/version.ml 2> /dev/null ||
