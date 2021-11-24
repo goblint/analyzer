3bd707aefedea97c121fdf143458e73bec6f1379
Simmo Saan <simmo.saan@gmail.com>
2021-11-24 17:10:25 +0200

    Fix indentation in ExpressionEvaluation

--- src/transform/expressionEvaluation.ml
+++ src/transform/expressionEvaluation.ml
@@ -41,14 +41,14 @@ module ExpEval : Transform.S =
       | None -> "UNKNOWN"
     let string_of_statement statement =
       statement
-        (* Pretty print *)
-        |> CilType.Stmt.show
-        (* Split into lines *)
-        |> String.split_on_char '\n'
-        (* Remove indentation *)
-        |> List.map String.trim
-        (* Concatenate lines into one *)
-        |> List.fold_left (^) ""
+      (* Pretty print *)
+      |> CilType.Stmt.show
+      (* Split into lines *)
+      |> String.split_on_char '\n'
+      (* Remove indentation *)
+      |> List.map String.trim
+      (* Concatenate lines into one *)
+      |> List.fold_left (^) ""
 
     class evaluator (file : Cil.file) (ask : Cil.location -> Queries.ask) =
       object (self)
