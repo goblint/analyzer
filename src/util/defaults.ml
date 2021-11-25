a1b00b46684af09e6236c642ed5845fb6d225ca0
Michael Schwarz <michael.schwarz93@gmail.com>
2021-11-25 11:11:53 +0100

    Reorder in defaults.ml

--- src/util/defaults.ml
+++ src/util/defaults.ml
@@ -222,14 +222,13 @@ let _ = ()
       ; reg Experimental "exp.partition-arrays.keep-expr" "'first'" "When using the partitioning which expression should be used for partitioning ('first', 'last')"
       ; reg Experimental "exp.partition-arrays.partition-by-const-on-return" "false" "When using the partitioning should arrays be considered partitioned according to a constant if a var in the expression used for partitioning goes out of scope?"
       ; reg Experimental "exp.partition-arrays.smart-join" "false" "When using the partitioning should the join of two arrays partitioned according to different expressions be partitioned as well if possible? If keep-expr is 'last' this behavior is enabled regardless of the flag value. Caution: Not always advantageous."
-      ; reg Experimental "exp.gcc_path"           "'/usr/bin/gcc'" "Location of gcc. Used to combine source files with cilly. Change to gcc-9 or another version on OS X (with gcc being clang by default cilly will fail otherwise)."
       ; reg Experimental "exp.structs.domain"          "'simple'" "The domain that should be used for structs."
-      ; reg Experimental "exp.structs.meet-condition"  "false"   "Try to raise deadcode when checking the condition by meeting the struct value, not just field value."
-      ; reg Experimental "exp.structs.key.forward"     "true"    "Whether the struct key should be picked going from first field to last."
-      ; reg Experimental "exp.structs.key.avoid-ints"  "true"    "Whether integers should be avoided for key."
-      ; reg Experimental "exp.structs.key.prefer-ptrs" "true"    "Whether pointers should be preferred for key."
+      ; reg Experimental "exp.structs.meet-condition"  "false"    "Try to raise deadcode when checking the condition by meeting the struct value, not just field value."
+      ; reg Experimental "exp.structs.key.forward"     "true"     "Whether the struct key should be picked going from first field to last."
+      ; reg Experimental "exp.structs.key.avoid-ints"  "true"     "Whether integers should be avoided for key."
+      ; reg Experimental "exp.structs.key.prefer-ptrs" "true"     "Whether pointers should be preferred for key."
+      ; reg Experimental "exp.gcc_path"           "'/usr/bin/gcc'" "Location of gcc. Used to combine source files with cilly. Change to gcc-9 or another version on OS X (with gcc being clang by default cilly will fail otherwise)."
       ; reg Experimental "exp.compdb.original-path" "" "Original absolute path of Compilation Database. Used to reroot all absolute paths in there if moved, e.g. in container mounts."
-
 
 (* {4 category [Debugging]} *)
 let _ = ()
