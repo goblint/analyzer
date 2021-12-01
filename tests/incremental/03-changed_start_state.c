d82c60fdd41a7f147aa4f972a19670e6dc10c83f
stilscher <66023521+stilscher@users.noreply.github.com>
2021-12-01 18:01:35 +0100

    refactor existing incremental test cases

--- tests/incremental/03-changed_start_state.c
+++ tests/incremental/03-changed_start_state.c
@@ -6,7 +6,7 @@ int main() {
     // After the presolve phase, g is in the start state but neither in the context nor in the start variables.
     // If the start state of main is not overwriten when main is destabilized because of a changed start state,
     // the first assert will wrongly succeed and the second fail in the incremental run
-    assert(g == 1); // success before, fail after
-    assert(g == 2); // fail before, success after
+    assert(g == 1); // SUCCESS
+    assert(g == 2); // FAIL
     return 0;
 }
--- tests/incremental/05-sideeffects.c
+++ tests/incremental/05-sideeffects.c
@@ -1,4 +1,3 @@
-// The patch for this test case is empty on purpose. The problem described does occur in the incremental run even without any changes.
 #include <assert.h>
 
 int g = 1;
@@ -9,7 +8,7 @@ void f() {
 
 int main() {
     f();
-    assert(g == 2); // unknown! before, unknown! after
-    assert(g == 1); // unknown! before, unknown! after (when wrongly overriding the start state of start functions this did succeed in the incremental run)
+    assert(g == 2); // UNKNOWN!
+    assert(g == 1); // UNKNOWN! (when wrongly overriding the start state of start functions this did succeed in the incremental run)
     return 0;
 }
