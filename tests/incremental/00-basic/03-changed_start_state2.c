a044f54b3ecca2277bc33f04a54a99aa82beea1c
stilscher <66023521+stilscher@users.noreply.github.com>
2021-12-09 12:54:10 +0100

    fix outdated changed start state tests

--- tests/incremental/00-basic/03-changed_start_state2.c
+++ tests/incremental/00-basic/03-changed_start_state2.c
@@ -4,9 +4,10 @@ int g = 1;
 
 int main() {
     // After the presolve phase, g is in the start state but neither in the context nor in the start variables.
-    // If the start state of main is not overwriten when main is destabilized because of a changed start state,
-    // the first assert will wrongly succeed and the second fail in the incremental run
-    assert(g == 1); // SUCCESS
-    assert(g == 2); // FAIL
+    // If the change of the start state of main would not be propagated by the call to side on all start variables, the
+    // asserts in the incremental run would wrongly fail. Side however only joins with the previous value instead of
+    // overwriting, therefore the current imprecision.
+    assert(g == 1);
+    assert(g != 2);
     return 0;
 }
