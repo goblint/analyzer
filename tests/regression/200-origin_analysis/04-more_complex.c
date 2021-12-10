bebf395f4dda08c6ca4fdf9d108eebf894ce2851
Ivana Zuzic <ivana.zuzic@tum.de>
2021-12-10 17:21:34 +0100

    Adding examples

---
+++ tests/regression/200-origin_analysis/04-more_complex.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int y = 3;
+    int z = 1;
+    int a, b;
+    int* x = &y;
+
+    if (a) {
+        if (b) {
+            x = NULL;
+        } else {
+            x = &y;
+        }
+        // if we split we know the correlation of b and x
+        int t  =3;
+    } else {
+        x = &z;
+    }
+    // another split because we might not even test b
+    if (!b) {
+        *x = 5; 
+        // Goblint thinks this may be dereferencing a NULL pointer, but it's not true
+        // Splitting should help 
+    }
+    return 0;
+}
---
+++ tests/regression/200-origin_analysis/05-more_variables.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, x, y, z;
+    if (a) {
+        x = 1;
+    } else {
+        x = 1;
+    }
+    // join point label b:    
+
+    if (b)  {
+        y = 1;
+    } else {
+        y = 2;
+    }
+    // join point label a: 
+    // The new domain must store
+    // x-> ([1], {}),  y -> ([1,2], {(y,a)}) 
+    
+    z = y+1;
+
+    // the join points for z depend on y
+    // x -> ([1], y->..., z -> ([2,3],{(y,a)})
+    
+    if (y ==1) {
+        x = 1;
+    } else {
+        x = 8;
+    }
+    // join point label c:
+    // x-> ([1, 8], {c}),  y -> ([1,2], {a}) 
+    assert (x < y);
+    return 0;
+
+}
