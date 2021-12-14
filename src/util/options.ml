8067e0a943ce699f44b152b217bf2d51474bd000
Simmo Saan <simmo.saan@gmail.com>
2021-12-14 15:33:31 +0200

    Refactor element_category_of_defaults_json

--- src/util/options.ml
+++ src/util/options.ml
@@ -1,6 +1,7 @@
 open Json_encoding
 open Json_schema
 
+(* TODO: deduplicate with GobConfig *)
 (** The ultimate convenience function for writing values. *)
 let one_quote = Str.regexp "\'"
 let parse_goblint_json s =
@@ -11,48 +12,69 @@ let parse_goblint_json s =
   with Yojson.Json_error _ ->
     `String s
 
-let rec convert_schema' (json: Yojson.Safe.t) opts (prefix: string): element * DefaultsCategory.category =
-  let element' ekind =
-    let (cat, (desc, def)) = List.assoc (BatString.lchop prefix) opts in
-    (* let name = BatString.lchop @@ Filename.extension prefix in *)
-    ({(element ekind) with title = Some (BatString.lchop prefix); description = Some desc; default = Some (Json_repr.repr_to_any (module Json_repr.Yojson) (parse_goblint_json def))}, cat)
+(* Convert old Defaults options to schema. *)
+
+type defaults = (string * (DefaultsCategory.category * string * string)) list
+
+let rec element_category_of_defaults_json (defaults: defaults) (json: Yojson.Safe.t) (path: string): element * DefaultsCategory.category =
+  let element_defaults kind = (* element with metadata from defaults *)
+    let name = BatString.lchop path in (* chop initial . *)
+    let (category, description, default) = List.assoc name defaults in
+    let default_json = parse_goblint_json default in
+    let element = { (element kind) with
+      title = Some name;
+      description = Some description;
+      default = Some (Json_repr.repr_to_any (module Json_repr.Yojson) default_json);
+    }
+    in
+    (element, category)
   in
   match json with
   | `String s ->
-    element' @@ String string_specs
+    element_defaults @@ String string_specs
   | `Bool b ->
-    element' @@ Boolean
+    element_defaults @@ Boolean
   | `Int i ->
-    element' @@ Integer numeric_specs
+    element_defaults @@ Integer numeric_specs
   | `List xs ->
-    let element_schema = match prefix with
+    let element_element = match path with
       | ".phases" -> element (Id_ref "") (* refer to entire schema itself *)
-      | _ -> element (String string_specs)
+      | _ -> element (String string_specs) (* all other lists contain strings *)
     in
-    element' @@ Monomorphic_array (element_schema, array_specs)
+    element_defaults @@ Monomorphic_array (element_element, array_specs)
   | `Assoc xs ->
-    let cat' = ref None in
+    let category = ref None in
     let properties = List.map (fun (key, value) ->
-        let (inner, cat) = convert_schema' value opts (prefix ^ "." ^ key) in
-        cat' := Some cat;
-        (key, inner, false, None)
+        let (field_element, field_category) = element_category_of_defaults_json defaults value (path ^ "." ^ key) in
+        category := Some field_category; (* category from defaults for object description *)
+        (key, field_element, false, None)
       ) xs
     in
-    let cat = Option.get !cat' in
-    let el = match BatString.index_after_n '.' 2 prefix with
-      | exception Not_found when prefix = ".interact" || prefix = "" -> (* isn't Std; is root *)
-        element @@ Object { object_specs with properties; additional_properties = None}
-      | exception Not_found ->
-        {(element @@ Object { object_specs with properties; additional_properties = None}) with title = Some (DefaultsCategory.show_category cat); description = Some (DefaultsCategory.catDescription cat)}
-      | _ ->
-        element @@ Object { object_specs with properties; additional_properties = None}
+    let category = Option.get !category in
+    let element = element @@ Object {
+        object_specs with
+        properties;
+        additional_properties = None; (* forbid additional properties *)
+      }
     in
-    (el, cat)
-  | _ -> failwith (Format.asprintf "convert_schema': %a" Yojson.Safe.pp json)
+    let element = match BatString.index_after_n '.' 2 path with
+      | exception Not_found when path = ".interact" || path = "" -> (* interact isn't Std or is root *)
+        element
+      | exception Not_found -> (* category *)
+        { element with
+          title = Some (DefaultsCategory.show_category category);
+          description = Some (DefaultsCategory.catDescription category);
+        }
+      | _ -> (* inner object, not category *)
+        element
+    in
+    (element, category)
+  | _ ->
+    failwith (Format.asprintf "element_category_of_defaults_json: %a" Yojson.Safe.pp json)
 
-let convert_schema json opts =
+let convert_schema opts json =
   try
-    let sch = create @@ {(fst @@ convert_schema' json opts "") with id = Some ""} in (* add id to make create defs check happy, doesn't get outputted apparently *)
+    let sch = create @@ {(fst @@ element_category_of_defaults_json opts json "") with id = Some ""} in (* add id to make create defs check happy, doesn't get outputted apparently *)
     JsonSchema2.global_schema := sch;
     (* Format.printf "schema: %a\n" Json_schema.pp sch; *)
     (* Format.printf "schema2: %a\n" (Yojson.Safe.pretty_print ~std:true) (JS.to_json sch) *)
@@ -69,4 +91,5 @@ let convert_schema json opts =
     Json_schema.print_error Format.err_formatter e
 
 let () =
-  convert_schema !GobConfig.json_conf @@ List.map (fun (c, (n, p)) -> (n, (c, p))) !Defaults.registrar
+  let defaults = List.map (fun (c, (n, (desc, def))) -> (n, (c, desc, def))) !Defaults.registrar in (* transform for assoc list lookup by name *)
+  convert_schema defaults !GobConfig.json_conf
