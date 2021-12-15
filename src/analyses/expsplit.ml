da112155b49c1f38458e1e6a744a908df3b56a4a
Simmo Saan <simmo.saan@gmail.com>
2021-12-15 11:52:28 +0200

    Implement expsplit logic

--- src/analyses/expsplit.ml
+++ src/analyses/expsplit.ml
@@ -1,11 +1,16 @@
 open Prelude.Ana
 open Analyses
 
+module M = Messages
+
+module ID = Queries.ID
+module BD = Basetype.Bools
+
 module Spec : Analyses.MCPSpec =
 struct
   let name () = "expsplit"
 
-  module D = MapDomain.MapBot (Basetype.CilExp) (Lattice.Unit)
+  module D = MapDomain.MapBot (Basetype.CilExp) (BD)
   module C = D
 
   let startstate v = D.bot ()
@@ -16,16 +21,37 @@ struct
   (* after IdentitySpec, because that would override... *)
   let should_join = D.equal
 
+  (* TODO: emit update splits from all transfer functions *)
+  let assign ctx (lval:lval) (rval:exp) =
+    D.iter (fun e _ ->
+        ctx.emit (UpdateExpSplit e)
+      ) ctx.local;
+    ctx.local
+
   let special ctx (lval: lval option) (f:varinfo) (arglist:exp list) =
+    (* TODO: emit update splits *)
     match f.vname with
     | "__goblint_split_begin" ->
       let exp = List.hd arglist in
-      D.add exp () ctx.local
+      D.add exp (BD.top ()) ctx.local (* TODO: immediately split *)
+      (* TODO: emit update splits _after_ adding expression, then doesn't have to eval for something better than top here (?) *)
     | "__goblint_split_end" ->
       let exp = List.hd arglist in
       D.remove exp ctx.local
     | _ ->
       ctx.local (* TODO: not identity *)
+
+  let event ctx (event: Events.t) octx =
+    match event with
+    | UpdateExpSplit exp ->
+      begin match ID.to_bool (ctx.ask (EvalInt exp)) with
+        | Some b ->
+          D.add exp (`Lifted b) ctx.local
+        | None -> (* unknown *)
+          ctx.local
+      end
+    | _ ->
+      ctx.local
 end
 
 let () =
--- src/domains/events.ml
+++ src/domains/events.ml
@@ -7,6 +7,7 @@ type t =
   | EnterMultiThreaded
   | SplitBranch of exp * bool (** Used to simulate old branch-based split. *)
   | AssignSpawnedThread of lval * ThreadIdDomain.Thread.t (** Assign spawned thread's ID to lval. *)
+  | UpdateExpSplit of exp (** Used by expsplit analysis to evaluate [exp] on post-state. *)
 
 let pretty () = function
   | Lock m -> dprintf "Lock %a" LockDomain.Addr.pretty m
@@ -15,3 +16,4 @@ let pretty () = function
   | EnterMultiThreaded -> text "EnterMultiThreaded"
   | SplitBranch (exp, tv) -> dprintf "SplitBranch (%a, %B)" d_exp exp tv
   | AssignSpawnedThread (lval, tid) -> dprintf "AssignSpawnedThread (%a, %a)" d_lval lval ThreadIdDomain.Thread.pretty tid
+  | UpdateExpSplit exp -> dprintf "UpdateExpSplit %a" d_exp exp
