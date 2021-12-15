be41c270b9b111574aabadf20d4eb77a395974ff
Vesal Vojdani <vesal.vojdani@gmail.com>
2021-12-15 17:25:45 +0200

    Add minimized test where abort fails.

---
+++ tests/regression/34-localwn_restart/21-restart_abort_aget.c
@@ -0,0 +1,23 @@
+// SKIP PARAM: --enable exp.solver.td3.restart.wpoint.enabled --enable exp.solver.td3.abort
+
+#include<stdlib.h>
+struct a {
+  int b;
+} c(), *f;
+
+int g;
+void d();
+void e();
+
+int main() {
+  void *h = calloc(1, sizeof(struct a));
+  f = h;
+  d(e);
+  while (1) {
+    g = c;
+    if (c)
+      break;
+    f->b = 0;
+  }
+  d(h);
+}
