399c6a2bf386b61e2cbf51277e123ade657b3dae
Vesal Vojdani <vesal.vojdani@gmail.com>
2021-12-22 15:28:43 +0200

    Fix file extension for patch headings.

--- scripts/fix_patch_headers.sh
+++ scripts/fix_patch_headers.sh
@@ -2,7 +2,8 @@
 #Run from root, e.g.,: ./scripts/fix_patch_headers.sh tests/incremental/11-restart/*.patch
 for file in "$@"
 do
-  efile="${file//\//\\/}"
-  echo "$file"
+  echo $file
+  cfile="${file%.patch}.c"
+  efile="${cfile//\//\\/}"
   perl -0777 -i -pe "s/.*?@/--- a\/$efile\n+++ b\/$efile\n@/is" $file
 done
