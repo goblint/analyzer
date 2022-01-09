193ce44c9a1ee35ea086feaed82c99906d364cd8
Kerem Çakırer <kcakirer@hotmail.com>
2022-01-10 02:19:31 +0300

    Workaround for primitive arguments

--- src/util/server.ml
+++ src/util/server.ml
@@ -29,23 +29,38 @@ let registry = Registry.make ()
 let handle_exn id exn =
   Response.Error.(make Code.InternalError (Printexc.to_string exn) () |> Response.error id)
 
+module ParamParser (C : Command) = struct
+  let parse params =
+    let args = 
+      params
+      |> Option.map_default Message.Structured.to_json `Null
+      |> C.args_of_yojson
+    in
+    match args with
+    | Ok args -> Ok args
+    | Error err ->
+      (* This is a hack to handle cases where C.args is primitive type like int or string. *)
+      match params with
+      | Some `List [param] -> (
+          match C.args_of_yojson param with
+          | Ok arg -> Ok arg
+          | _ -> Error err)
+      | _ -> Error err
+end
+
 let handle_request (serv : t) (message: Message.either) (id: Id.t) =
   let cmd = Hashtbl.find_option registry message.method_ in
   let response = match cmd with
     | Some (module C) ->
-      let args = 
-        message.params
-        |> Option.map_default Message.Structured.to_json `Null
-        |> C.args_of_yojson
-      in
-      (match args with
-       | Ok args -> (
-           try
-             C.process args serv
-             |> C.result_to_yojson
-             |> Response.ok id
-           with exn -> handle_exn id exn)
-       | Error s -> Response.Error.(make Code.InvalidParams s () |> Response.error id))
+      let module Parser = ParamParser (C) in (
+        match Parser.parse message.params with
+        | Ok args -> (
+            try
+              C.process args serv
+              |> C.result_to_yojson
+              |> Response.ok id
+            with exn -> handle_exn id exn)
+        | Error s -> Response.Error.(make Code.InvalidParams s () |> Response.error id))
     | _ -> Response.Error.(make Code.MethodNotFound message.method_ () |> Response.error id)
   in
   Response.yojson_of_t response |> Yojson.Safe.to_string |> print_endline
@@ -93,9 +108,9 @@ let () =
 
   register (module struct
     let name = "analyze"
-    type args = bool * unit [@@deriving of_yojson]
+    type args = bool [@@deriving of_yojson]
     type result = unit [@@deriving to_yojson]
-    let process (reset, ()) serve = analyze serve ~reset
+    let process reset serve = analyze serve ~reset
   end);
 
   register (module struct
@@ -107,9 +122,9 @@ let () =
 
   register (module struct
     let name = "merge_config"
-    type args = Yojson.Safe.t * unit [@@deriving of_yojson]
+    type args = Yojson.Safe.t [@@deriving of_yojson]
     type result = unit [@@deriving to_yojson]
-    let process (json, ()) _ = GobConfig.merge json
+    let process json _ = GobConfig.merge json
   end);
 
   register (module struct
