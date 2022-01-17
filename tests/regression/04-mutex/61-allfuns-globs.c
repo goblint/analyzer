d4b619237a4a0b83d600d3e1e26e88a30d448f5c
Michael Schwarz <michael.schwarz93@gmail.com>
2022-01-17 14:59:08 +0100

    04/61 Include assert.h and call unlock

--- tests/regression/04-mutex/61-allfuns-globs.c
+++ tests/regression/04-mutex/61-allfuns-globs.c
@@ -1,5 +1,6 @@
 // PARAM: --set allfuns true
 #include<pthread.h>
+#include<assert.h>
 
 int myglobal;
 int three = 3;
@@ -9,7 +10,7 @@ pthread_mutex_t B_mutex = PTHREAD_MUTEX_INITIALIZER;
 void t1() {
   pthread_mutex_lock(&A_mutex);
   myglobal++; //RACE!
-  pthread_mutex_lock(&A_mutex);
+  pthread_mutex_unlock(&A_mutex);
 
   assert(three == 3);
 }
@@ -17,7 +18,7 @@ void t1() {
 void t2() {
   pthread_mutex_lock(&B_mutex);
   myglobal++; //RACE!
-  pthread_mutex_lock(&B_mutex);
+  pthread_mutex_unlock(&B_mutex);
 
   assert(three == 3);
 }
