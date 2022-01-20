4536c2c1a5e0f8885ad83f3cfc636b20320a8e3b
Simmo Saan <simmo.saan@gmail.com>
2022-01-20 10:39:32 +0200

    Fix json-messages files indentation (PR #535)

--- src/framework/analyses.ml
+++ src/framework/analyses.ml
@@ -284,9 +284,9 @@ struct
         files |> SH.to_list
       in
       let json = `Assoc [
-        ("files", `Assoc (List.map (Tuple2.map2 [%to_yojson: string list option]) files));
-        ("messages", Messages.Table.to_yojson ());
-      ]
+          ("files", `Assoc (List.map (Tuple2.map2 [%to_yojson: string list option]) files));
+          ("messages", Messages.Table.to_yojson ());
+        ]
       in
       Yojson.Safe.pretty_to_channel ~std:true out json
     | "none" -> ()
