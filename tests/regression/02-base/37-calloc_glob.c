76df1d57c85cded8b43213a9fdf2d078d088b737
mireia <mcanopujol@gmail.com>
2022-01-25 15:43:49 +0100

    decide domain with flag  now

--- tests/regression/02-base/37-calloc_glob.c
+++ tests/regression/02-base/37-calloc_glob.c
@@ -1,6 +1,6 @@
 // Made after 02 22
 
-// PARAM: --set ana.int.interval true --enable ana.base.partition-arrays.enabled
+// PARAM: --set ana.int.interval true --set exp.arrays-domain partitioned
 
 #include <stdlib.h>
 #include <assert.h>
--- tests/regression/22-partitioned_arrays/18-large_arrays-nocalloc.c
+++ tests/regression/22-partitioned_arrays/18-large_arrays-nocalloc.c
@@ -1,4 +1,4 @@
-// PARAM: --enable ana.int.interval --enable ana.base.partition-arrays.enabled
+// PARAM: --enable ana.int.interval --set exp.arrays-domain partitioned
 #include <assert.h>
 #include <stdlib.h>
 #include <limits.h>
