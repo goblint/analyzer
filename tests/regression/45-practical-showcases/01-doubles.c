9a48033b5a2098b3cdd684a8045d3979bd44d886
Julian Erhard <julian.erhard@tum.de>
2022-02-01 10:31:50 +0100

    Fix testcase printfs

--- tests/regression/45-practical-showcases/01-doubles.c
+++ tests/regression/45-practical-showcases/01-doubles.c
@@ -4,26 +4,25 @@
 
 int main (){
     double x = 4.0;
-    double y = 3.0;
 
     double not_a_number = 0.0 / 0.0;
     double inifinty =  1.0 / 0.0;
     double neg_infinity = -1.0 / 0.0;
 
-    printf("not_a_number: %lf\ninfinity: %lf\neg_infinity: %lf\n", not_a_number, inifinty, neg_infinity);
+    printf("not_a_number: %lf\ninfinity: %lf\neg_infinity: %lf\n\n", not_a_number, inifinty, neg_infinity);
 
-    double mult = x * y;
+    double mult = x * 3.0;
 
-    printf("x < mult: %d", x < mult);
+    printf("x < mult: %d\n", x < mult);
     assert(x < mult);
 
-    printf("x < mult: %d", x <= inifinty);
+    printf("x <= inifinty: %d\n", x <= inifinty);
     assert(x <= inifinty);
 
-    printf("x < mult: %d", x >= neg_infinity);
+    printf("x >= neg_infinity: %d\n", x >= neg_infinity);
     assert(x >= neg_infinity);
 
-    printf("not_a_number != not_a_number: %d", not_a_number != not_a_number);
+    printf("not_a_number != not_a_number: %d\n", not_a_number != not_a_number);
     assert(not_a_number != not_a_number);
 
     return 0;
