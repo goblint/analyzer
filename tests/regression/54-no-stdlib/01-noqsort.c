ec38e063becc12cb979e4837ecb7afc49fd95d9e
Michael Schwarz <michael.schwarz93@gmail.com>
2022-02-01 09:30:41 +0100

    Cleanup test

--- tests/regression/54-no-stdlib/01-noqsort.c
+++ tests/regression/54-no-stdlib/01-noqsort.c
@@ -1,8 +1,7 @@
-
 // PARAM: --set cppflags[+] -DGOBLINT_NO_QSORT
 #include<stddef.h>
 
-// There should be no CIL warning about multiple defintions here
+// There should be no CIL warning about multiple definitions here
 void qsort(void *ptr, size_t count, size_t size, int (*comp)(const void*, const void*), int more) {
 }
 
