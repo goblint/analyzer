074ca0b66acb15aeacebdaf4d174a7227f8c4f70
Michael Schwarz <michael.schwarz93@gmail.com>
2022-02-10 15:14:34 +0100

    Do not print system deps

--- src/framework/analyses.ml
+++ src/framework/analyses.ml
@@ -269,8 +269,9 @@ struct
       Yojson.Safe.pretty_to_channel ~std:true out (Sarif.to_yojson (List.rev !Messages.Table.messages_list));
     | "json-messages" ->
       let files = Hashtbl.to_list Preprocessor.dependencies in
+      let filter_system = List.filter_map (fun (f,system) -> if system then None else Some f) in
       let json = `Assoc [
-          ("files", `Assoc (List.map (Tuple2.map2 [%to_yojson: (string*bool) list]) files));
+          ("files", `Assoc (List.map (Tuple2.map2 (fun deps -> [%to_yojson:string list] @@ filter_system deps)) files));
           ("messages", Messages.Table.to_yojson ());
         ]
       in
