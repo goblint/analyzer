6507801af41474f86a77273787787801c0faf885
stilscher <66023521+stilscher@users.noreply.github.com>
2022-02-10 11:22:20 +0100

    script to test different configs for zstd

---
+++ scripts/incremental_test_conf.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [ ! -f scripts/incremental_test_conf.sh ]; then
+  echo "Please run from root of analyzer repo."
+  exit 1
+fi
+
+rm -r out/zstd
+
+timeout --foreground 3h ./scripts/incremental.sh ../test-repos/zstd 7543085013db1a20a848d166e5931edc49e3cc2f big-benchmarks build_compdb_zstd.sh 10
+timeout --foreground 3h ./scripts/incremental.sh ../test-repos/zstd 7543085013db1a20a848d166e5931edc49e3cc2f big-benchmarks1 build_compdb_zstd.sh 10
+timeout --foreground 3h ./scripts/incremental.sh ../test-repos/zstd 7543085013db1a20a848d166e5931edc49e3cc2f big-benchmarks2 build_compdb_zstd.sh 10
