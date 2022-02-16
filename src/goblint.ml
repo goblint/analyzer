d88000068f716684929fd2cb73a701df0fefa561
Kerem Çakırer <kcakirer@hotmail.com>
2022-02-17 00:16:32 +0100

    Move main from maingoblint.ml to goblint.ml

--- src/goblint.ml
+++ src/goblint.ml
@@ -1,2 +1,46 @@
+open GobConfig
+open Goblintutil
+open Maingoblint
+open Prelude
+open Printf
+
+(** the main function *)
+let main () =
+  try
+    Stats.reset Stats.SoftwareTimer;
+    parse_arguments ();
+    check_arguments ();
+    AfterConfig.run ();
+
+    Sys.set_signal (Goblintutil.signal_of_string (get_string "dbg.solver-signal")) Signal_ignore; (* Ignore solver-signal before solving (e.g. MyCFG), otherwise exceptions self-signal the default, which crashes instead of printing backtrace. *)
+
+    Cilfacade.init ();
+
+    handle_extraspecials ();
+    create_temp_dir ();
+    handle_flags ();
+    if get_bool "dbg.verbose" then (
+      print_endline (localtime ());
+      print_endline command;
+    );
+    let file = Fun.protect ~finally:remove_temp_dir preprocess_and_merge in
+    if get_bool "server.enabled" then Server.start file preprocess_and_merge do_analyze else (
+      let changeInfo = if GobConfig.get_bool "incremental.load" || GobConfig.get_bool "incremental.save" then diff_and_rename file else Analyses.empty_increment_data file in
+      file|> do_analyze changeInfo;
+      do_stats ();
+      do_html_output ();
+      do_gobview ();
+      if !verified = Some false then exit 3)  (* verifier failed! *)
+  with
+  | Exit ->
+    exit 1
+  | Sys.Break -> (* raised on Ctrl-C if `Sys.catch_break true` *)
+    (* Printexc.print_backtrace BatInnerIO.stderr *)
+    eprintf "%s\n" (MessageUtil.colorize ~fd:Unix.stderr ("{RED}Analysis was aborted by SIGINT (Ctrl-C)!"));
+    exit 131 (* same exit code as without `Sys.catch_break true`, otherwise 0 *)
+  | Timeout ->
+    eprintf "%s\n" (MessageUtil.colorize ~fd:Unix.stderr ("{RED}Analysis was aborted because it reached the set timeout of " ^ get_string "dbg.timeout" ^ " or was signalled SIGPROF!"));
+    exit 124
+
 (* We do this since the evaluation order of top-level bindings is not defined, but we want `main` to run after all the other side-effects (e.g. registering analyses/solvers) have happened. *)
-let () = at_exit Maingoblint.main
+let () = at_exit main
