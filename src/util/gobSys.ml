63489badf9e2b036b670b5b8bee967f3a98bdb89
Simmo Saan <simmo.saan@gmail.com>
2022-02-23 14:12:30 +0200

    Remove .goblint directory if empty

--- src/util/gobSys.ml
+++ src/util/gobSys.ml
@@ -6,3 +6,9 @@ let rec mkdir_parents filename =
     mkdir_parents dirname;
     Unix.mkdir dirname 0o770; (* TODO: what permissions? *)
   )
+
+let rmdir_if_empty dirname =
+  try
+    Unix.rmdir dirname
+  with Unix.Unix_error (Unix.ENOTEMPTY, _, _) ->
+    ()
--- src/util/goblintutil.ml
+++ src/util/goblintutil.ml
@@ -108,7 +108,9 @@ let create_temp_dir () =
     tempDirName := preprocessed_dir
 
 let remove_temp_dir () =
-  if not (get_bool "pre.keep") then ignore (rm_rf !tempDirName)
+  if not (get_bool "pre.keep") then
+    ignore (rm_rf !tempDirName);
+  GobSys.rmdir_if_empty ".goblint"
 
 exception Timeout
 
