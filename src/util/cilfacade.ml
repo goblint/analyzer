62d759196717734cb01838080cdf5f9f568fe455
Simmo Saan <simmo.saan@gmail.com>
2022-02-28 15:27:41 +0200

    Re-enable CIL inlines merging to work around its bug

--- src/util/cilfacade.ml
+++ src/util/cilfacade.ml
@@ -36,6 +36,7 @@ let init () =
   initCIL ();
   lowerConstants := true;
   Mergecil.ignore_merge_conflicts := true;
+  Mergecil.merge_inlines := true; (* work around https://github.com/goblint/analyzer/pull/603#issuecomment-1054204635 *)
   (* lineDirectiveStyle := None; *)
   Rmtmps.keepUnused := true;
   print_CIL_Input := true
