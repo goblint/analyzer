83f9e5099c41bc6c9ef91329dd52a9c24ad67fae
Simmo Saan <simmo.saan@gmail.com>
2022-03-10 15:41:28 +0200

    Add option files (closes #636)

--- src/maingoblint.ml
+++ src/maingoblint.ml
@@ -107,13 +107,10 @@ let option_spec_list =
   ] @ defaults_spec_list (* lowest priority *)
 
 
-(** Parse arguments and fill [arg_files]. Print help if needed. *)
+(** Parse arguments. Print help if needed. *)
 let parse_arguments () =
-  let anon_arg filename =
-    arg_files := filename :: !arg_files
-  in
+  let anon_arg = set_string "files[+]" in
   Arg.parse option_spec_list anon_arg "Look up options using 'goblint --help'.";
-  arg_files := List.rev !arg_files; (* reverse files to get given order *)
   if !writeconffile <> "" then (GobConfig.write_file !writeconffile; raise Exit)
 
 (** Initialize some globals in other modules. *)
@@ -261,7 +258,7 @@ let preprocess_files () =
   if get_bool "ana.sv-comp.functions" then
     extra_arg_files := find_custom_include "sv-comp.c" :: !extra_arg_files;
 
-  let preprocessed = List.concat_map preprocess_arg_file (!extra_arg_files @ !arg_files) in
+  let preprocessed = List.concat_map preprocess_arg_file (!extra_arg_files @ get_string_list "files") in
   if not (get_bool "pre.exist") then (
     let preprocess_tasks = List.filter_map snd preprocessed in
     let terminated task = function
