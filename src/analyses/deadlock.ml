d5d9e5f22809a53065d180e6199fb6b3618828d4
Simmo Saan <simmo.saan@gmail.com>
2022-03-21 13:07:24 +0200

    Use global constraint unknown for deadlock orders (issue #650)

--- src/analyses/deadlock.ml
+++ src/analyses/deadlock.ml
@@ -4,8 +4,6 @@ open Prelude.Ana
 open Analyses
 open DeadlockDomain
 
-let forbiddenList : ( (myowntypeEntry*myowntypeEntry) list ref) = ref []
-
 module Spec =
 struct
   include Analyses.DefaultSpec
@@ -15,28 +13,40 @@ struct
   (* The domain for the analysis *)
   module D = DeadlockDomain.Lockset (* MayLockset *)
   module C = DeadlockDomain.Lockset
+  module V = Printable.UnitConf (struct let name = "deadlock" end)
+  module G =
+  struct
+    include SetDomain.Make (Printable.Prod (MyLock) (MyLock))
+    let leq x y = !GU.postsolving || leq x y (* HACK: to pass verify*)
+  end
 
-  let addLockingInfo newLock lockList =
+  let addLockingInfo ctx newLock lockList =
+
     let add_comb a b =
-      if List.exists (fun (x,y) -> MyLock.equal x a && MyLock.equal y b) !forbiddenList then ()
-      else forbiddenList := (a,b)::!forbiddenList
+      let d =
+        if !GU.should_warn then
+          G.singleton (a, b)
+        else
+          G.bot () (* HACK: just to pass validation with MCP DomVariantLattice *)
+      in
+      ctx.sideg () d
     in
 
     (* Check forbidden list *)
-    if !Goblintutil.postsolving then begin
-      D.iter (fun e -> List.iter (fun (a,b) ->
+    if true || !Goblintutil.postsolving then begin (* TODO: only postsolving *)
+      D.iter (fun e -> G.iter (fun (a,b) ->
           if ((MyLock.equal a e) && (MyLock.equal b newLock)) then (
             Messages.warn "Deadlock warning: Locking order %a, %a at %a, %a violates order at %a, %a." ValueDomain.Addr.pretty e.addr ValueDomain.Addr.pretty newLock.addr CilType.Location.pretty e.loc CilType.Location.pretty newLock.loc CilType.Location.pretty b.loc CilType.Location.pretty a.loc;
             Messages.warn ~loc:a.loc "Deadlock warning: Locking order %a, %a at %a, %a violates order at %a, %a." ValueDomain.Addr.pretty newLock.addr ValueDomain.Addr.pretty e.addr CilType.Location.pretty b.loc CilType.Location.pretty a.loc CilType.Location.pretty e.loc CilType.Location.pretty newLock.loc;
           )
-          else () ) !forbiddenList ) lockList;
+          else () ) (ctx.global ()) ) lockList;
 
       (* Add forbidden order *)
       D.iter (
         fun lock ->
           add_comb newLock lock;
-          let transAddList = List.find_all (fun (a,b) -> MyLock.equal a lock) !forbiddenList in
-          List.iter (fun (a,b) -> add_comb newLock b) transAddList
+          let transAddList = G.filter (fun (a,b) -> MyLock.equal a lock) (ctx.global ()) in
+          G.iter (fun (a,b) -> add_comb newLock b) transAddList
       ) lockList
     end
 
@@ -96,9 +106,9 @@ struct
       match LibraryFunctions.classify f.vname arglist with
       | `Lock (_, _, _) ->
         List.fold_left (fun d lockAddr ->
-          addLockingInfo {addr = lockAddr; loc = !Tracing.current_loc } ctx.local;
-          D.add {addr = lockAddr; loc = !Tracing.current_loc } ctx.local
-        ) ctx.local (eval_exp_addr (Analyses.ask_of_ctx ctx) (List.hd arglist))
+            addLockingInfo ctx {addr = lockAddr; loc = !Tracing.current_loc } ctx.local;
+            D.add {addr = lockAddr; loc = !Tracing.current_loc } ctx.local
+          ) ctx.local (eval_exp_addr (Analyses.ask_of_ctx ctx) (List.hd arglist))
       | `Unlock ->
         let lockAddrs = eval_exp_addr (Analyses.ask_of_ctx ctx) (List.hd arglist) in
         if List.compare_length_with lockAddrs 1 = 0 then
