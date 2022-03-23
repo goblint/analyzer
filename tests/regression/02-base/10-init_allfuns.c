dc878fc068c41946f83c2ac35a3904d88cdc117f
Simmo Saan <simmo.saan@gmail.com>
2022-03-23 14:59:40 +0200

    Make mutex analysis depend on mutexEvents

--- tests/regression/02-base/10-init_allfuns.c
+++ tests/regression/02-base/10-init_allfuns.c
@@ -1,4 +1,4 @@
-// PARAM: --enable allfuns --set ana.activated "['base','threadid','threadflag','escape','mutex','access','mallocWrapper']"
+// PARAM: --enable allfuns --set ana.activated "['base','threadid','threadflag','escape','mutexEvents','mutex','access','mallocWrapper']"
 int glob1 = 5;
 int glob2 = 7;
 
