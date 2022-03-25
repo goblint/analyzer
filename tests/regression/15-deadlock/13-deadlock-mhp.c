68d6d5bfbd6fbad3dedfb05ff9e63e48462c2f39
Michael Schwarz <michael.schwarz93@gmail.com>
2022-03-25 13:56:21 +0100

    Two sophisticated examples

---
+++ tests/regression/15-deadlock/13-deadlock-mhp.c
@@ -0,0 +1,36 @@
+// PARAM: --set ana.activated[+] deadlock --set ana.activated[+] threadJoins
+// From https://arxiv.org/abs/1607.06927
+#include <pthread.h>
+
+int x;
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m3 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m4 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m5 = PTHREAD_MUTEX_INITIALIZER;
+
+void *thread() {
+  pthread_mutex_lock(&m2); //DEADLOCK
+  pthread_mutex_lock(&m1); //DEADLOCK
+  pthread_mutex_unlock(&m2);
+  pthread_mutex_unlock(&m1);
+
+  return NULL;
+}
+
+void *noOpThread() {
+  return NULL;
+}
+
+int main() {
+  pthread_t tid1;
+  pthread_t tid2;
+
+  pthread_create(&tid1, NULL, noOpThread, NULL);
+
+  pthread_mutex_lock(&m1);
+  pthread_create(&tid2, NULL, thread, NULL);
+  pthread_mutex_lock(&m2); //DEADLOCK
+
+  return 0;
+}
---
+++ tests/regression/15-deadlock/15-deadlock-mhp2.c
@@ -0,0 +1,66 @@
+// PARAM: --set ana.activated[+] deadlock --set ana.activated[+] threadJoins
+#include <pthread.h>
+
+int x;
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m3 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m4 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m5 = PTHREAD_MUTEX_INITIALIZER;
+pthread_t decoy;
+
+
+void *noOpThread() {
+  return NULL;
+}
+
+void *thread2() {
+  pthread_mutex_lock(&m3);
+  pthread_mutex_lock(&m1);
+
+  pthread_mutex_unlock(&m3);
+  pthread_mutex_unlock(&m1);
+
+  return NULL;
+}
+
+void *thread() {
+  pthread_mutex_lock(&m2);
+  pthread_mutex_lock(&m3);
+
+  pthread_mutex_lock(&m5);
+  pthread_create(&decoy, NULL, noOpThread, NULL);
+  pthread_mutex_unlock(&m5);
+
+
+  pthread_mutex_unlock(&m3);
+  pthread_mutex_unlock(&m2);
+
+  return NULL;
+}
+
+int main() {
+  pthread_t tid1;
+  pthread_t tid2;
+
+  pthread_create(&tid1, NULL, thread, NULL);
+  pthread_create(&tid2, NULL, thread2, NULL);
+
+  pthread_mutex_lock(&m1);
+
+  pthread_mutex_lock(&m5);
+  pthread_t dec = decoy;
+  pthread_mutex_unlock(&m5);
+
+  if(dec != NULL) {
+    pthread_join(dec,NULL);
+    pthread_mutex_lock(&m2);
+
+    pthread_mutex_unlock(&m1);
+    pthread_mutex_unlock(&m2);
+  } else {
+    pthread_mutex_unlock(&m1);
+  }
+
+  return 0;
+}
