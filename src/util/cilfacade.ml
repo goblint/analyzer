04bd8936b216b50932cdf4aa93cf39642d7abd4e
Michael Schwarz <michael.schwarz93@gmail.com>
2022-03-27 16:48:29 +0200

    Fix merge timing

--- src/util/cilfacade.ml
+++ src/util/cilfacade.ml
@@ -261,7 +261,7 @@ class addConstructors cons = object
 end
 
 let getMergedAST fileASTs =
-  let merged = Stats.time "mergeCIL" Mergecil.merge fileASTs "stdout" in
+  let merged = Stats.time "mergeCIL"  (Mergecil.merge fileASTs) "stdout" in
   if !E.hadErrors then
     E.s (E.error "There were errors during merging\n");
   merged
