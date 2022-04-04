defe338a8c74972ed193ed69dda3aa9060eaf81c
Michael Schwarz <michael.schwarz93@gmail.com>
2022-04-04 19:08:18 +0200

    ex-earlyglobs: Remove extra level of caching

--- src/analyses/baseUtil.ml
+++ src/analyses/baseUtil.ml
@@ -9,15 +9,5 @@ let is_static (v:varinfo): bool = v.vstorage = Static
 
 let is_always_unknown variable = variable.vstorage = Extern || Ciltools.is_volatile_tp variable.vtype
 
-let exclude_from_earlyglobs = ref []
-let exclude_from_invalidation = ref []
-
-let is_excluded_from_earlyglobs v = List.mem v.vname !exclude_from_earlyglobs
-let is_excluded_from_invalidation v =  List.mem v.vname !exclude_from_invalidation
-
-let after_config () =
-  exclude_from_earlyglobs := get_string_list "exp.exclude_from_earlyglobs";
-  exclude_from_invalidation := get_string_list "exp.exclude_from_invalidation"
-
-let _ =
-  AfterConfig.register after_config
+let is_excluded_from_earlyglobs v = List.mem v.vname (get_string_list "exp.exclude_from_earlyglobs")
+let is_excluded_from_invalidation v =  List.mem v.vname (get_string_list "exp.exclude_from_invalidation")
