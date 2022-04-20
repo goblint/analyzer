dfe5f0a5a9e9b8b0a0012caf3ca6dc7144708296
Simmo Saan <simmo.saan@gmail.com>
2022-04-20 15:05:37 +0300

    Avoid Spec reconstruction in server mode

--- src/framework/control.ml
+++ src/framework/control.ml
@@ -8,8 +8,9 @@ open GobConfig
 open Constraints
 
 module type S2S = functor (X : Spec) -> Spec
-(* gets Spec for current options *)
-let get_spec () : (module Spec) =
+
+(* spec is lazy, so HConsed table in Hashcons lifters is preserved between analyses in server mode *)
+let spec_module: (module Spec) Lazy.t = lazy (
   let open Batteries in
   (* apply functor F on module X if opt is true *)
   let lift opt (module F : S2S) (module X : Spec) = (module (val if opt then (module F (X)) else (module X) : Spec) : Spec) in
@@ -29,6 +30,12 @@ let get_spec () : (module Spec) =
             |> lift (get_bool "ana.opt.hashcons") (module HashconsLifter)
           ) in
   (module S1)
+)
+
+(** gets Spec for current options *)
+let get_spec (): (module Spec) =
+  Lazy.force spec_module
+
 
 (** Given a [Cfg], a [Spec], and an [Inc], computes the solution to [MCP.Path] *)
 module AnalyzeCFG (Cfg:CfgBidir) (Spec:Spec) (Inc:Increment) =
