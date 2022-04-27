518e8870a5cdbcde352146329473be4d66ffaccd
stilscher <66023521+stilscher@users.noreply.github.com>
2022-04-27 16:32:40 +0200

    sparse accumulative

--- scripts/incremental/analyze_accumulative.py
+++ scripts/incremental/analyze_accumulative.py
@@ -20,7 +20,7 @@ build_compdb  = "build_compdb_zstd.sh" #sys.argv[4]
 conf          = "big-benchmarks1" #sys.argv[5]
 begin         = datetime(2021,8,1) #datetime.strptime(sys.argv[6], '%Y/%m/%d')
 from_c        = 0 #int(sys.argv[7])
-to_c          = 50 #int(sys.argv[8])
+to_c          = 51 #int(sys.argv[8])
 diff_exclude  = ["build", "doc", "examples", "tests", "zlibWrapper", "contrib"]
 ################################################################################
 
@@ -47,30 +47,28 @@ def analyze_series_in_repo():
 
     prev_commit = ""
 
-    for index, commit in enumerate(itertools.islice(Repository(url, since=begin, only_in_branch='dev', clone_repo_to=cwd).traverse_commits(), from_c, to_c)):
+    for commit in itertools.islice(Repository(url, since=begin, only_in_branch='dev', order='topo-order', clone_repo_to=cwd).traverse_commits(), from_c, to_c):
         gr = Git(repo_path)
 
         print("\n" + commit.hash)
         print('changed LOC: ', commit.lines)
         print('merge commit: ', commit.merge)
 
-        # for accumulative setup this filtering does not make sense!
-        # skip merge commits and commits that have less than maxCLOC of relevant code changes
-        relCLOC = utils.calculateRelCLOC(repo_path, commit, diff_exclude) # use this to filter commits by actually relevant changes
-        # print("relCLOC: ", relCLOC)
-        # if maxCLOC is not None and relCLOC > maxCLOC:
-        #     print('Skip this commit: merge commit or too many relevant changed LOC')
-        #     count_skipped+=1
-        #     continue
+        # find a path of sequential commits in the repository
+        if not(prev_commit == "" or prev_commit in commit.parents):
+            print("Skip commit because it is not on same path")
+            continue;
+
+        relCLOC = utils.calculateRelCLOC(repo_path, commit, diff_exclude)
 
         # analyze
-        try_num = from_c + count_analyzed + count_failed + 1
+        try_num = count_analyzed + count_failed
         outtry = os.path.join(outdir, str(try_num))
         os.makedirs(outtry)
         with open(os.path.join(outtry,'commit_properties.log'), "w+") as file:
             json.dump({"hash": commit.hash, "parent_hash": prev_commit, "CLOC": commit.lines, "relCLOC": relCLOC}, file)
 
-        if index == 0:
+        if try_num == 0:
             # analyze initial commit non-incrementally
             try:
                 print('Analyze ', str(commit.hash), ' as initial commit.')
@@ -85,26 +83,35 @@ def analyze_series_in_repo():
         else:
             # analyze every following commit based on the latest previous commit for which the analysis succeeded
             try:
-                print('Analyze', str(commit.hash), 'incrementally (#', try_num, ').')
                 if os.path.isdir("backup_incremental_data"):
                     shutil.rmtree("backup_incremental_data")
                 shutil.copytree("incremental_data", "backup_incremental_data")
-                # analyze commit non-incrementally and save run for comparison
-                out_nonincr = os.path.join(outtry, 'non-incr')
-                os.makedirs(out_nonincr)
-                file_original_run = os.path.join(out_nonincr, "compare-data-nonincr")
-                add_options = ['--enable', 'incremental.only-rename', '--enable', 'incremental.verify', '--set', 'save_run', file_original_run]
-                utils.analyze_commit(analyzer_dir, gr, repo_path, build_compdb, commit.hash, out_nonincr, conf, add_options)
+
+                # compare only for every 10th run
+                if try_num % 10 == 0:
+                    # analyze commit non-incrementally and save run for comparison
+                    print('Analyze', str(commit.hash), 'non-incrementally (#', try_num, ').')
+                    out_nonincr = os.path.join(outtry, 'non-incr')
+                    os.makedirs(out_nonincr)
+                    file_original_run = os.path.join(out_nonincr, "compare-data-nonincr")
+                    add_options = ['--enable', 'incremental.only-rename', '--enable', 'incremental.verify', '--set', 'save_run', file_original_run]
+                    utils.analyze_commit(analyzer_dir, gr, repo_path, build_compdb, commit.hash, out_nonincr, conf, add_options)
+
                 # analyze commit incrementally based on the previous commit and save run for comparison
+                print('Analyze', str(commit.hash), 'incrementally (#', try_num, ').')
                 out_incr = os.path.join(outtry, 'incr')
                 os.makedirs(out_incr)
                 file_incremental_run = os.path.join(out_incr, "compare-data-incr")
                 add_options = ['--enable', 'incremental.load', '--enable', 'incremental.save', '--enable', 'incremental.reluctant.on', '--enable', 'incremental.verify', '--set', 'save_run', file_incremental_run]
                 utils.analyze_commit(analyzer_dir, gr, repo_path, build_compdb, commit.hash, out_incr, conf, add_options)
-                # compare stored data of original and incremental run
-                out_compare = os.path.join(outtry, 'compare')
-                os.makedirs(out_compare)
-                utils.compare_runs(analyzer_dir, dummy_c_file, out_compare, conf, file_incremental_run, file_original_run)
+
+                if try_num % 10 == 0:
+                    # compare stored data of original and incremental run
+                    print('Compare both runs.')
+                    out_compare = os.path.join(outtry, 'compare')
+                    os.makedirs(out_compare)
+                    utils.compare_runs(analyzer_dir, dummy_c_file, out_compare, conf, file_incremental_run, file_original_run)
+
                 count_analyzed += 1
                 prev_commit = commit.hash
             except utils.subprocess.CalledProcessError as e:
@@ -112,7 +119,7 @@ def analyze_series_in_repo():
                 shutil.rmtree("incremental_data")
                 shutil.copytree("backup_incremental_data", "incremental_data")
                 count_failed+=1
-        analyzed_commits[try_num]=(str(commit.hash)[:6], relCLOC)
+        #analyzed_commits[try_num]=(str(commit.hash)[:6], relCLOC)
 
 if os.path.exists(outdir) and os.path.isdir(outdir):
    shutil.rmtree(outdir)
