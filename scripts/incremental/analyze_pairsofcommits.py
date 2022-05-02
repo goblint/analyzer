185f4c3fadbbbbcf5e58c5f8264f10014fdf9882
stilscher <66023521+stilscher@users.noreply.github.com>
2022-05-02 16:17:28 +0200

    refactor efficiency script into one

--- scripts/incremental/analyze_pairsofcommits.py
+++ scripts/incremental/analyze_pairsofcommits.py
@@ -1,11 +1,15 @@
 from pydriller import Repository, Git
+import utils
+import psutil
+import multiprocessing as mp
 import os
-from pathlib import Path
 import subprocess
 import itertools
 import shutil
 import re
 import json
+from datetime import datetime
+import sys
 import pandas as pd
 import matplotlib
 matplotlib.use("pgf")
@@ -21,46 +25,44 @@ matplotlib.rcParams.update(
 )
 import matplotlib.pyplot as plt
 
+################################################################################
+# Usage: python3 incremental_smallcommits.py <full_path_analyzer_dir> <repo_url> <repo_name> <name_of_build_script>
+#     <name_of_config> <begin> <from_commit_index> <to_commit_index>
+# Executing the script will overwrite the directory 'result_efficiency' in the cwd.
+# The script for building the compilation database is assumed to be found in the analyzers script directory and the
+# config file is assumed to be found in the conf directory of the analyzers repository.
+if len(sys.argv) != 3:
+      print("Wrong number of parameters.\nUse script like this: python3 parallel_benchmarking.py <path to goblint directory> <number of processes>")
+      exit()
+result_dir = os.path.join(os.getcwd(), 'result_efficiency')
+maxCLOC       = 50
+url           = "https://github.com/facebook/zstd"
+repo_name     = "zstd"
+build_compdb  = "build_compdb_zstd.sh"
+conf          = "big-benchmarks1"
+begin         = datetime(2021,8,1)
+to            = datetime(2022,2,1)
+diff_exclude  = ["build", "doc", "examples", "tests", "zlibWrapper", "contrib"]
+analyzer_dir  = sys.argv[1]
+try:
+    numcores = int(sys.argv[2])
+except ValueError:
+    print("Parameter should be a number.\nUse script like this: python3 parallel_benchmarking.py <path to goblint directory> <number of processes>")
+    exit()
+################################################################################
 
-def reset_incremental_data():
-    incr_data_dir = os.path.join(cwd, 'incremental_data')
+
+def reset_incremental_data(incr_data_dir):
     if os.path.exists(incr_data_dir) and os.path.isdir(incr_data_dir):
         shutil.rmtree(incr_data_dir)
 
-def analyze_commit(gr, commit_hash, outdir, extra_options):
-    gr.checkout(commit_hash)
+def analyze_small_commits_in_repo(cwd, outdir, from_c, to_c):
+    count_analyzed = 0
+    count_skipped = 0
+    count_failed = 0
+    analyzed_commits = {}
+    repo_path = os.path.join(cwd, repo_name)
 
-    prepare_command = ['sh', os.path.join(analyzer_dir, 'scripts', build_compdb)]
-    with open(outdir+'/prepare.log', "w+") as outfile:
-        subprocess.run(prepare_command, cwd = repo_path, check=True, stdout=outfile, stderr=subprocess.STDOUT)
-        outfile.close()
-
-    analyze_command = [os.path.join(analyzer_dir, 'goblint'), '--conf', os.path.join(analyzer_dir, 'conf', conf + '.json'), *extra_options, repo_path]
-    with open(outdir+'/analyzer.log', "w+") as outfile:
-        subprocess.run(analyze_command, check=True, stdout=outfile, stderr=subprocess.STDOUT)
-        outfile.close()
-
-def calculateRelCLOC(commit):
-    relcloc = 0
-    for f in commit.modified_files:
-        _, extension = os.path.splitext(f.filename)
-        if not (extension == ".h" or extension == ".c"):
-            continue
-        filepath = f.new_path
-        if filepath is None:
-            filepath = f.old_path
-        parents = Path(filepath).parents
-        parents = list(map(lambda x: os.path.join(repo_path, x), parents))
-        if any(dir in parents for dir in paths_to_exclude):
-            continue
-        relcloc = relcloc + f.added_lines + f.deleted_lines
-    return relcloc
-
-def analyze_small_commits_in_repo():
-    global count_analyzed
-    global count_skipped
-    global count_failed
-    global analyzed_commits
     for commit in itertools.islice(Repository(url, since=begin, to=to, only_no_merge=True, clone_repo_to=cwd).traverse_commits(), from_c, to_c):
         gr = Git(repo_path)
 
@@ -69,7 +71,7 @@ def analyze_small_commits_in_repo():
         #print('merge commit: ', commit.merge)
 
         # skip merge commits and commits that have less than maxCLOC of relevant code changes
-        relCLOC = calculateRelCLOC(commit) # use this to filter commits by actually relevant changes
+        relCLOC = utils.calculateRelCLOC(repo_path, commit, diff_exclude) # use this to filter commits by actually relevant changes
         #print("relCLOC: ", relCLOC)
         if maxCLOC is not None and relCLOC > maxCLOC:
             #print('Skip this commit: merge commit or too many relevant changed LOC')
@@ -82,32 +84,32 @@ def analyze_small_commits_in_repo():
         parent = gr.get_commit(commit.parents[0])
         #print('Analyze this commit incrementally. #', try_num)
 
-        reset_incremental_data()
+        reset_incremental_data(os.path.join(cwd, 'incremental_data'))
         failed = True
         try:
             #print('Starting from parent', str(parent.hash), ".")
             outparent = os.path.join(outtry, 'parent')
             os.makedirs(outparent)
             add_options = ['--disable', 'incremental.load', '--enable', 'incremental.save']
-            analyze_commit(gr, parent.hash, outparent, add_options)
+            utils.analyze_commit(analyzer_dir, gr, repo_path, build_compdb, parent.hash, outparent, conf, add_options)
 
             #print('And now analyze', str(commit.hash), 'incrementally.')
             outchild = os.path.join(outtry, 'child')
             os.makedirs(outchild)
             add_options = ['--enable', 'incremental.load', '--disable', 'incremental.save']
-            analyze_commit(gr, commit.hash, outchild, add_options)
+            utils.analyze_commit(analyzer_dir, gr, repo_path, build_compdb, commit.hash, outchild, conf, add_options)
 
             #print('And again incremental, this time reluctantly')
             outchildrel = os.path.join(outtry, 'child-rel')
             os.makedirs(outchildrel)
             add_options = ['--enable', 'incremental.load', '--disable', 'incremental.save', '--enable', 'incremental.reluctant.on']
-            analyze_commit(gr, commit.hash, outchildrel, add_options)
+            utils.analyze_commit(analyzer_dir, gr, repo_path, build_compdb, commit.hash, outchildrel, conf, add_options)
 
             #print('And again incremental, this time with exhaustive restarting')
             outchildexrest = os.path.join(outtry, 'child-ex-rest')
             os.makedirs(outchildexrest)
             add_options = ['--enable', 'incremental.load', '--disable', 'incremental.save', '--enable', 'incremental.reluctant.on', '--enable', 'incremental.restart.sided.enabled']
-            analyze_commit(gr, commit.hash, outchildexrest, add_options)
+            utils.analyze_commit(analyzer_dir, gr, repo_path, build_compdb, parent.hash, outchildexrest, conf, add_options)
 
             '''
             #print('And again incremental, this time with cfg comparison')
@@ -125,6 +127,12 @@ def analyze_small_commits_in_repo():
         with open(os.path.join(outtry,'commit_properties.log'), "w+") as file:
             json.dump({"hash": commit.hash, "parent_hash": parent.hash, "CLOC": commit.lines, "relCLOC": relCLOC, "failed": failed}, file)
         analyzed_commits[try_num]=(str(commit.hash)[:6], relCLOC)
+
+    num_commits = count_analyzed + count_skipped + count_failed
+    print("\nCommits traversed in total: ", num_commits)
+    print("Analyzed: ", count_analyzed)
+    print("Failed: ", count_failed)
+    print("Skipped: ", count_skipped)
 
 
 def extract_from_analyzer_log(log):
@@ -147,7 +155,7 @@ def extract_from_analyzer_log(log):
     return d
 
 
-def collect_data():
+def collect_data(outdir):
     index = []
     data = {"Failed?": [], "Changed LOC": [], "Relevant changed LOC": [], "Changed/Added/Removed functions": [],
       "Runtime for parent commit (non-incremental)": [], "Runtime for commit (incremental)": [],
@@ -201,49 +209,70 @@ def plot(data_set):
     plt.tight_layout()
     plt.savefig("figure.pdf")
 
+def runperprocess(core, from_c, to_c):
+    psutil.Process().cpu_affinity([core])
+    cwd  = os.getcwd()
+    outdir = os.path.join(cwd, 'out')
+    if os.path.exists(outdir) and os.path.isdir(outdir):
+      shutil.rmtree(outdir)
+    analyze_small_commits_in_repo(cwd, outdir, from_c, to_c)
+    data = collect_data(outdir)
+    plot(data)
 
-def main(full_path_analyzer, url_arg, repo_name_arg, build_script, conf_arg, begin_date, to_date, start, end):
-  global analyzer_dir
-  global url
-  global repo_name
-  global build_compdb
-  global conf
-  global begin
-  global to
-  global from_c, to_c
-  global analyzed_commits, count_analyzed, count_skipped, count_failed
-  global cwd, outdir, repo_path, paths_to_exclude, maxCLOC
+def analyze_chunks_of_commits_in_parallel():
+    avail_phys_cores = psutil.cpu_count(logical=False)
+    allowedcores = avail_phys_cores - 2
+    if numcores > allowedcores:
+        print("Not enough physical cores on this maching (exist: ", avail_phys_cores, " allowed: ", allowedcores, ")")
+        exit()
+    # For equal load distribution, choose a processes to core mapping,
+    # use only physical cores and have an equal number of processes per cache.
+    # The layout of physical/logical cores and sharing of caches is machine dependent. To find out use: 'lscpu --all --extended'.
+    # For our test server:
+    coremapping1 = [i for i in range(numcores - numcores//2)]
+    coremapping2 = [i for i in range(avail_phys_cores//2, avail_phys_cores//2 + numcores//2)]
+    coremapping = [coremapping1[i//2] if i%2==0 else coremapping2[i//2] for i in range(len(coremapping1) + len(coremapping2))]
+    processes = []
 
-  analyzer_dir = full_path_analyzer
-  url           = url_arg
-  repo_name     = repo_name_arg
-  build_compdb  = build_script
-  conf          = conf_arg
-  begin         = begin_date
-  to            = to_date
-  from_c        = start
-  to_c          = end
-  maxCLOC       = 50
-  dirs_to_exclude  = ["build", "doc", "examples", "tests", "zlibWrapper", "contrib"]
+    # calculate number of interesting commits
+    num_commits = sum(1 for _ in Repository(url, since=begin, to=to, only_no_merge=True).traverse_commits())
+    print("Number of potentially interesting commits:", num_commits)
+    perprocess = num_commits // numcores
 
-  cwd  = os.getcwd()
-  outdir = os.path.join(cwd, 'out')
-  repo_path = os.path.normpath(os.path.join(cwd, repo_name))
-  paths_to_exclude = list(map(lambda x: os.path.join(repo_path, x), dirs_to_exclude))
+    for i in range(numcores):
+        dir = "process" + str(i)
+        os.mkdir(dir)
+        os.chdir(dir)
+        # run script
+        start = perprocess * i
+        end = perprocess * (i + 1) if i < numcores - 1 else num_commits
+        p = mp.Process(target=runperprocess, args=[coremapping[i], start, end])
+        p.start()
+        processes.append(p)
+        # time.sleep(random.randint(5,60)) # add random delay between process creation to try to reduce interference
+        os.chdir(result_dir)
 
-  analyzed_commits = {}
-  count_analyzed = 0
-  count_skipped = 0
-  count_failed = 0
+    for p in processes:
+        p.join()
 
-  if os.path.exists(outdir) and os.path.isdir(outdir):
-    shutil.rmtree(outdir)
-  analyze_small_commits_in_repo()
-  num_commits = count_analyzed + count_skipped + count_failed
-  print("\nCommits traversed in total: ", num_commits)
-  print("Analyzed: ", count_analyzed)
-  print("Failed: ", count_failed)
-  print("Skipped: ", count_skipped)
+def merge_results():
+    filename = "results.csv"
+    frames = []
+    for process_dir in os.listdir("."):
+        path = os.path.join(process_dir, filename)
+        if os.path.exists(path):
+            t = pd.read_csv(path, index_col=0)
+            frames.append(t)
+    if len(frames) > 0:
+        df = pd.concat(frames)
+        df.sort_index(inplace=True, key=lambda idx: idx.map(lambda x: int(x.split(":")[0])))
+        df.to_csv('total_results.csv')
 
-  data = collect_data()
-  plot(data)
+
+if os.path.exists(result_dir):
+    shutil.rmtree(result_dir)
+os.mkdir(result_dir)
+os.chdir(result_dir)
+
+analyze_chunks_of_commits_in_parallel()
+merge_results()
