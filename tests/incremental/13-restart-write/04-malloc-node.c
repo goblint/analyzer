ed2ad3a42676c204df897f2a4986df59f931ed46
Simmo Saan <simmo.saan@gmail.com>
2022-05-03 11:29:54 +0300

    Annotate incremental 13-restart-write/04-malloc-node for races

--- tests/incremental/13-restart-write/04-malloc-node.c
+++ tests/incremental/13-restart-write/04-malloc-node.c
@@ -3,7 +3,7 @@
 
 void *t_fun(void *arg) {
   int *iarg = (int*) arg;
-  *iarg = 1;
+  *iarg = 1; // RACE (without mallocFresh)
   return NULL;
 }
 
@@ -13,7 +13,7 @@ int main() {
 
   for (int i = 0; i < 10; i++) {
     iarg = malloc(sizeof(int));
-    *iarg = 0;
+    *iarg = 0; // RACE (without mallocFresh)
     pthread_create(&id, NULL, t_fun, (void*) iarg);
   }
 
