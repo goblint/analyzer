51941d0e01da85a45796c5f3297fdf0309d6731f
Simmo Saan <simmo.saan@gmail.com>
2022-05-10 10:49:44 +0300

    Add LibraryDsl.Pattern.Expected

--- src/analyses/libraryDsl.ml
+++ src/analyses/libraryDsl.ml
@@ -4,13 +4,16 @@ module Pattern =
 struct
   type ('a, 'k, 'r) t = 'a -> 'k -> 'r
 
+  exception Expected of string
+  let fail s = raise (Expected s)
+
   let (__): _ t = fun x k -> k x
   let drop: _ t = fun _ k -> k
 
   let nil: _ t = fun x k ->
     match x with
     | [] -> k
-    | _ -> invalid_arg "nil"
+    | _ -> fail "nil"
 
   let ( ^:: ) (p1: _ t) (p2: _ t): _ t = fun x k ->
     match x with
@@ -18,7 +21,7 @@ struct
       let k = p1 x1 k in
       let k = p2 x2 k in
       k
-    | [] -> invalid_arg "^::"
+    | [] -> fail "^::"
 
   let map (p: _ t) ~(f): _ t = fun x k -> p x (f k)
   let map_result (p: _ t) ~(f): _ t = fun x k -> f (p x k)
