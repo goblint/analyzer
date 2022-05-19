c7e747d7cd6a5e2952a9930436806bb51999d8a7
Michael Schwarz <michael.schwarz93@gmail.com>
2022-05-19 17:15:29 +0200

    Add problematic example

---
+++ tests/regression/46-apron2/06-pointer-multilevel-two.c
@@ -0,0 +1,22 @@
+// SKIP PARAM: --set solver td3 --set ana.activated "['base','threadid','threadflag','mallocWrapper','apron','escape']" --set ana.base.privatization none --set ana.apron.privatization dummy
+extern int __VERIFIER_nondet_int();
+
+void change(int *p,int i) {
+    (*p)++;
+    int* ptr = &p;
+    assert(*p == 6);
+}
+
+int g;
+int main() {
+    int c = __VERIFIER_nondet_int();
+    g = 3;
+    assert(g != 3); // FAIL
+    assert(g == 3);
+    int a = 5;
+    int *p = &a;
+    change(p, a);
+    assert(a == 5); //FAIL
+    assert(a - 6 == 0); // Apron currently finds \bot here (!)
+    return 0;
+}
