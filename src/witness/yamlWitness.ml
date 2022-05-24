a558f0f457fda2d6fe71df3a8ed32a395885a7ee
Simmo Saan <simmo.saan@gmail.com>
2022-05-24 11:49:54 +0300

    Only emit yaml entries for Statement nodes

--- src/witness/yamlWitness.ml
+++ src/witness/yamlWitness.ml
@@ -51,44 +51,49 @@ struct
     let nh = join_contexts lh in
 
     let yaml_entries = NH.fold (fun n local acc ->
-        let context: Invariant.context = {
-            scope=Node.find_fundec n;
-            i = -1;
-            lval=None;
-            offset=Cil.NoOffset;
-            deref_invariant=(fun _ _ _ -> Invariant.none) (* TODO: should throw instead? *)
-          }
-        in
-        match Spec.D.invariant context local with
-        | Some inv ->
-          let inv = InvariantCil.exp_replace_original_name inv in
-          let loc = Node.location n in
-          let uuid = Uuidm.v4_gen uuid_random_state () in
-          let entry = `O [
-              ("entry_type", `String "loop_invariant");
-              ("metadata", `O [
-                  ("format_version", `String "0.1");
-                  ("uuid", `String (Uuidm.to_string uuid));
-                  ("creation_time", yaml_creation_time);
-                  ("producer", yaml_producer);
-                  ("task", yaml_task);
-                ]);
-              ("location", `O [
-                  ("file_name", `String loc.file);
-                  ("file_hash", `String (sha256_file loc.file));
-                  ("line", `Float (float_of_int loc.line));
-                  ("column", `Float (float_of_int (loc.column - 1)));
-                  ("function", `String (Node.find_fundec n).svar.vname);
-                ]);
-              ("loop_invariant", `O [
-                  ("string", `String (CilType.Exp.show inv));
-                  ("type", `String "assertion");
-                  ("format", `String "C");
-                ]);
-            ]
+        match n with
+        | Statement _ ->
+          let context: Invariant.context = {
+              scope=Node.find_fundec n;
+              i = -1;
+              lval=None;
+              offset=Cil.NoOffset;
+              deref_invariant=(fun _ _ _ -> Invariant.none) (* TODO: should throw instead? *)
+            }
           in
-          entry :: acc
-        | None ->
+          begin match Spec.D.invariant context local with
+            | Some inv ->
+              let inv = InvariantCil.exp_replace_original_name inv in
+              let loc = Node.location n in
+              let uuid = Uuidm.v4_gen uuid_random_state () in
+              let entry = `O [
+                  ("entry_type", `String "loop_invariant");
+                  ("metadata", `O [
+                      ("format_version", `String "0.1");
+                      ("uuid", `String (Uuidm.to_string uuid));
+                      ("creation_time", yaml_creation_time);
+                      ("producer", yaml_producer);
+                      ("task", yaml_task);
+                    ]);
+                  ("location", `O [
+                      ("file_name", `String loc.file);
+                      ("file_hash", `String (sha256_file loc.file));
+                      ("line", `Float (float_of_int loc.line));
+                      ("column", `Float (float_of_int (loc.column - 1)));
+                      ("function", `String (Node.find_fundec n).svar.vname);
+                    ]);
+                  ("loop_invariant", `O [
+                      ("string", `String (CilType.Exp.show inv));
+                      ("type", `String "assertion");
+                      ("format", `String "C");
+                    ]);
+                ]
+              in
+              entry :: acc
+            | None ->
+              acc
+          end
+        | _ -> (* avoid FunctionEntry/Function because their locations are not inside the function where assert could be inserted *)
           acc
       ) nh []
     in
