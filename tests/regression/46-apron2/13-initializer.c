df4d2d8931ce8b0ab6eb00b4a679617ceb46e575
Michael Schwarz <michael.schwarz93@gmail.com>
2022-06-08 13:47:17 +0200

    Rename initializer test and mark as TODO

--- tests/regression/46-apron2/13-initializer.c
+++ tests/regression/46-apron2/13-initializer.c
@@ -1,5 +1,4 @@
 // SKIP PARAM: --set solver td3 --set ana.activated "['base','threadid','threadflag','mallocWrapper','apron','escape']" --set ana.path_sens[+] threadflag --set ana.base.privatization none --set ana.apron.privatization mutex-meet-tid
-// Copy of 45 01 for apron
 #include <pthread.h>
 #include <assert.h>
 #include <stdio.h>
@@ -8,25 +7,17 @@ int g, h;
 
 void *foo(void* p){
     sleep(2);
-    int* ip = ((int*) p);
-    printf("ip is %d\n", *ip);
-    *ip = 42;
     return NULL;
 }
 
 int main(){
-    int x = 0;
-    int *xp = &x;
     pthread_t thread;
     int y;
     g = y;
     h = y;
-    pthread_create(&thread, NULL, foo, xp);
-    sleep(4); // to make sure that we actually fail the assert when running.
-    assert(x == 42); //UNKNOWN!
-    assert(x == 0); //UNKNOWN!
-    assert(x <= 50);
-    assert(g==h);
+    assert(g == h);
+    pthread_create(&thread, NULL, foo, NULL);
+    assert(g == h); //TODO We would like to be able to prove that this holds (but can't as we lose g = h)
     pthread_join(thread, NULL);
     return 0;
 }
