11a1f05f82471dec376f964d24c29d5edd0e4750
Simmo Saan <simmo.saan@gmail.com>
2022-06-16 10:07:35 +0300

    Add witness tests for base LOr handling

---
+++ tests/regression/56-witness/01-base-lor-enums.c
@@ -0,0 +1,33 @@
+// PARAM: --enable ana.int.enums --set witness.yaml.validate 01-base-lor-enums.yml
+
+int main() {
+  int r; // rand
+  int x;
+  switch (r) {
+    case 0:
+      x = 1;
+      break;
+    case 1:
+      x = 3;
+      break;
+    default:
+      x = 6;
+      break;
+  }
+  ; // SUCCESS (witness)
+  ; // UNKNOWN (witness)
+
+  int y;
+  switch (r) {
+    case 0:
+      y = 11;
+      break;
+    default:
+      y = x;
+      break;
+  }
+  ; // UNKNOWN (witness)
+  ; // SUCCESS (witness)
+  ; // UNKNOWN (witness)
+  return 0;
+}
