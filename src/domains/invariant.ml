b7cfc40dd170f7b400dd7704e456d5efc36b92d2
Simmo Saan <simmo.saan@gmail.com>
2022-06-23 10:31:06 +0300

    Add comments about symbolic invariant lattice

--- src/domains/invariant.ml
+++ src/domains/invariant.ml
@@ -1,22 +1,28 @@
 open Cil
 
+(** Symbolic (and fully syntactic) expression "lattice". *)
 module ExpLat =
 struct
   include CilType.Exp
 
   let bot () = zero (* false *)
   let top () = one (* true *)
-  let is_bot _ = failwith "ExpLat: is_bot"
-  let is_top _ = failwith "ExpLat: is_top"
+  let is_bot _ = failwith "ExpLat: is_bot" (* cannot say for sure, many contradictions exist *)
+  let is_top _ = failwith "ExpLat: is_top" (* cannot say for sure, many tautologies exist *)
 
-  let leq _ _ = failwith "ExpLat: leq"
-  let pretty_diff () _ = failwith "ExpLat: pretty_diff"
+  let leq _ _ = failwith "ExpLat: leq" (* cannot say for sure, requires general entailment check *)
+  let pretty_diff () _ = failwith "ExpLat: pretty_diff" (* irrelevant, no leq *)
 
+  (* join and meet are not idempotent, commutative and associative,
+     but it shouldn't be of issue since there's no leq either.
+     Would need at least AC-rewriting, if not more, to ensure those symbolically *)
   let join x y = BinOp (LOr, x, y, intType)
   let meet x y = BinOp (LAnd, x, y, intType)
   let widen x y = y
   let narrow = meet
 end
+
+(** Lift {!ExpLat} such that join/meet folds don't introduce excessive [|| 0|] or [&& 1] expressions. *)
 
 module N =
 struct
