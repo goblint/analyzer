1caaa79035ae362fd36fc4a7cddefc7f99021120
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2022-07-04 14:42:34 +0200

    Removed diffs.

--- tests/incremental/06-glob-var-rename/diffs/00-simple_rename.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int bar = 1;
-
-int main() {
-    printf("%d", bar);
-
-    return 0;
-}
--- tests/incremental/06-glob-var-rename/diffs/02-add_new_gvar.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int myVar = 1;
-int foo = 1;
-
-int main() {
-    printf("%d", myVar);
-    printf("%d", foo);
-}
