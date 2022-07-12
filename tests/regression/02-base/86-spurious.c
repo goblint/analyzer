ebbc5843d4aa677f0b5c9fbca41721cfb20f2bc5
Michael Schwarz <michael.schwarz93@gmail.com>
2022-07-12 14:49:30 +0200

    add nowarn test for #765

---
+++ tests/regression/02-base/86-spurious.c
@@ -0,0 +1,24 @@
+#include<pthread.h>
+#include<assert.h>
+int counter = 0;
+pthread_mutex_t lock1;
+
+void* producer(void* param) {
+    pthread_mutex_lock(&lock1);
+    counter = 0;
+    pthread_mutex_unlock(&lock1);
+}
+
+void* consumer(void* param) {
+    pthread_mutex_lock(&lock1);
+    assert(counter >= 0); //NOWARN
+    pthread_mutex_unlock(&lock1);
+}
+
+
+int main() {
+    pthread_t thread;
+    pthread_t thread2;
+    pthread_create(&thread,NULL,producer,NULL);
+    pthread_create(&thread2,NULL,consumer,NULL);
+}
