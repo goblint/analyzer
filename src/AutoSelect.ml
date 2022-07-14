60e58d44bc5f1e185fc0c6dc85200e68dd4ef32e
Manuel Pietsch <manuelpietsch@outlook.de>
2022-07-14 02:06:18 +0200

    Added Collection of factors to predict complexity fixed handling of __builtin functions when setting congruence attributes

--- src/AutoSelect.ml
+++ src/AutoSelect.ml
@@ -1,9 +1,97 @@
 open GobConfig
 open Cil
 
+(*Collect stats to be able to make decisions*)
+type complexityFactors = {
+  mutable functions : int; (*function definitions. Does not include extern functions, but functions that get added by goblint (e.g. bsearch or __VERIFIER_nondet_pointer (sv-comp))*)
+  mutable functionCalls : int; (*places where functions are called*)
+  mutable loops : int;
+  mutable loopBreaks : int; (*Breaks and continues. Cil converts the loop conditions to a break. Only set if collection is done before prepareCFG, afterwards they are replaced by GOTOs*)
+  mutable controlFlowStatements : int; (*Return, Goto, break, continue, if, switch. Includes at least one (implicit) return in each function*)
+  mutable expressions : int; (* recursively. e.g. x = x + 1 has 4 expressions*)
+  mutable instructions : int; (*function calls and assignments*)
+  mutable integralVars : (int * int); (*global, local. Does not consider the types that a pointer/array contains*)
+  mutable arrayVars : (int * int); 
+  mutable pointerVars : (int * int);
+}
+
+let printFactors f = 
+  Printf.printf "functions: %d\n" f.functions;
+  Printf.printf "functionCalls: %d\n" f.functionCalls;
+  Printf.printf "loops: %d\n" f.loops;
+  Printf.printf "loopBreaks: %d\n" f.loopBreaks;
+  Printf.printf "controlFlowStatements: %d\n" f.controlFlowStatements;
+  Printf.printf "expressions: %d\n" f.expressions;
+  Printf.printf "instructions: %d\n" f.instructions;
+  Printf.printf "integralVars: (%d,%d)\n" (fst f.integralVars) (snd f.integralVars);
+  Printf.printf "arrayVars: (%d,%d)\n" (fst f.arrayVars) (snd f.arrayVars);
+  Printf.printf "pointerVars: (%d,%d)\n" (fst f.pointerVars) (snd f.pointerVars);
+  flush stdout;
+
+class collectComplexityFactorsVisitor(factors) = object
+  inherit nopCilVisitor
+
+  method! vfunc _ = factors.functions <- factors.functions + 1; DoChildren
+
+  method! vvdec var =
+    let incVar (g,l) = if var.vglob then (g + 1, l) else (g, l+1) in
+    (if isIntegralType var.vtype then 
+      factors.integralVars <- incVar factors.integralVars
+    else if isPointerType var.vtype then 
+      factors.pointerVars <- incVar factors.pointerVars 
+    else if isArrayType var.vtype then 
+      factors.arrayVars <- incVar factors.arrayVars
+    ); DoChildren
+
+  method! vexpr _ = factors.expressions <- factors.expressions + 1; DoChildren
+
+  method! vinst = function 
+    | Set _ -> 
+      factors.instructions <- factors.instructions + 1; DoChildren
+    | Call (Some _, _,_,_,_) -> 
+      factors.instructions <- factors.instructions + 2; (*Count function call and assignment of the result seperately *)
+      factors.functionCalls <- factors.functionCalls + 1; DoChildren
+    | Call _ -> 
+      factors.instructions <- factors.instructions + 1;
+      factors.functionCalls <- factors.functionCalls + 1; DoChildren 
+    | _ -> DoChildren
+
+  method! vstmt stmt = match stmt.skind with 
+    | Loop _ -> 
+      factors.controlFlowStatements <- factors.controlFlowStatements + 1;
+      factors.loops <- factors.loops + 1; DoChildren
+    | If _
+    | Switch _
+    | Goto _
+    | ComputedGoto _
+    | Return _ -> factors.controlFlowStatements <- factors.controlFlowStatements + 1; DoChildren
+    | Break _
+    | Continue _ -> 
+      factors.controlFlowStatements <- factors.controlFlowStatements + 1;
+      factors.loopBreaks <- factors.loopBreaks + 1; DoChildren
+    | _ -> DoChildren
+
+end
+
+let collectFactors visitAction visitedObject =
+  let factors = {
+    functions = 0;
+    functionCalls = 0;
+    loops = 0;
+    loopBreaks = 0;
+    controlFlowStatements = 0;
+    expressions = 0;
+    instructions = 0;
+    integralVars = (0,0);
+    arrayVars = (0,0);
+    pointerVars = (0,0);
+  } in
+  let visitor = new collectComplexityFactorsVisitor(factors) in
+  ignore (visitAction visitor visitedObject);
+  factors
+
 (*Create maps that map each function to the ones called in it and the ones calling it
    Only considers static calls!*)
-
 module FunctionSet = Set.Make(CilType.Varinfo)
 module FunctionCallMap = Map.Make(CilType.Varinfo)
 
@@ -56,9 +144,9 @@ let callingFunctions fd = ResettableLazy.force functionCallMaps |> fun (_,x,_) -
 let functionArgs fd = ResettableLazy.force functionCallMaps |> fun (_,_,x) -> x |> FunctionCallMap.find_opt fd
 
 (*Functions for determining if the congruence analysis should be enabled *)
-let isNotExtern = function
-  | Extern -> false
-  | _ -> true
+let isExtern = function
+  | Extern -> true
+  | _ -> false
 
 let rec setCongruenceRecursive fd depth neigbourFunction =
   if depth >= 0 then (
@@ -69,7 +157,7 @@ let rec setCongruenceRecursive fd depth neigbourFunction =
         setCongruenceRecursive (Cilfacade.find_varinfo_fundec vinfo) (depth -1) neigbourFunction
       ) 
       (FunctionSet.filter 
-        (fun x -> isNotExtern x.vstorage) 
+        (fun x -> not (isExtern x.vstorage || String.starts_with ~prefix:"__builtin" x.vname)) (*for extern and builtin functions there is no function definition in CIL*)
         (neigbourFunction fd.svar) 
       )
     ;
@@ -191,11 +279,13 @@ let chooseConfig file =
 
   disableIntervalContextsInRecursiveFunctions ();
 
-(*crashes because sometimes bigints are needed?
+  (*crashes because sometimes bigints are needed?
   print_endline @@ "Upper thresholds: " ^ String.concat " " @@ List.map (fun z -> string_of_int (Z.to_int z)) @@ WideningThresholds.upper_thresholds ();
   print_endline @@ "Lower thresholds: " ^ String.concat " " @@ List.map (fun z -> string_of_int (Z.to_int z)) @@ WideningThresholds.lower_thresholds ();*)
   if get_string "ana.specification" <> "" then focusOnSpecification ();
   if hasEnums file then set_bool "ana.int.enums" true;
-  reduceThreadAnalyses ()
+  reduceThreadAnalyses ();
+  printFactors @@ collectFactors visitCilFileSameGlobals file(*;
+  ignore (dumpFile plainCilPrinter stdout "?" file)*)
 
 let reset_lazy () = ResettableLazy.reset functionCallMaps
