goblint/analyzer

commit 3d27f41c665c60a6080d66ccfb64dd85404cb314

Separate n and size for calloc, it now takes two arguments

src/analyses/base.ml
@@ -2065,7 +2065,7 @@ struct
                                   (eval_lv ctx.ask gs st lv, `Address heap_var)]
         | _ -> st
       end
-    | `Calloc size ->
+    | `Calloc (n, size) ->
       begin match lv with
         | Some lv -> (* array length is set to one, as num*size is done when turning into `Calloc *)
           let heap_var = heap_var ctx in

src/analyses/libraryFunctions.ml
@@ -7,7 +7,7 @@ module M = Messages
 
 type categories = [
   | `Malloc       of exp
-  | `Calloc       of exp
+  | `Calloc       of exp * exp
   | `Realloc      of exp * exp
   | `Assert       of exp
   | `Lock         of bool * bool * bool  (* try? * write? * return  on success *)
@@ -37,12 +37,12 @@ let classify' fn exps =
     end
   | "kzalloc" ->
     begin match exps with
-      | size::_ -> `Calloc size
+      | size::_ -> `Calloc (Cil.one, size)
       | _ -> M.bailwith (fn^" arguments are strange!")
     end
   | "calloc" ->
     begin match exps with
-      | n::size::_ -> `Calloc Cil.(BinOp (Mult, n, size, typeOf one))
+      | n::size::_ -> `Calloc (n, size)
       | _ -> M.bailwith (fn^" arguments are strange!")
     end
   | "realloc" ->

src/analyses/libraryFunctions.mli
@@ -4,7 +4,7 @@ open Prelude.Ana
 
 type categories = [
   | `Malloc       of exp
-  | `Calloc       of exp
+  | `Calloc       of exp * exp
   | `Realloc      of exp * exp
   | `Assert       of exp
   | `Lock         of bool * bool * bool (* try? * write? *)

tests/regression/02-base/35-calloc_int.c
@@ -1,11 +1,11 @@
 #include<stdlib.h>
 #include<assert.h>
+
 int main(void) {
     int *r = calloc(5,sizeof(int));
-    //r[3] = 4;
     r[0] = 3;
 
     int z = r[1];
 
-    assert(z == 0); 
+    assert(z == 0); //UNKNOWN 
 }

commit c0da1e4ba5515596561deac6be1865c262b1d8f9

Fix incorrect size for calloc blobs (Introduced in 3d27f41c665c60a6080d66ccfb64dd85404cb314)

src/analyses/base.ml
@@ -2428,8 +2428,10 @@ struct
             if get_bool "sem.malloc.fail"
             then AD.join addr AD.null_ptr (* calloc can fail and return NULL *)
             else addr in
+          let ik = Cilfacade.ptrdiff_ikind () in
+          let blobsize = ID.mul (ID.cast_to ik @@ eval_int (Analyses.ask_of_ctx ctx) gs st size) (ID.cast_to ik @@ eval_int (Analyses.ask_of_ctx ctx) gs st n) in
           (* the memory that was allocated by calloc is set to bottom, but we keep track that it originated from calloc, so when bottom is read from memory allocated by calloc it is turned to zero *)
-          set_many ~ctx (Analyses.ask_of_ctx ctx) gs st [(add_null (AD.from_var heap_var), TVoid [], `Array (CArrays.make (IdxDom.of_int (Cilfacade.ptrdiff_ikind ()) BI.one) (`Blob (VD.bot (), eval_int (Analyses.ask_of_ctx ctx) gs st size, false))));
+          set_many ~ctx (Analyses.ask_of_ctx ctx) gs st [(add_null (AD.from_var heap_var), TVoid [], `Array (CArrays.make (IdxDom.of_int (Cilfacade.ptrdiff_ikind ()) BI.one) (`Blob (VD.bot (), blobsize, false))));
                                   (eval_lv (Analyses.ask_of_ctx ctx) gs st lv, (Cilfacade.typeOfLval lv), `Address (add_null (AD.from_var_offset (heap_var, `Index (IdxDom.of_int  (Cilfacade.ptrdiff_ikind ()) BI.zero, `NoOffset)))))]
         | _ -> st
       end