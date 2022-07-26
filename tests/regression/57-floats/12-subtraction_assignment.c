c6a31b64d9f980ef3e26bd26e4688788c80d310b
Michael Schwarz <michael.schwarz93@gmail.com>
2022-07-26 13:41:56 +0200

    Modify test 57/12 to actually trigger the issue

--- tests/regression/57-floats/12-subtraction_assignment.c
+++ tests/regression/57-floats/12-subtraction_assignment.c
@@ -3,19 +3,19 @@
 
 // previously failed in line 7 with "exception Invalid_argument("Cilfacade.get_ikind: non-integer type double ")"
 //(same error as in sv-comp: float-newlib/float_req_bl_0220a.c)
-// similar error also occured in the additional examples when branching on a float argument
+// similar error also occurred in the additional examples when branching on a float argument
 int main()
 {
   double z;
+  int x;
 
   z = 1 - 1.0;
 
   assert(z == 0.); // SUCCESS
 
-  if (0.)
-    ;
+  if (0.) { x = z;}
 
-  if (0 == (0. + 1.))
-    ;
+  if (0 == (0. + 1.)) { x = z;}
+
   assert(0); // FAIL
 }
