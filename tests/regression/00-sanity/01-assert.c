52bdd5a702b1c929d36e720d1153f8d703e7ca6e
Simmo Saan <simmo.saan@gmail.com>
2022-07-28 10:10:09 +0300

    Add separate tests for assert and __goblint_check

--- tests/regression/00-sanity/01-assert.c
+++ tests/regression/00-sanity/01-assert.c
@@ -1,15 +1,15 @@
-// just a few sanity checks on the asserts
-#include<assert.h>
+// just a few sanity checks on asserts
+#include <assert.h>
 
 int main() {
   int success = 1;
   int silence = 1;
   int fail = 0;
   int unknown;
-  // TODO: change back to assert?
-  __goblint_check(success);
-  __goblint_check(fail); // FAIL!
-  __goblint_check(unknown == 4); // UNKNOWN!
+  // intentionally using assert, specific order to work with assert refine
+  assert(success);
+  assert(unknown == 4); // UNKNOWN!
+  assert(fail); // FAIL!
   return 0;
-  __goblint_check(silence); // NOWARN!
+  assert(silence); // NOWARN!
 }
