768b4fbac03c1488d5879565ab58af5f93fc0c61
Simmo Saan <simmo.saan@gmail.com>
2022-07-29 15:05:53 +0300

    Add tests to show buggy Hoare domain usage

---
+++ tests/regression/00-sanity/34-hoare-over-addrs.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+
+int main() {
+  int arr[10];
+  int r; // rand
+  int *p;
+  if (r) {
+    p = &arr[1];
+    assert(1); // extra node in cfg
+  }
+  else {
+    p = &arr[2];
+    assert(1); // extra node in cfg
+  }
+  // TODO: even though we use Hoare for addresses per-varinfo
+  // and lattice operations are on offsets
+  // neither p is leq of another, so Hoare should keep both
+  // but address domain actually joins them nevertheless
+  return 0;
+}
