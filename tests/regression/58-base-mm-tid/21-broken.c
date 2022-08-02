726e568390c5ef45420ae81ff9dda05a05dba775
Michael Schwarz <michael.schwarz93@gmail.com>
2022-08-02 16:56:51 +0200

    Add broken example

---
+++ tests/regression/58-base-mm-tid/21-broken.c
@@ -0,0 +1,41 @@
+// PARAM: --set ana.activated[+] symb_locks --set ana.activated[+] var_eq --set exp.extraspecials[+] ZSTD_customMalloc  --set ana.base.privatization mutex-meet-tid --set ana.path_sens[+] threadflag
+#include<stdlib.h>
+#include<pthread.h>
+#include<assert.h>
+
+pthread_mutex_t queueMutex;
+int threadLimit;
+
+static void* fun(void* arg) {
+    pthread_mutex_lock(&queueMutex);
+
+    int z = 5;
+    if(threadLimit == 0) {
+        z = 8;
+    } else {
+        z = 5;
+    }
+
+    if(threadLimit == 0) {
+        z = 8;
+    } else {
+        z = 5;
+    }
+
+    z = 9;
+
+    while (threadLimit == 0) {
+        z = 10;
+    }
+
+    pthread_mutex_unlock(&queueMutex);
+    __goblint_check(1);
+}
+
+int main() {
+    pthread_t thread;
+    pthread_create(&thread, NULL, &fun, NULL);
+    threadLimit = 10; // RACE!
+
+    return 0;
+}
