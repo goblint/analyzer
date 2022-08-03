51c8c01d6488f3ded9301ef4a7d1bd54efbf195a
Simmo Saan <simmo.saan@gmail.com>
2022-08-03 12:00:46 +0300

    Expand descriptions of hoare-over tests

--- tests/regression/00-sanity/33-hoare-over-paths.c
+++ tests/regression/00-sanity/33-hoare-over-paths.c
@@ -9,13 +9,26 @@ int main() {
   if (r) {
     r = 0;
     assert(1); // extra node in cfg
+    // r -> 0, lockset: {}
   }
   else {
     pthread_mutex_lock(&m);
     assert(1); // extra node in cfg
+    // r -> 0, lockset: {m}
   }
-  // TODO: even though we're path-sensitive w.r.t mutexes, there's only one path after join
+  // OLD undesired behavior: one path: r -> 0, lockset: {}
+  // NEW desired behavior: two paths: r -> 0, lockset: {} and r -> 0, lockset: {m}
+
+  // OLD explanation:
+  // even though we're path-sensitive w.r.t mutexes, there's only one path after join
   // because path set Hoare reduce is over all path partitions (ignores should_join)
   // and one path is leq of another, so Hoare maximality removes one
+
+  // NEW explanation:
+  // using SensitiveDomain correctly keeps both paths as path-sensitivity demands
+
+  // TODO: manually check final join node in HTML
+  // cannot be automated because concrete execution cannot check
+  // if _must_ lockset _may_ contain m on some path
   return 0;
 }
--- tests/regression/00-sanity/34-hoare-over-addrs.c
+++ tests/regression/00-sanity/34-hoare-over-addrs.c
@@ -7,15 +7,27 @@ int main() {
   if (r) {
     p = &arr[1];
     assert(1); // extra node in cfg
+    // p -> {arr[1]}
   }
   else {
     p = &arr[2];
     assert(1); // extra node in cfg
+    // p -> {arr[2]}
   }
-  // TODO: even though we use Hoare for addresses per-varinfo
+  // OLD undesired behavior: p -> {arr[top]}
+  // NEW desired behavior: p -> {arr[1], arr[2]}
+
+  // OLD explanation:
+  // even though we use HoarePO for addresses per-varinfo
   // and lattice operations are on offsets
-  // neither p is leq of another, so Hoare should keep both
+  // neither p is leq of another, so Hoare ordering should keep both
   // but address domain actually joins them nevertheless
+
+  // NEW explanation:
+  // using actual Hoare set instead of HoarePO
+  // correctly keeps both as maximal elements
+
+  // automatic check of new behavior
   assert(p == &arr[1] || p == &arr[2]);
   return 0;
 }
