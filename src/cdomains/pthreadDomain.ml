8f63bf14dd816bb32a0ca2f15d274f8a787d4975
Michael Schwarz <michael.schwarz93@gmail.com>
2022-08-04 11:45:47 +0200

    Fix promela output

--- src/cdomains/pthreadDomain.ml
+++ src/cdomains/pthreadDomain.ml
@@ -15,7 +15,9 @@ module Pred = struct
 
   let of_current_node () = of_node @@ Option.get !MyCFG.current_node
 
-  let string_of_elt = Base.show
+  let string_of_elt (loc:Base.t) =
+    let f i = (if i < 0 then "n" else "") ^ string_of_int (abs i) in
+    f loc.line ^ "b" ^ f loc.byte
 end
 
 type domain =
