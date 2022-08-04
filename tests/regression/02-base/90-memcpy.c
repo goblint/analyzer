2bc66f36fe9c019dceaa001aa9a5a061dae25abe
Julian Erhard <julian.erhard@tum.de>
2022-08-04 10:55:23 +0200

    Add test case that makes goblint crash

---
+++ tests/regression/02-base/90-memcpy.c
@@ -0,0 +1,23 @@
+
+// Test case taken from sqlite3.c
+#include <string.h>
+
+typedef unsigned long u64;
+
+# define EXP754 (((u64)0x7ff)<<52)
+# define MAN754 ((((u64)1)<<52)-1)
+# define IsNaN(X) (((X)&EXP754)==EXP754 && ((X)&MAN754)!=0)
+
+
+static int sqlite3IsNaN(double x){
+  int rc;   /* The value return */
+  u64 y;
+  memcpy(&y,&x,sizeof(y));  // Goblint used to crash here
+  rc = IsNaN(y);
+  return rc;
+}
+
+int main(){
+    sqlite3IsNaN(23.0);
+    return 0;
+}
