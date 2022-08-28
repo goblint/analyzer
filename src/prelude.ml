939b3ce9d45ffd52a126fc40f570e6ed17ce8354
Simmo Saan <simmo.saan@gmail.com>
2022-08-28 03:38:48 +0300

    Fix assert warning expression width

--- src/prelude.ml
+++ src/prelude.ml
@@ -30,7 +30,7 @@ module Ana = struct
   include GoblintCil
   let d_varinfo ppf x = d_lval ppf (Var x, NoOffset)
   include Pretty
-  let sprint f x = Pretty.sprint ~width:80 (fun ppf -> f ppf x)
+  let sprint f x = Pretty.sprint ~width:max_int (fun ppf -> f ppf x)
   (* Analyses.Spec etc. *)
   (* include Analyses (* circular build :( *) *)
   (* module M = Messages (* same, but this is in Analyses anyway *) *)
