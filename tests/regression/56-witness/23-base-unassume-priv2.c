a5ee11f86612ea05641f958d2b66c08ad43a4a02
Simmo Saan <simmo.saan@gmail.com>
2022-09-15 11:35:24 +0300

    Add test for widening tokens on globals

--- tests/regression/56-witness/23-base-unassume-priv2.c
+++ tests/regression/56-witness/23-base-unassume-priv2.c
@@ -1,4 +1,4 @@
-// PARAM: --enable ana.int.interval --set ana.activated[+] unassume --set witness.yaml.unassume 22-base-unassume-priv.yml
+// SKIP PARAM: --enable ana.int.interval --set ana.activated[+] unassume --set witness.yaml.unassume 23-base-unassume-priv2.yml --set solvers.td3.side_widen always
 #include <pthread.h>
 #include <assert.h>
 
@@ -7,6 +7,7 @@ pthread_mutex_t A = PTHREAD_MUTEX_INITIALIZER;
 
 void *t_fun(void *arg) {
   pthread_mutex_lock(&A);
+  assert(1); // TODO: fix Lock and Unassume event order so this extra node isn't necessary
   if (g < 10)
     g++;
   pthread_mutex_unlock(&A);
@@ -19,7 +20,7 @@ int main() {
 
   pthread_mutex_lock(&A);
   assert(g >= 0);
-  assert(g <= 10);
+  assert(g <= 10); // TODO: widening tokens on globals
   pthread_mutex_unlock(&A);
   return 0;
 }
