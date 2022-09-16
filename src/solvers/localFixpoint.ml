b82d35611d0f64ccd71d5da7516be94b22aef8bb
Simmo Saan <simmo.saan@gmail.com>
2022-09-16 13:07:48 +0300

    Optimize LocalFixpoint with narrowing reuse

--- src/solvers/localFixpoint.ml
+++ src/solvers/localFixpoint.ml
@@ -7,16 +7,18 @@ struct
       let x' = f x in
       let x'' = D.widen x (D.join x x') in
       if D.equal x x'' then
-        narrowing x (* switch to narrowing phase *)
+        narrowing_reuse x x' (* switch to narrowing phase *)
       else
         widening x''
-    and narrowing x =
-      let x' = f x in
+    and narrowing_reuse x x' =
       let x'' = D.narrow x x' in
       if D.equal x x'' then
         x (* end iteration *)
       else
         narrowing x''
+    and narrowing x =
+      let x' = f x in
+      narrowing_reuse x x'
     in
     widening init
 end
