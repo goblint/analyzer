43007eb9b3048f38040e8d3f7f2e8d203758259a
Simmo Saan <simmo.saan@gmail.com>
2022-09-19 13:14:47 +0300

    Add regexes for filtering invariant variable names

--- src/domains/invariantCil.ml
+++ src/domains/invariantCil.ml
@@ -36,9 +36,13 @@ let exp_is_in_scope scope e =
 (* TODO: detect temporaries created by Cil? *)
 (* let var_is_tmp {vdescrpure} = not vdescrpure (* doesn't exclude tmp___0 *) *)
 (* TODO: instead check if vdescr is nonempty? (doesn't cover all cases, e.g. ternary temporary) *)
-let tmp_var_regexp = Str.regexp "^\\(tmp\\(___[0-9]+\\)?\\|cond\\|RETURN\\)$"
-(* let var_is_tmp {vname; _} = Str.string_match tmp_var_regexp vname 0 *)
-let var_is_tmp vi = Option.is_none (Cilfacade.find_original_name vi)
+(* TODO: make option for regex cases *)
+let tmp_var_regexp = Str.regexp "^\\(tmp\\(___[0-9]+\\)?\\|__\\(cil_\\)?tmp_?[0-9]*\\(_[0-9]+\\)?\\|.*____CPAchecker_TMP_[0-9]+\\|cond\\|RETURN\\|__VERIFIER_assert__cond\\)$"
+let varname_is_tmp vname = Str.string_match tmp_var_regexp vname 0
+let var_is_tmp vi =
+  match Cilfacade.find_original_name vi with
+  | None -> true
+  | Some vname -> varname_is_tmp vname
 class exp_contains_tmp_visitor (acc: bool ref) = object
   inherit nopCilVisitor as super
   method! vvrbl (vi: varinfo) =
