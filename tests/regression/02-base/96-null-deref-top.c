b3a2fa880dc10002781ef990f5c7597ac6723c0e
Simmo Saan <simmo.saan@gmail.com>
2022-09-21 14:16:41 +0300

    Add NULL dereferenced top value test
    This is why we assumed this to be unreachable:
    https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks/-/merge_requests/1364

---
+++ tests/regression/02-base/96-null-deref-top.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <stddef.h>
+
+int main() {
+  int r; // rand
+  int i = 0;
+  int *p;
+
+  if (r)
+    p = &i;
+  else
+    p = NULL;
+
+  if (*p == 2) // WARN
+    assert(1); // reachable (via UB)
+  return 0;
+}
