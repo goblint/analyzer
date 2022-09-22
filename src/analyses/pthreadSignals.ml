0ffd04c0d222fce431a6eb43abc9a13f5ae62f52
Michael Schwarz <michael.schwarz93@gmail.com>
2022-09-22 10:20:20 +0200

    Generalize to several condition variables

--- src/analyses/pthreadSignals.ml
+++ src/analyses/pthreadSignals.ml
@@ -80,19 +80,38 @@ struct
       in
       (match ctx.ask CurrentThreadId with
        | `Lifted tid ->
-         (match possible_vinfos (Analyses.ask_of_ctx ctx) cond with
-          | [a] ->
-            (* Only report if there is one definite condition variable for which wait is called *)
-            let signalling_tids = ctx.global a in
-            if G.is_top signalling_tids then
-              ctx.local
-            else if G.is_empty signalling_tids then
-              (M.warn ~category:Deadcode "The condition variable %s is never signalled, succeeding code is live due to spurious wakeups only!" a.vname; ctx.local)
-            else if G.exists (may_be_signaller tid) signalling_tids then
-              Signals.add (ValueDomain.Addr.from_var a) ctx.local
-            else
-              (M.warn ~category:Deadcode "The condition variable %s is never signalled concurrently, succeeding code is live due to spurious wakeups only!" a.vname; ctx.local)
-          | _ -> ctx.local)
+         let module Signalled = struct
+           type signalled = Never | NotConcurrently | PossiblySignalled
+
+           let (|||) a b = match a,b with
+             | PossiblySignalled, _
+             | _, PossiblySignalled -> PossiblySignalled
+             | NotConcurrently , _
+             | _, NotConcurrently -> NotConcurrently
+             | Never, Never -> Never
+
+           let can_be_signalled a =
+             let signalling_tids = ctx.global a in
+             if G.is_top signalling_tids then
+               PossiblySignalled
+             else if G.is_empty signalling_tids then
+               Never
+             else if not @@ G.exists (may_be_signaller tid) signalling_tids then
+               NotConcurrently
+             else
+               PossiblySignalled
+         end
+         in
+         let open Signalled in
+         let add_if_singleton conds = match conds with | [a] -> Signals.add (ValueDomain.Addr.from_var a) ctx.local | _ -> ctx.local in
+         let conds = possible_vinfos (Analyses.ask_of_ctx ctx) cond in
+         (match List.fold_left (fun acc cond -> can_be_signalled cond ||| acc) Never conds with
+          | PossiblySignalled -> add_if_singleton conds
+          | NotConcurrently ->
+            (M.warn ~category:Deadcode "The condition variable(s) pointed to by %s are never signalled concurrently, succeeding code is live due to spurious wakeups only!" (Basetype.CilExp.show cond); ctx.local)
+          | Never ->
+            (M.warn ~category:Deadcode "The condition variable(s) pointed to by %s are never signalled, succeeding code is live due to spurious wakeups only!" (Basetype.CilExp.show cond); ctx.local)
+         )
        | _ -> ctx.local)
     | TimedWait _ ->
       (* Time could simply have elapsed *)
