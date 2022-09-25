15131fe318fd5109670bccb09c0c184ee40bd476
Michael Schwarz <michael.schwarz93@gmail.com>
2022-09-25 15:44:21 +0200

    Add test for recurisve mutexes

---
+++ tests/regression/60-doublelocking/05-rec.c
@@ -0,0 +1,40 @@
+// PARAM: --set ana.activated[+] 'maylocks' --set ana.activated[+] 'pthreadMutexType'
+#define _GNU_SOURCE
+#include<pthread.h>
+#include<stdio.h>
+#include<unistd.h>
+#include <assert.h>
+
+int g;
+
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mut2 = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
+
+void* f1(void* ptr) {
+    int top;
+
+    g = 1;
+    if(top) {
+        pthread_mutex_lock(&mut);
+    }
+    pthread_mutex_lock(&mut); //WARN
+    pthread_mutex_unlock(&mut);
+    return NULL;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    pthread_t t1;
+    pthread_t t2;
+
+    pthread_create(&t1,NULL,f1,NULL);
+    pthread_join(t1, NULL);
+
+    pthread_mutex_lock(&mut2); //NOWARN
+    pthread_mutex_lock(&mut2); //NOWARN
+    pthread_mutex_unlock(&mut2);
+    pthread_mutex_unlock(&mut2);
+
+    return 0;
+}
