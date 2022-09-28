4033381960bb61b6151b693b43890bc23c04a88a
Simmo Saan <simmo.saan@gmail.com>
2022-09-28 11:37:38 +0300

    Add base unassume partial struct test

---
+++ tests/regression/56-witness/25-base-unassume-partial-struct.c
@@ -0,0 +1,18 @@
+// PARAM: --enable ana.int.interval --set ana.activated[+] unassume --set witness.yaml.unassume 25-base-unassume-partial-struct.yml
+#include <assert.h>
+
+struct s {
+  int i;
+  int j;
+};
+
+int main() {
+  struct s s;
+  s.i = 2;
+  s.j = 3;
+
+  assert(1 <= s.i);
+  assert(s.i <= 3);
+  assert(s.j == 3); // TODO
+  return 0;
+}
