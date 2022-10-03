goblint/analyzer

commit e362a45a8ad7306f907268026d4ddf47cbbec4c1

Optimize messages by checking should_join before pretty printing

src/util/messages.ml
@@ -138,9 +138,6 @@ struct
     severity: Severity.t;
     multipiece: MultiPiece.t;
   } [@@deriving eq, ord, hash, yojson]
-
-  let should_warn {tags; severity; _} =
-    Tags.should_warn tags && Severity.should_warn severity
 end
 
 module Table =
@@ -236,11 +233,9 @@ let print ?(ppf= !formatter) (m: Message.t) =
 
 
 let add m =
-  if !GU.should_warn then (
-    if Message.should_warn m && not (Table.mem m) then (
-      print m;
-      Table.add m
-    )
+  if not (Table.mem m) then (
+    print m;
+    Table.add m
   )
 
 
@@ -253,33 +248,45 @@ let msg_context () =
     None (* avoid identical messages from multiple contexts without any mention of context *)
 
 let msg severity ?loc ?(tags=[]) ?(category=Category.Unknown) fmt =
-  let finish doc =
-    let text = Pretty.sprint ~width:max_int doc in
-    let loc = match loc with
-      | Some node -> Some node
-      | None -> Option.map (fun node -> Location.Node node) !Node0.current_node
+  if !GU.should_warn && Severity.should_warn severity && (Category.should_warn category || Tags.should_warn tags) then (
+    let finish doc =
+      let text = Pretty.sprint ~width:max_int doc in
+      let loc = match loc with
+        | Some node -> Some node
+        | None -> Option.map (fun node -> Location.Node node) !Node0.current_node
+      in
+      add {tags = Category category :: tags; severity; multipiece = Single {loc; text; context = msg_context ()}}
     in
-    add {tags = Category category :: tags; severity; multipiece = Single {loc; text; context = msg_context ()}}
-  in
-  Pretty.gprintf finish fmt
+    Pretty.gprintf finish fmt
+  )
+  else
+    Tracing.mygprintf fmt
 
 let msg_noloc severity ?(tags=[]) ?(category=Category.Unknown) fmt =
-  let finish doc =
-    let text = Pretty.sprint ~width:max_int doc in
-    add {tags = Category category :: tags; severity; multipiece = Single {loc = None; text; context = msg_context ()}}
-  in
-  Pretty.gprintf finish fmt
+  if !GU.should_warn && Severity.should_warn severity && (Category.should_warn category || Tags.should_warn tags) then (
+    let finish doc =
+      let text = Pretty.sprint ~width:max_int doc in
+      add {tags = Category category :: tags; severity; multipiece = Single {loc = None; text; context = msg_context ()}}
+    in
+    Pretty.gprintf finish fmt
+  )
+  else
+    Tracing.mygprintf fmt
 
 let msg_group severity ?(tags=[]) ?(category=Category.Unknown) fmt =
-  let finish doc msgs =
-    let group_text = Pretty.sprint ~width:max_int doc in
-    let piece_of_msg (doc, loc) =
-      let text = Pretty.sprint ~width:max_int doc in
-      Piece.{loc; text; context = None}
+  if !GU.should_warn && Severity.should_warn severity && (Category.should_warn category || Tags.should_warn tags) then (
+    let finish doc msgs =
+      let group_text = Pretty.sprint ~width:max_int doc in
+      let piece_of_msg (doc, loc) =
+        let text = Pretty.sprint ~width:max_int doc in
+        Piece.{loc; text; context = None}
+      in
+      add {tags = Category category :: tags; severity; multipiece = Group {group_text; pieces = List.map piece_of_msg msgs}}
     in
-    add {tags = Category category :: tags; severity; multipiece = Group {group_text; pieces = List.map piece_of_msg msgs}}
-  in
-  Pretty.gprintf finish fmt
+    Pretty.gprintf finish fmt
+  )
+  else
+    Tracing.mygprintf fmt
 
 (* must eta-expand to get proper (non-weak) polymorphism for format *)
 let warn ?loc = msg Warning ?loc

commit 3be08222c7a3cd5605bb35705b85c74a4b41da4b

Fix disabled msg_group segfault
Regression from e362a45a8ad7306f907268026d4ddf47cbbec4c1.

src/util/messages.ml
@@ -260,7 +260,7 @@ let msg severity ?loc ?(tags=[]) ?(category=Category.Unknown) fmt =
     Pretty.gprintf finish fmt
   )
   else
-    Tracing.mygprintf fmt
+    Tracing.mygprintf () fmt
 
 let msg_noloc severity ?(tags=[]) ?(category=Category.Unknown) fmt =
   if !GU.should_warn && Severity.should_warn severity && (Category.should_warn category || Tags.should_warn tags) then (
@@ -271,7 +271,7 @@ let msg_noloc severity ?(tags=[]) ?(category=Category.Unknown) fmt =
     Pretty.gprintf finish fmt
   )
   else
-    Tracing.mygprintf fmt
+    Tracing.mygprintf () fmt
 
 let msg_group severity ?(tags=[]) ?(category=Category.Unknown) fmt =
   if !GU.should_warn && Severity.should_warn severity && (Category.should_warn category || Tags.should_warn tags) then (
@@ -286,7 +286,7 @@ let msg_group severity ?(tags=[]) ?(category=Category.Unknown) fmt =
     Pretty.gprintf finish fmt
   )
   else
-    Tracing.mygprintf fmt
+    Tracing.mygprintf (fun msgs -> ()) fmt
 
 (* must eta-expand to get proper (non-weak) polymorphism for format *)
 let warn ?loc = msg Warning ?loc

src/util/tracing.ml
@@ -32,7 +32,7 @@ let traceIndent () = indent_level := !indent_level + 2
 let traceOutdent () = indent_level := !indent_level - 2
 
 (* Parses a format string to generate a nop-function of the correct type. *)
-let mygprintf (format : ('a, unit, doc, 'b) format4) : 'a =
+let mygprintf (finish: 'b) (format : ('a, unit, doc, 'b) format4) : 'a =
   let format = string_of_format format in
   let flen    = String.length format in
   let fget    = String.unsafe_get format in
@@ -46,7 +46,7 @@ let mygprintf (format : ('a, unit, doc, 'b) format4) : 'a =
     skipChars (succ i)
   and collect (acc: doc) (i: int) =
     if i >= flen then begin
-      Obj.magic (())
+      Obj.magic finish
     end else begin
       let c = fget i in
       if c = '%' then begin
@@ -104,7 +104,7 @@ let gtrace always f sys var ?loc do_subsys fmt =
     do_subsys ();
     gprintf (f sys) fmt
   end else
-    mygprintf fmt
+    mygprintf () fmt
 
 let trace sys ?var fmt = gtrace true printtrace sys var ignore fmt
 