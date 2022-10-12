4b04fa1bc6d86a7cb94333b683f9d7fa3f3770c4
Simmo Saan <simmo.saan@gmail.com>
2022-10-12 16:51:58 +0300

    Add Amato-Scozzari unassume example
    Their hh already exists as hh-ex3.
    Their nested already exists as hh-ex1b.
    Their nested2 already succeeds with TD3.

---
+++ tests/regression/56-witness/41-as-hybrid.c
@@ -0,0 +1,17 @@
+// PARAM: --enable ana.int.interval --set ana.activated[+] unassume --set witness.yaml.unassume 41-as-hybrid.yml
+
+int main() {
+  int i = 0;
+  while (1) {
+    i++;
+    int j = 0;
+    while (j < 10) {
+      __goblint_check(0 <= i);
+      __goblint_check(i <= 10);
+      j++;
+    }
+    if (i > 9)
+      i = 0;
+  }
+  return 0;
+}
