94dea44ef20e2678da25fe95fab08c36540f4591
Simmo Saan <simmo.saan@gmail.com>
2022-10-18 11:02:28 +0300

    Add Apron libraries to SV-COMP archive

--- sv-comp/archive.sh
+++ sv-comp/archive.sh
@@ -3,7 +3,20 @@
 # must have goblint checked out into goblint not analyzer directory
 
 make clean
-make
+
+dune build --profile=release src/goblint.exe
+rm -f goblint
+cp _build/default/src/goblint.exe goblint
+chmod +w goblint
+# must replace absolute apron runpath to C library with relative
+chrpath -r '$ORIGIN/lib' goblint
+# copy just necessary apron C libraries
+mkdir -p lib/
+cp _opam/share/apron/lib/libapron.so lib/
+cp _opam/share/apron/lib/liboctD.so lib/
+cp _opam/share/apron/lib/libboxD.so lib/
+cp _opam/share/apron/lib/libpolkaMPQ.so lib/
+cp _opam/.opam-switch/sources/apron/COPYING lib/LICENSE.APRON
 
 # done outside to ensure archive contains goblint/ directory
 cd ..
@@ -12,6 +25,11 @@ rm goblint/sv-comp/goblint.zip
 
 zip goblint/sv-comp/goblint.zip \
     goblint/goblint \
+    goblint/lib/libapron.so \
+    goblint/lib/liboctD.so \
+    goblint/lib/libboxD.so \
+    goblint/lib/libpolkaMPQ.so \
+    goblint/lib/LICENSE.APRON \
     goblint/conf/svcomp23.json \
     goblint/includes/assert.h \
     goblint/includes/stdlib.c \
