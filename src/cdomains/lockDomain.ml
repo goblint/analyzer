97c320d9849f51bb11bf2e9fd04d56c4a2007593
karoliineh <karoliine.holter@ut.ee>
2022-10-26 18:09:50 +0300

    Add emit to base eval

--- src/cdomains/lockDomain.ml
+++ src/cdomains/lockDomain.ml
@@ -1,8 +1,8 @@
-module Addr = ValueDomain.Addr
-module Offs = ValueDomain.Offs
+module Addr = PreValueDomain.Addr
+module Offs = PreValueDomain.Offs
 module Equ = MusteqDomain.Equ
 module Exp = CilType.Exp
-module IdxDom = ValueDomain.IndexDomain
+module IdxDom = PreValueDomain.IndexDomain
 
 open GoblintCil
 
--- src/cdomains/preValueDomain.ml
+++ src/cdomains/preValueDomain.ml
@@ -3,3 +3,4 @@ module FD = FloatDomain.FloatDomTupleImpl
 module IndexDomain = IntDomain.IntDomWithDefaultIkind (ID) (IntDomain.PtrDiffIkind)
 module AD = AddressDomain.AddressSet (IndexDomain)
 module Addr = Lval.NormalLat (IndexDomain)
+module Offs = Lval.Offset (IndexDomain)
