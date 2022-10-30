05342c382f44a2b03fc5b0305ecc6ff8fc80453a
Michael Schwarz <michael.schwarz93@gmail.com>
2022-10-30 14:49:05 +0100

    OS X uses `__fpclassifyd`

--- src/analyses/libraryFunctions.ml
+++ src/analyses/libraryFunctions.ml
@@ -254,6 +254,7 @@ let math_descs_list: (string * LibraryDesc.t) list = LibraryDsl.[
     ("__builtin_fpclassifyf", unknown [drop "nan" []; drop "infinite" []; drop "normal" []; drop "subnormal" []; drop "zero" []; drop "x" []]);
     ("__builtin_fpclassifyl", unknown [drop "nan" []; drop "infinite" []; drop "normal" []; drop "subnormal" []; drop "zero" []; drop "x" []]);
     ("__fpclassify", unknown [drop "x" []]);
+    ("__fpclassifyd", unknown [drop "x" []]);
     ("__fpclassifyf", unknown [drop "x" []]);
     ("__fpclassifyl", unknown [drop "x" []]);
   ]
