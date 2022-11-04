ccce649e63aa1b509a4d0ababd0689a4baffbd64
Michael Schwarz <michael.schwarz93@gmail.com>
2022-11-04 17:31:34 +0100

    Skip for OS X

--- tests/regression/29-svcomp/30-nondets.c
+++ tests/regression/29-svcomp/30-nondets.c
@@ -6,18 +6,22 @@
 int g1 = 5;
 
 int main(void) {
-  size_t x1 = __VERIFIER_nondet_size_t();
-  __U16_TYPE x2 = __VERIFIER_nondet_u16();
-  __uint8_t x3 =__VERIFIER_nondet_u8();
-  __U32_TYPE x4 = __VERIFIER_nondet_u32();
-  size_t x5 = __VERIFIER_nondet_loff_t();
-  __uint128_t x6 = __VERIFIER_nondet_uint128();
-  __int128_t x6andahalf = __VERIFIER_nondet_int128();
-  long long x7 = __VERIFIER_nondet_longlong();
-  unsigned long long x8 = __VERIFIER_nondet_ulonglong();
-  unsigned char x9 = __VERIFIER_nondet_unsigned_char();
-  const char* ptr1 = __VERIFIER_nondet_const_char_pointer();
-  char* ptr2 = __VERIFIER_nondet_charp();
+  #ifdef __APPLE__
+    // OS X headers have different names for some types
+  #else
+    size_t x1 = __VERIFIER_nondet_size_t();
+    __U16_TYPE x2 = __VERIFIER_nondet_u16();
+    __uint8_t x3 =__VERIFIER_nondet_u8();
+    __U32_TYPE x4 = __VERIFIER_nondet_u32();
+    size_t x5 = __VERIFIER_nondet_loff_t();
+    __uint128_t x6 = __VERIFIER_nondet_uint128();
+    __int128_t x6andahalf = __VERIFIER_nondet_int128();
+    long long x7 = __VERIFIER_nondet_longlong();
+    unsigned long long x8 = __VERIFIER_nondet_ulonglong();
+    unsigned char x9 = __VERIFIER_nondet_unsigned_char();
+    const char* ptr1 = __VERIFIER_nondet_const_char_pointer();
+    char* ptr2 = __VERIFIER_nondet_charp();
+  #endif
 
   __goblint_check(g1 == 5); // Should not invalidate
   return 0;
