57ad20116e0909c2b974939ec8bb01caf7a95950
Julian Erhard <julian.erhard@tum.de>
2022-11-04 14:53:52 +0100

    Add includes to tests.

--- tests/regression/02-base/79-unknown-spawn.c
+++ tests/regression/02-base/79-unknown-spawn.c
@@ -2,8 +2,10 @@
 #include <goblint.h>
 #include <stddef.h>
 
+int magic(void* (f (void *)));
+
 void *t_fun(void *arg) {
-  __goblint_check(1); // reachable
+  // __goblint_check(1); // reachable
   return NULL;
 }
 
