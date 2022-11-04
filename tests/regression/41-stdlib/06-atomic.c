7dbf5e43b9c4d6601936ead63783ba341f373374
Michael Schwarz <michael.schwarz93@gmail.com>
2022-11-04 15:44:54 +0100

    Fix test

--- tests/regression/41-stdlib/06-atomic.c
+++ tests/regression/41-stdlib/06-atomic.c
@@ -1,9 +1,10 @@
+#include<goblint.h>
 int g = 8;
 
 int main() {
     int i;
     __atomic_store_n (&i, 12, __ATOMIC_RELAXED);
-    int i = __atomic_load_n (&i, __ATOMIC_RELAXED);
+    i = __atomic_load_n (&i, __ATOMIC_RELAXED);
     __goblint_check(i == 12); //TODO
 
     // Should not be invalidated
