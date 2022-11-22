3613083bc90465e02271bd50e8104718f8ccfde6
Julian Erhard <julian.erhard@tum.de>
2022-11-22 13:29:39 +0100

    Add test case where both branches are erroneously considered dead.

---
+++ tests/regression/29-svcomp/31-dd-address-meet.c
@@ -0,0 +1,21 @@
+// PARAM: --enable annotation.int.enabled
+#include <stdlib.h>
+struct slotvec {
+   size_t size ;
+   char *val ;
+};
+static char slot0[256] ;
+static struct slotvec slotvec0 = {sizeof(slot0), slot0};
+
+static void install_signal_handlers(void)
+{
+  { if(!(slotvec0.val == & slot0[0LL])) { reach_error(); abort(); } };
+}
+
+int main(int argc , char **argv )
+{
+  // Goblint used to consider both branches in this condition to be dead, because the meet on addresses with different active int domains was broken
+  { if(!(slotvec0.val == & slot0[0LL])) { reach_error(); abort(); } };
+  install_signal_handlers();
+  return 0;
+}
