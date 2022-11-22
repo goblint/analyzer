88e1d2b3cb969fd0e08d4905b5c0d8f06335826b
Julian Erhard <julian.erhard@tum.de>
2022-11-22 14:13:06 +0100

    Add __goblint_check to test to check reachability.

--- tests/regression/29-svcomp/31-dd-address-meet.c
+++ tests/regression/29-svcomp/31-dd-address-meet.c
@@ -1,5 +1,6 @@
 // PARAM: --enable annotation.int.enabled
 #include <stdlib.h>
+#include <goblint.h>
 struct slotvec {
    size_t size ;
    char *val ;
@@ -9,7 +10,7 @@ static struct slotvec slotvec0 = {sizeof(slot0), slot0};
 
 static void install_signal_handlers(void)
 {
-  { if(!(slotvec0.val == & slot0[0LL])) { reach_error(); abort(); } };
+  { if(!(slotvec0.val == & slot0[0LL])) {  reach_error(); abort(); } };
 }
 
 int main(int argc , char **argv )
@@ -17,5 +18,8 @@ int main(int argc , char **argv )
   // Goblint used to consider both branches in this condition to be dead, because the meet on addresses with different active int domains was broken
   { if(!(slotvec0.val == & slot0[0LL])) { reach_error(); abort(); } };
   install_signal_handlers();
+
+  // Should be reachable
+  __goblint_check(1);
   return 0;
 }
