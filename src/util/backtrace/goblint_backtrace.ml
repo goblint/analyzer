a67e95d7790ec895ff41e0967dd9d4e598882caf
Simmo Saan <simmo.saan@gmail.com>
2022-11-27 22:25:38 +0200

    Replace backtrace Marked exception with Ephemeron

--- src/util/backtrace/goblint_backtrace.ml
+++ src/util/backtrace/goblint_backtrace.ml
@@ -1,21 +1,31 @@
 type mark = ..
 
-exception Marked of mark * exn
+module Exn =
+struct
+  type t = exn
+  let equal = (==)
+  let hash = Hashtbl.hash
+end
 
-let mark m e =
+module EWH = Ephemeron.K1.Make (Exn)
+
+let marks: mark EWH.t = EWH.create 10
+
+let add_mark e m =
   if Printexc.backtrace_status () then
-    Marked (m, e)
-  else
-    e
+    EWH.add marks e m
+
 
 (* Copied & modified from Fun. *)
-let protect ~mark:(m: unit -> mark) ~(finally: unit -> unit) work =
+let protect ~(mark: unit -> mark) ~(finally: unit -> unit) work =
   let finally_no_exn () =
     try
       finally ()
     with e ->
       let bt = Printexc.get_raw_backtrace () in
-      Printexc.raise_with_backtrace (mark (m ()) (Fun.Finally_raised e)) bt
+      let finally_exn = Fun.Finally_raised e in
+      add_mark finally_exn (mark ());
+      Printexc.raise_with_backtrace finally_exn bt
   in
   match work () with
   | result ->
@@ -24,18 +34,9 @@ let protect ~mark:(m: unit -> mark) ~(finally: unit -> unit) work =
   | exception work_exn ->
     let work_bt = Printexc.get_raw_backtrace () in
     finally_no_exn ();
-    Printexc.raise_with_backtrace (mark (m ()) work_exn) work_bt
+    add_mark work_exn (mark ());
+    Printexc.raise_with_backtrace work_exn work_bt
 
-
-let rec unmark = function
-  | Marked (_, e) -> unmark e
-  | e -> e
-
-let () = Printexc.register_printer (function
-    | Marked _ as e ->
-      Some (Printexc.to_string (unmark e))
-    | _ -> None (* for other exceptions *)
-  )
 
 let mark_printers: (mark -> string option) list ref = ref []
 
@@ -58,12 +59,11 @@ let mark_to_string m =
   | Some s -> s
   | None -> mark_to_string_default m
 
-let rec print_marktrace oc e =
-  match e with
-  | Marked (m, e) ->
-    print_marktrace oc e;
-    Printf.fprintf oc "Marked with %s\n" (mark_to_string m)
-  | e -> ()
+let print_marktrace oc e =
+  let ms = List.rev (EWH.find_all marks e) in
+  List.iter (fun m ->
+      Printf.fprintf oc "Marked with %s\n" (mark_to_string m)
+    ) ms
 
 let () =
   Printexc.set_uncaught_exception_handler (fun e bt ->
