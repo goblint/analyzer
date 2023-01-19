a1ac32e0014c6ec642a06b3b3ee579f6d1c948e0
Simmo Saan <simmo.saan@gmail.com>
2023-01-19 09:10:21 +0000

    Re-add unassume invariant messages for easier debugging

--- src/analyses/unassumeAnalysis.ml
+++ src/analyses/unassumeAnalysis.ml
@@ -225,7 +225,7 @@ struct
     match es with
     | x :: xs ->
       let e = List.fold_left (fun a {exp = b; _} -> Cil.(BinOp (LAnd, a, b, intType))) x.exp xs in
-      (* M.info ~category:Witness "unassume invariant: %a" CilType.Exp.pretty e; *)
+      M.info ~category:Witness "unassume invariant: %a" CilType.Exp.pretty e;
       if not !Goblintutil.postsolving then (
         let uuids = x.uuid :: List.map (fun {uuid; _} -> uuid) xs in
         ctx.emit (Unassume {exp = e; uuids});
