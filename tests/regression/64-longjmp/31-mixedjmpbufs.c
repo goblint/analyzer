57c21a68fee102fbc9814ba45cca1f55628461fd
Michael Schwarz <michael.schwarz93@gmail.com>
2023-02-14 17:37:38 +0100

    Example for non-unique jmpbufs

---
+++ tests/regression/64-longjmp/31-mixedjmpbufs.c
@@ -0,0 +1,47 @@
+#include <pthread.h>
+#include <goblint.h>
+#include <setjmp.h>
+#include <stdio.h>
+
+jmp_buf error0;
+jmp_buf error1;
+
+
+int blorg(int x) {
+  if(x > 8) {
+    longjmp(error1, 1); //NOWARN
+  }
+
+  return x;
+}
+
+int blub(int x,int y) {
+  if(x == 0) {
+    longjmp(error0, 1); //NOWARN
+  }
+
+  return blorg(x-27+3);
+}
+
+
+
+int main(void) {
+
+  if(setjmp(error0)) {
+    printf("error0 occured");
+    return -1;
+  }
+
+  if(setjmp(error1)) {
+    printf("error1 occured");
+    return -2;
+  }
+
+  int x, y;
+  scanf("%d", &x);
+  scanf("%d", &y);
+  int x = blub(x, y);
+  printf("%d", x);
+
+  return 0;
+}
