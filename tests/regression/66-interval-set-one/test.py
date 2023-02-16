fbebec655e172eb48a44b8de6f1db0e922c73af1
Bilel Ghorbel <ghorbelbilel@gmail.com>
2023-02-16 16:56:47 +0100

    delete script file for renaming test files

--- tests/regression/66-interval-set-one/test.py
+++
@@ -1,6 +0,0 @@
-import os
-for index, filename in enumerate(os.listdir(".")):
-    if ".c" in filename:
-        new_filename = f"{index}{filename[2:]}"
-        #print(new_filename)
-        os.rename(filename, new_filename)
