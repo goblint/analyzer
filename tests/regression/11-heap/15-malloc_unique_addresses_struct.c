1ba7e5bbc1679f096cb09d96e336c3c8e8eff398
Michael Schwarz <michael.schwarz93@gmail.com>
2023-03-01 13:27:18 +0100

    Add future test for malloc uniqueness

---
+++ tests/regression/11-heap/15-malloc_unique_addresses_struct.c
@@ -0,0 +1,26 @@
+// PARAM: --set ana.malloc.unique_address_count 2
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <goblint.h>
+
+struct s {
+  int x;
+  int y;
+};
+
+int main() {
+  struct s *ptr = malloc(sizeof(struct s));
+  int p;
+
+  ptr->x = 0;
+  ptr->y = 1;
+
+  __goblint_check(ptr->x == 0);
+  __goblint_check(ptr->y == 1);
+
+  ptr->x = 1;
+  ptr->y = 0;
+  __goblint_check(ptr->x == 1); //TODO
+  __goblint_check(ptr->y == 0); //TODO
+}
