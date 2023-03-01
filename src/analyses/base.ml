a7a0122717757e0d3662c1f914e3e3cce304d229
Michael Schwarz <michael.schwarz93@gmail.com>
2023-03-01 09:21:28 +0100

    Add possibility to trace setjmp

--- src/analyses/base.ml
+++ src/analyses/base.ml
@@ -2236,7 +2236,9 @@ struct
            | `Address jmp_buf ->
              let controlctx = ControlSpecC.hash (ctx.control_context ()) in
              let value = `JmpBuf (ValueDomain.JmpBufs.singleton (ctx.prev_node, IntDomain.Flattened.of_int (Int64.of_int controlctx))) in
-             set ~ctx (Analyses.ask_of_ctx ctx) gs st jmp_buf (Cilfacade.typeOf env) value
+             let r = set ~ctx (Analyses.ask_of_ctx ctx) gs st jmp_buf (Cilfacade.typeOf env) value in
+             M.tracel "setjmp" "setting setjmp %a on %a -> %a\n" d_exp env  D.pretty st  D.pretty r;
+             r
            | _      -> failwith "problem?!")
        in
        match lv with
