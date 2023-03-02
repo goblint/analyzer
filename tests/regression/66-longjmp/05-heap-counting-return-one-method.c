3053b7a37eaceea51ad7f0bd2a68f2a9d8776a97
Michael Schwarz <michael.schwarz93@gmail.com>
2023-03-02 15:34:19 +0100

    Modify test 66/05 so it does something different

---
+++ tests/regression/66-longjmp/05-heap-counting-return-one-method.c
@@ -0,0 +1,18 @@
+// PARAM: --enable ana.int.interval --enable ana.int.enums --enable exp.earlyglobs  --set solvers.td3.side_widen never
+// #include <goblint.h>
+#include <setjmp.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    jmp_buf* my_jump_buffer = malloc(sizeof(jmp_buf));
+
+    int count = setjmp(*my_jump_buffer);
+    // __goblint_check(count == 0); // UNKNOWN!
+    if (count < 5) {
+        // __goblint_check(count >= 0 & count < 5);
+        longjmp(*my_jump_buffer, count + 1);
+        // __goblint_check(0); // NOWARN
+    }
+    // __goblint_check(count == 5);
+}
