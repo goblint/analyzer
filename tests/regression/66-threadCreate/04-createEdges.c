67236b9820a9b58ccb31ebdbcf88a0f9a3347f6e
FelixKrayer <felix@krayernet.eu>
2023-03-07 22:30:21 +0100

    some tests for tracking create edges

---
+++ tests/regression/66-threadCreate/04-createEdges.c
@@ -0,0 +1,37 @@
+// PARAM: --disable ana.thread.context.createEdges --set ana.activated[+] threadCreateEdges
+#include <goblint.h>
+#include <pthread.h>
+
+int glob;
+
+void *t_init(void *arg) {
+}
+
+void *t_norace(void *arg) {
+  glob = 1; //NORACE
+}
+
+void *t_other(void *arg) {
+}
+
+int create_other () {
+  pthread_t id;
+  pthread_create(&id, NULL, t_other, NULL);
+}
+
+
+int main() {
+  //enter multithreaded mode
+  pthread_t id;
+  pthread_create(&id, NULL, t_init, NULL);
+
+  create_other();
+
+  glob = 2; //NORACE
+
+  pthread_t id;
+  pthread_create(&id, NULL, t_norace, NULL);
+
+  create_other();
+
+}
---
+++ tests/regression/66-threadCreate/05-fromThread.c
@@ -0,0 +1,35 @@
+// PARAM: --disable ana.thread.context.createEdges --set ana.activated[+] threadCreateEdges
+#include <goblint.h>
+#include <pthread.h>
+
+int glob;
+
+void *t_norace(void *arg) {
+  glob = 1; //NORACE
+}
+
+void *t_other(void *arg) {
+}
+
+int create_other () {
+  pthread_t id;
+  pthread_create(&id, NULL, t_other, NULL);
+}
+
+void *t_fun(void *arg) {
+  create_other();
+
+  glob = 2; //NORACE
+
+  pthread_t id;
+  pthread_create(&id, NULL, t_norace, NULL);
+
+  create_other();
+}
+
+int main() {
+  pthread_t id;
+  pthread_create(&id, NULL, t_fun, NULL);
+
+  create_other();
+}
