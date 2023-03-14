b1e37704aefc4d657c21fc746163c5f05fcca935
Simmo Saan <simmo.saan@gmail.com>
2023-03-14 13:52:56 +0200

    Add Logs prototype

---
+++ src/util/logs.ml
@@ -0,0 +1,46 @@
+module Pretty = GoblintCil.Pretty
+
+module type Kind =
+sig
+  type b
+  type c
+  val log: ('a, b, c, unit) format4 -> 'a
+end
+
+module PrettyKind =
+struct
+  type b = unit
+  type c = Pretty.doc
+  let log fmt =
+    (* Pretty.eprintf returns doc instead of unit *)
+    let finish doc =
+      Pretty.fprint stderr ~width:max_int doc
+    in
+    Pretty.gprintf finish fmt
+end
+
+module FormatKind =
+struct
+  type b = Format.formatter
+  type c = unit
+  let log fmt =
+    Format.eprintf fmt
+end
+
+
+module MakeKind (Kind: Kind) =
+struct
+  open Kind
+
+  let log = log
+
+  let debug = log
+  let info = log
+  let warn = log
+  let error = log
+end
+
+module Pretty = MakeKind (PrettyKind)
+module Format = MakeKind (FormatKind)
+
+include Pretty (* current default *)
