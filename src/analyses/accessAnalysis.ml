76df5fa1ec80f2b530e5e00409fb543e17a7dae3
Simmo Saan <simmo.saan@gmail.com>
2023-03-20 16:24:56 +0200

    Emit accesses to VLA length

--- src/analyses/accessAnalysis.ml
+++ src/analyses/accessAnalysis.ml
@@ -56,6 +56,16 @@ struct
 
   (** Transfer functions: *)
 
+  let vdecl ctx v =
+    let rec distribute_access_typ = function
+      | TArray (et, len, _) ->
+        Option.may (access_one_top ctx Read false) len;
+        distribute_access_typ et
+      | _ -> ()
+    in
+    distribute_access_typ v.vtype;
+    ctx.local
+
   let assign ctx lval rval : D.t =
     (* ignore global inits *)
     if !GU.global_initialization then ctx.local else begin
