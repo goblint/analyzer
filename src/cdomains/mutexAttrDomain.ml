53112a0ac95da4c4b3ab841372f6c9d96c9874e7
Michael Schwarz <michael.schwarz93@gmail.com>
2023-03-27 19:48:39 +0200

    Dynamically lookup constants

--- src/cdomains/mutexAttrDomain.ml
+++ src/cdomains/mutexAttrDomain.ml
@@ -16,11 +16,26 @@ end
 
 include Lattice.Flat(MutexKind) (struct let bot_name = "Uninitialized" let top_name = "Top" end)
 
+(* Needed because OS X is weird and assigns different constants than normal systems... :( *)
+let recursive_int = lazy (
+  let res = ref None in
+  GoblintCil.iterGlobals !Cilfacade.current_file (function
+      | GEnumTag (einfo, _) ->
+        List.iter (fun (name, exp, _) ->
+            if name = "PTHREAD_MUTEX_RECURSIVE" then
+              res := GoblintCil.getInteger exp
+          ) einfo.eitems
+      | _ -> ()
+    );
+  !res
+)
+
 
 let of_int z =
   if Z.equal z Z.zero then
     `Lifted MutexKind.NonRec
-  else if Z.equal z Z.one then
-    `Lifted MutexKind.Recursive
   else
-    `Top
+    let recursive_int = Lazy.force recursive_int in
+    match recursive_int with
+    | Some r when Z.equal z r -> `Lifted MutexKind.Recursive
+    | _ -> `Top
