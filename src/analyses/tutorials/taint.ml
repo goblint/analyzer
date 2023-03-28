d8dff77fd55d1eaf40551036fc234e122196265c
Simmo Saan <simmo.saan@gmail.com>
2023-03-28 12:48:23 +0300

    Document combine_env in taint tutorial

--- src/analyses/tutorials/taint.ml
+++ src/analyses/tutorials/taint.ml
@@ -85,7 +85,7 @@ struct
 
   (** For a function call "lval = f(args)" or "f(args)",
       [enter] returns a caller state, and the initial state of the callee.
-      In [enter], the caller state can usually be returned unchanged, as [combine] (below)
+      In [enter], the caller state can usually be returned unchanged, as [combine_env] and [combine_assign] (below)
       will compute the caller state after the function call, given the return state of the callee. *)
   let enter ctx (lval: lval option) (f:fundec) (args:exp list) : (D.t * D.t) list =
     let caller_state = ctx.local in
@@ -101,11 +101,15 @@ struct
     (* first component is state of caller, second component is state of callee *)
     [caller_state, callee_state]
 
-  let combine_env ctx lval fexp f args fc au f_ask =
+  (** For a function call "lval = f(args)" or "f(args)",
+      computes the global environment state of the caller after the call.
+      Argument [callee_local] is the state of [f] at its return node. *)
+  let combine_env ctx (lval:lval option) fexp (f:fundec) (args:exp list) fc (callee_local:D.t) (f_ask: Queries.ask): D.t =
+    (* Nothing needs to be done *)
     ctx.local
 
   (** For a function call "lval = f(args)" or "f(args)",
-      computes the state of the caller after the call.
+      computes the state of the caller after assigning the return value from the call.
       Argument [callee_local] is the state of [f] at its return node. *)
   let combine_assign ctx (lval:lval option) fexp (f:fundec) (args:exp list) fc (callee_local:D.t) (f_ask: Queries.ask): D.t =
     let caller_state = ctx.local in
