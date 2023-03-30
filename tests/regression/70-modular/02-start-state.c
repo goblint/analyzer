4b2ca32d8efd8d354b025f38244366814e174565
Julian Erhard <julian.erhard@tum.de>
2023-03-30 15:49:36 +0200

    Modular: Add test case with dereferenced argument pointers.

---
+++ tests/regression/70-modular/02-start-state.c
@@ -0,0 +1,18 @@
+//PARAM: --enable modular
+
+#include <goblint.h>
+
+typedef struct node {
+  int data;
+  struct node *next;
+} node_t;
+
+void list_op(node_t *n){
+	// Check that modular analysis can handle access to values via pointers
+	__goblint_check((n->next)->data == 0); //UNKNOWN!
+	__goblint_check(n->data == 0); //UNKNOWN!
+}
+
+
+
+
