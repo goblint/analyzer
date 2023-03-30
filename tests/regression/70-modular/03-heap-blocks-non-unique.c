3a701c22145155fc62f751f221e21df59c670e2f
Julian Erhard <julian.erhard@tum.de>
2023-03-30 16:49:33 +0200

    Add test that typed-based objects are not considered unique.

---
+++ tests/regression/70-modular/03-heap-blocks-non-unique.c
@@ -0,0 +1,20 @@
+//PARAM: --enable modular --set ana.activated[+] "'modular_queries'"
+#include <goblint.h>
+#include <stdlib.h>
+
+typedef struct node {
+  int data;
+  struct node *next;
+} node_t;
+
+void list_op(node_t *n){
+	// Check that modular analysis does not assume that the type-based objects are unique
+	__goblint_check(n->next == n); //UNKNOWN!
+}
+
+void list_op2(node_t *n, node_t *m){
+	__goblint_check(n == m); //UNKNOWN!
+	__goblint_check(n->next == m); //UNKNOWN!
+	__goblint_check(m->next == n); //UNKNOWN!
+	__goblint_check(n->next == m->next); //UNKNOWN!
+}
