ecfab57a5c920c5ff1ab750c17b7ff9343da7464
Julian Erhard <julian.erhard@tum.de>
2023-03-31 17:46:04 +0200

    Add test with writes through param, reads through global, and vice-versa.

---
+++ tests/regression/70-modular/06-global.c
@@ -0,0 +1,42 @@
+//PARAM: --enable modular --set ana.activated[+] "'modular_queries'"
+#include<goblint.h>
+#include<stdlib.h>
+
+int *g;
+
+// Check that modular analysis treats reads from globals soundly
+void read_global(int** i){ // i may point to g
+	int z;
+	int *p = &z;
+
+	__goblint_check(g == *i); //UNKNOWN!
+
+	__goblint_check(p != g);
+	__goblint_check(p != *i);
+
+	int *j = malloc(sizeof(int));
+	*i = j;
+	__goblint_check(g == j); //UNKNOWN!
+
+	int *k = g;
+	__goblint_check(k == j); //UNKNOWN!
+}
+
+// Check that modular analysis treats writes to globals soundly
+void write_global(int** i){ // i may point to g
+	int z;
+	int *p = &z;
+
+	__goblint_check(g == *i); //UNKNOWN!
+
+	__goblint_check(p != g);
+	__goblint_check(p != *i);
+
+	int *j = malloc(sizeof(int));
+	g = j;
+
+	__goblint_check(g == j); //UNKNOWN!
+
+	int *k = g;
+	__goblint_check(k == j); //UNKNOWN!
+}
