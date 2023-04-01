f368ffde6126c67c14674b15d01d5362079f8b54
Julian Erhard <julian.erhard@tum.de>
2023-04-01 12:22:06 +0200

    Modular: Add test case for funciton calls.

---
+++ tests/regression/70-modular/09-function-calls.c
@@ -0,0 +1,35 @@
+//SKIP PARAM: --enable modular --set ana.activated[+] "'modular_queries'"
+#include<goblint.h>
+
+#define X_VALUE 12
+#define Y_VALUE 3
+
+typedef struct {
+    int x;
+    int y;
+} tuple_t;
+
+int modify_x(tuple_t *p){
+    p->x = 123;
+}
+
+int modify_y(tuple_t *p){
+    p->y = 13;
+}
+
+int foo(){
+    tuple_t t = {X_VALUE, Y_VALUE};
+
+    __goblint_check(t.x == X_VALUE);
+    __goblint_check(t.y == Y_VALUE);
+
+    modify_x(&t);
+
+    __goblint_check(t.x != X_VALUE); //UNKNOWN
+    __goblint_check(t.y == Y_VALUE);
+
+    modify_y(&t);
+
+    __goblint_check(t.x != X_VALUE); //UNKNOWN
+    __goblint_check(t.y != Y_VALUE); //UNKNOWN
+}
