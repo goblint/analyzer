39e610423d54a4788bf3265ba98429add93a7e5b
Julian Erhard <julian.erhard@tum.de>
2023-04-02 10:53:24 +0200

    Extend test case with sample cases for writes on parameters, and blocks reachable by parameters.

--- tests/regression/70-modular/10-global-vals-top.c
+++ tests/regression/70-modular/10-global-vals-top.c
@@ -7,8 +7,20 @@ typedef struct {
 
 tuple_t g = {0, 0};
 
-int foo(){
+int write_global(){
     __goblint_check(g.x == 0); // UNKNOWN! (May be modified by other code)
     g.x = 0;
     __goblint_check(g.x == 0); // UNKNOWN! (May be modified by other code in parallel)
+}
+
+int write_param(tuple_t tuple){
+    __goblint_check(tuple.x == 0); // UNKNOWN! (May be modified by other code)
+    tuple.x = 0;
+    __goblint_check(tuple.x == 0);
+}
+
+int write_param_pointer(tuple_t *tuple){
+    __goblint_check(tuple->x == 0); // UNKNOWN! (May be modified by other code)
+    tuple->x = 0;
+    __goblint_check(tuple->x == 0); // UNKNOWN! (May be modified by other code in parallel)
 }
