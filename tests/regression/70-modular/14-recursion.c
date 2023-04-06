d35d80b53658ec864271c26e9d51eaa8eb197888
Julian Erhard <julian.erhard@tum.de>
2023-04-06 17:26:57 +0200

    Modular: Add test with recursion.

---
+++ tests/regression/70-modular/14-recursion.c
@@ -0,0 +1,24 @@
+//PARAM: --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'written'" --enable ana.int.interval
+
+#include<goblint.h>
+
+int fib(int *n, int* acc){
+	if(*n == 0){
+		return *acc;
+	} else {
+		*acc = (*acc) * (*n);
+		*n = (*n) - 1;
+		fib(n, acc);
+	}
+}
+
+int call_fib(){
+	int n = 10;
+	int acc = 1;
+
+	__goblint_check(n == 10);
+	__goblint_check(acc == 1);
+	fib(&n, &acc);
+	__goblint_check(n != 10); //UNKNOWN
+	__goblint_check(acc != 1); //UNKNOWN
+}
