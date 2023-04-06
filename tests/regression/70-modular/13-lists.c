134984bbb5c8d1f206461047de0e49d4d9646db5
Julian Erhard <julian.erhard@tum.de>
2023-04-06 17:15:20 +0200

    Extend test case checking that NULL is not unnecessarily added.

--- tests/regression/70-modular/13-lists.c
+++ tests/regression/70-modular/13-lists.c
@@ -18,11 +18,41 @@ void call_prepend(){
 	node_t m = {0};
 	node_t o = {0};
 
+	n.next = &n;
+
 	__goblint_check(n.next != &m);
 	__goblint_check(n.next != &o);
+	__goblint_check(n.next != NULL);
+
 	prepend(&n, &m);
 	__goblint_check(n.next != &m); //UNKNOWN
 	__goblint_check(n.next != m.next); //UNKNOWN
+	__goblint_check(n.next != NULL); //UNKNOWN
 	__goblint_check(n.next != &o);
 
+}
+
+void prepend2(node_t *hd, node_t *tail){
+	if(hd != NULL){
+		if(tail != NULL){
+			hd->next = tail;
+		}
+	}
+}
+
+void call_prepend2(){
+	node_t n = {0};
+	node_t m = {0};
+	node_t o = {0};
+	n.next = &n;
+
+	__goblint_check(n.next != &m);
+	__goblint_check(n.next != &o);
+	__goblint_check(n.next != NULL);
+
+	prepend2(&n, &m);
+	__goblint_check(n.next != &m); //UNKNOWN
+	__goblint_check(n.next != m.next); //UNKNOWN
+	__goblint_check(n.next != &o);
+	__goblint_check(n.next != NULL);
 }
