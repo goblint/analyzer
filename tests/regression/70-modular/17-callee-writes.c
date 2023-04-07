799f8b170ccb29423572d1c13bc7b31953d21753
Julian Erhard <julian.erhard@tum.de>
2023-04-07 16:00:59 +0200

    Add test case for written recording callee writes.

---
+++ tests/regression/70-modular/17-callee-writes.c
@@ -0,0 +1,17 @@
+//PARAM: --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'"
+#include<goblint.h>
+
+void write_arg(int *x){
+    *x = 10;
+}
+
+void delegate_writing(int *x){
+    write_arg(x);
+}
+
+void call_delegate_writing(){
+    int x = 3;
+    __goblint_check(x == 3);
+    delegate_writing(&x);
+    __goblint_check(x != 3); //UNKNOWN
+}
