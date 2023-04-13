32e1ead5b721eade9218e54fe2fadf1747220478
Julian Erhard <julian.erhard@tum.de>
2023-04-13 09:29:25 +0200

    Add test case where current handling of arrays by modular anlaysis yields Top and thus too much imprecision.

---
+++ tests/regression/70-modular/20-arrays-top.c
@@ -0,0 +1,32 @@
+//PARAM: --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'"
+#include <stdlib.h>
+#include <goblint.h>
+
+int g = 0;
+
+void write_into_array(int *a[], int size){
+	a[size - 1] = &g;
+}
+
+void test_array(){
+	int size = 10;
+	int j;
+	int *a[size];
+
+	for(int i = 0; i < size; i++){
+		a[i] = NULL;
+	}
+
+	__goblint_check(a[0] == NULL);
+	__goblint_check(a[size - 1] == NULL);
+	__goblint_check(a[size - 1] != &j);
+
+	write_into_array(a, size);
+
+	__goblint_check(a[size - 1] != NULL); //UNKNOWN
+	__goblint_check(a[size - 1] == &g); //UNKNOWN
+
+	// Requires not having `Top as the value a.
+	__goblint_check(a[size - 1] != &j);
+
+}
