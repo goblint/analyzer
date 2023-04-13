986045453ab10056a641ee9318afee26b29a0a5a
Julian Erhard <julian.erhard@tum.de>
2023-04-13 14:45:41 +0200

    Add test case where global is modified.

---
+++ tests/regression/70-modular/21-call-global-modifying-function.c
@@ -0,0 +1,23 @@
+//PARAM: --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'"
+#include<stdlib.h>
+#include<goblint.h>
+
+int g;
+int *h = NULL;
+
+void set_global_to(int* p){
+	h = p;
+}
+
+void call_set_global_to(int *p){
+	set_global_to(p);
+}
+
+// Test to check whether the write through call of call_change_param is handled
+void test_call_change_param(){
+	int x = 0;
+
+	__goblint_check(h != &x);
+	call_set_global_to(&x);
+	__goblint_check(h == &x); //UNKNOWN
+}
