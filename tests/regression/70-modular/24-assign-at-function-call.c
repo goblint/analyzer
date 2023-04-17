ab9429171f33c91b572ee0db87910a704391ac74
Julian Erhard <julian.erhard@tum.de>
2023-04-17 21:29:41 +0200

    Add test case for Written.combine_assign.

---
+++ tests/regression/70-modular/24-assign-at-function-call.c
@@ -0,0 +1,18 @@
+//PARAM: --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'"
+#include<goblint.h>
+
+int five(){
+    return 5;
+}
+
+void write_arg(int *x){
+    *x = five();
+}
+
+void call_delegate_writing(){
+    int x = 3;
+    __goblint_check(x == 3);
+    write_arg(&x);
+    __goblint_check(x != 3); //UNKNOWN
+    __goblint_check(x == 5); //UNKNOWN
+}
