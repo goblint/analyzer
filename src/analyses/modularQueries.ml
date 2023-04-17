657585102f686805887da0abf585f11fa4dc2d99
Julian Erhard <julian.erhard@tum.de>
2023-04-17 18:35:38 +0200

    ModularQueries: include UnitAnalysis.Spec, remove other transfer functions copied from it.

--- src/analyses/modularQueries.ml
+++ src/analyses/modularQueries.ml
@@ -3,47 +3,15 @@ open Analyses
 
 module Spec =
 struct
-  include Analyses.DefaultSpec
+  include UnitAnalysis.Spec
 
   let name () = "modular_queries"
-  module D = Lattice.Unit
-  module C = Lattice.Unit
 
   let query ctx (type a) (q: a Queries.t): a Queries.result =
     match q with
     | IsMultiple v ->
       TypeVarinfoMap.mem_varinfo v
     | _ -> Queries.Result.top q
-
-  (* transfer functions *)
-  let assign ctx (lval:lval) (rval:exp) : D.t =
-    ctx.local
-
-  let branch ctx (exp:exp) (tv:bool) : D.t =
-    ctx.local
-
-  let body ctx (f:fundec) : D.t =
-    ctx.local
-
-  let return ctx (exp:exp option) (f:fundec) : D.t =
-    ctx.local
-
-  let enter ctx (lval: lval option) (f:fundec) (args:exp list) : (D.t * D.t) list =
-    [ctx.local, ctx.local]
-
-  let combine_env ctx lval fexp f args fc au f_ask =
-    au
-
-  let combine_assign ctx (lval:lval option) fexp (f:fundec) (args:exp list) fc (au:D.t) (f_ask: Queries.ask) : D.t =
-    ctx.local
-
-  let special ctx (lval: lval option) (f:varinfo) (arglist:exp list) : D.t =
-    ctx.local
-
-  let startstate v = D.bot ()
-  let threadenter ctx lval f args = [D.top ()]
-  let threadspawn ctx lval f args fctx = ctx.local
-  let exitstate  v = D.top ()
 end
 
 let _ =
