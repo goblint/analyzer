1e490b6444a696f3e8acc779b5aee35cb45b54f4
Julian Erhard <julian.erhard@tum.de>
2023-04-21 11:06:27 +0200

    varinfo_or_canonical: Do not make allocated addresses canonical.

--- src/util/modularUtil0.ml
+++ src/util/modularUtil0.ml
@@ -16,6 +16,6 @@ let is_modular () = match !modular with
     m
   | Some m -> m
 
-(** If [x] is global and we are in ["modular"] mode, retrieves the varinfo_to_canonical for [x], else returns [x] itself *)
+(** If [x] is global (but not a heap variable) and we are in ["modular"] mode, retrieves the varinfo_to_canonical for [x], else returns [x] itself *)
 let varinfo_or_canonical x =
-  if is_modular () && x.vglob then varinfo_to_canonical x else x
+  if is_modular () && x.vglob && not (InvariantCil.var_is_heap x) then varinfo_to_canonical x else x
