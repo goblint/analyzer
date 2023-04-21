407133cc24383c4904707dd609d903e59707239b
Julian Erhard <julian.erhard@tum.de>
2023-04-21 11:06:48 +0200

    Extend test case for malloced blocks.

--- tests/regression/70-modular/25-malloced-block.c
+++ tests/regression/70-modular/25-malloced-block.c
@@ -1,4 +1,4 @@
-//SKIP PARAM: --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'"
+//SKIP PARAM: --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'" --enable ana.int.interval
 #include<goblint.h>
 #include<stdlib.h>
 
@@ -15,6 +15,17 @@ void allocate_and_call_modify(){
     modify(m);
     __goblint_check(*m != 0); //UNKNOWN
     __goblint_check(*m == 12); //UNKNOWN
+    __goblint_check(0 <= *m);
+    __goblint_check(*m <= 12);
+
+    j = *m;
+
+    __goblint_check(j != 0); //UNKNOWN
+    __goblint_check(j == 12); //UNKNOWN
+    __goblint_check(0 <= j);
+    __goblint_check(j <= 12);
+
+
 }
 
 int main(){
