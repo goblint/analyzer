11a3b24fcf3c67ea426e783ad88f04d317c2ded5
Simmo Saan <simmo.saan@gmail.com>
2023-05-02 15:57:49 +0300

    Reword fixed 00-sanity/36-strict-loop-dead comment

--- tests/regression/00-sanity/36-strict-loop-dead.c
+++ tests/regression/00-sanity/36-strict-loop-dead.c
@@ -6,8 +6,8 @@ int basic2(int n) {
     return 0;
 
   for (int i = 0; i < n; i++) // NOWARN
-    // Bug in dead code warnings: no dead code warning is emitted, because body is not included
-    // in the result. Transformation checks all CFG nodes, and therefore works.
+    // Was bug in dead code warnings: no dead code warning was emitted, because body was not included
+    // in the result. Transformation checks all CFG nodes, and therefore worked.
     a += i + n; // NOWARN
 
   return a; // NOWARN
