2c465aba7164e13122e099f2de3c6dd7e29c7cce
Julian Erhard <julian.erhard@tum.de>
2023-05-03 14:44:03 +0200

    Modify multithreaded test case to use wrappers, fix use of wrong variable in condition.

--- tests/regression/70-modular/33-multithreading.c
+++ tests/regression/70-modular/33-multithreading.c
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <goblint.h>
 
-int global = 0;
+int global = 1;
 int **pp = NULL;
 
 void *change_pointer(void *p){
@@ -12,10 +12,18 @@ void *change_pointer(void *p){
 }
 
 void *write_through_pointer(void *p){
-	if(p != NULL && *pp != NULL){
+	if(pp != NULL && *pp != NULL){
 		**pp = 12;
 	}
 	return NULL;
+}
+
+void *change_pointer_wrapper(void *p){
+	return change_pointer(p);
+}
+
+void *write_through_pointer_wrapper(void *p){
+	return write_through_pointer(p);
 }
 
 int main(){
@@ -23,11 +31,11 @@ int main(){
 	pp = malloc(sizeof(int*));
 	*pp = NULL;
 
-	pthread_create(&t1, NULL, change_pointer, NULL);
-	// pthread_create(&t2, NULL, write_through_pointer, NULL);
-	write_through_pointer(NULL);
+	pthread_create(&t1, NULL, change_pointer_wrapper, NULL);
+	pthread_create(&t2, NULL, write_through_pointer_wrapper, NULL);
 
 	if(pp != NULL && *pp != NULL){
+		printf("%d\n", **pp);
 		__goblint_check(**pp == 12); //UNKNOWN!
 	}
 	__goblint_check(global == 12); //UNKNOWN!
