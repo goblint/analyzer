7d4e7a7bf71dd5e7d17de7f9dc37fb5f372a7148
Julian Erhard <julian.erhard@tum.de>
2023-05-11 15:20:37 +0200

    Add script for use with creduce that checks for unreached Fixpoint.

---
+++ scripts/creduce/fixpoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GOBLINTDIR="/home/julian/Entwicklung/Programm_Analyse/analyzer"
+CONF="--conf conf/modular.json --set ana.modular.funs ['getndelim2']"
+INPUT="/home/julian/Entwicklung/Programm_Analyse/bench/coreutils/cp_comb.c"
+
+$GOBLINTDIR/goblint $CONF $INPUT -v &> out.txt
+if [ $? -eq 3 ]; then
+    grep Fixpoint out.txt >/dev/null 2>&1
+else
+    exit 5
+fi
