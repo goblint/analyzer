8facb0c9ca8c382194a88bcdb4c01f14b051efd9
Johanna Schinabeck <johanna@Johanna>
2023-05-11 19:58:15 +0200

    added preprocessing file for loop termination analysis: this file contains code adding before it is analyzed

--- src/analyses/termination.ml
+++ src/analyses/termination.ml
@@ -240,6 +240,7 @@ class loopInstrVisitor (fd : fundec) = object(self)
           (match stripCastsDeep e with
             (* J: if x' + e2 or x' - e2 with x' = x and the type arithmetic:
                   - adds incrementation for d1 and d2 to the code*)
+            (* J: if the loopVar is changed*)
            | BinOp (op, Lval x', e2, typ) when (op = PlusA || op = MinusA) && x' = x && isArithmeticType typ -> (* TODO x = 1 + x, MinusA! *)
              (* increase diff by same expr *)
              let d1_inc = mkStmtOneInstr @@ Set (var d1, BinOp (PlusA, Lval (var d1), e2, typ), loc, eloc) in (* J: d1 = d1 + e2*)
@@ -259,6 +260,7 @@ class loopInstrVisitor (fd : fundec) = object(self)
           )
       | _ -> s
     in
+    (* J: *)
     ChangeDoChildrenPost (s, action) (* J: continue with the children*)
 end
 
---
+++ src/util/terminationPreprocessing.ml
@@ -0,0 +1,3 @@
+(* - code in src/analysis/termination.ml contains loopCounterVisitor which might be interesting
+   - check if overflow happend with new variable
+   - how do we deal with nested loops?*)
