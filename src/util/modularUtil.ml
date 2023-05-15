291fb4e3fce824a7a5bef011e6afbcdad80b4fa9
Julian Erhard <julian.erhard@tum.de>
2023-05-15 11:14:59 +0200

    Do not add index offsets in map_back.
    This is to avoid non-termination of the analyzer in loops with writes onto arrays.

--- src/util/modularUtil.ml
+++ src/util/modularUtil.ml
@@ -22,7 +22,10 @@ let represented_by ~(canonical:varinfo) ~(reachable: AD.t) =
     CilType.Varinfo.equal canonical a_c
   in
   let collect_represented (a: Addr.t) (acc: AD.t) =
-    if is_represented a then AD.add a acc else acc
+    if is_represented a then
+      AD.add a acc
+    else
+      acc
   in
   AD.fold collect_represented reachable (AD.bot ())
 
@@ -40,8 +43,14 @@ struct
   open ValueDomain.Compound
 
   let rec map_back (can_v: t) ~(reachable: AD.t) : t =
-    let add_offset (addr: AD.t) offs =
-      AD.map (fun a -> Addr.add_offset a offs) addr
+    let add_field_offset (addr: AD.t) offs =
+      match offs with
+      | `NoOffset
+      | `Index _ ->
+        (* Adding index may lead to non-termination, e.g. on 71/42 *)
+        addr
+      | `Field (f,x) ->
+        AD.map (fun a -> Addr.add_offset a offs) addr
     in
     match can_v with
     | `Top
@@ -57,7 +66,7 @@ struct
           match Addr.to_var_offset a with
           | Some (vi, off) when is_canonical vi ->
             let represented_no_offs = represented_by ~canonical:vi ~reachable in
-            add_offset represented_no_offs off
+            add_field_offset represented_no_offs off
           | _ ->
             AD.singleton a
         in
