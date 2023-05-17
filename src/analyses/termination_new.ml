14f2bdd90c4f5fb1ad69913d87441feedb0ed4fe
Alexander Schlenga <alexander.schlenga@tum.de>
2023-05-17 18:00:23 +0200

    Add required functions and values

--- src/analyses/termination_new.ml
+++ src/analyses/termination_new.ml
@@ -8,6 +8,17 @@ let terminates loop = () (* TODO *)
 module Spec : Analyses.MCPSpec =
 struct
 
+  let name () = "termination"
+
+  module D = Lattice.Unit (* TODO *)
+  module C = D (* TODO *)
+
+  let startstate _ = D.bot () (* TODO *)
+  let exitstate = startstate (* TODO *)
+
+  (** Provides some default implementations *)
+  include Analyses.IdentitySpec
+
   let query ctx (type a) (q: a Queries.t): a Queries.result =
     let open Queries in
       Result.top q (* TODO *)
@@ -15,5 +26,5 @@ struct
 end
 
 let _ =
-  (* Register this analysis within the master control program *)
+  (** Register this analysis within the master control program *)
   MCP.register_analysis (module Spec : MCPSpec)
