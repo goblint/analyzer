c4e92dba282891d8bd4e92d624cb26bb72b86abd
Alexander Schlenga <alexander.schlenga@tum.de>
2023-05-17 12:15:06 +0200

    Better dummy query

--- src/analyses/termination_new.ml
+++ src/analyses/termination_new.ml
@@ -1,12 +1,19 @@
+(** Work in progress *)
+
 open Prelude.Ana
 open Analyses
+
+let terminates loop = () (* TODO *)
 
 module Spec : Analyses.MCPSpec =
 struct
 
-  let query ctx (type a) (q: a Queries.t): a Queries.result = ()
+  let query ctx (type a) (q: a Queries.t): a Queries.result =
+    let open Queries in
+      Result.top q (* TODO *)
 
 end
 
 let _ =
+  (* Register this analysis within the master control program *)
   MCP.register_analysis (module Spec : MCPSpec)
