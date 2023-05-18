ec89bc2be9b31350b4f7cd135ab8cb128bc3f8a9
Johanna Schinabeck <johanna@Johanna>
2023-05-18 15:38:59 +0200

    added code adding to terminationPreprocessing file in util; created sh script to execute the code easily and store the cil output in the output.txt file; NOTE: the signs.ml file does currently contain the termination analysis; signsOrig.ml contains the original signs analysis

---
+++ runningGob.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./goblint --enable dbg.debug tests/regression/55-loop-unrolling/01-simple-cases.c --set "ana.activated[+]" signs --enable ana.int.interval --enable justcil > output.txt
+
--- src/util/terminationPreprocessing.ml
+++ src/util/terminationPreprocessing.ml
@@ -1,3 +1,43 @@
 (* - code in src/analysis/termination.ml contains loopCounterVisitor which might be interesting
    - check if overflow happend with new variable
-   - how do we deal with nested loops?*)
+   - how do we deal with nested loops?
+   - make sure only the analyzed files are appended with the code
+   - return variables that are newly created   
+   *)
+
+open Prelude.Ana
+
+let show_location_id l =
+   string_of_int l.line ^ "_" ^ string_of_int l.column
+
+
+class loopCounterVisitor (fd : fundec) = object(self)
+inherit nopCilVisitor
+method! vstmt s =
+   let action s = match s.skind with
+      | Loop (b, loc, eloc, _, _) ->
+      let name = "term"^show_location_id loc in
+      let typ = intType in 
+      let v = Goblintutil.create_var (makeLocalVar fd name ~init:(SingleInit zero) typ) in
+      let inc_stmt = mkStmtOneInstr @@ Set (var v, increm (Lval (var v)) 1, loc, eloc) in
+      b.bstmts <- inc_stmt :: b.bstmts;
+      let nb = mkBlock [mkStmt s.skind] in
+      s.skind <- Block nb;
+      s
+      | _ -> s
+   in ChangeDoChildrenPost (s, action)
+end
+ 
+(*let action (fd : fundec) s =
+   let a s = match s.skind with
+   | Loop (b, loc, eloc, _, _) ->
+      let name = "term"^show_location_id loc in
+      let typ = intType in 
+      let v = Goblintutil.create_var (makeLocalVar fd name ~init:(SingleInit zero) typ) in
+      let inc_stmt = mkStmtOneInstr @@ Set (var v, increm (Lval (var v)) 1, loc, eloc) in
+      b.bstmts <- inc_stmt :: b.bstmts;
+      let nb = mkBlock [mkStmt s.skind] in
+      s.skind <- Block nb;
+      s
+   | _ -> s
+in ChangeDoChildrenPost (s, a)*)
