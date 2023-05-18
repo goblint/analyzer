03868cbb1abff86d08184a0ec464f2cc0836e9e1
J2000A <email@jonasaugust.de>
2023-05-18 15:28:10 +0200

    Add Unary Operator Inversion Mutation

---
+++ scripts/incremental-test-generation/clang-mutations/UnaryOperatorInversionCheck.cpp
@@ -0,0 +1,41 @@
+//===--- UnaryOperatorInversionCheck.cpp - clang-tidy ---------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "UnaryOperatorInversionCheck.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+
+using namespace clang::ast_matchers;
+
+namespace clang::tidy::readability {
+
+void UnaryOperatorInversionCheck::registerMatchers(MatchFinder *Finder) {
+  Finder->addMatcher(stmt(ifStmt()).bind("unary-operator-inversion"), this);
+}
+
+// Replace if(e) with if(!(e))
+void UnaryOperatorInversionCheck::check(const MatchFinder::MatchResult &Result) {
+  const auto *MatchedDecl = Result.Nodes.getNodeAs<IfStmt>("unary-operator-inversion");
+  // Get locations
+  SourceLocation Start = MatchedDecl->getLParenLoc().getLocWithOffset(1);
+  SourceLocation End = MatchedDecl->getRParenLoc();
+  auto Range = CharSourceRange::getCharRange(Start, End);
+  // Get the current expression in the if statement
+  const SourceManager *SM = Result.SourceManager;
+  const char *StartPtr = SM->getCharacterData(Start);
+  const char *EndPtr = SM->getCharacterData(End);
+  size_t Length = EndPtr - StartPtr;
+  std::string Expression(StartPtr, Length);
+  // Invert the expression with the unary operator
+  std::string Replacement = "!(" + Expression + ")" " /* Inverted if statement */";
+  diag(Start, "If Statement %0 was inverted")
+      << Expression
+      << FixItHint::CreateReplacement(Range, Replacement);
+}
+
+} // namespace clang::tidy::readability
