13d70602b4b6ae8b152bbea95dd4b68b72a75f6d
Johanna Schinabeck <johanna@Johanna>
2023-05-18 16:10:36 +0200

    made the runningGob.sh script even fancyer; added the make and make install also to the script

--- runningGob.sh
+++ runningGob.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+make
+make install
 ./goblint --enable dbg.debug tests/regression/55-loop-unrolling/01-simple-cases.c --set "ana.activated[+]" signs --enable ana.int.interval --enable justcil > output.txt
 
