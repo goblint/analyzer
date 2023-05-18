ac053d88bff66ca84fb93b22d54c86e07218c8ae
J2000A <email@jonasaugust.de>
2023-05-18 16:35:06 +0200

    Remove unnecesary comments

--- scripts/incremental-test-generation/clang-mutations/UnaryOperatorInversionCheck.cpp
+++ scripts/incremental-test-generation/clang-mutations/UnaryOperatorInversionCheck.cpp
@@ -1,11 +1,3 @@
-//===--- UnaryOperatorInversionCheck.cpp - clang-tidy ---------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 #include "UnaryOperatorInversionCheck.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
