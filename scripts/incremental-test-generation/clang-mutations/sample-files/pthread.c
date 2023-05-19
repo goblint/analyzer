7875a58d863ed15129a1191c820f168c6d02df41
J2000A <email@jonasaugust.de>
2023-05-19 15:24:18 +0200

    New sample file for pthreads

---
+++ scripts/incremental-test-generation/clang-mutations/sample-files/pthread.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void* thread_function(void* arg) {
+    printf("Thread function\n");
+    return NULL;
+}
+
+void cleanup_function(void* arg) {
+    printf("Cleanup function\n");
+}
+
+int main() {
+    pthread_t thread;
+    pthread_attr_t attr;
+    pthread_cond_t cond;
+    pthread_condattr_t cond_attr;
+    pthread_key_t key;
+    pthread_mutex_t mutex;
+    pthread_mutexattr_t mutex_attr;
+    pthread_once_t once_control;
+    pthread_rwlock_t rwlock;
+    pthread_rwlockattr_t rwlock_attr;
+    struct sched_param param;
+
+    // Attribute functions
+    pthread_attr_destroy(&attr);
+    pthread_attr_getdetachstate(&attr, NULL);
+    pthread_attr_getguardsize(&attr, NULL);
+    pthread_attr_getinheritsched(&attr, NULL);
+    pthread_attr_getschedparam(&attr, &param);
+    pthread_attr_getschedpolicy(&attr, NULL);
+    pthread_attr_getscope(&attr, NULL);
+    pthread_attr_getstackaddr(&attr, NULL);
+    pthread_attr_getstacksize(&attr, NULL);
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, 0);
+    pthread_attr_setguardsize(&attr, 0);
+    pthread_attr_setinheritsched(&attr, 0);
+    pthread_attr_setschedparam(&attr, &param);
+    pthread_attr_setschedpolicy(&attr, 0);
+    pthread_attr_setscope(&attr, 0);
+    pthread_attr_setstackaddr(&attr, NULL);
+    pthread_attr_setstacksize(&attr, 0);
+
+    // Cancellation functions
+    pthread_cancel(thread);
+    pthread_setcancelstate(0, NULL);
+    pthread_setcanceltype(0, NULL);
+    pthread_testcancel();
+
+    // Condition variable functions
+    pthread_cond_broadcast(&cond);
+    pthread_cond_destroy(&cond);
+    pthread_cond_init(&cond, &cond_attr);
+    pthread_cond_signal(&cond);
+    pthread_cond_timedwait(&cond, &mutex, NULL);
+    pthread_cond_wait(&cond, &mutex);
+
+    // Condition attribute functions
+    pthread_condattr_destroy(&cond_attr);
+    pthread_condattr_getpshared(&cond_attr, NULL);
+    pthread_condattr_init(&cond_attr);
+    pthread_condattr_setpshared(&cond_attr, 0);
+
+    // Creation and termination functions
+    pthread_create(&thread, &attr, thread_function, NULL);
+    pthread_detach(thread);
+    pthread_equal(thread, thread);
+    pthread_exit(NULL);
+    pthread_join(thread, NULL);
+
+    // Thread-specific data functions
+    pthread_key_create(&key, cleanup_function);
+    pthread_key_delete(key);
+    pthread_getspecific(key);
+    pthread_setspecific(key, NULL);
+
+    // Mutex functions
+    pthread_mutex_destroy(&mutex);
+    pthread_mutex_getprioceiling(&mutex, NULL);
+    pthread_mutex_init(&mutex, &mutex_attr);
+    pthread_mutex_lock(&mutex);
+    pthread_mutex_setprioceiling(&mutex, 0, NULL);
+    pthread_mutex_trylock(&mutex);
+    pthread_mutex_unlock(&mutex);
+
+    // Mutex attribute functions
+    pthread_mutexattr_destroy(&mutex_attr);
+    pthread_mutexattr_getprioceiling(&mutex_attr, NULL);
+    pthread_mutexattr_getprotocol(&mutex_attr, NULL);
+    pthread_mutexattr_getpshared(&mutex_attr, NULL);
+    pthread_mutexattr_gettype(&mutex_attr, NULL);
+    pthread_mutexattr_init(&mutex_attr);
+    pthread_mutexattr_setprioceiling(&mutex_attr, 0);
+    pthread_mutexattr_setprotocol(&mutex_attr, 0);
+    pthread_mutexattr_setpshared(&mutex_attr, 0);
+    pthread_mutexattr_settype(&mutex_attr, 0);
+
+    // One-time initialization
+    pthread_once(&once_control, thread_function);
+
+    // Read-write lock functions
+    pthread_rwlock_destroy(&rwlock);
+    pthread_rwlock_init(&rwlock, &rwlock_attr);
+    pthread_rwlock_rdlock(&rwlock);
+    pthread_rwlock_tryrdlock(&rwlock);
+    pthread_rwlock_trywrlock(&rwlock);
+    pthread_rwlock_unlock(&rwlock);
+    pthread_rwlock_wrlock(&rwlock);
+
+    // Read-write lock attribute functions
+    pthread_rwlockattr_destroy(&rwlock_attr);
+    pthread_rwlockattr_getpshared(&rwlock_attr, NULL);
+    pthread_rwlockattr_init(&rwlock_attr);
+    pthread_rwlockattr_setpshared(&rwlock_attr, 0);
+
+    // Other functions
+    pthread_self();
+    pthread_getschedparam(thread, NULL, &param);
+    pthread_setschedparam(thread, 0, &param);
+    pthread_getconcurrency();
+    pthread_setconcurrency(0);
+
+    return 0;
+}
