24702c65fb57bd4655b6dfa91c3b58a6d636b010
Alexander Schlenga <alexander.schlenga@tum.de>
2023-05-20 15:48:57 +0200

    Some stuff

--- src/analyses/termination_new.ml
+++ src/analyses/termination_new.ml
@@ -1,8 +1,12 @@
 (** Work in progress *)
 
 open Analyses
+open GoblintCil
+open TerminationPreprocessing
 
-let terminates loop = () (* TODO *)
+let terminates ctx loop exp =
+  match ctx.ask (EvalInt exp) with
+    _ -> () (* TODO *)
 
 module Spec : Analyses.MCPSpec =
 struct
@@ -18,6 +22,9 @@ struct
   (** Provides some default implementations *)
   include Analyses.IdentitySpec
 
+  let branch ctx (exp:exp) (tv:bool) =
+    ctx.local (* TODO *)
+
   let query ctx (type a) (q: a Queries.t): a Queries.result =
     let open Queries in
     Result.top q (* TODO *)
@@ -25,5 +32,7 @@ struct
 end
 
 let _ =
+  (** Register the preprocessing *)
+  Cilfacade.register_preprocess (Spec.name ()) (new loopCounterVisitor);
   (** Register this analysis within the master control program *)
   MCP.register_analysis (module Spec : MCPSpec)
