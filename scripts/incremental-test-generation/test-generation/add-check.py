aea65aaacf3f9d54271a1b6d6eda71fb1bb1fdca
Jonas August <ge59duv@mytum.de>
2023-05-24 21:21:03 +0200

    Implementing add-check

---
+++ scripts/incremental-test-generation/test-generation/add-check.py
@@ -0,0 +1,52 @@
+# Takes a file and generates the goblint checks
+# Stores the file with an additional "_c"
+# When there is a compilation error the process writes [COMPILE_FAIL] into the meta data file for the given index
+
+import argparse
+import subprocess
+import sys
+
+import yaml
+
+def process_file(file_path, index, goblint_path, meta_path):
+    command = [
+    goblint_path,
+    "--enable",
+    "trans.goblint-check",
+    "--set",
+    "trans.activated",
+    '["assert"]',
+    "--set",
+    "trans.output",
+    file_path.rsplit('.', 1)[0] + '_c.c',
+    file_path
+    ]
+
+    result = subprocess.run(command)
+
+    compiling = result.returncode == 0
+
+    with open(meta_path, 'r') as file:
+        yaml_data = yaml.safe_load(file)
+        yaml_data["p_" + str(index)]['compilation'] = compiling
+    with open(meta_path, 'w') as file:
+        yaml.safe_dump(yaml_data, file)
+    
+    if not compiling :
+        print(f"Error compiling program with index {index}.")
+        sys.exit(-1)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate __goblint_check() for a C file. When not compiling write [NOT COMPILING] in the meta file')
+
+    # Add the required arguments
+    parser.add_argument('file', help='Path to the C file')
+    parser.add_argument('index', help='Index of the file (needed for meta data)')
+    parser.add_argument('goblint', help='Path to the Goblint executable')
+    parser.add_argument('meta', help='Path to the meta data file')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the process_file function with the provided arguments
+    process_file(args.file, args.index, args.goblint, args.meta)
