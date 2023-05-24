36a2b21033baf1f0e9d5540f9eab809c64604585
Jonas August <ge59duv@mytum.de>
2023-05-24 20:05:55 +0200

    Use argparser

--- scripts/incremental-test-generation/test-generation/add-check-comments.py
+++ scripts/incremental-test-generation/test-generation/add-check-comments.py
@@ -1,17 +1,20 @@
-# Adds "//UNDEFINED" or "//SUCCESS" to the Goblint checks __goblint_check(exp);
-# Run with ´python3 script.py example.txt -u´ or ´python3 script.py example.txt -s´
+# Adds "//UNDEFINED" or "//SUCCESS" to the Goblint checks "__goblint_check(exp);".
+# Stores the file with the same name when adding "//SUCCESS".
+# Stores the file with the appendix _u when adding "//UNDEFINED".
+# Run with ´python3 script.py example.txt -u´ or ´python3 script.py example.txt -s´.
 
+import argparse
 import sys
 import re
 
 def process_file(file_name, option):
-    if option == '-u':
+    if option == 'u':
         print("Add \"//UNDEFINED\" to the Goblint checks __goblint_check(exp);")
-    elif option == '-s':
+    elif option == 's':
         print("Add \"//SUCCESS\" to the Goblint checks __goblint_check(exp);")
 
     try:
-        with open(file_name, 'r+') as file:
+        with open(file_name, 'r') as file:
             lines = file.readlines()
             modified_lines = []
             
@@ -27,22 +30,28 @@ def process_file(file_name, option):
                         line = line.replace(match.group(1), modified_line)
                 modified_lines.append(line)
             
-            file.seek(0)
-            file.writelines(modified_lines)
-            file.truncate()
-            print("Processing complete. Modified lines have been added.")
+            if option == '-u':
+                new_file_name = file_name.rsplit('.', 1)[0] + '_u.c'
+                with open(new_file_name, 'w') as new_file:
+                    new_file.writelines(modified_lines)
+                print("Processing complete. Modified lines have been added to the new file:", new_file_name)
+            elif option == '-s':
+                with open(file_name, 'w') as file:
+                    file.writelines(modified_lines)
+                print("Processing complete. Modified lines have been added to the existing file:", file_name)
     except FileNotFoundError:
         print("Error: File not found.")
     except:
         print("An error occurred while processing the file.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Error: Invalid number of arguments. Provide first a file and then -u for \"//UNDEFINED\" or -s for \"//SUCCESS\".")
-    else:
-        file_name = sys.argv[1]
-        option = sys.argv[2]
-        if option not in ['-u', '-s']:
-            print("Error: Invalid option. Provide -u for \"//UNDEFINED\" or -s for \"//SUCCESS\".")
-        else:
-            process_file(file_name, option)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="file name")
+    parser.add_argument("-u", "--undefined", action="store_true", help="option for //UNDEFINED")
+    parser.add_argument("-s", "--success", action="store_true", help="option for //SUCCESS")
+    args = parser.parse_args()
+
+    if not (args.undefined or args.success):
+        parser.error("Error: Invalid option. Provide -u for \"//UNDEFINED\" or -s for \"//SUCCESS\".")
+
+    process_file(args.file, "u" if args.undefined else "s")
