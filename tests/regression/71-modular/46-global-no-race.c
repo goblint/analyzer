b1ca0e1a5f2dd56e8adda693a45af775ddf0ada3
Julian Erhard <julian.erhard@tum.de>
2023-05-26 15:22:00 +0200

    Add test cases for no-races in modular mode.

---
+++ tests/regression/71-modular/46-global-no-race.c
@@ -0,0 +1,33 @@
+// PARAM: --set ana.modular.funs "['write']" --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'"
+#include<stdlib.h>
+#include<pthread.h>
+
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
+int g = 0;
+
+int write(int *p){
+	g = 23;
+}
+
+void *thread1(void *p){
+	pthread_mutex_lock(&m);
+	write(p); // NORACE
+	pthread_mutex_unlock(&m);
+	return NULL;
+}
+
+void *thread2(void *p){
+	pthread_mutex_lock(&m);
+	g = 12; // NORACE
+	pthread_mutex_unlock(&m);
+	return NULL;
+}
+
+int main(){
+	pthread_t t1;
+	pthread_t t2;
+	pthread_create(&t1, NULL, thread1, NULL);
+	pthread_create(&t2, NULL, thread2, NULL);
+}
+
---
+++ tests/regression/71-modular/47-local-no-race.c
@@ -0,0 +1,35 @@
+// PARAM: --set ana.modular.funs "['write']" --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'"
+#include<stdlib.h>
+#include<pthread.h>
+
+
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
+int write(int *p){
+	*p = 23;
+}
+
+void *thread1(void *p){
+	pthread_mutex_lock(&m);
+	write(p); // RACE
+	pthread_mutex_unlock(&m);
+	return NULL;
+}
+
+void *thread2(void *p){
+	int *ip = (int *) p;
+	pthread_mutex_lock(&m);
+	*ip = 12; // RACE
+	pthread_mutex_unlock(&m);
+	return NULL;
+}
+
+int main(){
+	pthread_t t1;
+	pthread_t t2;
+	int j = 0;
+
+	pthread_create(&t1, NULL, thread1, &j);
+	pthread_create(&t2, NULL, thread2, &j);
+}
+
