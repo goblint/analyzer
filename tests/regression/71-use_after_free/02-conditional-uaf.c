748d77d37ae50fc4aa1fd8cb4fb7c4829adad1db
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-05-26 15:58:19 +0200

    Add conditional UAF regression test case

---
+++ tests/regression/71-use_after_free/02-conditional-uaf.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int *ptr = malloc(sizeof(int));
+    *ptr = 42;
+
+    int input1;
+
+    if (input1) {
+        free(ptr);   
+    }
+
+    *ptr = 43; // Should report "Use After Free (CWE-416)"
+    free(ptr); // Should report "Double Free (CWE-415)"
+}
