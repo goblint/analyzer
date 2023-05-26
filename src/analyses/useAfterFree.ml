b1059012b361d57bcb99513010e5a278e00267c5
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-05-26 15:28:56 +0200

    Make the first UAF version now work

--- src/analyses/useAfterFree.ml
+++ src/analyses/useAfterFree.ml
@@ -2,66 +2,121 @@
 
 open GoblintCil
 open Analyses
+open MessageCategory
+
+(* TODO: Maybe come up with a better name for top at some point? *)
+module ToppedVarInfoSet = SetDomain.ToppedSet(CilType.Varinfo)(struct let topname = "Unknown" end)
 
 module Spec : Analyses.MCPSpec =
 struct
   include Analyses.DefaultSpec
 
   let name () = "useafterfree"
-  (* module D = Lattice.Unit *)
-  module D = SetDomain.Make(ValueDomain.Blobs)
+
+  module D = ToppedVarInfoSet
   module C = Lattice.Unit
 
-  (** TODO: What about context-sensitivity? *)
+  (** TODO: Try out later in benchmarks to see how we perform with and without context-sensititivty *)
   let context _ _ = ()
 
 
   (* HELPER FUNCTIONS *)
 
-  let check_exp (exp:exp) ctx =
-    let state = ctx.local in
-    match ctx.ask (Queries.EvalValue exp) with
-    | a when not (Queries.VD.is_top a) ->
-      begin match a with
-        | `Blob (v, s, t) -> if D.mem (v, s, t) state then true else false
-        | _ -> false
-      end
-    | _ -> false
+  (* Took inspiration from malloc_null *)
+  let may (f:'a -> 'b) (x:'a option) : unit =
+    match x with
+    | Some x -> f x
+    | None -> ()
 
-  let check_lval (lval:lval) ctx =
+  (* Also took inspiration from malloc_null *)
+  let get_concrete_lval (ask:Queries.ask) (lval:lval) =
+    match ask.f (Queries.MayPointTo (mkAddrOf lval)) with
+    | a when Queries.LS.cardinal a = 1 && not (Queries.LS.mem (dummyFunDec.svar, `NoOffset) a) ->
+      let v, o = Queries.LS.choose a in
+      Some v
+    | _ -> None
+
+  (* And also took inspiration from malloc_null *)
+  let get_concrete_exp (exp:exp) =
+    match constFold true exp with
+    | CastE (_, Lval (Var v, _))
+    | Lval (Var v, _) -> Some v
+    | _ -> None
+
+  let warn_lval_might_contain_freed ?(is_double_free = false) (transfer_fn_name:string) (lval:lval) ctx =
     let state = ctx.local in
+    let cwe_number = if is_double_free then 415 else 416 in
     match lval with
+    (* Case: lval is a variable *)
+    | (Var v, _) ->
+      if D.mem v state then
+        M.warn ~category:(Behavior (Undefined UseAfterFree)) ~tags:[CWE cwe_number] "lval in \"%s\" is a maybe freed pointer" transfer_fn_name
+      else ()
+    (* Case: lval is an object whose address is in a pointer *)
     | (Mem e, _) ->
-      begin match ctx.ask (Queries.EvalValue e) with
-        | a when not (Queries.VD.is_top a) ->
-          begin match a with
-            | `Blob (v, s, t) -> if D.mem (v, s, t) state then true else false
-            | _ -> false
-          end
-        | _ -> false
+      begin match get_concrete_exp e with
+        | Some v ->
+          if D.mem v state then
+            M.warn ~category:(Behavior (Undefined UseAfterFree)) ~tags:[CWE cwe_number] "lval in \"%s\" points to a maybe freed pointer" transfer_fn_name
+          else ()
+        | None -> ()
       end
-    | _ -> false
+  (* TODO: Wasn't sure about the snippet below. Clean it up at some point *)
+  (* begin match ctx.ask (Queries.MayPointTo e) with
+      (* TODO: Do we need the second conjunct? *)
+      | a when not (Queries.LS.is_top a) && not (Queries.LS.mem (dummyFunDec.svar, `NoOffset) a) ->
+      (* TODO: Took inspiration from malloc_null. Double check if it makes sense. *)
+      let v, o = Queries.LS.choose a in
+      if D.mem v state then
+        M.warn ~category:(Behavior (Undefined UseAfterFree)) "lval in \"%s\" points to a maybe freed pointer" transfer_fn_name
+      else ()
+      | _ -> ()
+      end *)
+
+  let rec warn_exp_might_contain_freed ?(is_double_free = false) (transfer_fn_name:string) (exp:exp) ctx =
+    match exp with
+    (* Base recursion cases *)
+    | Const _
+    | SizeOf _
+    | SizeOfStr _
+    | AlignOf _
+    | AddrOfLabel _ -> ()
+    (* Non-base cases *)
+    | Real e
+    | Imag e
+    | SizeOfE e
+    | AlignOfE e
+    | UnOp (_, e, _)
+    | CastE (_, e) -> warn_exp_might_contain_freed ~is_double_free transfer_fn_name e ctx
+    | BinOp (_, e1, e2, _) ->
+      warn_exp_might_contain_freed ~is_double_free transfer_fn_name e1 ctx;
+      warn_exp_might_contain_freed ~is_double_free transfer_fn_name e2 ctx
+    | Question (e1, e2, e3, _) ->
+      warn_exp_might_contain_freed ~is_double_free transfer_fn_name e1 ctx;
+      warn_exp_might_contain_freed ~is_double_free transfer_fn_name e2 ctx;
+      warn_exp_might_contain_freed ~is_double_free transfer_fn_name e3 ctx
+    (* Lval cases (need [warn_lval_might_contain_freed] for them) *)
+    | Lval lval
+    | StartOf lval
+    | AddrOf lval -> warn_lval_might_contain_freed ~is_double_free transfer_fn_name lval ctx
 
 
   (* TRANSFER FUNCTIONS *)
 
   let assign ctx (lval:lval) (rval:exp) : D.t =
     let state = ctx.local in
-    (* Intuition:
-      * Check if lval and/or lval has an expression that points to a "maybe freed" blob
-      * If yes -> send out a WARNING; otherwise -> don't WARN
-      * In either case above -> don't change the CFG node's state
-    *)
-    match check_lval lval ctx, check_exp rval ctx with
-    | true, true -> M.warn "WARN: lval and rval contain maybe freed blob"; state
-    | false, true -> M.warn "WARN: rval contains maybe freed blob"; state
-    | true, false -> M.warn "WARN: lval contains maybe freed blob"; state
-    | false, false -> state
+    warn_lval_might_contain_freed "assign" lval ctx;
+    warn_exp_might_contain_freed "assign" rval ctx;
+    match get_concrete_exp rval, get_concrete_lval (Analyses.ask_of_ctx ctx) lval with
+    | Some v_exp, Some v_lval ->
+      if D.mem v_exp state then
+        D.add v_lval state
+      else state
+    | _ -> state
 
   let branch ctx (exp:exp) (tv:bool) : D.t =
     let state = ctx.local in
-    if check_exp exp ctx then
-      M.warn "WARN: branch exp contains maybe freed blob";
+    warn_exp_might_contain_freed "branch" exp ctx;
     state
 
   let body ctx (f:fundec) : D.t =
@@ -69,112 +124,45 @@ struct
 
   let return ctx (exp:exp option) (f:fundec) : D.t =
     let state = ctx.local in
-    match exp with
-    | Some e ->
-      if check_exp e ctx then
-        M.warn "WARN: return expression contains maybe freed blob";
-      state
-    | None -> state
+    may (fun x -> warn_exp_might_contain_freed "return" x ctx) exp;
+    state
 
+  (* TODO: FINISH *)
   let enter ctx (lval:lval option) (f:fundec) (args:exp list) : (D.t * D.t) list =
     let caller_state = ctx.local in
-    let filtered_args = List.filter (fun x -> check_exp x ctx) args in
-    let args_to_add_to_callee_state = List.map (fun x ->
-        match ctx.ask (Queries.EvalValue x) with
-        | a when not (Queries.VD.is_top a) ->
-          begin match a with
-            | `Blob (v, s, t) -> (v, s, t)
-            | _ -> ValueDomain.Blobs.top () (* TODO: Is this correct? *)
-          end
-        | _ -> ValueDomain.Blobs.top () (* TODO: Is this correct? *)
-      ) filtered_args in
-    let callee_state = D.of_list args_to_add_to_callee_state in
-    match lval with
-    | Some lval ->
-      if check_lval lval ctx then
-        M.warn "WARN: lval in enter contains maybe freed blob";
-      [caller_state, callee_state]
-    | None -> [caller_state, callee_state]
+    (* TODO: Decided to keep callee_state the same as caller_state for now. Discuss this. *)
+    let callee_state = caller_state in
+    may (fun x -> warn_lval_might_contain_freed "enter" x ctx) lval;
+    List.iter (fun arg -> warn_exp_might_contain_freed "enter" arg ctx) args;
+    [caller_state, callee_state]
 
-  (* TODO: Unsure about this transfer function. Should we leave it as an identity function? *)
+  (* TODO: FINISH *)
   let combine_env ctx (lval:lval option) fexp (f:fundec) (args:exp list) fc (callee_local:D.t) (f_ask:Queries.ask) : D.t =
     callee_local
 
+  (* TODO: FINISH *)
   let combine_assign ctx (lval:lval option) fexp (f:fundec) (args:exp list) fc (callee_local:D.t) (f_ask: Queries.ask): D.t =
     let state = ctx.local in
-    let () = List.iter (fun x ->
-        if check_exp x ctx then
-          M.warn "argument in combine_assign contains maybe freed blob"
-      ) args in
-    match lval with
-    | Some lval ->
-      if check_lval lval ctx then
-        M.warn "lval in combine_assign contains maybe freed blob";
-      state
-    | None -> state
+    may (fun x -> warn_lval_might_contain_freed "combine_assign" x ctx) lval;
+    List.iter (fun arg -> warn_exp_might_contain_freed "combine_assign" arg ctx) args;
+    state
 
   let special ctx (lval:lval option) (f:varinfo) (arglist:exp list) : D.t =
     let state = ctx.local in
+    may (fun x -> warn_lval_might_contain_freed ~is_double_free:(f.vname = "free") ("special: " ^ f.vname) x ctx) lval;
+    List.iter (fun arg -> warn_exp_might_contain_freed ~is_double_free:(f.vname = "free") ("special: " ^ f.vname) arg ctx) arglist;
     let desc = LibraryFunctions.find f in
     match desc.special arglist with
     | Free ptr ->
-      if not (check_exp ptr ctx) then
-        begin match ctx.ask (Queries.EvalValue ptr) with
-          | a when not (Queries.VD.is_top a) ->
-            begin match a with
-              | `Blob (v, s, t) -> D.add (v, s, t) state
-              | _ -> state
-            end
-          | _ -> state
-        end
-      else
-        (* TODO: Is this way of printing in the else block okayish? *)
-        let () = M.warn "WARN: trying to free an already freed ptr" in
-        state
-    (*
-     * Check if we're allocating a maybe freed ptr -> in this case remove
-     * it from the maybe freed set
-     * TODO: Does this make sense?
-     *)
-    | Malloc _
-    | Calloc _
-    | Realloc _ ->
-      begin match lval with
-        | Some lval ->
-          begin match lval with
-            | (Mem e, _) ->
-              begin match ctx.ask (Queries.EvalValue e) with
-                | a when not (Queries.VD.is_top a) ->
-                  begin match a with
-                    | `Blob (v, s, t) ->
-                      (*
-                       * If we're trying to allocate a blob that is maybe freed,
-                       * then we can remove it from the maybe freed set
-                       * TODO: Makes sense?
-                       *)
-                      if D.mem (v, s, t) state then
-                        D.remove (v, s, t) state
-                      else state
-                    | _ -> state
-                  end
-                | _ -> state
-              end
-            | _ -> state
-          end
+      begin match get_concrete_exp ptr with
+        | Some v ->
+          if D.mem v state then
+            state
+          else
+            D.add v state
         | None -> state
       end
-    (*
-     * If we're not dealing with free or *alloc,
-     * then check if the lval we're assigning to is a ptr to a maybe freed blob.
-     * If yes, then WARN
-     *)
-    | _ ->
-      match lval with
-      | Some lval ->
-        if check_lval lval ctx then
-          M.warn "WARN: lval in special contains maybe freed blob";
-        state
-      | None -> state
+    | _ -> state
 
   let threadenter ctx lval f args = [ctx.local]
   let threadspawn ctx lval f args fctx = ctx.local
