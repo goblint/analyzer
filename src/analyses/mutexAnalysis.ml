951fcdcf8a4b91e8d69d59757423d68dbe654e56
Michael Schwarz <michael.schwarz93@gmail.com>
2023-05-26 13:05:25 +0200

    Indentation

--- src/analyses/mutexAnalysis.ml
+++ src/analyses/mutexAnalysis.ml
@@ -270,10 +270,10 @@ struct
                     let protected =
                       if write then
                         (vs_empty, vs_empty, vs_empty, vs)
-                    else
-                      (vs_empty, vs_empty, vs, vs_empty)
-                  in
-                  ctx.sideg (V.protected addr) (G.create_protected protected)
+                      else
+                        (vs_empty, vs_empty, vs, vs_empty)
+                    in
+                    ctx.sideg (V.protected addr) (G.create_protected protected)
                   ) held_recovery;
             )
         | None -> M.info ~category:Unsound "Write to unknown address: privatization is unsound."
