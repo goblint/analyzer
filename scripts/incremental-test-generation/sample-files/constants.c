9cc1e8c83d7737ac657ad39ad924a6f16214fecb
J2000A <email@jonasaugust.de>
2023-05-30 21:11:04 +0200

    Add main to test programs

--- scripts/incremental-test-generation/sample-files/constants.c
+++ scripts/incremental-test-generation/sample-files/constants.c
@@ -1,6 +1,11 @@
 #define MY_MACRO_5 5
 #define MY_MACRO_0 0
-int a = 42;
-int b = 0;
-int c = MY_MACRO_5;
-int d = MY_MACRO_0;
+
+int main() {
+    int a = 42;
+    int b = 0;
+    int c = MY_MACRO_5;
+    int d = MY_MACRO_0;
+
+    return 0;
+}
