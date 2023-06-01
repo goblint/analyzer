e284596c15e02a722ecf4cadfcd96de949b102c4
Julian Erhard <julian.erhard@tum.de>
2023-06-01 17:23:36 +0200

    Add test case for modular race detection.

---
+++ tests/regression/73-modular/49-return-race.c
@@ -0,0 +1,30 @@
+// SKIP PARAM: --set ana.modular.funs "['value']" --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'"
+#include<stdlib.h>
+#include<pthread.h>
+
+
+int g = 0;
+
+int value(){
+	return 23;
+}
+
+void *thread1(void *p){
+	int *a = malloc(sizeof(int));
+	// Race on g
+	g = value();  // RACE
+	return NULL;
+}
+
+void *thread2(void *p){
+	g = 12; // RACE
+	return NULL;
+}
+
+int main(){
+	pthread_t t1;
+	pthread_t t2;
+	pthread_create(&t1, NULL, thread1, NULL);
+	pthread_create(&t2, NULL, thread2, NULL);
+}
+
