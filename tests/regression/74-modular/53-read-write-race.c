2692824a64b2d9fc82ac47f48dbb4b741247245a
Julian Erhard <julian.erhard@tum.de>
2023-06-02 16:24:10 +0200

    Rename two modular tests, add race test with read in modularly analyzed function.

---
+++ tests/regression/74-modular/53-read-write-race.c
@@ -0,0 +1,36 @@
+// SKIP PARAM: --set ana.modular.funs "['write', 'read']" --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'used_globals'"
+#include<stdlib.h>
+#include<pthread.h>
+#include<stdio.h>
+
+int g = 0;
+int h = 0;
+
+void read(int* i){
+	int x = *i;
+	printf("%d\n", x);
+}
+
+void write(int* i){
+	*i = 23;
+}
+
+void *thread1(void *p){
+	read(&g); // RACE
+	read(&h); // NORACE
+	return NULL;
+}
+
+void *thread2(void *p){
+	write(&g); // RACE
+	read(&h); // NORACE
+	return NULL;
+}
+
+int main(){
+	pthread_t t1;
+	pthread_t t2;
+	pthread_create(&t1, NULL, thread1, NULL);
+	pthread_create(&t2, NULL, thread2, NULL);
+}
+
