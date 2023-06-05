8dcc0b00406839ce0782e52132f91a4ec06a7629
Julian Erhard <julian.erhard@tum.de>
2023-06-05 14:45:15 +0200

    Add test case where escaped local has unsound value.

---
+++ tests/regression/74-escape/01-local-esacpe.c
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include<pthread.h>
+#include<goblint.h>
+#include<unistd.h>
+
+int g = 0;
+int *p = &g;
+
+int let_escape(){
+	int x = 23;
+	g = 23;
+
+	__goblint_check(x == 23);
+	p = &x;
+	sleep(5);
+	__goblint_check(x == 23); //UNKNOWN!
+}
+
+void *thread1(void *pp){
+	let_escape(); //RACE
+	return NULL;
+}
+
+void write_through_pointer(){
+	sleep(2);
+	*p = 1; //RACE
+}
+
+void *thread2(void *p){
+	write_through_pointer();
+	return NULL;
+}
+
+int main(){
+	pthread_t t1;
+	pthread_t t2;
+	pthread_create(&t1, NULL, thread1, NULL);
+	pthread_create(&t2, NULL, thread2, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+}
+
