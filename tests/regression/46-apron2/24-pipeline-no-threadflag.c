60d0a561aa6a68ee110bc4fcaa456cda361c58e6
Michael Schwarz <michael.schwarz93@gmail.com>
2023-06-07 11:38:06 +0200

    Unskip working test (References #860)

--- tests/regression/46-apron2/24-pipeline-no-threadflag.c
+++ tests/regression/46-apron2/24-pipeline-no-threadflag.c
@@ -1,16 +1,20 @@
-// SKIP PARAM: --set ana.activated[+] apron --set ana.activated[-] threadflag
+//PARAM: --set ana.activated[+] apron --set ana.activated[-] threadflag --set ana.activated[-] thread --set ana.activated[-] threadid
 // Minimized from sv-benchmarks/c/systemc/pipeline.cil-1.c
 #include <assert.h>
+#include <goblint.h>
 
 int main_clk_pos_edge;
 int main_in1_req_up;
 
 int main()
 {
-  // main_clk_pos_edge = 2; // TODO: uncomment to unskip apron test
-  if (main_in1_req_up == 1) // TODO: both branches are dead
-    assert(0); // TODO: uncomment to unskip apron test, FAIL (unreachable)
+  int litmus;
+  main_clk_pos_edge = 2;
+  if (main_in1_req_up == 1)
+    litmus = 0; // unreachable
   else
-    assert(1); // reachable
+    litmus = 1;
+
+  __goblint_check(litmus == 1);
   return (0);
 }
