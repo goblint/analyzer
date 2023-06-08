7718a50786b098a0d8b92b6862a3199182a87a33
J2000A <email@jonasaugust.de>
2023-06-08 12:13:42 +0200

    Seperate parts of the script with options

--- scripts/incremental-test-generation/test-generation/generators/generate_git_build.sh
+++ scripts/incremental-test-generation/test-generation/generators/generate_git_build.sh
@@ -4,7 +4,7 @@
 if [[ -n $1 ]]; then
   source $1
 else
-  echo "Please give the following arguments: build_info_path output_path [--path]"
+  echo "Please give the following arguments: build_info_path output_path [--clone][--build][--path]"
   echo ""
   echo "The build_info sh file should look like this:"
   cat generate_git_build_USER_INFO_PRESET.sh
@@ -20,8 +20,19 @@ else
   exit 1
 fi
 
+if [ "$3" == "" ]; then
+  echo "Please provide one of the options [--clone][--build][--path]"
+  exit 1
+fi
+
 # Exit if any command fails
 set -e
+
+# Check bool variables
+if { $use_cmake && $use_make ; } || { ! $use_cmake && ! $use_make ; }; then
+  echo "Error: Either cmake or make should be used, but not both or neither."
+  exit 1
+fi
 
 # Get the name of the repo
 repo_name=$(basename "$git_url" .git)
@@ -32,23 +43,22 @@ if [ "$3" == "--path" ]; then
   exit 0
 fi
 
-# Check bool variables
-if { $use_cmake && $use_make ; } || { ! $use_cmake && ! $use_make ; }; then
-  echo "Error: Either cmake or make should be used, but not both or neither."
-  exit 1
+# Clone repo
+if [ "$3" == "--clone" ]; then
+  rm -rf "$output_path/$repo_name"
+  git clone $git_url "$output_path/$repo_name"
 fi
 
-# Main script
-rm -rf "$output_path/$repo_name"
-git clone $git_url "$output_path/$repo_name"
+# Build repo
+if [ "$3" == "--build" ]; then
+  cd "$output_path/$repo_name/$path_to_build"
+  pre_build_commands
 
-cd "$output_path/$repo_name/$path_to_build"
-pre_build_commands
+  if $use_cmake; then
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+  elif $use_make; then
+    bear -- make
+  fi
 
-if $use_cmake; then
-  cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-elif $use_make; then
-  bear -- make
+  post_build_commands
 fi
-
-post_build_commands
