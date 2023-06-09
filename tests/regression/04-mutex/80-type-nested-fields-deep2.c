9fced76e586c63b99a6891f5aa8fe532d942c752
Simmo Saan <simmo.saan@gmail.com>
2023-06-09 17:07:41 +0300

    Add failing deeper type nested fields race tests

--- tests/regression/04-mutex/80-type-nested-fields-deep2.c
+++ tests/regression/04-mutex/80-type-nested-fields-deep2.c
@@ -10,24 +10,29 @@ struct T {
   struct S s;
 };
 
+struct U {
+  struct T t;
+};
+
 // struct S s;
 // struct T t;
 
 extern struct S* getS();
 extern struct T* getT();
+extern struct U* getU();
 
 // getS could return the same struct as is contained in getT
 
 void *t_fun(void *arg) {
-  // should write to (struct T).s.field in addition to (struct S).field
+  // should write to (struct U).t.s.field in addition to (struct T).s.field
   // but easier to implement the other way around?
-  getS()->field = 1; // RACE!
+  getT()->s.field = 1; // RACE!
   return NULL;
 }
 
 int main(void) {
   pthread_t id;
   pthread_create(&id, NULL, t_fun, NULL);
-  getT()->s.field = 2; // RACE!
+  getU()->t.s.field = 2; // RACE!
   return 0;
 }
