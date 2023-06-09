ea3edf8caa52087f9bbe488800afbbdf4d17b783
J2000A <email@jonasaugust.de>
2023-06-09 15:14:15 +0200

    Consider  not compiling programs

--- scripts/incremental-test-generation/test-generation/generators/generate_git.py
+++ scripts/incremental-test-generation/test-generation/generators/generate_git.py
@@ -1,11 +1,8 @@
 import argparse
-import copy
 import os
 import subprocess
 import sys
-from git import Repo
 from datetime import datetime
-import time
 from pydriller import Repository
 import yaml
 
@@ -17,7 +14,7 @@ checkout_errors = 0
 cil_errors = 0
 ids_errors = []
 
-def generate_git(goblint_path, temp_dir, meta_path, git_info_sh_path, start_window_ms, end_window_ms):
+def generate_git(goblint_path, temp_dir, meta_path, git_info_sh_path, start_commit, end_commit):
     goblint_path = os.path.expanduser(os.path.abspath(goblint_path))
     temp_dir = os.path.expanduser(os.path.abspath(temp_dir))
     temp_repo_dir = os.path.join(temp_dir, 'repo')
@@ -26,61 +23,70 @@ def generate_git(goblint_path, temp_dir, meta_path, git_info_sh_path, start_wind
 
     print(SEPERATOR)
     print('[GIT] Cloning into temp/repo')
-    _clone_repo(args.git_info_sh_path, temp_repo_dir)
-    build_path = _get_build_path(args.git_info_sh_path, temp_repo_dir)
+    _clone_repo(git_info_sh_path, temp_repo_dir)
+    build_path = _get_build_path(git_info_sh_path, temp_repo_dir)
 
-    repo = Repository(build_path)
-    all_commits = list(repo.traverse_commits())
-    if not all_commits:
-        print(f"{COLOR_RED}No commits found in the repository{COLOR_RESET}")
-        sys.exit(-1)
-    earliest_commit = all_commits[0]
-    latest_commit = all_commits[-1]
-    if start_window_ms <= 0:
-        start = datetime.fromtimestamp(earliest_commit.committer_date.timestamp())
-    else:
-        start = datetime.fromtimestamp(start_window_ms / 1000.0)
-    if end_window_ms <= 0:
-        end = datetime.fromtimestamp(latest_commit.committer_date.timestamp())
-    else:
-        end = datetime.fromtimestamp(end_window_ms / 1000.0)
     if not os.path.exists(temp_repo_dir):
         os.mkdir(temp_repo_dir)
+
+    def get_commit_traverser():
+        all_commits = list(Repository(build_path).traverse_commits())
+        if start_commit == None:
+            start_index = 0
+        else:
+            start_index = next((i for i, commit in enumerate(all_commits) if commit.hash == start_commit), None)
+        if end_commit == None:
+            end_index = len(all_commits) - 1
+        else:
+            end_index = next((i for i, commit in enumerate(all_commits) if commit.hash == end_commit), None)
+
+        if start_index is None or end_index is None:
+            raise ValueError("One or both commit hashes not found in the repository")
+
+        return all_commits[start_index:end_index + 1]
+
 
     with open(meta_path, 'r') as file:
         yaml_data = yaml.safe_load(file)
         index: int = yaml_data[META_N]
 
-    def get_commit_traverser():
-        return Repository(build_path, since=start, to=end).traverse_commits()
-
     num_of_commits = sum(1 for _ in get_commit_traverser())
     print(f'[GIT] Start traversing {num_of_commits} commits. Including checkout, build and cil generation. This may take a while...')
+    if num_of_commits <= 2:
+        print(f'{COLOR_RED}You must traverse at least two commits to generate a test!')
+        sys.exit(-1)
     print(SEPERATOR)
     t = 0
+    #last_failed = False
     for commit in get_commit_traverser():
         t += 1
         if commit.merge:
             print(f"[GIT][{t}/{num_of_commits}] {COLOR_YELLOW}Skipping merge commit {commit.hash}{COLOR_RESET}, continuing with the next commit...")
+            last_failed = True
             continue
         #TODO Skip small commits and summarize them in one big commit
         try:
             _checkout(build_path, meta_path, commit.hash)
-            _build_repo(args.git_info_sh_path, temp_repo_dir, meta_path, commit.hash)
+            _build_repo(git_info_sh_path, temp_repo_dir, meta_path, commit.hash)
             new_path = os.path.join(temp_dir, f"p_{index}.c")
             _create_cil_file(goblint_path, build_path, new_path, meta_path, commit.hash)
             
-            if t % 10 == 0:
-                print(f"[GIT][{t}/{num_of_commits}] Written cil file with index {index}, continuing with the next commit...")
+            #if t % 10 == 0 or last_failed:
+                #last_failed = False
+                #print(f"{COLOR_GREEN}[GIT][{t}/{num_of_commits}] Written cil file with index {index}{COLOR_RESET}, continuing with the next commits...")
+            print(f"{COLOR_GREEN}[{t}/{num_of_commits}] Written cil file with index [{index}] for commit {commit.hash}{COLOR_RESET}, continuing with the next commits...")
             _write_meta_data(meta_path, commit.hash, index)
             index += 1
         except Exception as e:
-            print(f"{COLOR_RED}[GIT][{t}/{num_of_commits}][FAIL] Analysis for commit {commit.hash} failed ({e}){COLOR_RESET}, continuing with the next commit...")
-    print(f"{COLOR_GREEN}[GIT][FINISHED] Finished creating cil files for the commits.{COLOR_RESET}")
+            #last_failed = True
+            print(f"{COLOR_RED}[{t}/{num_of_commits}][FAIL] Generating cil for commit {commit.hash} failed ({e}){COLOR_RESET}, continuing with the next commit...")
+    print(SEPERATOR)
+    print(f"{COLOR_GREEN}[FINISHED] Finished creating cil files for the commits.{COLOR_RESET}")
     if build_errors > 0 or checkout_errors > 0 or cil_errors > 0:
         print(f"{COLOR_RED}There were the following errors: {build_errors} build errors, {checkout_errors} checkout errors and {cil_errors} cil errors.{COLOR_RESET}")
+        print(f"{COLOR_RED}You can read the error messages in the {meta_path} file")
         print(f"{COLOR_RED}The following commit ids resulted in errors:{COLOR_RESET} {', '.join(ids_errors)}")
-    return index
+    return index - 1
         
 
 def _write_meta_data(meta_path, commit_hash, index):
@@ -105,7 +111,7 @@ def _write_meta_data_failure(meta_path, commit_hash, stdout_msg, stderr_msg):
         yaml.safe_dump(yaml_data, file)
 
 def _clone_repo(git_info_sh_path, temp_repo_path):
-    command = ["./generate_git_build.sh", git_info_sh_path, temp_repo_path, "--clone"]
+    command = ["generators/generate_git_build.sh", git_info_sh_path, temp_repo_path, "--clone"]
     result = subprocess.run(command, text=True, capture_output=True)
     if result.returncode != 0:
          print(result.stdout)
@@ -114,7 +120,7 @@ def _clone_repo(git_info_sh_path, temp_repo_path):
          sys.exit(-1)
 
 def _get_build_path(git_info_sh_path, temp_repo_path):
-    command = ["./generate_git_build.sh", git_info_sh_path, temp_repo_path, "--path"]
+    command = ["generators/generate_git_build.sh", git_info_sh_path, temp_repo_path, "--path"]
     result = subprocess.run(command, text=True, capture_output=True)
     if result.returncode != 0:
         print(result.stdout)
@@ -126,7 +132,7 @@ def _get_build_path(git_info_sh_path, temp_repo_path):
 
 def _build_repo(git_info_sh_path, temp_repo_path, meta_path, commit_hash):
     global build_errors
-    command = ["./generate_git_build.sh", git_info_sh_path, temp_repo_path, "--build"]
+    command = ["generators/generate_git_build.sh", git_info_sh_path, temp_repo_path, "--build"]
     result = subprocess.run(command, text=True, capture_output=True)
     if result.returncode != 0:
         build_errors += 1
