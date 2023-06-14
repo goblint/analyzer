44d560e3b015522f3996504e6cd0b4f09eba7dc9
Julian Erhard <julian.erhard@tum.de>
2023-06-14 12:39:57 +0200

    ThreadEscape.threadenter: set local state (i.e., variables secaped in thread) to empty set.
    Previous solution that had a non-empty state was required only due to RelationAnalysis that did not pass reachable variables to the created thread in threadenter.

--- src/analyses/threadEscape.ml
+++ src/analyses/threadEscape.ml
@@ -140,13 +140,7 @@ struct
   let exitstate  v = D.bot ()
 
   let threadenter ctx lval f args =
-    match args with
-    | [ptc_arg] ->
-      let escaped = reachable (Analyses.ask_of_ctx ctx) ptc_arg in
-      let escaped = D.filter (fun v -> not v.vglob) escaped in
-      emit_escape_event ctx escaped;
-      [D.join ctx.local escaped]
-    | _ -> [ctx.local]
+    [D.bot ()]
 
   let threadspawn ctx lval f args fctx =
     D.join ctx.local @@
