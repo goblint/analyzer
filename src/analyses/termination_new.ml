6ee3312220b2838ea34033be3fc81ca995232be6
Alexander Schlenga <alexander.schlenga@tum.de>
2023-06-14 13:17:44 +0200

    Make things nice, update comments

--- src/analyses/termination_new.ml
+++ src/analyses/termination_new.ml
@@ -7,7 +7,9 @@ open TerminationPreprocessing
 exception PreProcessing of string
 
 let loopCounters : varinfo list ref = ref []
-let upjumpingGotos : location list ref = ref [] (*contains the locations of the upjumping gotos*)
+
+(* Contains the locations of the upjumping gotos *)
+let upjumpingGotos : location list ref = ref []
 
 let loopExit : varinfo ref = ref (makeVarinfo false "-error" Cil.intType)
 
@@ -17,8 +19,9 @@ let is_loop_counter_var (x : varinfo) =
 let is_loop_exit_indicator (x : varinfo) =
   x = !loopExit
 
-(* checks if at the current location (=loc) of the analysis an upjumping goto was already reached
-   true: no upjumping goto was reached till now*)
+(** Checks whether at the current location (=loc) of the analysis an
+ * upjumping goto was already reached. Returns true if no upjumping goto was
+ * reached until now *)
 let currrently_no_upjumping_gotos (loc : location) =
   List.for_all (function (l) -> (l >= loc)) upjumpingGotos.contents
 
@@ -49,33 +52,33 @@ struct
   include Analyses.IdentitySpec
 
   let assign ctx (lval : lval) (rval : exp) =
-    (* Detect loop counter variable assignment to 0 *)
+    (* Detect assignment to loop counter variable *)
     match lval, rval with
-    (* Assume that the following loop does not terminate *)
       (Var x, NoOffset), _ when is_loop_counter_var x ->
+      (* Assume that the following loop does not terminate *)
       if not (no_upjumping_gotos ()) then printf "\n4 problem\n";
       D.add x false ctx.local
-    (* Loop exit: Check whether loop counter variable is bounded *)
     | (Var y, NoOffset), Lval (Var x, NoOffset) when is_loop_exit_indicator y ->
+      (* Loop exit: Check whether loop counter variable is bounded *)
       let is_bounded = check_bounded ctx x in
       if not (no_upjumping_gotos ()) then printf "\n5 problem\n";
       D.add x is_bounded ctx.local
     | _ -> ctx.local
 
-  (* provides information to Goblint*)
+  (** Provides information to Goblint *)
   let query ctx (type a) (q: a Queries.t): a Queries.result =
     let open Queries in
     match q with
     | Queries.MustTermLoop v when check_bounded ctx v ->
-      true (* TODO should we use the checl_bound function?*)
+      true (* TODO should we use the check_bounded function? *)
     | Queries.MustTermProg ->
-      true (*TODO check if all values in the domain are true -> true*)
+      true (*TODO check if all values in the domain are true -> true *)
     | _ -> Result.top q
 
 end
 
 let () =
-  (** Register the preprocessing *)
+  (* Register the preprocessing *)
   Cilfacade.register_preprocess_cil (Spec.name ()) (new loopCounterVisitor loopCounters upjumpingGotos loopExit);
-  (** Register this analysis within the master control program *)
+  (* Register this analysis within the master control program *)
   MCP.register_analysis (module Spec : MCPSpec)
