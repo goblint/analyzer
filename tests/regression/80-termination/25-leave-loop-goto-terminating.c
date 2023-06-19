9bba9a1061b6ec1504b247cf26d866d91f5dfc8f
Thomas Lagemann <thomas@lagets.de>
2023-06-19 14:24:56 +0200

    Param unified

--- tests/regression/80-termination/25-leave-loop-goto-terminating.c
+++ tests/regression/80-termination/25-leave-loop-goto-terminating.c
@@ -1,3 +1,4 @@
+// PARAM: --set "ana.activated[+]" termination --enable warn.debug --set ana.activated[+] apron --enable ana.int.interval --set ana.apron.domain polyhedra
 #include <stdio.h>
 
 int main() {
