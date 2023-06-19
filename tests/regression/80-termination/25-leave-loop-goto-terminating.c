8a47aa678be4ce6d66c0aa159b0878b5b6b77fef
Thomas Lagemann <thomas@lagets.de>
2023-06-19 13:34:23 +0200

    Goto test cases

---
+++ tests/regression/80-termination/25-leave-loop-goto-terminating.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int counter = 0;
+
+    while (1) {
+        counter++;
+
+        // Dummy code
+        printf("Iteration %d\n", counter);
+        int result = counter * 2;
+        printf("Result: %d\n", result);
+
+        // Condition to terminate the loop
+        if (result >= 10) {
+            goto end;
+        }
+    }
+
+end:
+    printf("Loop exited. Result is greater than or equal to 10.\n");
+
+    return 0;
+}
---
+++ tests/regression/80-termination/27-upjumping-goto-nonterminating.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    goto mark2;
+
+mark1:
+    printf("This is mark1\n");
+    goto mark3;
+
+mark2:
+    printf("This is mark2\n");
+    goto mark1;
+
+mark3:
+    printf("This is mark3\n");
+    goto mark1;
+
+    return 0;
+}
