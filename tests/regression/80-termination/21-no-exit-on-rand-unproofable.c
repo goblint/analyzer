6f3921876d5408062d0289a33f7343431044a7fe
Thomas Lagemann <thomas@lagets.de>
2023-06-21 15:09:41 +0200

    Adapted TODO comments to current state

--- tests/regression/80-termination/21-no-exit-on-rand-unproofable.c
+++ tests/regression/80-termination/21-no-exit-on-rand-unproofable.c
@@ -1,4 +1,4 @@
-// TODO NON_LOCAL_TERM PARAM: --set "ana.activated[+]" termination --enable warn.debug --set ana.activated[+] apron --enable ana.int.interval --set ana.apron.domain polyhedra
+// NON_LOCAL_TERM PARAM: --set "ana.activated[+]" termination --enable warn.debug --set ana.activated[+] apron --enable ana.int.interval --set ana.apron.domain polyhedra
 #include <stdlib.h>
 
 int main()
