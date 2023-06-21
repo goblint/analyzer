4a402ca5373f2fe6eeb8e5dcc357368fd4f7b118
Johanna Schinabeck <johanna@Johanna>
2023-06-21 14:10:44 +0200

    reverted test case

--- tests/regression/55-loop-unrolling/01-simple-cases.c
+++ tests/regression/55-loop-unrolling/01-simple-cases.c
@@ -3,27 +3,6 @@
 
 int global;
 
-int f(int x);
-int g(int x);
-
-int f(int x)
-{
-if (x <= 0) return 0;
-else return g(x) + g(x + 1);
-}
-
-int g(int x)
-{
-if (x <= 0) return 0;
-else return f(x - 1) + f(x - 2);
-}
-
-int main() {
-int x = __VERIFIER_nondet_int();
-g(x);
-}
-
-/*
 int main(void)
 {
     example1();
@@ -216,4 +195,3 @@ void example10(void)
 	}
 	return 0;
 }
-*/
