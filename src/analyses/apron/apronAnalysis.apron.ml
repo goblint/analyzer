5b0d72ff32fd06809751696b5713a5a5aab897dc
Thomas Lagemann <thomas@lagets.de>
2023-06-22 18:16:03 +0200

    Indentation with ocp-indent

--- src/analyses/apron/apronAnalysis.apron.ml
+++ src/analyses/apron/apronAnalysis.apron.ml
@@ -36,7 +36,7 @@ let after_config () =
   let module Spec = (val get_spec ()) in
   MCP.register_analysis (module Spec : MCPSpec);
   GobConfig.set_string "ana.path_sens[+]" (Spec.name ())
-  
+
 
 let _ =
   AfterConfig.register after_config
