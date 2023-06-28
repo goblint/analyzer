b0c207f141bc5336754ee1a479a7d7a2520cd7b3
Simmo Saan <simmo.saan@gmail.com>
2023-06-28 12:39:42 +0300

    Use lattice deriver for PthreadDomain.D

--- src/cdomains/pthreadDomain.ml
+++ src/cdomains/pthreadDomain.ml
@@ -25,8 +25,7 @@ end
 module D = struct
   include Printable.StdLeaf
 
-  type domain = { tid : Tid.t; pred : Pred.t; ctx : Ctx.t } [@@deriving to_yojson]
-  type t = domain
+  type t = { tid : Tid.t; pred : Pred.t; ctx : Ctx.t } [@@deriving to_yojson, lattice]
 
   (** printing *)
   let show x =
@@ -36,7 +35,7 @@ module D = struct
       (Pred.show x.pred)
       (Ctx.show x.ctx)
 
-  include Printable.SimpleShow(struct type  t = domain let show = show end)
+  include Printable.SimpleShow(struct type nonrec t = t let show = show end)
 
   let name () = "pthread state"
 
@@ -59,21 +58,10 @@ module D = struct
   (** let hash = Hashtbl.hash *)
   let hash x = Hashtbl.hash (Tid.hash x.tid, Pred.hash x.pred, Ctx.hash x.ctx)
   let make tid pred ctx = { tid; pred; ctx }
-  let bot () = { tid = Tid.bot (); pred = Pred.bot (); ctx = Ctx.bot () }
-  let is_bot x = Tid.is_bot x.tid && Pred.is_bot x.pred && Ctx.is_bot x.ctx
   let any_is_bot x = Tid.is_bot x.tid || Pred.is_bot x.pred
-  let top () = { tid = Tid.top (); pred = Pred.top (); ctx = Ctx.top () }
-  let is_top x = Tid.is_top x.tid && Pred.is_top x.pred && Ctx.is_top x.ctx
-
-  let leq x y = Tid.leq x.tid y.tid && Pred.leq x.pred y.pred && Ctx.leq x.ctx y.ctx
 
   let op_scheme op1 op2 op3 x y : t =
     { tid = op1 x.tid y.tid; pred = op2 x.pred y.pred; ctx = op3 x.ctx y.ctx }
-
-  let join = op_scheme Tid.join Pred.join Ctx.join
-  let widen = join
-  let meet = op_scheme Tid.meet Pred.meet Ctx.meet
-  let narrow = meet
 
   let pretty_diff () (x,y) =
     if not (Tid.leq x.tid y.tid) then
