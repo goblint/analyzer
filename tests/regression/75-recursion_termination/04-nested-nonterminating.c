7f484cb2abf7f438313e7730951dfa1a73ebd928
Thomas Lagemann <thomas@lagets.de>
2023-06-29 13:43:41 +0200

    Skip crashing tests

--- tests/regression/75-recursion_termination/04-nested-nonterminating.c
+++ tests/regression/75-recursion_termination/04-nested-nonterminating.c
@@ -1,4 +1,4 @@
-// NONTERM PARAM: --set "ana.activated[+]" termination --enable warn.debug --set ana.activated[+] apron --enable ana.int.interval --set ana.apron.domain polyhedra
+// SKIP NONTERM PARAM: --set "ana.activated[+]" termination --enable warn.debug --set ana.activated[+] apron --enable ana.int.interval --set ana.apron.domain polyhedra
 #include <stdio.h>
 
 void innerRecursiveFunction() {
