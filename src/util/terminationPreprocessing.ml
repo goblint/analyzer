e6ba5597dd0138b773ac21621d445b6eeca7e372
Isidor Zweckstetter <isidor.zweckstetter@tum.de>
2023-06-29 12:59:49 +0200

    removed temporary comment

--- src/util/terminationPreprocessing.ml
+++ src/util/terminationPreprocessing.ml
@@ -37,8 +37,6 @@ class loopCounterVisitor lc lg le (fd : fundec) = object(self)
          (match b.bstmts with
             | ss ->
                b.bstmts <- inc_stmt :: check_stmt :: ss; (*cont :: cond :: inc_stmt :: ss = it is also possible, but for loops with cond at the end, inc is also at the end*)
-            (*| cont :: ss ->
-               b.bstmts <- cont :: inc_stmt :: check_stmt :: ss;*)
             | _ -> ());
          lc := VarToStmt.add (v: varinfo) (s: stmt) !lc;
          let nb = mkBlock [init_stmt; mkStmt s.skind] in
