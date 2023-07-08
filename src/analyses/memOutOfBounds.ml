4d5e0dcabe77afa11102f07c50971b12f18b1037
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-07-08 16:26:07 +0200

    Rewrite the majority of the logic and checks performed in memOutOfBounds

--- src/analyses/memOutOfBounds.ml
+++ src/analyses/memOutOfBounds.ml
@@ -15,15 +15,12 @@ struct
 
   (* HELPER FUNCTIONS *)
 
-  let exp_points_to_heap ctx (exp:exp) =
-    match ctx.ask (Queries.MayPointTo exp) with
-    | a when not (Queries.LS.is_top a) && not (Queries.LS.mem (dummyFunDec.svar, `NoOffset) a) ->
-      Queries.LS.elements a
-      |> List.map fst
-      |> List.exists (fun x -> ctx.ask (Queries.IsHeapVar x))
-    | _ -> false
 
-  let lval_points_to_heap ctx (lval:lval) = exp_points_to_heap ctx (mkAddrOf lval)
+  (* A safe way to call [cilint_to_int] without having to worry about exceptions *)
+  let cilint_to_int_wrapper i =
+    try
+      Some (cilint_to_int i)
+    with _ -> None
 
   let rec exp_contains_a_ptr (exp:exp) =
     match exp with
@@ -47,90 +44,131 @@ struct
     | StartOf lval -> lval_contains_a_ptr lval
 
   and lval_contains_a_ptr (lval:lval) =
-    let lval_is_of_ptr_type lval =
-      match typeOfLval lval with
-      | TPtr _ -> true
-      | _ -> false
-    in
     let (host, offset) = lval in
-    let host_contains_a_ptr h =
-      match h with
+    let host_contains_a_ptr = function
       | Var v -> isPointerType v.vtype
       | Mem e -> exp_contains_a_ptr e
     in
-    let rec offset_contains_a_ptr o =
-      match o with
+    let rec offset_contains_a_ptr = function
       | NoOffset -> false
       | Index (e, o) -> exp_contains_a_ptr e || offset_contains_a_ptr o
       | Field (f, o) -> isPointerType f.ftype || offset_contains_a_ptr o
     in
-    lval_is_of_ptr_type lval || host_contains_a_ptr host || offset_contains_a_ptr offset
+    host_contains_a_ptr host || offset_contains_a_ptr offset
 
-  let calc_lval_type_size (lval:lval) =
-    begin try
-        let t = typeOfLval lval in
-        match sizeOf t with
-        | Const (CInt(i, _, _)) -> Some (cilint_to_int i)
-        | _ -> None
-      with _ -> None
-    end
+  let lval_is_ptr_var (lval:lval) =
+    let (host, _) = lval in
+    match host with
+    | Var v -> isPointerType v.vtype
+    (* Intuition: If the lval has a Mem host, then it's not a direct ptr which is what we're looking for here *)
+    | Mem e -> false
 
-  let get_ptr_size_for_lval ctx (lval:lval) =
-    match lval_points_to_heap ctx lval with
-    | true -> (* We're dealing with a ptr that points to the heap *)
-      begin match ctx.ask (Queries.BlobSize (mkAddrOf lval)) with
-        | a when not (Queries.ID.is_top a) ->
-          begin match Queries.ID.to_int a with
-            | Some i -> Some (IntOps.BigIntOps.to_int i)
-            | None -> None
-          end
-        | _ -> None
+  let exp_points_to_heap ctx (exp:exp) =
+    match ctx.ask (Queries.MayPointTo exp) with
+    | a when not (Queries.LS.is_top a) && not (Queries.LS.mem (dummyFunDec.svar, `NoOffset) a) ->
+      Queries.LS.elements a
+      |> List.map fst
+      |> List.exists (fun x -> ctx.ask (Queries.IsHeapVar x))
+    | _ -> false (* TODO: Is this sound? Maybe not quite. *)
+
+  let get_size_for_heap_ptr ctx (exp:exp) =
+    (* TODO:
+        BlobSize always seems to respond with top when it's passed an Lval exp of a ptr var which in turn contains mem allocated via malloc.
+        Am I doing smth wrong here?
+    *)
+    match ctx.ask (Queries.BlobSize exp) with
+    | a when not (Queries.ID.is_top a) ->
+      begin match Queries.ID.to_int a with
+        | Some i -> Some (IntOps.BigIntOps.to_int i)
+        | None -> None
       end
-    (* Assumption here is that if it's not a heap ptr, then it's a stack ptr and the ptr size would be the lval type's size *)
-    | false -> calc_lval_type_size lval
+    | _ -> None
 
-  let rec get_offset_size ctx offset =
-    match offset with
-    | NoOffset -> Some 0
-    | Index (e, o) ->
-      begin match ctx.ask (Queries.EvalInt e) with
-        | a when not (Queries.ID.is_top a) ->
-          begin match Queries.ID.to_int a with
-            | Some i ->
-              begin match get_offset_size ctx o with
-                | Some os -> Some (IntOps.BigIntOps.to_int i + os)
-                | None -> None
-              end
-            | None -> None
-          end
-        | _ -> None
-      end
-    | Field (f, o) ->
-      let f_size =
-        begin match sizeOf f.ftype with
-          | Const (CInt (i, _, _)) -> Some (cilint_to_int i)
+  (* TODO: Here we assume that the given exp is a Lval exp *)
+  let get_size_for_stack_ptr ctx (exp:exp) =
+    match exp with
+    | Lval lval ->
+      if lval_is_ptr_var lval then
+        let (host, _) = lval in
+        begin match host with
+          | Var v ->
+            begin match sizeOf v.vtype with
+              | Const (CInt (i, _, _)) -> cilint_to_int_wrapper i
+              | _ -> None
+            end
           | _ -> None
         end
-      in
-      begin match f_size, get_offset_size ctx o with
-        | Some fs, Some os -> Some (fs + os)
+      else None
+    | _ -> None
+
+  let get_ptr_size_for_exp ctx (exp:exp) =
+    match exp_points_to_heap ctx exp with
+    (* We're dealing with a ptr that points to the heap *)
+    | true -> get_size_for_heap_ptr ctx exp
+    (* Assumption here is that if it doesn't point to the heap, then it points to the stack *)
+    | false -> get_size_for_stack_ptr ctx exp
+
+  (**
+    * If we get [None], then the offset's size/value is unknown
+    * In the case [NoOffset], [Some 0] indicates that this offset type simply has value 0
+  *)
+  let rec get_offset_size = function
+    | NoOffset -> Some 0
+    | Index (e, o) ->
+      let exp_val = begin match constFold true e with
+        | Const (CInt (i, _, _)) -> cilint_to_int_wrapper i
         | _ -> None
       end
-
-  let check_lval_for_oob_access ctx (lval:lval) =
-    match lval_contains_a_ptr lval with
-    | false -> ()
-    | true ->
-      let (_, offset) = lval in
-      begin match get_ptr_size_for_lval ctx lval, get_offset_size ctx offset with
-        | _, None -> M.warn "Offset unknown, potential out-of-bounds access for lval %a" CilType.Lval.pretty lval
-        | None, _ -> M.warn "Pointer size unknown, potential out-of-bounds access for lval %a" CilType.Lval.pretty lval
-        | Some pi, Some oi ->
-          if oi > pi then
-            M.warn "Must out-of-bounds memory access: Offset size (%d) is larger than pointer's memory size (%d) for lval %a" oi pi CilType.Lval.pretty lval
+      in
+      begin match exp_val, get_offset_size o with
+        | Some ei, Some oi -> Some (ei + oi)
+        | _, _ -> None
+      end
+    | Field (f, o) ->
+      begin match get_offset_size o, sizeOf f.ftype with
+        | Some oi, Const (CInt (i, _, _)) ->
+          begin match cilint_to_int_wrapper i with
+            | Some i -> Some (oi + i)
+            | None -> None
+          end
+        | _, _ -> None
       end
 
-  let rec check_exp_for_oob_access ctx (exp:exp) =
+  let rec check_lval_for_oob_access ctx (lval:lval) =
+    match lval_contains_a_ptr lval with
+    | false -> () (* Nothing to do here *)
+    | true ->
+      let (host, offset) = lval in
+      match host, get_offset_size offset with
+      | _, None -> M.warn "Offset size for lval %a not known. May have a memory out-of-bounds access" CilType.Lval.pretty lval
+      | Var v, Some oi ->
+        begin match sizeOf v.vtype with
+          | Const (CInt (i, _, _)) ->
+            begin match cilint_to_int_wrapper i with
+              | Some i ->
+                if  i < oi then
+                  M.warn "Offset bigger than var type's size for lval %a. A memory out-of-bounds access must occur" CilType.Lval.pretty lval
+              | _ -> M.warn "Unknown size of var %a for lval %a. A memory out-of-bounds access might occur" CilType.Varinfo.pretty v CilType.Lval.pretty lval
+            end
+          | _ -> M.warn "Unknown size of var %a for lval %a. A memory out-of-bounds access might occur" CilType.Varinfo.pretty v CilType.Lval.pretty lval
+        end
+      | Mem e, Some oi ->
+        check_exp_for_oob_access ctx e;
+        (* TODO:
+          * Not sure if we actually need these checks below.
+          * They never seem to apply
+          * I.e., for ptrs, it always seems to be the case that we have a binop which adds the offset to the ptr
+            instead of having the offset represented as an offset of the lval
+          * For this reason, the checks below are currently commented out
+        *)
+        (* begin match get_ptr_size_for_exp ctx e with
+            | Some ei ->
+            if ei < oi then
+              M.warn "Offset bigger than size of pointer memory, denoted by expression %a in lval %a" d_exp e CilType.Lval.pretty lval
+            | _ -> M.warn "Unknown size of pointer memory, denoted by exp %a for lval %a" d_exp e CilType.Lval.pretty lval
+            end *)
+
+  and check_exp_for_oob_access ctx (exp:exp) =
     match exp with
     | Const _
     | SizeOf _
@@ -143,7 +181,8 @@ struct
     | AlignOfE e
     | UnOp (_, e, _)
     | CastE (_, e) -> check_exp_for_oob_access ctx e
-    | BinOp (_, e1, e2, _) ->
+    | BinOp (bop, e1, e2, _) ->
+      check_binop_exp ctx bop e1 e2;
       check_exp_for_oob_access ctx e1;
       check_exp_for_oob_access ctx e2
     | Question (e1, e2, e3, _) ->
@@ -154,10 +193,35 @@ struct
     | StartOf lval
     | AddrOf lval -> check_lval_for_oob_access ctx lval
 
+  and check_binop_exp ctx (binop:binop) (e1:exp) (e2:exp) =
+    match binop with
+    | PlusPI
+    | IndexPI
+    | MinusPI ->
+      let ptr_size = get_ptr_size_for_exp ctx e1 in
+      let offset_size = eval_ptr_offset_in_binop e2 in
+      begin match ptr_size, offset_size with
+        | Some pi, Some oi ->
+          if pi < oi then
+            M.warn "Pointer size in expression %a %a %a is smaller than offset for pointer arithmetic" d_exp e1 CilType.Binop.pretty binop d_exp e2
+        | None, _ -> M.warn "Pointer (%a) size in expression %a %a %a not known" d_exp e1 d_exp e1 CilType.Binop.pretty binop d_exp e2
+        | _, None -> M.warn "Operand value for pointer arithmetic in expression %a %a %a not known" d_exp e1 CilType.Binop.pretty binop d_exp e2
+      end
+    | _ -> ()
+
+  and eval_ptr_offset_in_binop (exp:exp) =
+    match constFold true exp with
+    | Const (CInt (i, _, _)) -> cilint_to_int_wrapper i
+    | _ -> None (* TODO: Maybe try to also Eval the exp via Queries and not rely only on constFold *)
+
 
   (* TRANSFER FUNCTIONS *)
 
   let assign ctx (lval:lval) (rval:exp) : D.t =
+    let (host,_) = lval in
+    match host with
+    | Mem e -> ignore (Pretty.printf "Lval host is Mem e and e is %a\n" d_plainexp e);
+    | _ -> ();
     check_lval_for_oob_access ctx lval;
     check_exp_for_oob_access ctx rval;
     ctx.local
