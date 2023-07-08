ee80fd1761a7a780e46c2e9af25389bd268af65e
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-07-08 11:30:04 +0200

    Handle case when points-to set is top

--- src/analyses/base.ml
+++ src/analyses/base.ml
@@ -1999,13 +1999,17 @@ struct
 
   let check_free_of_non_heap_mem ctx special_fn ptr =
     let points_to_set = ctx.ask (Queries.MayPointTo ptr) in
-    let exists_non_heap_var =
-      Queries.LS.elements points_to_set
-      |> List.map fst
-      |> List.exists (fun var -> not (ctx.ask (Queries.IsHeapVar var)))
-    in
-    if exists_non_heap_var then
-      M.warn ~category:(Behavior (Undefined InvalidMemoryDeallocation)) ~tags:[CWE 590] "Free of non-dynamically allocated memory in function %s for pointer %a" special_fn.vname d_exp ptr
+    begin try
+        let exists_non_heap_var =
+          (* elements throws Unsupported if the points-to set is top *)
+          Queries.LS.elements points_to_set
+          |> List.map fst
+          |> List.exists (fun var -> not (ctx.ask (Queries.IsHeapVar var)))
+        in
+        if exists_non_heap_var then
+          M.warn ~category:(Behavior (Undefined InvalidMemoryDeallocation)) ~tags:[CWE 590] "Free of non-dynamically allocated memory in function %s for pointer %a" special_fn.vname d_exp ptr
+      with _ -> M.warn ~category:(Behavior (Undefined InvalidMemoryDeallocation)) ~tags:[CWE 590] "Points-to set for pointer %a in function %s is top. Potential free of non-dynamically allocated memory may occur" d_exp ptr special_fn.vname
+    end
 
   let special ctx (lv:lval option) (f: varinfo) (args: exp list) =
     let invalidate_ret_lv st = match lv with
