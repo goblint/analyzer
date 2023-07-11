109046df5041ff7c92c56d075f39a4c8e2d40d29
Michael Schwarz <michael.schwarz93@gmail.com>
2023-07-11 21:38:07 +0200

    Fix BI cast, make def_exc cast more precise

--- tests/regression/03-practical/28-bool.c
+++ tests/regression/03-practical/28-bool.c
@@ -1,12 +1,20 @@
 #include <stdbool.h>
+#include <goblint.h>
 
 int main() {
   int a;
   int *p = &a;
 
-  bool x = p; // Unknown int([0,1])
+  bool x = p;
   __goblint_check(x);
-  bool y = !!p; // 1
-  bool z = p != 0; // 1
+  bool y = !!p;
+  __goblint_check(y);
+  bool z = p != 0;
+  __goblint_check(z);
+
+  int b = 10;
+  bool bb = b;
+  __goblint_check(bb);
+
   return 0;
 }
