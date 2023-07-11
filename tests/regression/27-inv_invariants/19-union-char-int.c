b45d527d1e4d6f66fe0e0e035376bfbd8c45d0cc
Julian Erhard <julian.erhard@tum.de>
2023-07-11 15:43:41 +0200

    Add tests where branching on unions yields two bottom branches / an IncompatibleIKinds-exception.

---
+++ tests/regression/27-inv_invariants/19-union-char-int.c
@@ -0,0 +1,29 @@
+// PARAM: --enable ana.float.interval
+#include <goblint.h>
+union u {
+	int x;
+	char c;
+};
+
+int main(){
+	union u a;
+	union u b;
+
+	a.x = 12;
+	b.c = 12;
+
+	int i = 0;
+	if(a.x == b.x){
+		i++;
+	}
+	// Should not be dead after if
+	__goblint_check(1);
+
+	if(a.c == b.c){
+		i++;
+	}
+	// Should not be dead after if
+	__goblint_check(1);
+
+	return 0;
+}
