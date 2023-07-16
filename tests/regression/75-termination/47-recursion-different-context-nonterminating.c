10260bde16acbb131cd2053eefe0099cf4ca03b2
Thomas Lagemann <thomas@lagets.de>
2023-07-16 11:59:32 +0200

    Added more recursive tests

---
+++ tests/regression/75-termination/47-recursion-different-context-nonterminating.c
@@ -0,0 +1,32 @@
+// NONTERM PARAM: --set "ana.activated[+]" termination --set ana.activated[+] apron --enable ana.int.interval --set ana.apron.domain polyhedra
+#include <stdio.h>
+
+void functionC(int n);
+
+void functionA(int n) {
+    if (n > 0) {
+        printf("Function A: %d\n", n);
+        functionC(n - 1);
+    }
+}
+
+void functionB(int n) {
+    if (n > 0) {
+        printf("Function B: %d\n", n);
+        functionC(n - 1);
+    }
+}
+
+void functionC(int n) {
+    if (n > 0) {
+        printf("Function C: %d\n", n);
+        functionC(n);
+    }
+}
+
+int main() {
+    int n = 5;
+    functionA(n + 1);
+    functionB(n + 7);
+    return 0;
+}
