782b21c4a65521e0104f789df853e4bf140669f1
Alexander Schlenga <alexander.schlenga@tum.de>
2023-07-25 21:31:18 +0200

    Fix indentation

--- src/util/terminationPreprocessing.ml
+++ src/util/terminationPreprocessing.ml
@@ -18,50 +18,50 @@ let show_location_id l =
   string_of_int l.line ^ "_" ^ string_of_int l.column ^ "-file" ^ "_" ^  extract_file_name l.file
 
 class loopCounterVisitor lc lg (fd : fundec) = object(self)
-   inherit nopCilVisitor
-   
-   method! vstmt s =
+  inherit nopCilVisitor
 
-      let specialFunction name =
-         { svar  = makeGlobalVar name (TFun(voidType, Some [("exp", intType, [])], false,[]));
-           smaxid = 0;
-           slocals = [];
-           sformals = [];
-           sbody = mkBlock [];
-           smaxstmtid = None;
-           sallstmts = [];
-         } in
+  method! vstmt s =
 
-      let min_int_exp = Const(CInt(Cilint.shift_left_cilint Cilint.mone_cilint ((bytesSizeOfInt IInt)*8-1), IInt, None)) in
-   
-      let f_bounded  = Lval (var (specialFunction "__goblint_bounded").svar) in
-      
-      let action s = match s.skind with
-         | Loop (b, loc, eloc, _, _) ->
-         let name = "term"^show_location_id loc in
-         let typ = Cil.intType in 
-         let v = (Cil.makeLocalVar fd name typ) in (*Not tested for incremental mode*)
-         let init_stmt = mkStmtOneInstr @@ Set (var v, min_int_exp, loc, eloc) in
-         let inc_stmt = mkStmtOneInstr @@ Set (var v, increm (Lval (var v)) 1, loc, eloc) in
-         let inc_stmt2 = mkStmtOneInstr @@ Set (var v, increm (Lval (var v)) 1, loc, eloc) in
-         let exit_stmt = mkStmtOneInstr @@ Call (None, f_bounded, [Lval (var v)], loc, locUnknown) in
-         (match b.bstmts with
-            | s :: ss ->   (*duplicate increment statement here to fix inconsistencies in nested loops*)
-               b.bstmts <- exit_stmt :: inc_stmt :: s :: inc_stmt2 :: ss;
-            | ss ->
-               b.bstmts <- exit_stmt :: inc_stmt :: ss;
-         );
-         lc := VarToStmt.add (v: varinfo) (s: stmt) !lc;
-         let nb = mkBlock [init_stmt; mkStmt s.skind] in
-         s.skind <- Block nb;
-         s
-         | Goto (sref, l) -> 
-            let goto_jmp_stmt = sref.contents.skind in 
-            let loc_stmt = get_stmtLoc goto_jmp_stmt in 
-            if CilType.Location.compare l loc_stmt >= 0 (*is pos if first loc is greater -> below the second loc*)
-               then 
-                  lg := List.append !lg ([l] : location list); (*problem: the program might not terminate!*)
-            s
-         | _ -> s
-      in ChangeDoChildrenPost (s, action);
-   end
+    let specialFunction name =
+      { svar  = makeGlobalVar name (TFun(voidType, Some [("exp", intType, [])], false,[]));
+        smaxid = 0;
+        slocals = [];
+        sformals = [];
+        sbody = mkBlock [];
+        smaxstmtid = None;
+        sallstmts = [];
+      } in
+
+    let min_int_exp = Const(CInt(Cilint.shift_left_cilint Cilint.mone_cilint ((bytesSizeOfInt IInt)*8-1), IInt, None)) in
+
+    let f_bounded  = Lval (var (specialFunction "__goblint_bounded").svar) in
+
+    let action s = match s.skind with
+      | Loop (b, loc, eloc, _, _) ->
+        let name = "term"^show_location_id loc in
+        let typ = Cil.intType in
+        let v = (Cil.makeLocalVar fd name typ) in (*Not tested for incremental mode*)
+        let init_stmt = mkStmtOneInstr @@ Set (var v, min_int_exp, loc, eloc) in
+        let inc_stmt = mkStmtOneInstr @@ Set (var v, increm (Lval (var v)) 1, loc, eloc) in
+        let inc_stmt2 = mkStmtOneInstr @@ Set (var v, increm (Lval (var v)) 1, loc, eloc) in
+        let exit_stmt = mkStmtOneInstr @@ Call (None, f_bounded, [Lval (var v)], loc, locUnknown) in
+        (match b.bstmts with
+         | s :: ss ->   (*duplicate increment statement here to fix inconsistencies in nested loops*)
+           b.bstmts <- exit_stmt :: inc_stmt :: s :: inc_stmt2 :: ss;
+         | ss ->
+           b.bstmts <- exit_stmt :: inc_stmt :: ss;
+        );
+        lc := VarToStmt.add (v: varinfo) (s: stmt) !lc;
+        let nb = mkBlock [init_stmt; mkStmt s.skind] in
+        s.skind <- Block nb;
+        s
+      | Goto (sref, l) ->
+        let goto_jmp_stmt = sref.contents.skind in
+        let loc_stmt = get_stmtLoc goto_jmp_stmt in
+        if CilType.Location.compare l loc_stmt >= 0 (*is pos if first loc is greater -> below the second loc*)
+        then
+          lg := List.append !lg ([l] : location list); (*problem: the program might not terminate!*)
+        s
+      | _ -> s
+    in ChangeDoChildrenPost (s, action);
+end
