3573182fa4e22ce8f448f6337b007f279c892e18
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-08-04 10:24:36 +0200

    Add two simple regression tests

---
+++ tests/regression/76-memleak/02-simple-mem-leak.c
@@ -0,0 +1,8 @@
+//PARAM: --set ana.malloc.unique_address_count 1 --set ana.activated[+] memLeak
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+    int *p = malloc(sizeof(int));
+    // No free => memory is leaked
+    return 0; //TODO: `make test` detects OTHER and not WARN here
+}
