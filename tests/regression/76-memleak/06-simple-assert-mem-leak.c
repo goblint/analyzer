79d13cd3e1f5a24026f3ee9ac45239c493f330d6
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-08-08 18:41:41 +0200

    Add further regression test cases

---
+++ tests/regression/76-memleak/06-simple-assert-mem-leak.c
@@ -0,0 +1,8 @@
+//PARAM: --set ana.malloc.unique_address_count 1 --set ana.activated[+] memLeak
+#include <stdlib.h>
+#include <assert.h>
+
+int main(int argc, char const *argv[]) {
+    int *p = malloc(sizeof(int));
+    assert(0); //WARN
+}
