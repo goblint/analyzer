8614b522db13b1586bbe0f6ad0d162d07695e13d
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-08-18 12:01:24 +0200

    Fix two test cases by taking addresses of struct fields

--- tests/regression/75-invalid_dealloc/07-free-at-struct-offset.c
+++ tests/regression/75-invalid_dealloc/07-free-at-struct-offset.c
@@ -8,8 +8,8 @@ typedef struct custom_t {
 int main(int argc, char const *argv[]) {
     custom_t *struct_ptr = malloc(sizeof(custom_t));
     struct_ptr->x = malloc(10 * sizeof(char));
-    free(struct_ptr->x); //NOWARN
-    free(struct_ptr->y); //WARN
+    free(&struct_ptr->x); //NOWARN
+    free(&struct_ptr->y); //WARN
     free(struct_ptr); //NOWARN
     return 0;
 }
--- tests/regression/75-invalid_dealloc/08-realloc-at-struct-offset.c
+++ tests/regression/75-invalid_dealloc/08-realloc-at-struct-offset.c
@@ -8,8 +8,8 @@ typedef struct custom_t {
 int main(int argc, char const *argv[]) {
     custom_t *struct_ptr = malloc(sizeof(custom_t));
     struct_ptr->x = malloc(10 * sizeof(char));
-    realloc(struct_ptr->x, 50); //NOWARN
-    realloc(struct_ptr->y, 50); //WARN
+    realloc(&struct_ptr->x, 50); //NOWARN
+    realloc(&struct_ptr->y, 50); //WARN
     realloc(struct_ptr, 2 * sizeof(custom_t)); //NOWARN
     return 0;
 }
