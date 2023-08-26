32714309d38154f3af572c8186879cc6eb7da176
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-08-26 12:30:01 +0200

    Adapt regression tests to not WARN for UAF if there's no deref

--- tests/regression/74-use_after_free/06-uaf-struct.c
+++ tests/regression/74-use_after_free/06-uaf-struct.c
@@ -17,13 +17,15 @@ int main(int argc, char **argv) {
     char line[128];
 
     while (1) {
-        printf("[ auth = %p, service = %p ]\n", auth, service); //WARN
+        // No deref happening here => nothing to warn about
+        printf("[ auth = %p, service = %p ]\n", auth, service); //NOWARN
 
         if (fgets(line, sizeof(line), stdin) == NULL) break;
         
         if (strncmp(line, "auth ", 5) == 0) {
-            auth = malloc(sizeof(auth)); //WARN
-            memset(auth, 0, sizeof(auth)); //WARN
+            // No deref happening in either of the 2 lines below => no need to warn
+            auth = malloc(sizeof(auth)); //NOWARN
+            memset(auth, 0, sizeof(auth)); //NOWARN
             if (strlen(line + 5) < 31) {
                 strcpy(auth->name, line + 5); //WARN
             }
