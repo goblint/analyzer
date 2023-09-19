goblint/analyzer

commit f7b38a0ca666e433f79e2057de8cd4eb59a52a7b

Add ReachableFrom query with address domain

src/analyses/base.ml
@@ -1317,6 +1317,17 @@ struct
             addrs
         | _ -> Q.LS.empty ()
       end
+    | Q.ReachableFromA e -> begin
+        match eval_rv_address (Analyses.ask_of_ctx ctx) ctx.global ctx.local e with
+        | Top -> Queries.Result.top q
+        | Bot -> Queries.Result.bot q (* TODO: remove *)
+        | Address a ->
+          let a' = AD.remove Addr.UnknownPtr a in (* run reachable_vars without unknown just to be safe: TODO why? *)
+          let xs = reachable_vars (Analyses.ask_of_ctx ctx) [a'] ctx.global ctx.local in
+          let addrs = List.fold_left (Q.AD.join) (Q.AD.empty ()) xs in
+          addrs
+        | _ -> Q.AD.empty ()
+      end
     | Q.ReachableUkTypes e -> begin
         match eval_rv_address (Analyses.ask_of_ctx ctx) ctx.global ctx.local e with
         | Top -> Queries.Result.top q

src/domains/queries.ml
@@ -75,6 +75,7 @@ type _ t =
   | MayPointTo: exp -> LS.t t
   | MayPointToA: exp -> AD.t t
   | ReachableFrom: exp -> LS.t t
+  | ReachableFromA: exp -> AD.t t
   | ReachableUkTypes: exp -> TS.t t
   | Regions: exp -> LS.t t
   | MayEscape: varinfo -> MayBool.t t
@@ -145,6 +146,7 @@ struct
     | MayPointTo _ -> (module LS)
     | MayPointToA _ -> (module AD)
     | ReachableFrom _ -> (module LS)
+    | ReachableFromA _ -> (module AD)
     | Regions _ -> (module LS)
     | MustLockset -> (module LS)
     | EvalFunvar _ -> (module LS)
@@ -210,6 +212,7 @@ struct
     | MayPointTo _ -> LS.top ()
     | MayPointToA _ -> AD.top ()
     | ReachableFrom _ -> LS.top ()
+    | ReachableFromA _ -> AD.top ()
     | Regions _ -> LS.top ()
     | MustLockset -> LS.top ()
     | EvalFunvar _ -> LS.top ()
@@ -271,6 +274,7 @@ struct
     | Any (MayPointTo _) -> 1
     | Any (MayPointToA _) -> 999
     | Any (ReachableFrom _) -> 2
+    | Any (ReachableFromA _) -> 666
     | Any (ReachableUkTypes _) -> 3
     | Any (Regions _) -> 4
     | Any (MayEscape _) -> 5
@@ -328,6 +332,7 @@ struct
       | Any (MayPointTo e1), Any (MayPointTo e2) -> CilType.Exp.compare e1 e2
       | Any (MayPointToA e1), Any (MayPointToA e2) -> CilType.Exp.compare e1 e2
       | Any (ReachableFrom e1), Any (ReachableFrom e2) -> CilType.Exp.compare e1 e2
+      | Any (ReachableFromA e1), Any (ReachableFromA e2) -> CilType.Exp.compare e1 e2
       | Any (ReachableUkTypes e1), Any (ReachableUkTypes e2) -> CilType.Exp.compare e1 e2
       | Any (Regions e1), Any (Regions e2) -> CilType.Exp.compare e1 e2
       | Any (MayEscape vi1), Any (MayEscape vi2) -> CilType.Varinfo.compare vi1 vi2
@@ -374,6 +379,7 @@ struct
     | Any (MayPointTo e) -> CilType.Exp.hash e
     | Any (MayPointToA e) -> CilType.Exp.hash e
     | Any (ReachableFrom e) -> CilType.Exp.hash e
+    | Any (ReachableFromA e) -> CilType.Exp.hash e
     | Any (ReachableUkTypes e) -> CilType.Exp.hash e
     | Any (Regions e) -> CilType.Exp.hash e
     | Any (MayEscape vi) -> CilType.Varinfo.hash vi
@@ -417,6 +423,7 @@ struct
     | Any (MayPointTo e) -> Pretty.dprintf "MayPointTo %a" CilType.Exp.pretty e
     | Any (MayPointToA e) -> Pretty.dprintf "MayPointToA %a" CilType.Exp.pretty e
     | Any (ReachableFrom e) -> Pretty.dprintf "ReachableFrom %a" CilType.Exp.pretty e
+    | Any (ReachableFromA e) -> Pretty.dprintf "ReachableFromA %a" CilType.Exp.pretty e
     | Any (ReachableUkTypes e) -> Pretty.dprintf "ReachableUkTypes %a" CilType.Exp.pretty e
     | Any (Regions e) -> Pretty.dprintf "Regions %a" CilType.Exp.pretty e
     | Any (MayEscape vi) -> Pretty.dprintf "MayEscape %a" CilType.Varinfo.pretty vi

commit 83978e953dbbf7d6a8c920d071b380bbd1441637

Fix ReachableFrom not adding back unknown pointer
Regression from f7b38a0.

src/analyses/base.ml
@@ -1287,7 +1287,11 @@ struct
         | Address a ->
           let a' = AD.remove Addr.UnknownPtr a in (* run reachable_vars without unknown just to be safe: TODO why? *)
           let addrs = reachable_vars (Analyses.ask_of_ctx ctx) [a'] ctx.global ctx.local in
-          List.fold_left (AD.join) (AD.empty ()) addrs
+          let addrs' = List.fold_left (AD.join) (AD.empty ()) addrs in
+          if AD.may_be_unknown a then
+            AD.add UnknownPtr addrs' (* add unknown back *)
+          else
+            addrs'
         | _ -> AD.empty ()
       end
     | Q.ReachableUkTypes e -> begin