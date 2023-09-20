18f1049cb892e8f0265726bc7227c2949c87fd55
Simmo Saan <simmo.saan@gmail.com>
2023-09-20 13:49:31 +0300

    Extract symbolic-thread-struct example from C-Thread-Pool

---
+++ tests/regression/03-practical/39-symbolic-thread-struct.c
@@ -0,0 +1,39 @@
+// Per-thread structs passed via argument.
+// Extracted from concrat/C-Thread-Pool.
+#include <stdlib.h>
+#include <pthread.h>
+#include <goblint.h>
+extern int __VERIFIER_nondet_int();
+
+struct thread {
+  int data;
+};
+
+void *thread(void *arg) {
+  struct thread *t = arg;
+  t->data = __VERIFIER_nondet_int(); // NORACE
+  free(t);
+  return NULL;
+}
+
+int main() {
+  int threads_total = __VERIFIER_nondet_int();
+  __goblint_assume(threads_total >= 0);
+
+  pthread_t *tids = malloc(threads_total * sizeof(pthread_t));
+
+  // create threads
+  for (int i = 0; i < threads_total; i++) {
+    struct thread *t = malloc(sizeof(struct thread));
+    pthread_create(&tids[i], NULL, &thread, t); // may fail but doesn't matter
+  }
+
+  // join threads
+  for (int i = 0; i < threads_total; i++) {
+    pthread_join(tids[i], NULL);
+  }
+
+  free(tids);
+
+  return 0;
+}
