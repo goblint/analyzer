48d0e5dec19cddd3a0e78febc562b26126ad8446
Michael Schwarz <michael.schwarz93@gmail.com>
2023-09-24 12:05:44 +0200

    Make also fail in the CI

--- tests/regression/73-strings/09-malloc.c
+++ tests/regression/73-strings/09-malloc.c
@@ -1,5 +1,4 @@
 // PARAM: --disable ana.base.limit-string-addresses --enable ana.int.interval --enable ana.base.arrays.nullbytes
-
 #include <goblint.h>
 #include <string.h>
 #include <stdlib.h>
@@ -11,6 +10,7 @@ int main () {
     char s2[50];
     s2[0] = 'a';
 
-    int len1 = strlen(s1); //WARN
-    int len2 = strlen(s2); //WARN
+    // Use size_t to avoid integer warnings hiding the lack of string warnings
+    size_t len1 = strlen(s1); //WARN
+    size_t len2 = strlen(s2); //WARN
 }
