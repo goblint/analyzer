621153fb3dd43fbe1436d18d492f8c097263dacd
Simmo Saan <simmo.saan@gmail.com>
2023-09-26 18:02:47 +0300

    Add test for free-free not racing

--- tests/regression/04-mutex/64-free_direct_rc.c
+++ tests/regression/04-mutex/64-free_direct_rc.c
@@ -8,11 +8,23 @@ void *t_fun(void *arg) {
   return NULL;
 }
 
+void *t_fun2(void *arg) {
+  int *p = (int *) arg;
+  free(p); // NORACE
+  return NULL;
+}
+
 int main(void) {
   pthread_t id;
-  int *p = malloc(sizeof(int));
+  int *p;
+  p = malloc(sizeof(int));
   pthread_create(&id, NULL, t_fun, (void *) p);
   free(p); // RACE!
+  pthread_join (id, NULL);
+
+  p = malloc(sizeof(int));
+  pthread_create(&id, NULL, t_fun2, (void *) p);
+  free(p); // NORACE
   pthread_join (id, NULL);
   return 0;
 }
