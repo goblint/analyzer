1211a9fe95fb881e8d53a7ab474498c31d675b7e
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-10-04 12:25:10 +0200

    Use rand() in 77/10

--- tests/regression/77-mem-oob/10-oob-two-loops.c
+++ tests/regression/77-mem-oob/10-oob-two-loops.c
@@ -1,15 +1,17 @@
 // PARAM: --set ana.activated[+] memOutOfBounds --enable ana.int.interval --disable warn.info --set sem.int.signed_overflow assume_none
+#include <stdlib.h>
+
 int main() {
 	int *p = malloc(1048 * sizeof(int));
 
     for (int i = 0; i < 1048; ++i) {
-		p[i] = __VERIFIER_nondet_int(); //NOWARN
+		p[i] = rand(); //NOWARN
 	}
 
 	int *q = p;
 
 	while (*q >= 0 && q < p + 1048 * sizeof(int)) { //WARN
-		if (__VERIFIER_nondet_int()) {
+		if (rand()) {
 			q++;
 		} else {
 			(*q)--; //WARN
