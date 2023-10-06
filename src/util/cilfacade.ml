91aeee732f1f97afdb406e189176f22c46049ff1
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-10-06 23:57:46 +0300

    Set `Cabs2cil.addNestedScopeAttr` based on the Goblint config option
    Co-authored-by: Michael Schwarz <michael.schwarz93@gmail.com>

--- src/util/cilfacade.ml
+++ src/util/cilfacade.ml
@@ -41,8 +41,7 @@ let init_options () =
   Mergecil.merge_inlines := get_bool "cil.merge.inlines";
   Cil.cstd := Cil.cstd_of_string (get_string "cil.cstd");
   Cil.gnu89inline := get_bool "cil.gnu89inline";
-  if get_bool "cil.addNestedScopeAttr" then
-    Cabs2cil.addNestedScopeAttr := true
+  Cabs2cil.addNestedScopeAttr := get_bool "cil.addNestedScopeAttr";
 
 let init () =
   initCIL ();
