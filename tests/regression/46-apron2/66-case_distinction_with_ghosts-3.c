4e7312b45d3b0956db71756a5d5e45d5bb22ccfd
Simmo Saan <simmo.saan@gmail.com>
2023-10-13 17:40:47 +0300

    Add simpler case_distinction tests

--- tests/regression/46-apron2/66-case_distinction_with_ghosts-3.c
+++ tests/regression/46-apron2/66-case_distinction_with_ghosts-3.c
@@ -11,18 +11,11 @@ int g = 0;
 
 void* inc()
 {
-  int n = __VERIFIER_nondet_int();
+  // x = 10; // TODO: what is this?
 
-  while (x < n) {
-    __VERIFIER_atomic_begin();
-    x++;
-    __VERIFIER_atomic_end();
-
-    __VERIFIER_atomic_begin();
-    assert(g != 1 || x >= 42); // TODO
-    __VERIFIER_atomic_end();
-  }
-
+  __VERIFIER_atomic_begin();
+  assert(g != 1 || x >= 42); // TODO
+  __VERIFIER_atomic_end();
   return 0;
 }
 
@@ -33,7 +26,5 @@ int main()
   __VERIFIER_atomic_begin();
   g = 1; x = 42;
   __VERIFIER_atomic_end();
-
-  assert(x >= 42); // TODO
   return 0;
 }
