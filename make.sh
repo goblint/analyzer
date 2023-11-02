1e7c6fe5241f77a9e134c8d4ff99dce6a7a827ac
stilscher <66023521+stilscher@users.noreply.github.com>
2023-11-02 16:54:16 +0100

    copy gobview distribution files to gobview_dist

--- make.sh
+++ make.sh
@@ -38,8 +38,8 @@ rule() {
       dune build gobview &&
       rm -f goblint-http &&
       cp ./_build/default/gobview/goblint-http-server/goblint_http.exe goblint-http &&
-      mkdir -p run &&
-      cp -R ./_build/default/gobview/dist/* ./run/
+      mkdir -p gobview_dist &&
+      cp -R ./_build/default/gobview/dist/* ./gobview_dist/
     # alternatives to .exe: .bc (bytecode), .bc.js (js_of_ocaml), see https://dune.readthedocs.io/en/stable/dune-files.html#executable
     ;; js) # https://dune.readthedocs.io/en/stable/jsoo.html
       dune build $TARGET.bc.js &&
