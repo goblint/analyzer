c056b32954f095f4ee4ab3a2fcab536eec35bdf2
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-11-08 20:51:19 +0100

    Remove //TODO comment from `76/09`

--- tests/regression/76-memleak/09-unreachable-with-local-var.c
+++ tests/regression/76-memleak/09-unreachable-with-local-var.c
@@ -7,11 +7,9 @@ int main(int argc, char const *argv[]) {
     g = malloc(sizeof(int));
     // Reference to g's heap contents is lost here
     g = NULL;
-    // We get a false positive for p's memory being unreachable
-    // It's still leaked, but due to free() being commented out
-    // TODO: Should we only improve the error reporting for unreachable memory in this case?
+
+    // According to `valid-memtrack`, the memory of p is unreachable and we don't have a false positive
     int *p = malloc(sizeof(int));
-    //free(p);
 
     return 0; //WARN
 }
