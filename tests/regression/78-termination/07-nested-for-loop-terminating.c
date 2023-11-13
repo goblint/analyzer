5d5482726f5464605e59a5ac2263830a03358002
Julian Erhard <julian.erhard@tum.de>
2023-11-13 18:01:02 +0100

    Set regresion tests to todo that are now imprecise.

--- tests/regression/78-termination/07-nested-for-loop-terminating.c
+++ tests/regression/78-termination/07-nested-for-loop-terminating.c
@@ -1,4 +1,4 @@
-// SKIP TERM PARAM: --set "ana.activated[+]" termination --set ana.activated[+] apron --enable ana.int.interval --set ana.apron.domain polyhedra
+// SKIP TODO TERM PARAM: --set "ana.activated[+]" termination --set ana.activated[+] apron --enable ana.int.interval --set ana.apron.domain polyhedra
 #include <stdio.h>
 
 int main()
