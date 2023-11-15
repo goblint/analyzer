4abb8efe405eaff06731a19e3861200490466fe8
jennieliangga <hareljennie@gmail.com>
2023-11-15 17:27:42 +0100

    test

---
+++ tests/anotherTest.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int shared_data = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *increment(void *arg) {
+    for (int i = 0; i < 100000; ++i) {
+        pthread_mutex_lock(&mutex);
+        shared_data++;
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+void *decrement(void *arg) {
+    for (int i = 0; i < 100000; ++i) {
+        pthread_mutex_lock(&mutex);
+        shared_data--;
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread1, thread2;
+
+    pthread_create(&thread1, NULL, increment, NULL);
+    pthread_create(&thread2, NULL, decrement, NULL);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Final shared_data value: %d\n", shared_data);
+
+    return 0;
+}
