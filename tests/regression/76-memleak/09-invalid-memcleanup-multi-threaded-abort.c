c0fe89e93352f530317cf1a7836684de65b216f3
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2023-11-16 07:53:21 +0100

    Add regr. test cases for multi-threaded valid-memcleanup

---
+++ tests/regression/76-memleak/09-invalid-memcleanup-multi-threaded-abort.c
@@ -0,0 +1,33 @@
+//PARAM: --set ana.malloc.unique_address_count 1 --set ana.activated[+] memLeak
+#include <stdlib.h>
+#include <pthread.h>
+
+int *g;
+int *m1;
+int *m2;
+
+void *f1(void *arg) {
+    m1 = malloc(sizeof(int));
+    // Thread t1 leaks m1 here
+    exit(2); //WARN
+}
+
+void *f2(void *arg) {
+    m2 = malloc(sizeof(int));
+    free(m2); // No leak for thread t2, since it calls free before exiting
+    pthread_exit(NULL); //NOWARN
+}
+
+int main(int argc, char const *argv[]) {
+    g = malloc(sizeof(int));
+    pthread_t t1;
+    pthread_create(&t1, NULL, f1, NULL);
+
+    pthread_t t2;
+    pthread_create(&t2, NULL, f2, NULL);
+    
+    free(g);
+
+    // main thread is not leaking anything
+    return 0; //NOWARN
+}
