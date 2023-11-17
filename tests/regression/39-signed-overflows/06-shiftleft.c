c8c49a56b60cc90de5f3a0fbed97ed2de8832db1
Michael Schwarz <michael.schwarz93@gmail.com>
2023-11-17 15:35:41 +0100

    Be more precise for `<<` of Intervals

---
+++ tests/regression/39-signed-overflows/06-shiftleft.c
@@ -0,0 +1,23 @@
+// PARAM: --enable ana.int.interval
+#include <goblint.h>
+int main()
+{
+    int r;
+    int zero_or_one = 0;
+    int top;
+    char c;
+    r = c << 1U; //NOWARN
+
+    r = c << 128U; //WARN
+    r = r << 1U; //WARN
+    r = 8 << -2; //WARN
+
+    if(top) { zero_or_one = 1; }
+
+    r = 8 << zero_or_one;
+
+    __goblint_check(r >= 8);
+    __goblint_check(r <= 16);
+
+    return 0;
+}
