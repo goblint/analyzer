88957a8005c2eff0d51c8bd1402c9d91e2e2a84e
Michael Schwarz <michael.schwarz93@gmail.com>
2023-11-18 11:32:52 +0100

    Add test which can be run in isolation

---
+++ tests/regression/39-signed-overflows/06-abs.c
@@ -0,0 +1,15 @@
+//PARAM: --enable ana.int.interval --set ana.activated[+] tmpSpecial
+#include<math.h>
+int main() {
+    int data;
+    if (data > (-0x7fffffff - 1))
+    {
+        if (abs(data) < 100)
+        {
+            int result = data * data;
+        }
+        
+        int result = data * data;
+    }
+    return 8;
+}
