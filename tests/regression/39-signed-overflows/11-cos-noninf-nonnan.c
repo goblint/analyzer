fde7d4d222775151e6da44c2d3114417433dcea2
stilscher <66023521+stilscher@users.noreply.github.com>
2023-11-23 14:26:20 +0100

    add test case

---
+++ tests/regression/39-signed-overflows/11-cos-noninf-nonnan.c
@@ -0,0 +1,15 @@
+// PARAM: --enable ana.int.interval --enable ana.float.interval --set ana.activated[+] tmpSpecial --html
+
+#include <math.h>
+
+int main() /* check_lower_bound */
+{
+    float x;
+
+    if(!(!__builtin_isnan (x) && !__builtin_isinf_sign (x))) abort();
+    float y = cosf(x);
+    if(!(__builtin_isgreaterequal(y, -1.0f)))
+      __goblint_check(0); // NOWARN (unreachable)
+    return 0;
+
+}
