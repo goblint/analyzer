209a5607204f960a0de6d6d7f81c754354306211
Michael Schwarz <michael.schwarz93@gmail.com>
2023-11-27 15:18:49 +0100

    Reduce activated analsyses and add test

--- tests/regression/74-invalid_deref/31-multithreaded.c
+++ tests/regression/74-invalid_deref/31-multithreaded.c
@@ -1,4 +1,4 @@
-//PARAM: --set ana.activated[+] useAfterFree --set ana.activated[+] threadJoins --set ana.path_sens[+] threadflag  --set ana.activated[+] memOutOfBounds --enable ana.int.interval --set ana.base.arrays.domain partitioned --set ana.base.privatization mutex-meet-tid
+//PARAM: --set ana.path_sens[+] threadflag  --set ana.activated[+] memOutOfBounds --set ana.base.privatization mutex-meet-tid
 #include <pthread.h>
 
 int data;
@@ -15,7 +15,7 @@ int main() {
   pthread_create(&id, ((void *)0), t_fun, ((void *)0));
   q = p;
   pthread_mutex_lock(&mutex);
-  *q = 8;
+  *q = 8; //NOWARN
   pthread_mutex_unlock(&mutex);
   return 0;
 }
