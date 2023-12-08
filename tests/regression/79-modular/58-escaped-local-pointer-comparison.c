48291ad2086d0774d2cbd79debbbde695a51929a
Julian Erhard <julian.erhard@tum.de>
2023-12-08 11:11:39 +0100

    Modular analysis: Add two test cases for locals escaping.

--- tests/regression/79-modular/58-escaped-local-pointer-comparison.c
+++ tests/regression/79-modular/58-escaped-local-pointer-comparison.c
@@ -4,23 +4,28 @@
 #include <pthread.h>
 #include <goblint.h>
 
+int global;
+int *global_pointer;
 int *escaped_local;
 
-void foo(void *arg) {
+void foo() {
     int local = 5;
     escaped_local = &local;
-	__goblint_check(local == 5); //UNKNOWN!
+
+    __goblint_check(global_pointer == &local); //UNKNOWN!
+    __goblint_check(global_pointer != &global); //UNKNOWN!
+    __goblint_check(global_pointer != escaped_local); //UNKNOWN!
 }
 
 void *thread_f(void *arg){
-	foo(NULL);
+	foo();
 	return NULL;
 }
 
 int main() {
     pthread_t thread;
     pthread_create(&thread, NULL, thread_f, NULL);
-    *escaped_local = 10;
+    global_pointer = escaped_local;
     pthread_join(thread, NULL);
     return 0;
 }
---
+++ tests/regression/79-modular/59-write-to-escaped-local.c
@@ -0,0 +1,37 @@
+//PARAM: --set ana.modular.funs "['foo']" --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'read'" --set ana.activated[+] "'used_globals'"
+
+#include <stdio.h>
+#include <pthread.h>
+#include <goblint.h>
+
+int global;
+int global2;
+int **escaped_local;
+
+void foo() {
+    int *local = NULL;
+    escaped_local = &local;
+
+    __goblint_check(local == NULL); //UNKNOWN!
+    if(local != NULL){
+        __goblint_check(*local == &global); //UNKNOWN!
+    }
+}
+
+void *thread_f(void *arg){
+	foo();
+	return NULL;
+}
+
+int main() {
+    int top;
+    pthread_t thread;
+    pthread_create(&thread, NULL, thread_f, NULL);
+    if(top){
+        *escaped_local = &global;
+    } else {
+        *escaped_local = &global2;
+    }
+    pthread_join(thread, NULL);
+    return 0;
+}
