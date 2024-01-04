edb93b39838973a85c6a3edaaf6faae70bf0b195
Rebecca Ghidini <rebecca.ghidini@tum.de>
2024-01-04 18:49:16 +0100

    remove type annotation

--- src/cdomains/apron/linearTwoVarEqualityDomain.apron.ml
+++ src/cdomains/apron/linearTwoVarEqualityDomain.apron.ml
@@ -426,9 +426,8 @@ struct
       let const_offset t = Tuple2.second t in
       let diff t1 t2 = Z.(const_offset t1 - const_offset t2) in
       (*compare two variables for grouping depending on delta function and reference index*)
-      let cmp_z (_, t1i, t2i) (_, t1j, t2j) = 
-        (*Tuple3.compare (fst t1i, fst t2i, diff t1i t2i) (fst t1j, fst t2j, diff t1j t2j)*)
-        let cmp_ref : (int option -> int option -> int )= Option.compare ~cmp:Int.compare in
+      let cmp_z (_, t1i, t2i) (_, t1j, t2j) =
+        let cmp_ref = Option.compare ~cmp:Int.compare in
         Tuple3.compare ~cmp1:cmp_ref ~cmp2:cmp_ref ~cmp3:Z.compare (fst t1i, fst t2i, diff t1i t2i) (fst t1j, fst t2j, diff t1j t2j)
       in
       (*Calculate new components as groups*)
