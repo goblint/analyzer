97f25bea042f1e122b711bc7a23f8a265bb43fc8
Rebecca Ghidini <rebecca.ghidini@tum.de>
2024-01-12 10:57:47 +0100

    add unsound test case so that we can debug

---
+++ tests/regression/77-lin2vareq/34-svcomp-signextension.c
@@ -0,0 +1,26 @@
+// SKIP PARAM: --set ana.activated[+] lin2vareq --set sem.int.signed_overflow assume_none
+
+#include <stdio.h>
+int main() {
+
+  unsigned short int allbits = -1;
+  short int signedallbits = allbits;
+  int unsignedtosigned = allbits;
+  unsigned int unsignedtounsigned = allbits;
+  int signedtosigned = signedallbits;
+  unsigned int signedtounsigned = signedallbits;
+
+  /*
+  printf ("unsignedtosigned: %d\n", unsignedtosigned);
+  printf ("unsignedtounsigned: %u\n", unsignedtounsigned);
+  printf ("signedtosigned: %d\n", signedtosigned);
+  printf ("signedtounsigned: %u\n", signedtounsigned);
+  */
+
+  if (unsignedtosigned == 65535 && unsignedtounsigned == 65535 &&
+      signedtosigned == -1 && signedtounsigned == 4294967295) {
+    return (-1);
+  }
+
+  return (0);
+}
