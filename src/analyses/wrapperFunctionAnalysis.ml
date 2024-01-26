2849a6890fb469d70f544f8ebe8715df7f48f80a
oliver <ge96xey@mytum.de>
2024-01-26 02:32:20 +0100

    remove german comment

--- src/analyses/wrapperFunctionAnalysis.ml
+++ src/analyses/wrapperFunctionAnalysis.ml
@@ -146,7 +146,6 @@ module MallocWrapper : MCPSpec = struct
 
   let query (ctx: (D.t, G.t, C.t, V.t) ctx) (type a) (q: a Q.t): a Q.result =
     let wrapper_node, counter = ctx.local in
-    (* Variable von Allozierten Block zurückgeben -> block in dieser Zeile zu repräsentieren*)
     match q with
     | Q.AllocVar {on_stack = on_stack} ->
       let node = match wrapper_node with
