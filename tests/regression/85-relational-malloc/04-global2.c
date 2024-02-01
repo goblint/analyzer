56ef428896f7f45002760aa216cc9fde94330a3b
oliver <ge96xey@mytum.de>
2024-02-02 00:33:12 +0100

    add source to test case

--- tests/regression/85-relational-malloc/04-global2.c
+++ tests/regression/85-relational-malloc/04-global2.c
@@ -1,4 +1,5 @@
 // PARAM: --set ana.activated[+] memOutOfBounds --enable ana.int.interval --set ana.activated[+] apron  --set ana.apron.domain polyhedra --set sem.int.signed_overflow assume_none --enable ana.sv-comp.functions --disable warn.integer
+// source : https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks/-/blob/main/c/array-memsafety/array01-alloca-2.c
 #include <stdlib.h>
 extern int __VERIFIER_nondet_int(void);
 
