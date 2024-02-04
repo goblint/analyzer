ae643cda7d32da5b7776d0effdcc922a7885c0da
Johanna Schinabeck <johanna@Johanna>
2024-02-04 12:42:53 +0100

    callstringAnalysis: renamed pushElem to stackElem; inlined the depth computation; adjusted comments

--- src/analyses/callstringAnalysis.ml
+++ src/analyses/callstringAnalysis.ml
@@ -2,38 +2,37 @@ open Analyses
 open GoblintCil
 open GobConfig
 
-(* Specifies the type of the callstack elements for the callstring analysis*)
+(* Specifies the type of the call-stack elements for the call-string analysis *)
 module type Callstack_Type =
 sig
   include CilType.S
   val stackTypeName: string
-  val pushElem: fundec -> ('d,'g,'c,'v) ctx -> t option (* returns an elements that should be pushed to the Callstack *)
+  val stackEle: fundec -> ('d,'g,'c,'v) ctx -> t option (* returns an element that should be pushed to the call-stack *)
 end
 
-(** Analysis with the k-callsting approach, which uses the last k callstack elements as context.
-    With the CT argument it is possible to specify the type of the callstack elements*)
+(** Analysis with the k-call-string approach, which uses the last k call-stack elements as context.
+    With the CT argument it is possible to specify the type of the call-stack elements *)
 module Spec (CT:Callstack_Type) : MCPSpec = 
 struct
   include Analyses.IdentitySpec
 
-  (* simulates a call stack of depth k*)
+  (* simulates a call-stack of depth k *)
   module CallStack = struct
     include Printable.PQueue (CT)
-    let depth = get_int "ana.context.callStack_height" (* must be >= 0 *)
 
-    let push stack elem = (* pushes elem to the stack, guarantees stack depth of k*)
+    let push stack elem = (* pushes elem to the call-stack, guarantees a depth of k *)
       match elem with
       | None -> stack
       | Some e -> 
-        let new_stack = BatDeque.cons e stack in (* pushes new element to stack*)         
-        (* removes element from stack, if stack was filled with k elements*)
-        match (BatDeque.size new_stack - depth) with
+        let new_stack = BatDeque.cons e stack in (* pushes new element to stack *)         
+        (* removes element from stack, if stack was filled with k elements *)
+        match (BatDeque.size new_stack - (get_int "ana.context.callStack_height")) with
         | x when x <= 0 -> new_stack
         | 1 -> fst @@ Option.get (BatDeque.rear new_stack)
         | _ -> failwith "Callstack Error: It shouldn't happen that more than one element must be deleted to maintain the correct height!"
   end
 
-  module D = Lattice.Flat (CallStack)
+  module D = Lattice.Flat (CallStack) (* should be the CallStack (C=D). Since a Lattice is required, Lattice.Flat is used to fulfill the type *) 
   module C = CallStack
   module V = EmptyV
   module G = Lattice.Unit
@@ -44,11 +43,11 @@ struct
 
   let context fd x = match x with 
     | `Lifted x -> x
-    | _ -> failwith "Callstring: Context error! The context cannot be derived from Top or Bottom!" (* TODO: is there a possibility???*)
+    | _ -> failwith "Callstring: Context error! The context cannot be derived from Top or Bottom!"
 
   let callee_state ctx f = 
-    let elem = CT.pushElem f ctx in
-    let new_stack = CallStack.push (context f ctx.local) elem in (*TODO: is it ok to use context here??? *)
+    let elem = CT.stackEle f ctx in
+    let new_stack = CallStack.push (context f ctx.local) elem in
     `Lifted new_stack
 
   let enter ctx r f args = [ctx.local, callee_state ctx f]
@@ -61,27 +60,27 @@ end
 module Fundec:Callstack_Type = struct
   include CilType.Fundec
   let stackTypeName = "fundec"
-  let pushElem f ctx = Some f
+  let stackEle f ctx = Some f
 end
 
 module Stmt:Callstack_Type = struct
   include CilType.Stmt
   let stackTypeName = "stmt"
-  let pushElem f ctx = 
+  let stackEle f ctx = 
     match ctx.prev_node with
     | Statement stmt -> Some stmt
-    | _ -> None (* first statement is filtered*)
+    | _ -> None (* first statement is filtered *)
 end
 
 module Location:Callstack_Type = struct
   include CilType.Location
   let stackTypeName = "loc"
-  let pushElem f ctx =
+  let stackEle f ctx =
     Some !Goblint_tracing.current_loc
 end
 
-(* Lifters for the Callstring approach with different Callstack element types*)
+(* Lifters for the call-string approach with different call-stack element types *)
 let _ =
-  MCP.register_analysis (module Spec (Fundec) : MCPSpec); (* name: callstring_fundec*)
-  MCP.register_analysis (module Spec (Stmt) : MCPSpec); (* name: callstring_stmt*)
-  MCP.register_analysis (module Spec (Location) : MCPSpec) (* name: callstring_loc*)
+  MCP.register_analysis (module Spec (Fundec) : MCPSpec); (* name: callstring_fundec *)
+  MCP.register_analysis (module Spec (Stmt) : MCPSpec); (* name: callstring_stmt *)
+  MCP.register_analysis (module Spec (Location) : MCPSpec) (* name: callstring_loc *)
