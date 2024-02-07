520c742793059990b49e10f975fadb445e9d5768
Simmo Saan <simmo.saan@gmail.com>
2024-02-07 10:10:03 +0200

    Replace WTF error in Goblint_tracing

--- src/util/tracing/goblint_tracing.ml
+++ src/util/tracing/goblint_tracing.ml
@@ -28,7 +28,7 @@ let activate (sys:string) (subsys: string list): unit =
   activated := Strs.union !activated subs;
   Hashtbl.add active_dep sys subs
 let deactivate (sys:string): unit =
-  activated := Strs.diff !activated (try Hashtbl.find active_dep sys with Not_found -> Goblint_logs.Logs.error "WTF? %s" sys; Strs.empty)
+  activated := Strs.diff !activated (try Hashtbl.find active_dep sys with Not_found -> Goblint_logs.Logs.error "Missing tracing active_dep for %s" sys; Strs.empty)
 
 let indent_level = ref 0
 let traceIndent () = indent_level := !indent_level + 2
