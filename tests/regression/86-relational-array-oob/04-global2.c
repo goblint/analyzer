5d9d20d1549e0b9270d440030395f5cbf3bb8d3d
oliver <ge96xey@mytum.de>
2024-02-09 04:34:02 +0100

    add test case 86 04 again

---
+++ tests/regression/86-relational-array-oob/04-global2.c
@@ -0,0 +1,42 @@
+// PARAM: --enable ana.arrayoob --enable ana.int.interval   --set ana.activated[+] apron --disable warn.integer --set ana.apron.domain polyhedra 
+
+int readUntil(char ptr[], unsigned len)
+{
+    for (unsigned int i = 0; i < len; i++)
+    {
+        char s = ptr[0];     // NOWARN
+        char s = ptr[i ]; // WARN
+
+        ptr[i] = 42;      // NOWARN
+        ptr[i - 1] = 42;  // WARN
+    }
+}
+
+int main()
+{
+    unsigned int len;
+    unsigned int top;
+
+    if (top)
+        len = 5;
+    else
+        len = 10;
+
+    char ptr[len];
+    char *ptr2 = ptr+7;
+    readUntil(ptr, len);
+
+    for (unsigned int i = 0; i < len; i++)
+    {
+        char s = ptr[i];     // NOWARN
+        char s = ptr[i - 1]; // WARN
+        char s = ptr[i + 1]; // WARN
+        char s = ptr[i - i]; // NOWARN
+        char s = ptr[i + i]; // WARN
+
+        ptr[i] = 42;      // NOWARN
+        ptr[i - 1] = 42;  // WARN
+        ptr[i + 1] = -42; // WARN
+        ptr[i + i] = -42; // WARN
+    }
+}
