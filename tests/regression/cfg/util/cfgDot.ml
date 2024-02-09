3093d27482a7c66777a773dc1c641d988f1d3190
Simmo Saan <simmo.saan@gmail.com>
2024-02-09 12:44:17 +0200

    Use locations in CFG test nodes

--- tests/regression/cfg/util/cfgDot.ml
+++ tests/regression/cfg/util/cfgDot.ml
@@ -1,4 +1,5 @@
 open Goblint_lib
+open CfgTools
 
 let main () =
   Cilfacade.init ();
@@ -7,6 +8,19 @@ let main () =
   CilCfg.createCFG ast;
   let _cfgF, cfgB, _skippedByEdge = CfgTools.createCFG ast in
 
-  CfgTools.fprint_hash_dot cfgB
+  let module NoExtraNodeStyles =
+  struct
+    let defaultNodeStyles = []
+    let extraNodeStyles node =
+      let loc = Node.location node in
+      [Printf.sprintf "label=\"%s\\n(synthetic: %B)\"" (CilType.Location.show loc) loc.synthetic]
+  end
+  in
+  let out = open_out "cfg.dot" in
+  let iter_edges f = H.iter (fun n es -> List.iter (f n) es) cfgB in
+  let ppf = Format.formatter_of_out_channel out in
+  fprint_dot (module CfgPrinters (NoExtraNodeStyles)) iter_edges ppf;
+  Format.pp_print_flush ppf ();
+  close_out out
 
 let () = main ()
