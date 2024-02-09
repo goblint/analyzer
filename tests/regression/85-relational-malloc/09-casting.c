132d5d4ac9f8a5a316f3cad34d39d6440ea0ecf5
oliver <ge96xey@mytum.de>
2024-02-09 04:23:52 +0100

    fix  test case 85 09

--- tests/regression/85-relational-malloc/09-casting.c
+++ tests/regression/85-relational-malloc/09-casting.c
@@ -1,4 +1,4 @@
-// PARAM: --set ana.activated[+] memOutOfBounds --enable ana.int.interval --set ana.activated[+] apron  --set ana.apron.domain polyhedra --set sem.int.signed_overflow assume_none
+// PARAM: --set ana.activated[+] memOutOfBounds --enable ana.int.interval --set ana.activated[+] apron  --set ana.apron.domain polyhedra --set sem.int.signed_overflow assume_none --disable warn.integer
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -11,7 +11,7 @@ int main()
     int *p = malloc(sizeof(int) * len);
 
     char *t = p;
-    for (int i = 0; i < sizeof(int) * len / sizeof(char); i++)
+    for (int i = 0; i < sizeof(int) * len; i++)
     {
         *(t + i) = 2;        // NOWARN
         *(t + 1) = 2;        // NOWARN
@@ -26,6 +26,16 @@ int main()
     long *p = malloc(sizeof(long) * len);
 
     int *t = p;
+    for (int i = 0; i < len; i++)
+    {
+        *(t + i) = 2;       // NOWARN
+        *(t + 1) = 2;       // NOWARN
+        *(t + 2) = 2;       // WARN
+        *(t + 4) = 2;       // WARN
+        *(t - 1) = 2;       // WARN
+        int tmp = *(t + i); // NOWARN
+    }
+
     for (int i = 0; i < sizeof(long) / sizeof(int) * len; i++)
     {
         *(t + i) = 2;       // NOWARN
