a1483d6c8644186a5f7446249f40b93b71a3d380
Simmo Saan <simmo.saan@gmail.com>
2024-02-09 15:10:15 +0200

    Fix instructions in CFG testing

--- tests/regression/cfg/util/cfgDot.ml
+++ tests/regression/cfg/util/cfgDot.ml
@@ -1,7 +1,26 @@
+(* Part of CilCfg *)
+class allBBVisitor = object (* puts every instruction into its own basic block *)
+  inherit GoblintCil.nopCilVisitor
+  method! vstmt s =
+    match s.skind with
+    | Instr(il) ->
+      let list_of_stmts =
+        List.map (fun one_inst -> GoblintCil.mkStmtOneInstr one_inst) il in
+      let block = GoblintCil.mkBlock list_of_stmts in
+      ChangeDoChildrenPost(s, (fun _ -> s.skind <- Block(block); s))
+    | _ -> DoChildren
+
+  method! vvdec _ = SkipChildren
+  method! vexpr _ = SkipChildren
+  method! vlval _ = SkipChildren
+  method! vtype _ = SkipChildren
+end
+
 let main () =
   Cilfacade.init ();
 
   let ast = Cilfacade.getAST (Fpath.v Sys.argv.(1)) in
+  GoblintCil.visitCilFileSameGlobals (new allBBVisitor) ast;
   (* Part of CilCfg.createCFG *)
   GoblintCil.iterGlobals ast (function
       | GFun (fd, _) ->
