3f20225ba1abcb85323e6504437cf04cf72be60b
Simmo Saan <simmo.saan@gmail.com>
2024-02-21 16:58:29 +0200

    Remove commented-out CilLocation.get_labelsLoc

--- src/common/util/cilLocation.ml
+++ src/common/util/cilLocation.ml
@@ -11,14 +11,7 @@ let get_labelLoc = function
   | CaseRange (_, _, loc, eloc) -> {loc; eloc}
   | Default (loc, eloc) -> {loc; eloc}
 
-(* let rec get_labelsLoc = function
-  | [] -> {loc = locUnknown; eloc = locUnknown}
-  | label :: labels ->
-    let loc = get_labelLoc label in
-    if CilType.Location.equal loc Cil.locUnknown then
-      get_labelsLoc labels (* maybe another label has known location *)
-    else
-      loc *)
+(* TODO: need get_labelsLoc? *)
 
 (** Following functions are similar to [Cil] versions, but return expression location instead of entire statement location, where possible. *)
 (* Ideally we would have both copies of the functions available, but UpdateCil would have to be adapted per-stmtkind/instr to store and update either one or two locations. *)
