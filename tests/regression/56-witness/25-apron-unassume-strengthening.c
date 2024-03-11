7c625f994b1491f8763d761ee9ffec891f63ee3e
Simmo Saan <simmo.saan@gmail.com>
2024-03-11 13:36:37 +0200

    Add old variant of 56-witness/25-apron-unassume-strengthening back with explanation

--- tests/regression/56-witness/25-apron-unassume-strengthening.c
+++ tests/regression/56-witness/25-apron-unassume-strengthening.c
@@ -1,4 +1,4 @@
-// SKIP PARAM: --set ana.activated[+] apron --set ana.activated[+] unassume --set witness.yaml.unassume 25-apron-unassume-strengthening.yml --enable ana.apron.strengthening --set sem.int.signed_overflow assume_none
+// SKIP PARAM: --set ana.activated[+] apron --set ana.activated[+] unassume --set witness.yaml.unassume 25-apron-unassume-strengthening.yml --enable ana.apron.strengthening --set sem.int.signed_overflow assume_none --set ana.apron.domain octagon
 #include <goblint.h>
 
 int main() {
@@ -9,5 +9,16 @@ int main() {
     __goblint_check(x >= 0);
     __goblint_check(x <= y);
   }
+
+  // With type bounds we have y <= 2147483647.
+  if (x < y) { // Assuming this implies x <= 2147483646 in closure.
+    // Unassuming 0 <= x actually unassumes 0 <= x <= 2147483647.
+    // Thus, strengthening cannot add x < y back.
+    __goblint_check(x == 0); // UNKNOWN (intentional by unassume)
+    __goblint_check(x >= 0);
+    __goblint_check(x < y); // TODO? Used to work without type bounds: https://github.com/goblint/analyzer/issues/1373.
+  }
+
+  // TODO: Why swapped with polyhedra?
   return 0;
 }
