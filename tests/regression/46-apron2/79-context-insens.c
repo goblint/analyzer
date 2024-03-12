11a7f31f83b7fc413fd482865cd7583c70dff95c
Simmo Saan <simmo.saan@gmail.com>
2024-03-12 14:49:20 +0200

    Add SKIP to apron test (PR #1354)

--- tests/regression/46-apron2/79-context-insens.c
+++ tests/regression/46-apron2/79-context-insens.c
@@ -1,4 +1,4 @@
-// PARAM: --enable annotation.goblint_relation_track --set ana.activated[+] apron --set ana.activated[+] memLeak --set ana.ctx_insens "['base','mallocWrapper','mutexEvents','assert','apron','memLeak']" --set ana.malloc.unique_address_count 2
+// SKIP PARAM: --enable annotation.goblint_relation_track --set ana.activated[+] apron --set ana.activated[+] memLeak --set ana.ctx_insens "['base','mallocWrapper','mutexEvents','assert','apron','memLeak']" --set ana.malloc.unique_address_count 2
 
 // Adapted from https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks/-/blob/main/c/list-properties/list_search-1.c
 #include <goblint.h>
