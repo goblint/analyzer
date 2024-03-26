6961ad23c5e32832924d155427b15685f73165b7
Michael Schwarz <michael.schwarz93@gmail.com>
2024-03-26 16:16:57 +0100

    Comment on `ThreadDigest` vs `ThreadNotStartedDigest`

--- src/analyses/commonPriv.ml
+++ src/analyses/commonPriv.ml
@@ -174,6 +174,9 @@ sig
   val accounted_for: Q.ask -> current:t -> other:t -> bool
 end
 
+(** Digest to be be used for analyses that account for all join-local contributions in some locally tracked datastructure, akin to the L component from the analyses in
+    @see <https://doi.org/10.1007/978-3-031-30044-8_2> Schwarz, M., Saan, S., Seidl, H., Erhard, J., Vojdani, V. Clustered Relational Thread-Modular Abstract Interpretation with Local Traces.
+*)
 module ThreadDigest: Digest =
 struct
   include ThreadIdDomain.ThreadLifted
@@ -197,6 +200,9 @@ struct
     | _ -> false
 end
 
+(** Ego-Lane Derived digest based on whether given threads have been started yet, can be used to refine any analysis
+    @see PhD thesis of M. Schwarz once it is published ;)
+*)
 module ThreadNotStartedDigest:Digest =
 struct
   include ThreadIdDomain.ThreadLifted
