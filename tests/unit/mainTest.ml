cf85b9107dd8b822ed10c56a385741904d8db18d
Simmo Saan <simmo.saan@gmail.com>
2024-04-04 12:00:21 +0300

    Reformat unit test lists to fix indentation

--- tests/unit/mainTest.ml
+++ tests/unit/mainTest.ml
@@ -1,7 +1,8 @@
 open OUnit2
 
-let all_tests = ("" >:::
-  [ IntDomainTest.test ();
+let all_tests =
+  "" >::: [
+    IntDomainTest.test ();
     FloatDomainTest.test ();
     MapDomainTest.test ();
     SolverTest.test ();
@@ -12,6 +13,6 @@ let all_tests = ("" >:::
     (* etc *)
     "domaintest" >::: QCheck_ounit.to_ounit2_test_list Maindomaintest.all_testsuite;
     IntOpsTest.tests;
-  ])
+  ]
 
 let () = run_test_tt_main all_tests
