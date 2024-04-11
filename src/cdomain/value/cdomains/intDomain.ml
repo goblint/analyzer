bb4c7ff132d515b80194c8a7fbb6c7cd47b6986b
Michael Schwarz <michael.schwarz93@gmail.com>
2024-04-11 11:22:24 +0200

    Apply norm

--- src/cdomain/value/cdomains/intDomain.ml
+++ src/cdomain/value/cdomains/intDomain.ml
@@ -2081,7 +2081,7 @@ struct
       let a, b = Size.min_range_sign_agnostic x, Size.min_range_sign_agnostic y in
       let r = R.join (R.of_interval range_ikind a) (R.of_interval range_ikind b) in
       let ex = if Z.gt x Z.zero || Z.lt y Z.zero then S.singleton Z.zero else  S.empty () in
-      (`Excluded (ex, r))
+      norm ik @@ (`Excluded (ex, r))
 
   let starting ?(suppress_ovwarn=false) ikind x = if Z.compare x Z.zero > 0 then not_zero ikind else top_of ikind
   let ending ?(suppress_ovwarn=false) ikind x = if Z.compare x Z.zero < 0 then not_zero ikind else top_of ikind
