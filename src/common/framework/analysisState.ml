bb35e15c395ec886a82dce9f72022ffbb01b612e
Michael Schwarz <michael.schwarz93@gmail.com>
2024-04-15 16:41:15 +0200

    Fix doc comment
    Co-authored-by: Simmo Saan <simmo.saan@gmail.com>

--- src/common/framework/analysisState.ml
+++ src/common/framework/analysisState.ml
@@ -5,7 +5,7 @@
 let should_warn = ref false
 
 (** If this is true, any overflows happening in IntDomains will not lead to warnings being produced or
-    {!svcomp_may_overflow} being set to true. This is useful when, e.g., {!BaseInvariant.refine} executes computations that
+    {!svcomp_may_overflow} being set to true. This is useful when, e.g., {!BaseInvariant.Make.invariant} executes computations that
     are not in the actual program
 *)
 let executing_speculative_computations = ref false
