d67c083ba9755e6d1ecc38daaf28d3a266a1ee38
Simmo Saan <simmo.saan@gmail.com>
2024-04-18 14:49:58 +0300

    Revert "Rewrite mutexGhosts with may locksets per node"
    This partially reverts commits e235ba70d1b187a0395f83729ef53f667fc41e6e and 21ae83a40215d181e92d31fa81b962c0937b0534.

--- src/analyses/mutexGhosts.ml
+++ src/analyses/mutexGhosts.ml
@@ -5,80 +5,49 @@ open Analyses
 
 module Spec =
 struct
-  (* Copied & modified from MayLocks. *)
-  module Arg =
-  struct
-    module D = LockDomain.MayLocksetNoRW
-    module V =
-    struct
-      include Node
-      let is_write_only _ = true
-    end
-
-    module Locked =
-    struct
-      include D
-      let name () = "locked"
-    end
-    module MultiThread =
-    struct
-      include BoolDomain.MayBool
-      let name () = "multithread"
-    end
-    module G = Lattice.Prod (Locked) (MultiThread)
-
-    let add ctx (l,r) =
-      D.add l ctx.local
-
-    let remove ctx l =
-      match D.Addr.to_mval l with
-      | Some (v,o) ->
-        let mtype = ctx.ask (Queries.MutexType (v, Offset.Unit.of_offs o)) in
-        begin match mtype with
-          | `Lifted MutexAttrDomain.MutexKind.NonRec -> D.remove l ctx.local
-          | _ -> ctx.local (* we cannot remove them here *)
-        end
-      | None -> ctx.local (* we cannot remove them here *)
-  end
-
-  include LocksetAnalysis.MakeMay (Arg)
+  include UnitAnalysis.Spec
   let name () = "mutexGhosts"
 
-  open Arg
+  module V =
+  struct
+    include Node
+    let is_write_only _ = true
+  end
 
-  let sync ctx reason =
-    if !AnalysisState.postsolving then (
-      match reason with
-      | `Return -> ctx.sideg ctx.node (ctx.local, MultiThread.bot ())
-      | _ -> ctx.sideg ctx.prev_node (ctx.local, MultiThread.bot ())
-    );
-    ctx.local
+  module Locked =
+  struct
+    include LockDomain.Mutexes
+    let name () = "locked"
+  end
+  module Unlocked =
+  struct
+    include LockDomain.Mutexes
+    let name () = "unlocked"
+  end
+  module MultiThread =
+  struct
+    include BoolDomain.MayBool
+    let name () = "multithread"
+  end
+  module G = Lattice.Prod3 (Locked) (Unlocked) (MultiThread)
 
   let event ctx e octx =
     begin match e with
+      | Events.Lock (l, _) ->
+        ctx.sideg ctx.prev_node (Locked.singleton l, Unlocked.bot (), MultiThread.bot ())
+      | Events.Unlock l ->
+        ctx.sideg ctx.prev_node (Locked.bot (), Unlocked.singleton l, MultiThread.bot ())
       | Events.EnterMultiThreaded ->
-        ctx.sideg ctx.prev_node (Locked.bot (), true)
+        ctx.sideg ctx.prev_node (Locked.bot (), Unlocked.bot (), true)
       | _ -> ()
     end;
-    event ctx e octx (* delegate to must lockset analysis *)
+    ctx.local
 
   let query ctx (type a) (q: a Queries.t): a Queries.result =
     match q with
     | YamlEntryGlobal (g, task) ->
       let g: V.t = Obj.obj g in
-      let module Cfg = (val !MyCFG.current_cfg) in
-      let (lockset, multithread) = ctx.global g in
-      let next_lockset =
-        match Cfg.next g with
-        | [] -> lockset (* HACK for return nodes *)
-        | nexts ->
-          List.fold_left (fun acc (_, next_node) ->
-              let (locked, _) = ctx.global next_node in
-              D.join acc locked
-            ) (D.bot ()) nexts
-      in
-      let unlocked = D.diff lockset next_lockset in
-      let locked = D.diff next_lockset lockset in
+      let (locked, unlocked, multithread) = ctx.global g in
       let entries =
         (* TODO: do variable_entry-s only once *)
         Locked.fold (fun l acc ->
@@ -93,7 +62,7 @@ struct
           ) locked entries
       in
       let entries =
-        Locked.fold (fun l acc ->
+        Unlocked.fold (fun l acc ->
             let entry = WitnessGhost.update_entry ~task ~node:g (Locked l) GoblintCil.zero in
             Queries.YS.add entry acc
           ) unlocked entries
