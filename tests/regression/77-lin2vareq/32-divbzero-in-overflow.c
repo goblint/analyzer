287db936790ec8932c055c1580d5b9c57eb219a9
Dr. Michael Petter <petter@in.tum.de>
2024-04-19 09:35:05 +0200

    even simpler example

--- tests/regression/77-lin2vareq/32-divbzero-in-overflow.c
+++ tests/regression/77-lin2vareq/32-divbzero-in-overflow.c
@@ -2,17 +2,15 @@
 
 /**
  * This test shows an instance where MaySignedOverflow raised
- * an uncaught division by zero in the treatment of c's body
+ * an uncaught division by zero in the treatment of main's sole statement
  * 
  * Fixed in #1419
  */
 int a;
 char b;
-int c() { 0 == a && 1 / b; }
-void d(int (*func)()) { func(); }
 int main()
 {
-    d(c);
+     0 == a && 1 / b;
     __goblint_check(1); // (reachable)
     return 0;
 }
