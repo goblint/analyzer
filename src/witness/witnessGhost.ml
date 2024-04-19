8985d64633a0b3d0f6812df8ee784c87f332f499
Simmo Saan <simmo.saan@gmail.com>
2024-04-19 13:57:37 +0300

    Extract WitnessGhostVar to break dependency cycle

--- src/witness/witnessGhost.ml
+++ src/witness/witnessGhost.ml
@@ -1,34 +1,6 @@
 (** Ghost variables for YAML witnesses. *)
 
-module Var =
-struct
-  type t =
-    | Locked of LockDomain.Addr.t
-    | Multithreaded
-  [@@deriving eq, hash]
-
-  let name_varinfo = function
-    | Locked (Addr (v, _) as l) ->
-      let name =
-        if RichVarinfo.BiVarinfoMap.Collection.mem_varinfo v then
-          Printf.sprintf "alloc_%s%d" (if v.vid < 0 then "m" else "") (abs v.vid) (* turn minus into valid C name *)
-        else
-          LockDomain.Addr.show l (* TODO: valid names with interval offsets, etc *)
-      in
-      name ^ "_locked"
-    | Locked _ -> assert false
-    | Multithreaded -> "multithreaded"
-
-  let describe_varinfo _ _ = ""
-
-  let typ = function
-    | Locked _ -> GoblintCil.intType
-    | Multithreaded -> GoblintCil.intType
-
-  let initial = function
-    | Locked _ -> GoblintCil.zero
-    | Multithreaded -> GoblintCil.zero
-end
+module Var = WitnessGhostVar
 
 include Var
 
