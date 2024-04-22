a6d0f69447ddec3b1fd206a7eeccf56765a71012
Julian Erhard <julian.erhard@tum.de>
2024-04-22 16:32:02 +0200

    Add script for checking whether output contains a certain string.

---
+++ scripts/creduce/string_in_output.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GOBLINTDIR="/home/julian/Entwicklung/goblint/analyzer"
+CONF="--disable ana.opt.hashcons  --set dbg.timeout 900 --sem.unknown_function.spawn false --sem.unknown_function.call false --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'read'" --set ana.activated[+] "'used_globals'" --set ana.activated[+] "'startstate'" --enable ana.sv-comp.functions  --enable allglobs --enable dbg.timing.enabled  --trace combine_env_modular_update --trace collect_targets_with_graph --set ana.modular.funs[+] "'knuth_morris_pratt_multibyte'" --enable ana.modular.only  --trace combine_env_modular_basic"
+INPUT="cp_comb.c"
+STRING=".buf[def_exc:Unknown int([-63,63])].buf[def_exc:Unknown int([-63,63])]"
+
+timeout 30 $GOBLINTDIR/goblint $CONF $INPUT -v &> out.txt
+# if [ $? -eq 3 ]; then
+grep  $STRING out.txt -F -q >/dev/null 2>&1
+# else
+#     exit 5
+# fi
