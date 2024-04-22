3e8882b4e484c9dafd317ddcf5d082f2785fe108
Simmo Saan <simmo.saan@gmail.com>
2024-04-22 17:34:39 +0300

    Disable expRelation in 36-apron/34-large-bigint
    This makes the check meaningful again.

--- tests/regression/36-apron/34-large-bigint.c
+++ tests/regression/36-apron/34-large-bigint.c
@@ -1,4 +1,4 @@
-// SKIP PARAM: --set ana.activated[+] apron --set ana.path_sens[+] threadflag
+// SKIP PARAM: --set ana.activated[+] apron --set ana.path_sens[+] threadflag --set ana.activated[-] expRelation
 #include <goblint.h>
 
 void main() {
@@ -17,6 +17,7 @@ void main() {
       __goblint_check(18446744073709551614ull <= z); // TODO (unknown with D, success with MPQ)
       __goblint_check(z <= 18446744073709551615ull); // TODO (unknown with D, success with MPQ)
 
+      // disable expRelation to prevent base from simplifying x - x to 0
       __goblint_check(x >= x - x); // avoid base from answering to check if apron doesn't say x == -3
       __goblint_check(y >= y - y); // avoid base from answering to check if apron doesn't say y == -3
       __goblint_check(z >= z - z); // avoid base from answering to check if apron doesn't say z == -3
