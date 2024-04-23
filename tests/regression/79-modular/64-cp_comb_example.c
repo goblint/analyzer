32d56a7132a781c3193ade7d0a4fc0a584d27c2c
Julian Erhard <julian.erhard@tum.de>
2024-04-23 15:56:29 +0200

    Make test case type-correct in more places.

--- tests/regression/79-modular/64-cp_comb_example.c
+++ tests/regression/79-modular/64-cp_comb_example.c
@@ -1,16 +1,18 @@
-//PARAM: --disable ana.opt.hashcons  --set dbg.timeout 900 --sem.unknown_function.spawn false --sem.unknown_function.call false --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'read'" --set ana.activated[+] "'used_globals'" --set ana.activated[+] "'startstate'" --enable ana.sv-comp.functions  --enable allglobs --enable dbg.timing.enabled  --trace combine_env_modular_update --trace collect_targets_with_graph --set ana.modular.funs[+] "'knuth_morris_pratt_multibyte'" --enable ana.modular.only  --trace combine_env_modular_basic
+//PARAM: --disable ana.opt.hashcons  --set dbg.timeout 900 --sem.unknown_function.spawn false --sem.unknown_function.call false --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'read'" --set ana.activated[+] "'used_globals'" --set ana.activated[+] "'startstate'" --enable ana.sv-comp.functions  --enable allglobs --enable dbg.timing.enabled  --set ana.modular.funs[+] "'knuth_morris_pratt_multibyte'" --enable ana.modular.only --trace base
+// --trace combine_env_modular_update --trace collect_targets_with_graph  --trace combine_env_modular_basic
 
 // Output for this file should not contain the following address offset: .buf[def_exc:Unknown int([-63,63])].buf[def_exc:Unknown int([-63,63])]
-void *d;
+struct a *d;
 long e, f;
 struct a {
-  const *b;
+  const char *b;
   char buf;
-} c(struct a *g, struct a *h) {
+};
+struct a c(struct a *g, struct a *h) {
   g->b = &g->buf;
-  g = h->b;
+  g = (struct a*) h->b;
 }
-knuth_morris_pratt_multibyte() {
+int knuth_morris_pratt_multibyte() {
   struct a i;
   while (1) {
     c(d + f, &i);
