71c6ce62f7960a180c2efc08e4284c5fac08df42
Julian Erhard <julian.erhard@tum.de>
2024-04-24 14:35:55 +0200

    Adadpt teset description.

--- tests/regression/79-modular/64-cp_comb_example.c
+++ tests/regression/79-modular/64-cp_comb_example.c
@@ -1,7 +1,7 @@
-//PARAM: --disable ana.opt.hashcons  --set dbg.timeout 900 --sem.unknown_function.spawn false --sem.unknown_function.call false --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'read'" --set ana.activated[+] "'used_globals'" --set ana.activated[+] "'startstate'" --enable ana.sv-comp.functions  --enable allglobs --enable dbg.timing.enabled  --set ana.modular.funs[+] "'foo'" --enable ana.modular.only --trace base
-// --trace combine_env_modular_update --trace collect_targets_with_graph  --trace combine_env_modular_basic
+//PARAM: --disable ana.opt.hashcons  --set dbg.timeout 900 --sem.unknown_function.spawn false --sem.unknown_function.call false --enable modular --set ana.activated[+] "'modular_queries'" --set ana.activated[+] "'is_modular'" --set ana.activated[+] "'written'" --set ana.activated[+] "'read'" --set ana.activated[+] "'used_globals'" --set ana.activated[+] "'startstate'" --enable ana.sv-comp.functions  --enable allglobs --enable dbg.timing.enabled  --set ana.modular.funs[+] "'foo'" --enable ana.modular.only
 
-// Output for this file should not contain the following address offset: .buf[def_exc:Unknown int([-63,63])].buf[def_exc:Unknown int([-63,63])] or .buf.buf
+// Previously did not terminate because an infinite sequence of addresses with some repeated offset was generated.
+// Output for this file when tracing the local states of Base should not contain the following address offsets: .buf[def_exc:Unknown int([-63,63])].buf[def_exc:Unknown int([-63,63])] or .buf.buf
 struct a {
   char *b;
   char buf;
