9734040eadfe49545c0958c43bb4038bca16fad3
Simmo Saan <simmo.saan@gmail.com>
2024-04-29 12:36:45 +0300

    Fix PARAM for 56-witness/{46,47}-top-{bool,int}-invariant

--- tests/regression/56-witness/46-top-bool-invariant.c
+++ tests/regression/56-witness/46-top-bool-invariant.c
@@ -1,4 +1,4 @@
-// PARAM: --enable witness.yaml.enabled --set witness.yaml.entry-types '["location_invariant"]' --enable ana.int.def_exc --enable ana.int.interval --enable ana.int.enums --enable ana.int.congruence --enable ana.int.interval_set 46-top-bool-invariant.c --disable witness.invariant.inexact-type-bounds
+// PARAM: --enable witness.yaml.enabled --set witness.yaml.entry-types '["location_invariant"]' --enable ana.int.def_exc --enable ana.int.interval --enable ana.int.enums --enable ana.int.congruence --enable ana.int.interval_set --disable witness.invariant.inexact-type-bounds
 
 int main() {
   _Bool x;
