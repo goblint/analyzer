1fa4e660cb6a97562036c135c262920825effd8b
Rebecca Ghidini <rebecca.ghidini@tum.de>
2024-05-09 17:25:42 +0200

    add test for disequalities

--- tests/regression/79-wrpointer/19-disequalities.c
+++ tests/regression/79-wrpointer/19-disequalities.c
@@ -16,5 +16,25 @@ void main(void) {
   if (*i != **(j + 3)) {
     __goblint_check(i != *(j + 3));
     __goblint_check(&i != j + 3);
+    j = NULL;
+    __goblint_check(i != *(j + 3)); // UNKNOWN
+  }
+
+  int *k2 = (int *)malloc(sizeof(int));
+  *j = k2;
+  k = k2;
+
+  __goblint_check(*j == k);
+  __goblint_check(k2 == k);
+
+  int *f1 = (int *)malloc(sizeof(int));
+  int *f2 = f2;
+
+  if (*j != f2) {
+    __goblint_check(*j != f2);
+    __goblint_check(k != f1);
+    j = NULL;
+    __goblint_check(*j != f2); // UNKNOWN
+    __goblint_check(k != f1);
   }
 }
