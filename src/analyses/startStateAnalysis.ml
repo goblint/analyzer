ad80a1925dfb8f57c43adf6c39fec59ee10bcd31
Rebecca Ghidini <rebecca.ghidini@tum.de>
2024-05-13 12:12:19 +0200

    fixed bugs in startState analysis and remembered tainted variables in return of startState

--- src/analyses/startStateAnalysis.ml
+++ src/analyses/startStateAnalysis.ml
@@ -1,4 +1,4 @@
-(** Remebers the Value of each parameter at the beginning of each function.
+(** Remembers the Value of each parameter at the beginning of each function.
     Used by the wrpointer anaylysis. *)
 
 open GoblintCil
@@ -21,30 +21,35 @@ struct
   include Analyses.IdentitySpec
 
 
-  let duplicated_variable var = { var with vid = - var.vid; vname = var.vname ^ "'" }
-  let original_variable var = { var with vid = - var.vid; vname = String.rchop var.vname }
+  let duplicated_variable var = { var with vid = - var.vid - 4; vname = var.vname ^ "'" }
+  let original_variable var = { var with vid = - (var.vid + 4); vname = String.rchop var.vname }
+  let return_varinfo = {dummyFunDec.svar with vid=(-2);vname="@return"}
 
   let get_value (ask: Queries.ask) exp = ask.f (MayPointTo exp)
 
   (** If e is a known variable, then it returns the value for this variable.
       If e is an unknown variable, then it returns bot.
-      If e is another expression that is not simply a ariable, then it returns top. *)
+      If e is another expression that is not simply a variable, then it returns top. *)
   let eval (ask: Queries.ask) (d: D.t) (exp: exp): Value.t = match exp with
     | Lval (Var x, NoOffset) -> begin match D.find_opt x d with
-        | Some v -> v
+        | Some v -> if M.tracing then M.trace "wrpointer-tainted" "QUERY %a : res = %a\n" d_exp exp AD.pretty v;v
         | None -> Value.top()
       end
-    | AddrOf (Var x, NoOffset) -> if x.vid < 0 then get_value ask (AddrOf (Var (original_variable x), NoOffset)) else Value.top()
+    | AddrOf (Var x, NoOffset) -> if x.vid < -1 then (let res = get_value ask (AddrOf (Var (original_variable x), NoOffset)) in if M.tracing then M.trace "wrpointer-tainted" "QUERY %a : res = %a\n" d_exp exp AD.pretty res;res) else Value.top()
     | _ -> Value.top ()
 
   let startstate v = D.bot ()
   let exitstate = startstate
 
   let return ctx exp_opt f =
-    (*remember all value of local vars*)
-    List.fold_left (fun st var -> let value = get_value (ask_of_ctx ctx) (Lval (Var var, NoOffset)) in
-                     if M.tracing then M.trace "startState" "return: added value: var: %a; value: %a" d_lval (Var var, NoOffset) Value.pretty value;
-                     D.add (var) value st) (D.empty()) (f.sformals @ f.slocals)
+    (*remember all values of local vars*)
+    let st = List.fold_left (fun st var -> let value = get_value (ask_of_ctx ctx) (Lval (Var var, NoOffset)) in
+                              if M.tracing then M.trace "startState" "return: added value: var: %a; value: %a" d_lval (Var var, NoOffset) Value.pretty value;
+                              D.add (var) value st) (D.empty()) (f.sformals @ f.slocals) in
+    (* remember value of tainted vars in the return variable *)
+    let tainted = ctx.ask (MayBeTainted) in
+    let st = D.add return_varinfo tainted st
+    in if M.tracing then M.tracel "wrpointer-tainted" "startState: %a; state: %a\n" AD.pretty tainted D.pretty st;st
 
 
   let query ctx (type a) (q: a Queries.t): a Queries.result =
@@ -55,19 +60,20 @@ struct
     | _ -> Result.top q
 
   let enter ctx var_opt f args =
-    (* assign function parameters *)
     [ctx.local, ctx.local]
 
   let body ctx (f:fundec) =
+    (* assign function parameters *)
     List.fold_left (fun st var -> let value = get_value (ask_of_ctx ctx) (Lval (Var var, NoOffset)) in
                      if M.tracing then M.trace "startState" "added value: var: %a; value: %a" d_lval (Var (duplicated_variable var), NoOffset) Value.pretty value;
                      D.add (duplicated_variable var) value st) (D.empty()) f.sformals
 
+  let combine_env ctx (lval:lval option) fexp (f:fundec) (args:exp list) fc au (f_ask: Queries.ask) =
+    ctx.local
+
   let combine_assign ctx var_opt expr f exprs t_context_opt t ask =
-    (*remove duplicated vars and local vars *)
-    List.fold_left (fun st var ->
-        if M.tracing then M.trace "startState" "removing var: var: %a" d_lval (Var var, NoOffset);
-        D.remove (var) st) (D.empty()) (f.sformals @ f.slocals @ (List.map duplicated_variable f.sformals))
+    (* remove duplicated vars and local vars *)
+    ctx.local
 
 end
 
