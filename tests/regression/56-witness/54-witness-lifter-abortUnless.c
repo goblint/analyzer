68e8fff09321573026c0f08efde245cfbc496725
Simmo Saan <simmo.saan@gmail.com>
2024-05-14 16:49:27 +0300

    Add test 56-witness/54-witness-lifter-abortUnless (issue #1453)

---
+++ tests/regression/56-witness/54-witness-lifter-abortUnless.c
@@ -0,0 +1,17 @@
+// PARAM: --set ana.activated[+] abortUnless --enable exp.arg
+// NOCRASH
+void printLine()
+{
+
+}
+
+void goodB2G1Sink(int data)
+{
+    printLine();
+}
+
+int main()
+{
+    goodB2G1Sink(0);
+    return 0;
+}
