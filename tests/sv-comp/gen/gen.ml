4900fd5a32a89d4084d2b7c23b6391a3b90d7d81
Simmo Saan <simmo.saan@gmail.com>
2024-05-17 10:23:50 +0300

    Clean up ARG test rule generation, conditional on graph-easy

--- tests/sv-comp/gen/gen.ml
+++ tests/sv-comp/gen/gen.ml
@@ -1,23 +1,30 @@
-let generate_rule file =
-  let dir = Filename.dirname file in
-  let file'' = Filename.basename file in
-  let file' = Filename.chop_extension file'' in
+let generate_rule c_dir_file =
+  let dir = Filename.dirname c_dir_file in
+  let c_file = Filename.basename c_dir_file in
+  let file = Filename.chop_extension c_file in
   Printf.printf {|
-  (subdir %s
-    (rule
-     (alias runtest)
-     (deps (sandbox always) (package goblint) (:c %s) (:prop %%{project_root}/tests/sv-comp/unreach-call-__VERIFIER_error.prp))
-     (target %s.output)
-     (action
-      (progn
-       (ignore-outputs (run goblint --conf svcomp21.json --set ana.specification %%{prop} %%{c} --enable exp.argdot --enable exp.arg --set exp.argdotlabel empty))
-       (with-stdout-to %%{target} (run graph-easy --as=boxart arg.dot)))))
+(subdir %s
+ (rule
+  (deps
+   (sandbox always) ; must sandbox to prevent arg.dot-s from parallel runs from interfering
+   (package goblint) ; depend on entire goblint package for svcomp21 conf
+   (:c %s)
+   (:prop %%{project_root}/tests/sv-comp/unreach-call-__VERIFIER_error.prp))
+  (target %s.output)
+  (enabled_if %%{bin-available:graph-easy})
+  (action
+   (progn
+    (ignore-outputs
+     (run goblint --conf svcomp21.json --set ana.specification %%{prop} %%{c} --enable exp.argdot --enable exp.arg --set exp.argdotlabel empty))
+    (with-stdout-to %%{target}
+     (run graph-easy --as=boxart arg.dot)))))
 
-    (rule
-     (alias runtest)
-     (action (diff %s.expected %s.output)))
-  )
-  |} dir file'' file' file' file'
+ (rule
+  (alias runtest)
+  (enabled_if %%{bin-available:graph-easy})
+  (action
+   (diff %s.expected %s.output))))
+|} dir c_file file file file
 
 let () =
   Sys.argv
