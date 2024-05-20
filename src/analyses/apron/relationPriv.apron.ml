d008a2c50f6da39416488099069c1b1cac3ff894
Simmo Saan <simmo.saan@gmail.com>
2024-05-20 17:56:08 +0300

    Make relational mutex-meet join sync more precise when threadflag is path-sensitive (PR #1464)

--- src/analyses/apron/relationPriv.apron.ml
+++ src/analyses/apron/relationPriv.apron.ml
@@ -635,7 +635,7 @@ struct
           st
       end
     | `Join ->
-      if (ask.f (Q.MustBeSingleThreaded {since_start = true})) then
+      if ConfCheck.no_branched_thread_creation () || (ask.f (Q.MustBeSingleThreaded {since_start = true})) then
         st
       else
         let rel = st.rel in
