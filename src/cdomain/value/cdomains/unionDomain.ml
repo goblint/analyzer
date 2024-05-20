2f29e90e7fcd5b032cdc6e6f4c2721f244e2a17c
Michael Schwarz <michael.schwarz93@gmail.com>
2024-05-20 15:03:33 +0200

    Union Domain: Cleanup meet
    Co-authored-by: Simmo Saan <simmo.saan@gmail.com>

--- src/cdomain/value/cdomains/unionDomain.ml
+++ src/cdomain/value/cdomains/unionDomain.ml
@@ -23,13 +23,9 @@ module Field = struct
     end) (CilType.Fieldinfo)
 
   let meet f g =
-    if equal f g then
-      f
-    else
-      match f, g with
-      | `Top, f
-      | f, `Top -> f
-      | _ -> raise Lattice.Uncomparable
+    match meet f g with
+    | `Bot -> raise Lattice.Uncomparable
+    | m -> m
 end
 
 module Simple (Values: Arg) =
