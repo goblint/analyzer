94a31d4ef61f6e4227c0329f96342ae1f471eeb5
Fabian Stemmler <stemmler@in.tum.de>
2024-06-05 14:53:00 +0200

    test interval-set narrowing and lower bounds

--- tests/regression/03-practical/33-threshold-narrowing-intervals.c
+++ tests/regression/03-practical/33-threshold-narrowing-intervals.c
@@ -5,4 +5,9 @@ int main() {
     int i;
     for(i = 0; i < 10 && i < 20; i += 3);
     __goblint_check(i <= 12);
+
+    int j;
+    for(j = 0; j > -10 && j > -20; j-= 3);
+    __goblint_check(j >= -12);
+    
 }
