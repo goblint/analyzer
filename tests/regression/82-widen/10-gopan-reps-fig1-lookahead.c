e5305bc273a9ca293e0199cd4724115bfdc9fcc2
Simmo Saan <simmo.saan@gmail.com>
2024-06-07 16:52:28 +0300

    Add more checks to 82-widen/10-gopan-repos-fig1-lookahead

--- tests/regression/82-widen/10-gopan-reps-fig1-lookahead.c
+++ tests/regression/82-widen/10-gopan-reps-fig1-lookahead.c
@@ -1,4 +1,4 @@
-// SKIP PARAM: --set ana.activated[+] apron --set ana.apron.domain polyhedra --enable ana.widen.lookahead
+// SKIP PARAM: --set ana.activated[+] apron --set ana.apron.domain polyhedra --set sem.int.signed_overflow assume_none --enable ana.widen.lookahead
 // From "Lookahead Widening", Fig. 1: https://doi.org/10.1007/11817963_41
 #include <goblint.h>
 
@@ -9,8 +9,10 @@ int main() {
   while (1) {
     __goblint_check(x >= 0); // TODO
     __goblint_check(x <= 102); // TODO
-    __goblint_check(y >= 0);
+    __goblint_check(y >= 0); // TODO (with assume_none)
     __goblint_check(y <= 51); // TODO
+    __goblint_check(y <= x); // TODO (without assume_none?)
+    __goblint_check(x + y <= 102); // TODO
     if (x <= 50)
       y++;
     else
@@ -19,7 +21,7 @@ int main() {
       break;
     x++;
   }
-  __goblint_check(x == 102); // TODO
+  __goblint_check(x == 102); // TODO (needs narrowing?)
   __goblint_check(y == -1); // TODO
   return 0;
 }
