496b5ef26373e4e7b06832657f0ba196db671806
Michael Schwarz <michael.schwarz93@gmail.com>
2024-06-12 18:05:34 +0200

    Add misbehaving program with gas

---
+++ tests/regression/80-context_gas/21-sync.c
@@ -0,0 +1,35 @@
+// PARAM: --set ana.context.gas_value 3
+// Like 00/35 but with gas this time!
+// Misbehaves for gas <= 3
+#include <goblint.h>
+#include <pthread.h>
+
+int g = 1;
+
+void foo() {
+  // Single-threaded: g = 1 in local state
+  // Multi-threaded: g = 2 in global unprotected invariant
+  // Joined contexts: g is unprotected, so read g = 2 from global unprotected invariant (only)
+  // Was soundly claiming that check will succeed!
+  int x = g;
+  __goblint_check(x == 2); // UNKNOWN!
+}
+
+void *t_fun(void *arg) {
+  foo();
+}
+
+int do_stuff() {
+  foo();
+  g = 2;
+
+  pthread_t id;
+  pthread_create(&id, NULL, t_fun, NULL);
+
+  return 0;
+}
+
+int main() {
+  do_stuff();
+  return 0;
+}
