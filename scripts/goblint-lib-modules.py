cd793b9e7293bd94920a09b7388e5cc19bf8518c
Simmo Saan <simmo.saan@gmail.com>
2024-07-18 20:17:00 +0300

    Exclude PPX-s from Goblint_lib documentation check

--- scripts/goblint-lib-modules.py
+++ scripts/goblint-lib-modules.py
@@ -42,6 +42,10 @@ exclude_module_names = set([
     "Goblint_build_info",
     "Dune_build_info",
 
+    # ppx-s
+    "Ppx_deriving_printable",
+    "Ppx_deriving_lattice",
+
     "MessageCategory", # included in Messages
     "PreValueDomain", # included in ValueDomain
 
