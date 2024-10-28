01bff20b3fea1bb077c12ba9b0b7d7eba6d27c72
Simmo Saan <simmo.saan@gmail.com>
2024-10-28 14:00:52 +0200

    Make 51-threadjoins/09-join-main runnable

--- tests/regression/51-threadjoins/09-join-main.c
+++ tests/regression/51-threadjoins/09-join-main.c
@@ -1,13 +1,16 @@
 //PARAM: --set ana.activated[+] threadJoins
 #include <pthread.h>
+#include <stdio.h>
 
 pthread_t mainid;
 
 int g = 10;
 
 void *t_fun(void *arg) {
-  pthread_join(mainid, NULL);
+  int r = pthread_join(mainid, NULL); // TSan doesn't like this...
+  printf("j: %d\n", r);
   g++; // NORACE
+  printf("t_fun: %d\n", g);
   return NULL;
 }
 
@@ -19,5 +22,8 @@ int main(void) {
   pthread_create(&id2, NULL, t_fun, NULL);
 
   g++; // NORACE
+  printf("main: %d\n", g);
+
+  pthread_exit(NULL); // exit main thread but keep id2 alive, otherwise main returning kills id2
   return 0;
 }
