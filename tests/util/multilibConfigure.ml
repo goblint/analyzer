7170d9a8944706a1adc0acaeb81a4fc6d914af7b
Simmo Saan <simmo.saan@gmail.com>
2024-11-28 15:00:16 +0200

    Fix unused open in multilibConfigure

--- tests/util/multilibConfigure.ml
+++ tests/util/multilibConfigure.ml
@@ -1,4 +1,4 @@
 open GoblintCil
 
 let () =
-  Printf.printf "%B" (Option.is_some GoblintCil.Machdep.gcc32 && Option.is_some GoblintCil.Machdep.gcc64)
+  Printf.printf "%B" (Option.is_some Machdep.gcc32 && Option.is_some Machdep.gcc64)
