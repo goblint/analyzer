79f890a9ac0488921c69dbe4eb35af2433bfa59d
Havercake <philip@feniuk.net>
2024-12-10 15:53:51 +0100

    Added some comments on the test setup

--- tests/unit/mainTest.ml
+++ tests/unit/mainTest.ml
@@ -14,10 +14,12 @@ let all_tests =
     (* etc *)
     "domaintest" >::: QCheck_ounit.to_ounit2_test_list Maindomaintest.all_testsuite;
     IntOpsTest.tests;
+    (* SparseMatrixImplementationTest.tests; *) (* Uncomment this to add the sparse matrix tests to all tests *)
   ]
 
 let subset_tests = "" >::: [SparseMatrixImplementationTest.tests]
 
 let () =
   print_string "\027[0;1munit: \027[0;0;00m";
-  run_test_tt_main subset_tests
+  run_test_tt_main subset_tests (* Remove this and uncomment the line below to run all tests.*)
+ (* run_test_tt_main all_tests *)
