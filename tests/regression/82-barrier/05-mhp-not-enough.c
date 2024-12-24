298d345abf54bf3aa16a1a17e7eaf65c3a7a6de8
Michael Schwarz <michael.schwarz93@gmail.com>
2024-12-24 21:32:46 +0100

    More involved MHP check

--- tests/regression/82-barrier/05-mhp-not-enough.c
+++ tests/regression/82-barrier/05-mhp-not-enough.c
@@ -7,17 +7,12 @@ int g;
 
 pthread_barrier_t barrier;
 
-void* f2(void* ptr) {
-    pthread_barrier_wait(&barrier);
-    return NULL;
-}
 
 void* f1(void* ptr) {
     pthread_barrier_wait(&barrier);
 
-    // This is past the barrier, so it will not be reached
-    pthread_t t2;
-    pthread_create(&t2,NULL,f2,NULL);
+    // This should not be reached as either main calls wait or the other thread is created
+    // -> In the concrete, there never are three threads calling wait
 
     return NULL;
 }
@@ -25,6 +20,7 @@ void* f1(void* ptr) {
 int main(int argc, char const *argv[])
 {
     int top;
+    int top2;
     int i = 0;
     
     pthread_barrier_init(&barrier, NULL, 3);
@@ -36,7 +32,11 @@ int main(int argc, char const *argv[])
         pthread_barrier_wait(&barrier);
         // Unreachable
         i = 1;
+    } else {
+        pthread_t t2;
+        pthread_create(&t2,NULL,f1,NULL);    
     }
+
 
     __goblint_check(i == 0);
 
