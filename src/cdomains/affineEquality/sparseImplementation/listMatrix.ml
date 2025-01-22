5e169850b6967623961e267913a645640e2798e6
Charlotte Brandt <lotte.brandt.53@gmail.com>
2025-01-22 12:02:43 +0100

    Remove V.nth from map2 - not better?

--- src/cdomains/affineEquality/sparseImplementation/listMatrix.ml
+++ src/cdomains/affineEquality/sparseImplementation/listMatrix.ml
@@ -58,11 +58,43 @@ module ListMatrix: AbstractMatrix =
       let vector_length = V.length v in
       List.mapi (fun index row -> if index < vector_length then f row (V.nth v index) else row ) m
 
+    let map2 f m v =
+      let vector_length = V.length v in
+      let vector_entries = V.to_sparse_list v in
+      let rec map2_helper acc index m v = 
+        match m, v with 
+        | [], _ -> List.rev acc
+        | row :: rs, [] when index >= vector_length -> map2_helper (row :: acc) (index + 1) rs []
+        | row :: rs, [] ->  map2_helper ((f row A.zero):: acc) (index + 1) rs []
+        | row :: rs, (i, value) :: vs -> 
+          if i = index then 
+            map2_helper ((f row value):: acc) (index + 1) rs vs 
+          else
+            map2_helper ((f row A.zero) :: acc) (index + 1) rs v 
+      in
+      map2_helper [] 0 m vector_entries
+
     let map2 f m v = Timing.wrap "Matrix.map2" (map2 f m) v
 
     let map2i f m v =
       let vector_length = V.length v in
       List.mapi (fun index row -> if index < vector_length then f index row (V.nth v index) else row ) m
+
+    let map2i f m v =
+      let vector_length = V.length v in
+      let vector_entries = V.to_sparse_list v in
+      let rec map2_helper acc index m v = 
+        match m, v with 
+        | [], _ -> List.rev acc
+        | row :: rs, [] when index >= vector_length -> map2_helper (row :: acc) (index + 1) rs []
+        | row :: rs, [] ->  map2_helper ((f index row A.zero):: acc) (index + 1) rs []
+        | row :: rs, (i, value) :: vs -> 
+          if i = index then 
+            map2_helper ((f index row value):: acc) (index + 1) rs vs 
+          else
+            map2_helper ((f index row A.zero) :: acc) (index + 1) rs v 
+      in
+      map2_helper [] 0 m vector_entries
 
     let map2i f m v = Timing.wrap "Matrix.map2i" (map2i f m) v
 
