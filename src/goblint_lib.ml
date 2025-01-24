e255c60b92d4861248b0fc75351b4170ac665ea4
Michael Schwarz <michael.schwarz93@gmail.com>
2025-01-24 18:24:05 +0100

    Document `pthread_once`

--- src/goblint_lib.ml
+++ src/goblint_lib.ml
@@ -130,6 +130,7 @@ module RelationPriv = RelationPriv
 module ThreadEscape = ThreadEscape
 module PthreadSignals = PthreadSignals
 module ExtractPthread = ExtractPthread
+module PthreadOnce = PthreadOnce
 
 (** {2 Longjmp}
 
