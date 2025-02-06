6d163cfaa0555c366f643356012322d987f4f1a2
ManuelLerchner <manuel.lerchner1111@gmail.com>
2025-02-06 22:21:19 +0100

    fix comments and indentation of tests

--- tests/regression/82-bitfield/12-precision.c
+++ tests/regression/82-bitfield/12-precision.c
@@ -6,42 +6,42 @@ void example1(void) __attribute__((goblint_precision("no-bitfield")));
 void example2(void) __attribute__((goblint_precision("bitfield")));
 
 int main() {
-    example1();
-    example2();
+  example1();
+  example2();
 }
 
-void example1(){
-    int state;
-    int r = rand() % 3;
-    switch (r) {
-        case 0:
-        state = 0; /* 0b0000 */
-         break;
-        case 1:
-        state = 8; /* 0b1000 */
-         break;
-        default:
-        state = 10; /* 0b1010 */
-         break;
+void example1() {
+  int state;
+  int r = rand() % 3;
+  switch (r) {
+    case 0:
+      state = 0; /* 0b0000 */
+      break;
+    case 1:
+      state = 8; /* 0b1000 */
+      break;
+    default:
+      state = 10; /* 0b1010 */
+      break;
   }
 
-  __goblint_check((state & ANY_ERROR) == 0); //UNKNOWN
+  __goblint_check((state & ANY_ERROR) == 0);  // UNKNOWN
 }
 
-void example2(){
-    int state;
-    int r = rand() % 3;
-    switch (r) {
-        case 0:
-        state = 0; /* 0b0000 */
-         break;
-        case 1:
-        state = 8; /* 0b1000 */
-         break;
-        default:
-        state = 10; /* 0b1010 */
-         break;
+void example2() {
+  int state;
+  int r = rand() % 3;
+  switch (r) {
+    case 0:
+      state = 0; /* 0b0000 */
+      break;
+    case 1:
+      state = 8; /* 0b1000 */
+      break;
+    default:
+      state = 10; /* 0b1010 */
+      break;
   }
 
-  __goblint_check((state & ANY_ERROR) == 0); //SUCCESS
+  __goblint_check((state & ANY_ERROR) == 0);  // SUCCESS
 }
