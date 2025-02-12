7329091318f6f2a0094a734f34c6fa7820df6bbd
Karoliine Holter <karoliine.holter@hotmail.com>
2025-02-12 21:36:38 +0200

    Add regtest for named pointer comparison

---
+++ tests/regression/01-cpa/76-pointer-typedef.c
@@ -0,0 +1,31 @@
+// PARAM: --set ana.malloc.unique_address_count 2
+// Extracted (using creduce) from SV-COMP task list-simple/dll2c_remove_all.i
+#include <stdlib.h>
+
+typedef struct node {
+  struct node *next;
+  struct node *prev;
+} * DLL;
+
+void dll_remove(DLL *head) {
+  DLL temp = (*head)->next;
+  if (temp == *head) {
+    __goblint_check(temp == *head);
+    __goblint_check(temp != *head); // FAIL
+    free(*head);
+  }
+  else {
+    __goblint_check(temp != *head);
+    __goblint_check(temp == *head); // FAIL
+    (*head)->prev->next = temp;
+    free(*head);
+    *head = temp;
+  }
+}
+main() {
+  DLL s = malloc(sizeof(struct node));
+  s->next = s->prev = malloc(sizeof(struct node));
+
+  dll_remove(&s);
+  dll_remove(&s);
+}
