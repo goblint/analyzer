5df3b5eb7501fc3a89c24b22b2d96dfe4655bdce
Julian Erhard <julian.erhard@tum.de>
2025-02-14 11:12:27 +0100

    C2PO DuplicateVars: Remark todos for incremental, improve readability.

--- src/cdomains/duplicateVars.ml
+++ src/cdomains/duplicateVars.ml
@@ -14,40 +14,82 @@ module M = Messages
 module VarType = struct
   (* the hash/compare values should not depend on the type.
      But they have to be defined even though they are not used, for some reason.*)
-  let equal_typ a b = Stdlib.compare a b = 0
-  let hash_typ x = Hashtbl.hash x
-  let compare_typ a b = Stdlib.compare a b
+  let equal_typ a b =
+    (* TODO: Structural equality on (possibly cyclic) data type typ may not terminate in incremental analysis. *)
+    a = b
 
-  type t = AssignAux of typ
-         | ReturnAux of typ
-         | NormalVar of Varinfo.t
-         | DuplicVar of Varinfo.t [@@deriving eq,ord,hash]
+  let hash_typ x =
+    Hashtbl.hash x
+
+  let compare_typ a b =
+    (* TODO: Structural compare on (possibly cyclic) data type typ may not terminate in incremental analysis. *)
+    Stdlib.compare a b
+
+  type t =
+    | AssignAux of typ
+    | ReturnAux of typ
+    | NormalVar of Varinfo.t
+    | DuplicVar of Varinfo.t [@@deriving eq,ord,hash]
 
   let from_varinfo normal duplicated =
-    List.map (fun v -> NormalVar v) normal @ List.map (fun v -> DuplicVar v) duplicated
+    let to_normal v =
+      NormalVar v
+    in
+    let to_duplicated v =
+      DuplicVar v
+    in
+
+    let normal = Seq.of_list normal in
+    let duplicated = Seq.of_list duplicated in
+
+    let normal = Seq.map to_normal normal in
+    let duplicated = Seq.map to_duplicated duplicated in
+    let complete = Seq.append normal duplicated in
+
+    List.of_seq complete
+
+  let duplic_var_prefix =
+    "c2po__"
+  let duplic_var_postfix =
+    "'"
 
   let show v = match v with
-    | AssignAux t -> "AuxAssign"
-    | ReturnAux t -> "AuxReturn"
-    | NormalVar v -> v.vname
-    | DuplicVar v -> "c2po__" ^ v.vname ^ "'"
+    | AssignAux t ->
+      "AuxAssign"
+    | ReturnAux t ->
+      "AuxReturn"
+    | NormalVar v ->
+      v.vname
+    | DuplicVar v ->
+      duplic_var_prefix ^ v.vname ^ duplic_var_postfix
 
   let get_type v = match v with
-    | AssignAux t 
+    | AssignAux t
     | ReturnAux t -> t
-    | NormalVar v 
+    | NormalVar v
     | DuplicVar v -> v.vtype
 
-  let is_assign_aux = function | AssignAux _ -> true | _ -> false
-  let is_return_aux = function | ReturnAux _ -> true | _ -> false
+  let is_assign_aux = function
+    | AssignAux _ -> true
+    | _ -> false
+
+  let is_return_aux = function
+    | ReturnAux _ -> true
+    | _ -> false
 
   let varinfo_attributes v =
     let open RichVarinfo.VarinfoDescription in
     match v with
-    | AssignAux t -> ({(empty "AuxAssign") with vtype_=Some t})
-    | ReturnAux t -> ({(empty "AuxReturn") with vtype_=Some t})
-    | NormalVar v -> from_varinfo v
-    | DuplicVar v -> ({(from_varinfo v) with vname_="c2po__" ^ string_of_int v.vid ^ "'"})
+    | AssignAux t ->
+      ({(empty "AuxAssign") with vtype_=Some t})
+    | ReturnAux t ->
+      ({(empty "AuxReturn") with vtype_=Some t})
+    | NormalVar v ->
+      from_varinfo v
+    | DuplicVar v ->
+      let vname_ = duplic_var_prefix ^ string_of_int v.vid ^ duplic_var_postfix in
+      let v' = from_varinfo v in
+      {v' with vname_ }
 
   (* Description that gets appended to the varinfo-name in user output. *)
   let describe_varinfo (var: varinfo) v =
@@ -59,12 +101,13 @@ module VarVarinfoMap = RichVarinfo.BiVarinfoMap.Make(VarType)
 module Var =
 struct
   include VarType
-  let dummy_varinfo typ: varinfo = VarVarinfoMap.to_varinfo (AssignAux typ)
-  let return_varinfo typ = VarVarinfoMap.to_varinfo (ReturnAux typ)
+  let dummy_varinfo typ: varinfo =
+    VarVarinfoMap.to_varinfo (AssignAux typ)
+  let return_varinfo typ =
+    VarVarinfoMap.to_varinfo (ReturnAux typ)
   let to_varinfo v =
     let res = VarVarinfoMap.to_varinfo v in
     if M.tracing then M.trace "c2po-varinfo" "to_varinfo: %a -> %a" d_type (get_type v) d_type res.vtype;
     res
-
 
 end
