5f9b2d7aa7211934c695f0e28c9e25a156762127
Julian Erhard <julian.erhard@tum.de>
2025-02-17 17:29:05 +0100

    C2PO: Fix test case to incur no warnings, besides for __goblint_check.

--- tests/regression/84-c2po/28-return-value.c
+++ tests/regression/84-c2po/28-return-value.c
@@ -1,16 +1,35 @@
 // PARAM: --set ana.activated[+] c2po --set ana.activated[+] startState --set ana.activated[+] taintPartialContexts --set ana.c2po.askbase false
-int a, b, c;
-void *d(const *e) { return e + 200; }
-int *f() {}
-main() {
-  g(a, c, b);
+int a;
+
+struct h {
+  int c;
+};
+
+struct i {
+  int d;
+};
+
+struct h* b;
+struct i* c;
+
+const void *d(const int* e) {
+  return e + 200;
+}
+
+int *f() {
+  return (int*) 0;
+}
+
+int g(int, struct h *, struct i *) {
+  int *j = f();
+  d(j);
+  __goblint_check(1); // reachable
+}
+
+int main() {
+  g(a, b, c);
   if (0) {
     __goblint_check(0); // NOWARN (unreachable)
   }
-  __goblint_check(1); // reachable
-}
-g(int, struct h *, struct i *) {
-  int *j = f();
-  d(j);
   __goblint_check(1); // reachable
 }
