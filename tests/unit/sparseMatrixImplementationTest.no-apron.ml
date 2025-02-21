a8e1987bcf5f1558adc6231059e4afbae75bb9d1
Kevin Adameit <kevin.adameit@icloud.com>
2025-02-21 15:45:45 +0100

    Update dead link to dune alternative dependencies

--- tests/unit/sparseMatrixImplementationTest.no-apron.ml
+++ tests/unit/sparseMatrixImplementationTest.no-apron.ml
@@ -1,9 +1,9 @@
 open OUnit2
 
 (* Note: 
-  There are currently no tests independent of apron.
-  The .apron tests use apron's Mpqf module as an implementation of RatOps.
-  There are currently no other RatOps implementations, so this file is empty.
+   There are currently no tests independent of apron.
+   The .apron tests use apron's Mpqf module as an implementation of RatOps.
+   There are currently no other RatOps implementations, so this file is empty.
 *)
 let test () =
   "SparseMatrixImplementationTest-No-Apron"
