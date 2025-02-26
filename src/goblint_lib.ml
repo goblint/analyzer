463f3600d42c9485f4706e4c41d0d99cabcdfd0e
Kevin Adameit <82610882+GollokG@users.noreply.github.com>
2025-02-26 11:45:32 +0100

    Fix docs/api-build CI of AffineEqualityDenseDomain
    Co-authored-by: Simmo Saan <simmo.saan@gmail.com>

--- src/goblint_lib.ml
+++ src/goblint_lib.ml
@@ -260,7 +260,7 @@ module ValueDomainQueries = ValueDomainQueries
 module RelationDomain = RelationDomain
 module ApronDomain = ApronDomain
 module AffineEqualityDomain = AffineEqualityDomain
-module AffineEqualityDomainDense = AffineEqualityDenseDomain
+module AffineEqualityDenseDomain = AffineEqualityDenseDomain
 module LinearTwoVarEqualityDomain = LinearTwoVarEqualityDomain
 
 (** {3 Concurrency} *)
