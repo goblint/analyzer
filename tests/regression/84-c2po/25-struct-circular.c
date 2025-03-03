2c2168744bc6288513721363138eaf269f50bb54
Julian Erhard <julian.erhard@tum.de>
2025-03-03 16:45:30 +0100

    Add missing include goblint.h to regression tests.

--- tests/regression/84-c2po/25-struct-circular.c
+++ tests/regression/84-c2po/25-struct-circular.c
@@ -1,6 +1,7 @@
 // PARAM: --set ana.activated[+] c2po --set ana.activated[+] startState --set ana.activated[+] taintPartialContexts --set ana.c2po.askbase false
 
 #include <stdlib.h>
+#include <goblint.h>
 
 struct mem {
     int val;
