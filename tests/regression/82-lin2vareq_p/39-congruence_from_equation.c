937c0fc6501c46e5285e7639c3ae26dd6bb1db3f
Manuel Pietsch <manuelpietsch@outlook.de>
2025-03-20 14:33:35 +0100

    fix accidentialy duplicated regression test

--- tests/regression/82-lin2vareq_p/39-congruence_from_equation.c
+++ tests/regression/82-lin2vareq_p/39-congruence_from_equation.c
@@ -2,15 +2,9 @@
 
 int main() {
     int x, y, z;
-    x = 3 * y + 1; // a
-    z = 5 * x + 7; // b
-    if (y < 14)
-    {
-        __goblint_check( x <= 42);
-        __goblint_check(y < 14); // A
-        __goblint_check(z != 500);  // B
-        __goblint_check(z != 14);  // Because of eqution for z
-        __goblint_check(z != 17);  // Because of combination of equation for z and x 
+    if (4 * x == 3 * y + 1) {
+        __goblint_check( y % 4 == 1);
+        __goblint_check( x % 3 == 1);
 
     }
 }
