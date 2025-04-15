1452a934a280932c31969c0f49c028381ef27658
Simmo Saan <simmo.saan@gmail.com>
2025-04-15 21:38:06 +0300

    Add test to reveal def_exc unsoundness for __int128

---
+++ tests/regression/01-cpa/60-def_exc-int128.c
@@ -0,0 +1,12 @@
+#include <goblint.h>
+#include <limits.h>
+
+// There are no 128bit integer literals...
+__int128 int128max = ((__int128) LLONG_MAX) << 64 | ULLONG_MAX;
+
+int main() {
+  __int128 x, y, z;
+  z = x + y;
+  __goblint_check(z < int128max); // TODO UNKNOWN!
+  return 0;
+}
