4b16aad01072e9dc16d56dadb5e02836ed678edf
Ali Rasim Kocal <ali.rasim.kocal@tum.de>
2025-05-22 10:58:01 +0200

    Whitespace: To resolve 'ambiguous documentation comment'

--- src/util/parallel/data.mli
+++ src/util/parallel/data.mli
@@ -25,9 +25,12 @@ sig
   val to_hashtbl : t -> D.t HM.t
 
   val find_option : t -> key -> value option
+
   (** [find t k] returns the value associated with [k] in [t]. If [k] is not present, it raises Not_found. Use this method if you expect the value to be present in the hashmap *)
   val find : t -> key -> value
+
   val mem : t -> key -> bool
+
   (** [find_create t k] returns the value associated with [k] in [t]. If [k] is not present, it creates a new value using the default factory and adds it to [t]. It returns the new value and a boolean indicating whether it was created. *)
   val find_create : t -> key -> value * bool
 end
