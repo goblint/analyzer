92f5f00ac78edb87db96511c26b703ad25c42c46
Dr. Michael Petter <petter@in.tum.de>
2025-05-23 09:47:18 +0200

    focus on reduce_col - documented matrix version and streamlined list version

--- src/cdomains/affineEquality/arrayImplementation/arrayMatrix.ml
+++ src/cdomains/affineEquality/arrayImplementation/arrayMatrix.ml
@@ -137,18 +137,18 @@ module ArrayMatrix: ArrayMatrixFunctor =
     let reduce_col_with m j =
       if not @@ is_empty m then
         (let r = ref (-1) in
-         for i' = 0 to num_rows m - 1 do
-           let rev_i' = num_rows m - i' - 1 in
-           if !r < 0 && m.(rev_i').(j) <>: A.zero then r := rev_i';
-           if !r <> rev_i' then
-             let g = m.(rev_i').(j) in
+         for i' = 0 to num_rows m - 1 do                            (* i' runs through all row indices *)
+           let rev_i' = num_rows m - i' - 1 in                      (* rev_i' runs through all row indices in reverse order *)
+           if !r < 0 && m.(rev_i').(j) <>: A.zero then r := rev_i'; (* if we found the j-column (non-zero element), store its row index in r *)
+           if !r <> rev_i' then                                     (* exclude the non-zero element's row from the transformation *)
+             let g = m.(rev_i').(j) in                              (* only act if rev_i'/j element is non-zero*)
              if g <>: A.zero then
-               let s = g /: m.(!r).(j) in
-               for j' = 0 to num_cols m - 1 do
+               let s = g /: m.(!r).(j) in                           (* determine the kill factor*)
+               for j' = 0 to num_cols m - 1 do                      (* kill the rev_i' element, eintrailing a modification of the rest columns*)
                  m.(rev_i').(j') <- m.(rev_i').(j') -: s *: m.(!r).(j')
                done
          done;
-         if !r >= 0 then Array.fill m.(!r) 0 (num_cols m) A.zero)
+         if !r >= 0 then Array.fill m.(!r) 0 (num_cols m) A.zero)   (* kill row r *)
 
     let reduce_col_with m j  = timing_wrap "reduce_col_with" (reduce_col_with m) j
     let reduce_col m j =
