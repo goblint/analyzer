5f415cf10c96416fc3372b04d3d9e20fa03ad37d
Philip Feniuk <p.feniuk@tum.de>
2025-05-28 15:59:22 +0200

    i broke everything on purpose

--- src/cdomains/apron/pentagonDomain.apron.ml
+++ src/cdomains/apron/pentagonDomain.apron.ml
@@ -8,207 +8,7 @@ open GoblintCil
 open Pretty
 module M = Messages
 open GobApron
-open IntDomain0
 
-module Inequalities = struct
-  module Idx = Int
-  module VarMap = BatMap.Make(Idx)
-  module VarSet = BatSet.Make(Idx)
-
-  type t =  (VarSet.t VarMap.t) [@@deriving eq, ord]
-  let hash : (t -> int)  = fun _ -> failwith "TODO"
-  let copy (x: t) = x
-  let empty () = (VarMap.empty: t)
-  let is_empty ineq  = VarMap.is_empty ineq
-
-  (** See https://antoinemine.github.io/Apron/doc/api/ocaml/Dim.html for the semantic of Dim.change *)
-  let dim_add (dim_change: Apron.Dim.change) sub_map =
-    let dim_array, intdim, realdim = dim_change.dim, dim_change.intdim, dim_change.realdim in
-    failwith "dim_add needs implementing!"
-
-
-  let dim_remove : (Apron.Dim.change -> t -> del:bool-> t)  = fun _ -> failwith "dim_remove needs implementing"
-
-  let to_string (t:t) = 
-    (* { y1, y2, ..., yn }*)
-    let set_string set = "{" ^ (VarSet.to_list set |> List.map (Int.to_string) |> String.concat ",") ^ "}" in
-    (* x_1 -> {y1, y2, ..., yn} *)
-    let relations_string = Seq.fold_left (fun acc (x, six) -> (Int.to_string x) ^ " -> " ^ (set_string six) ^ "\n") "" (VarMap.to_seq t) in
-    (* {
-        x_1 -> {y1, y2, ..., yn}
-        }
-    *)
-    "{\n" ^ relations_string ^ "}\n"
-end
-
-
-module VariableManagement =
-struct
-  include SharedFunctions.VarManagementOps (Inequalities)
-
-  let get_map_opt t = t.d
-
-  let get_env t = t.env
-
-end
-
-module SUB =
-struct
-  module VarMan = VariableManagement
-  module VarMap = Inequalities.VarMap
-  module VarSet = Inequalities.VarSet
-
-  type t = VarMan.t
-
-  let unify_from_env (sub1:t) (sub2:t) lce = 
-    match sub1.d, sub2.d with
-    | Some(sub_map_1), Some(sub_map_2) -> ( 
-        let ext_sub1 = Inequalities.dim_add (Environment.dimchange sub1.env lce) sub_map_1 in
-        let ext_sub2 = Inequalities.dim_add (Environment.dimchange sub2.env lce) sub_map_2 in
-        (ext_sub1, ext_sub2)
-      )
-    | _ -> failwith "There is no need to unify the environments here"
-
-
-
-  let equal (sub1:t) (sub2:t) = 
-    match sub1.d, sub2.d with
-    | None, None -> true
-    | Some(sub_map_1), Some(sub_map_2) -> (
-        VarMap.equal (VarSet.equal) sub_map_1 sub_map_2
-      )
-    | _ -> false
-
-  let bot_of env = ({ d = None; env = env}: t)
-
-  let bot () = ({ d = None; env= VarMan.empty_env } :t )
-
-  (** 
-      When possible, we use `d = None` to signal a bot value.
-      However, we cannot be sure if a value differs from bot, when
-      `d != None`, as there might still be contradictions in our map,
-      which indicates a bot value. This search is expensive and
-      we try to shortcut it as best as possible.
-  *)
-  let is_bot (sub:t) = 
-    match sub.d with
-    | None -> true
-    | Some(sub_map) ->
-      VarMap.exists (
-        fun k1 v ->
-          VarSet.mem k1 v 
-          ||
-          (** 
-             TODO: Implement further contradiction search -- transitive closure in the worst-case.
-          *)
-          VarSet.exists (
-            fun k2 ->
-              VarSet.exists (fun k -> k == k1) (VarMap.find_default VarSet.empty k2 sub_map)
-          ) v
-      ) sub_map 
-
-  (** The environment (env) manages the number of variables stored in our datatype and 
-      therefore the dimensions of our value space. The inequalities should
-      return empty on variables not found in the underlying map.*)
-  let top_of env = ({ d = Some(Inequalities.empty ()); env = env }: t)
-
-  (** This is the top value for the null-space i.e. the space with dimension 0 or no stored variables.
-      In the case where there are no variables, it holds that bot == top. *)
-  let top () = ({ d = Some(Inequalities.empty ()); env = VarMan.empty_env }: t)
-  let is_top (sub: t) = 
-    match VarMan.get_map_opt sub with
-    | None -> false
-    | Some(sub_map) -> VarMap.for_all (fun _ set -> VarSet.is_empty set) sub_map 
-
-  let subseteq set1 set2 = VarSet.subset set1 set2 || VarSet.equal set1 set2 (** helper, missing in batteries *)
-
-  (**
-     The inequalities map s1 is less than or equal to s2 iff
-      forall x in s2.
-      s2(x) subseteq s1(x)
-  *)
-  let leq (sub1: t) (sub2: t) =
-    match sub1.d, sub2.d with
-    | None, _ -> true
-    | _, None -> false
-    | Some(sub_map_1), Some(sub_map_2) ->
-      let lce = Environment.lce sub1.env sub2.env in
-      let sub_map_1, sub_map_2 = unify_from_env sub1 sub2 lce in
-      VarMap.for_all (
-        fun x s2x -> 
-          let s1x = VarMap.find x sub_map_1 in
-          subseteq s1x s2x
-      ) sub_map_2
-
-  let join (sub1: t) (sub2: t) =
-    let lce = Environment.lce sub1.env sub2.env in
-    match sub1.d, sub2.d with
-    | None, None -> bot ()
-    | None, _ -> sub2
-    | _, None -> sub1
-    | Some(sub_map_1), Some(sub_map_2) when is_top sub1 || is_top sub2 ->
-      top_of lce
-    | Some(sub_map_1), Some(sub_map_2) ->
-      (** Make sure that the maps contain keys for all variables before comparing. *)
-      let sub_map_1, sub_map_2 = unify_from_env sub1 sub2 lce in
-      let intersect_sets_of_key var_key var_set1_opt var_set2_opt =
-        match var_set1_opt, var_set2_opt with
-        | Some(var_set1), Some(var_set2) -> Some(VarSet.inter var_set1 var_set2)
-        | None, None -> failwith "This should never happen :)"
-        | _ -> failwith "unify_from_env should take care of that :)"
-      in
-      { d = Some(VarMap.merge (intersect_sets_of_key) sub_map_1 sub_map_2); env = lce }
-
-  let meet (sub1: t) (sub2: t) =
-    let lce = Environment.lce sub1.env sub2.env in
-    match sub1.d, sub2.d with
-    | Some(sub_map_1), Some(sub_map_2) -> (
-        (** Make sure that the maps contain keys for all variables before comparing. *)
-        let sub_map_1, sub_map_2 = unify_from_env sub1 sub2 lce in
-        let union_sets_of_key var_key var_set1_opt var_set2_opt =
-          match var_set1_opt, var_set2_opt with
-          | Some(var_set1), Some(var_set2) -> Some(VarSet.union var_set1 var_set2)
-          | None, None -> failwith "This should never happen :)"
-          | _ -> failwith "unify_from_env should take care of that :)"
-        in
-        ({ d = Some(VarMap.merge union_sets_of_key sub_map_1 sub_map_2); env=lce }: t)
-      )
-    | _ -> bot_of lce
-
-  let widen (sub1: t) (sub2: t) =
-    let lce = Environment.lce sub1.env sub2.env in
-    match sub1.d, sub2.d with
-    | Some(sub_map_1), Some(sub_map_2) -> (
-        (** Make sure that the maps contain keys for all variables before comparing. *)
-        let sub_map_1, sub_map_2 = unify_from_env sub1 sub2 lce in
-        let widen_sets_of_key var_key var_set1_opt var_set2_opt =
-          match var_set1_opt, var_set2_opt with
-          | Some(var_set1), Some(var_set2) ->
-            if subseteq var_set1 var_set2 then Some (var_set2) else Some (VarSet.empty)
-          | None, None -> failwith "This should never happen :)"
-          | _ -> failwith "unify_from_env should take care of that :)"
-        in
-        ({ d = Some(VarMap.merge widen_sets_of_key sub_map_1 sub_map_2); env = lce }:t)
-      )
-    | _ -> top_of lce (** Naively extrapolate to top. We are unsure if this is intented by the papers definitions. *)
-
-  (** No narrowing mentioned in the paper. *)
-  let narrow sub1 sub2 = meet sub1 sub2
-
-
-  let to_string (sub: t) = 
-    if is_bot sub then
-      "bot"
-    else if is_top sub then
-      "top"
-    else
-      match sub.d with
-      | None -> failwith "is_bot should take care of that :)"
-      | Some(sub_map) -> (
-          Inequalities.to_string sub_map
-        )
-
-end
 
 module INTERVALS  = 
 struct
@@ -216,7 +16,6 @@ struct
 
   type interval = Z.t * Z.t
   type t = interval VarMap.t
-  module IArith = IntervalArith(IntOps.BigIntOps)
 
   let equal intv1 intv2 =
     let tuple_equal (a1, b1) (a2, b2) = Z.equal a1 a2 && Z.equal b1 b2 in
@@ -299,6 +98,175 @@ struct
   let sup (x: interval) = snd x
 
   let inf (x: interval) = fst x
+
+  let dim_add _ _ = failwith "TODO"
+
+  let dim_remove _ _ = failwith "TODO"
+end
+
+module SUB =
+struct
+  module Idx = Int
+  module VarMap = BatMap.Make(Idx)
+  module VarSet = BatSet.Make(Idx)
+
+  (*
+  TODO: 
+    Moved from Inequalities
+      let to_string (t:t) = 
+    (* { y1, y2, ..., yn }*)
+    let set_string set = "{" ^ (VarSet.to_list set |> List.map (Int.to_string) |> String.concat ",") ^ "}" in
+    (* x_1 -> {y1, y2, ..., yn} *)
+    let relations_string = Seq.fold_left (fun acc (x, six) -> (Int.to_string x) ^ " -> " ^ (set_string six) ^ "\n") "" (VarMap.to_seq t) in
+    (* {
+        x_1 -> {y1, y2, ..., yn}
+        }
+    *)
+    "{\n" ^ relations_string ^ "}\n"
+  *)
+
+  let dim_add _ _ = failwith "TODO"
+
+  let dim_remove _ _ = failwith "TODO"
+
+
+  type t = (VarSet.t VarMap.t) [@@deriving eq, ord]
+
+  let equal (sub1:t) (sub2:t) = 
+    match sub1.d, sub2.d with
+    | None, None -> true
+    | Some(sub_map_1), Some(sub_map_2) -> (
+        VarMap.equal (VarSet.equal) sub_map_1 sub_map_2
+      )
+    | _ -> false
+
+  let bot_of env = ({ d = None; env = env}: t)
+
+  let bot () = ({ d = None; env= VarMan.empty_env } :t )
+
+  (** 
+      When possible, we use `d = None` to signal a bot value.
+      However, we cannot be sure if a value differs from bot, when
+      `d != None`, as there might still be contradictions in our map,
+      which indicates a bot value. This search is expensive and
+      we try to shortcut it as best as possible.
+  *)
+  let is_bot (sub:t) = 
+    match sub.d with
+    | None -> true
+    | Some(sub_map) ->
+      VarMap.exists (
+        fun k1 v ->
+          VarSet.mem k1 v 
+          ||
+          (** 
+             TODO: Implement further contradiction search -- transitive closure in the worst-case.
+          *)
+          VarSet.exists (
+            fun k2 ->
+              VarSet.exists (fun k -> k == k1) (VarMap.find_default VarSet.empty k2 sub_map)
+          ) v
+      ) sub_map 
+
+  (** The environment (env) manages the number of variables stored in our datatype and 
+      therefore the dimensions of our value space. The inequalities should
+      return empty on variables not found in the underlying map.*)
+  let top_of env = ({ d = Some(Inequalities.empty ()); env = env }: t)
+
+  (** This is the top value for the null-space i.e. the space with dimension 0 or no stored variables.
+      In the case where there are no variables, it holds that bot == top. *)
+  let top () = ({ d = Some(Inequalities.empty ()); env = VarMan.empty_env }: t)
+  let is_top (sub: t) = 
+    match VarMan.get_map_opt sub with
+    | None -> false
+    | Some(sub_map) -> VarMap.for_all (fun _ set -> VarSet.is_empty set) sub_map 
+
+  let subseteq set1 set2 = VarSet.subset set1 set2 || VarSet.equal set1 set2 (** helper, missing in batteries *)
+
+  (**
+     The inequalities map s1 is less than or equal to s2 iff
+      forall x in s2.
+      s2(x) subseteq s1(x)
+  *)
+  let leq (sub1: t) (sub2: t) =
+    match sub1.d, sub2.d with
+    | None, _ -> true
+    | _, None -> false
+    | Some(sub_map_1), Some(sub_map_2) ->
+      let lce = Environment.lce sub1.env sub2.env in
+      let sub_map_1, sub_map_2 = unify_from_env sub1 sub2 lce in
+      VarMap.for_all (
+        fun x s2x -> 
+          let s1x = VarMap.find x sub_map_1 in
+          subseteq s1x s2x
+      ) sub_map_2
+
+  let join (sub1: t) (sub2: t) =
+    match sub1.d, sub2.d with
+    | None, None -> bot ()
+    | None, _ -> sub2
+    | _, None -> sub1
+    | Some(sub_map_1), Some(sub_map_2) when is_top sub1 || is_top sub2 ->
+      top_of lce
+    | Some(sub_map_1), Some(sub_map_2) ->
+      (** Make sure that the maps contain keys for all variables before comparing. *)
+      let intersect_sets_of_key var_key var_set1_opt var_set2_opt =
+        match var_set1_opt, var_set2_opt with
+        | Some(var_set1), Some(var_set2) -> Some(VarSet.inter var_set1 var_set2)
+        | None, None -> failwith "This should never happen :)"
+        | _ -> failwith "unify_from_env should take care of that :)"
+      in
+      { d = Some(VarMap.merge (intersect_sets_of_key) sub_map_1 sub_map_2); env = lce }
+
+  let meet (sub1: t) (sub2: t) =
+    let lce = Environment.lce sub1.env sub2.env in
+    match sub1.d, sub2.d with
+    | Some(sub_map_1), Some(sub_map_2) -> (
+        (** Make sure that the maps contain keys for all variables before comparing. *)
+        let sub_map_1, sub_map_2 = unify_from_env sub1 sub2 lce in
+        let union_sets_of_key var_key var_set1_opt var_set2_opt =
+          match var_set1_opt, var_set2_opt with
+          | Some(var_set1), Some(var_set2) -> Some(VarSet.union var_set1 var_set2)
+          | None, None -> failwith "This should never happen :)"
+          | _ -> failwith "unify_from_env should take care of that :)"
+        in
+        ({ d = Some(VarMap.merge union_sets_of_key sub_map_1 sub_map_2); env=lce }: t)
+      )
+    | _ -> bot_of lce
+
+  let widen (sub1: t) (sub2: t) =
+    let lce = Environment.lce sub1.env sub2.env in
+    match sub1.d, sub2.d with
+    | Some(sub_map_1), Some(sub_map_2) -> (
+        (** Make sure that the maps contain keys for all variables before comparing. *)
+        let sub_map_1, sub_map_2 = unify_from_env sub1 sub2 lce in
+        let widen_sets_of_key var_key var_set1_opt var_set2_opt =
+          match var_set1_opt, var_set2_opt with
+          | Some(var_set1), Some(var_set2) ->
+            if subseteq var_set1 var_set2 then Some (var_set2) else Some (VarSet.empty)
+          | None, None -> failwith "This should never happen :)"
+          | _ -> failwith "unify_from_env should take care of that :)"
+        in
+        ({ d = Some(VarMap.merge widen_sets_of_key sub_map_1 sub_map_2); env = lce }:t)
+      )
+    | _ -> top_of lce (** Naively extrapolate to top. We are unsure if this is intented by the papers definitions. *)
+
+  (** No narrowing mentioned in the paper. *)
+  let narrow sub1 sub2 = meet sub1 sub2
+
+
+  let to_string (sub: t) = 
+    if is_bot sub then
+      "bot"
+    else if is_top sub then
+      "top"
+    else
+      match sub.d with
+      | None -> failwith "is_bot should take care of that :)"
+      | Some(sub_map) -> (
+          Inequalities.to_string sub_map
+        )
+
 end
 
 module type Tracked =
@@ -307,10 +275,31 @@ sig
   val varinfo_tracked: varinfo -> bool
 end
 
+module PNTG =
+struct
+  type t = { intv: INTERVALS.t; sub: SUB.t }
+
+  let hash : (t -> int)  = fun _ -> failwith "TODO"
+  let equal _ _ = failwith "TODO"
+  let compare _ _ = failwith "TODO"
+  let copy (x: t) = x
+  let empty () = failwith "TODO"
+
+  let is_empty _ = failwith "TODO"
+
+  (** See https://antoinemine.github.io/Apron/doc/api/ocaml/Dim.html for the semantic of Dim.change *)
+  let dim_add (dim_change: Apron.Dim.change) sub_map =
+    let dim_array, intdim, realdim = dim_change.dim, dim_change.intdim, dim_change.realdim in
+    failwith "dim_add needs implementing!"
+
+
+  let dim_remove : (Apron.Dim.change -> t -> del:bool-> t)  = fun _ -> failwith "dim_remove needs implementing"
+
+end
+
 module D =
 struct
-  include Printable.Std
-  module SUB = SUB
+  include SharedFunctions.VarManagementOps (PNTG)
 
   module Bounds = ExpressionBounds
   module V = RelationDomain.V
@@ -324,7 +313,6 @@ struct
   module Tracked = struct let varinfo_tracked _ = failwith "TODO";; let type_tracked _ = failwith "TODO";; end
 
   type var = V.t
-  type t = { intv: INTERVALS.t; sub: SUB.t }
   type marshal
 
   let name () = "pentagon"
@@ -338,6 +326,18 @@ struct
   let top () = { intv = INTERVALS.top (); sub = SUB.top () }
 
   let is_top t = INTERVALS.is_top t.intv && SUB.is_top t.sub
+
+
+  let unify_from_env (pntg1: t) (pntg2: t) (lce: Environment.t) =
+    let env1, env2 = pntg1.env, pntg2.env in
+    let dim_change_1, dim_change_2 = (Environment.dimchange env1 lce), (Environment.dimchange env1 lce) in
+    match pntg1.d, pntg2.d with
+    | Some(pntg1), Some(pntg2) -> (
+        let intv1, sub1 = INTERVALS.dim_add dim_change_1 pntg1.intv, SUB.dim_add dim_change_1 pntg1.sub in
+        let intv2, sub2 = INTERVALS.dim_add dim_change_1 pntg2.intv, SUB.dim_add dim_change_2 pntg2.intv in
+        ({}:t, {}: t)
+      )
+    | _ -> failwith "Do not use unify on bottom values!"
 
   let show varM = failwith "TODO"
   let pretty () (x:t) = failwith "TODO"
@@ -466,18 +466,18 @@ struct
 
   (* S2 Specific functions of RelationDomain *)
   let is_bot_env _ = failwith "TODO"
-  let vars _ = failwith "TODO"
-  let add_vars _ = failwith "TODO"
-  let remove_vars _ = failwith "TODO"
+  let vars _ = failwith "SF"
+  let add_vars _ = failwith "SF"
+  let remove_vars _ = failwith "SF"
 
-  let remove_vars_with _ = failwith "TODO"
+  let remove_vars_with _ = failwith "SF"
 
-  let remove_filter _ = failwith "TODO"
-  let remove_filter_with _ = failwith "TODO"
+  let remove_filter _ = failwith "SF"
+  let remove_filter_with _ = failwith "SF"
 
-  let copy _ = failwith "TODO"
-  let keep_vars _ = failwith "TODO"
-  let keep_filter _ = failwith "TODO"
+  let copy _ = failwith "SF"
+  let keep_vars _ = failwith "SF"
+  let keep_filter _ = failwith "SF"
   let forget_vars _ = failwith "TODO"
 
   let assign_exp _ = failwith "TODO"
@@ -487,18 +487,17 @@ struct
 
   let assign_var_parallel' _ = failwith "TODO"
   let substitute_exp _ = failwith "TODO"
-  let unify _ = failwith "TODO"
-  let marshal _ = failwith "TODO"
-  let unmarshal _ = failwith "TODO"
-  let mem_var _ = failwith "TODO"
-  let assert_inv _ = failwith "TODO"
-  let elet_int _ = failwith "TODO"
-  let cil_exp_of_lincons1 _ = failwith "TODO"
+  let unify _ = failwith "Probably meet"
+  let marshal _ = failwith "identity function in other domains"
+  let unmarshal _ = failwith "identity function in other domains"
+  let mem_var _ = failwith "SF"
+  let assert_inv _ = failwith "SF but we most likely need assert_constraint"
+  let cil_exp_of_lincons1 _ = failwith "SF"
   let invariant _ = failwith "TODO"
   let equal _ = failwith "TODO"
   let hash _ = failwith "TODO"
   let compare _ = failwith "TODO"
   let relift _ = failwith "TODO"
-  let eval_int _ = failwith "TODO"
+  let eval_int _ = failwith "SF"
 end
 
