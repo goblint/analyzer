8af1e7d9ee75db0c0cfcf4d73ce91783183447f4
Philip Feniuk <p.feniuk@tum.de>
2025-06-17 13:56:01 +0200

    Added running command to c file

--- tests/regression/86-pentagon/01-pentagon-simple.c
+++ tests/regression/86-pentagon/01-pentagon-simple.c
@@ -2,6 +2,8 @@
 
 // Adapted example from https://link.springer.com/content/pdf/10.1007/BF00268497.pdf
 
+// Run with ./goblint tests/regression/86-pentagon/01-pentagon-simple.c --conf ./conf/empty.json
+
 #include <goblint.h>
 
 void main(void) {
