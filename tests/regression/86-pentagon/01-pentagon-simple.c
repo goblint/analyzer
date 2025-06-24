7600bbf591aebb6aefb73387cd42377e3ed96c0d
Philip Feniuk <p.feniuk@tum.de>
2025-06-24 16:35:53 +0200

    Pentagon regression tests with confusion

--- tests/regression/86-pentagon/01-pentagon-simple.c
+++ tests/regression/86-pentagon/01-pentagon-simple.c
@@ -1,6 +1,4 @@
-//SKIP PARAM: --set ana.activated[+] lin2vareq --set sem.int.signed_overflow assume_none
-
-// Adapted example from https://link.springer.com/content/pdf/10.1007/BF00268497.pdf
+// PARAM: --set "ana.activated[+]" pentagon
 
 // Run with ./goblint tests/regression/86-pentagon/01-pentagon-simple.c --conf ./conf/empty.json
 
