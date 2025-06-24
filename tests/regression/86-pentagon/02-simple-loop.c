676facfa2e0255d5c0d967d138bc3647c3239524
Philip Feniuk <p.feniuk@tum.de>
2025-06-24 16:31:52 +0200

    Pentagon regression tests with confusion

---
+++ tests/regression/86-pentagon/02-simple-loop.c
@@ -0,0 +1,20 @@
+// PARAM: --set "ana.activated[+]" pentagon
+
+// Taken from tests/regression/66-interval-set-one/73-intervals.c
+#include <goblint.h>
+
+int main(void) {
+    int x = 0;
+
+    //while(x < 42 || x > 42) {
+    //    x++;
+    //    __goblint_check(x >= 1); // SUCC
+    //}
+
+
+    while(x != 42) {
+        x++;
+        __goblint_check(x >= 1); // UNKNOWN WHY?
+    }
+
+}
