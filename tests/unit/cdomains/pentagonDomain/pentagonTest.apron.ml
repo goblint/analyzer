4bd9a24e305b313bc214f7b82f7ba3d9e8d7d9a2
Philip Feniuk <p.feniuk@tum.de>
2025-06-26 18:07:08 +0200

    Unit test for string

--- tests/unit/cdomains/pentagonDomain/pentagonTest.apron.ml
+++ tests/unit/cdomains/pentagonDomain/pentagonTest.apron.ml
@@ -6,23 +6,23 @@ open PentagonDomain
 open Intv
 
 let stringing _ =
-  let res = "{ boxes = ( ) { 0->[1, 1]; 1->[2, 2]; 2->[-I32, +I32] }; sub = (⊤) { 0->∅; 1->{0#, 2#}; 2->{0#} } }" in
+  let s = "{ boxes = ( ) { 0->[1, 1]; 1->[2, 2]; 2->[-I32, +I32] }; sub = (⊤) { 0->∅; 1->{0#, 2#}; 2->{0#} } }" in
+  let expected = "{ boxes = ( ) { one->[1, 1]; two->[2, 2]; three->[-I32, +I32] }; sub = (⊤) { one->∅; two->{one, three}; three->{one} } }" in
   let vars = [|"one"; "two"; "three" |] in
   (* First pass to adjust boxes. *)
   let boxes_re = Str.regexp {|\([0-9]+\)->|} in
   let subs_re = Str.regexp {|\([0-9]+\)#|} in
   Str.global_substitute boxes_re (
     fun m -> 
-      let idx = int_of_string (Str.matched_group 1 res) in
+      let idx = int_of_string (Str.matched_group 1 m) in
       if idx < Array.length vars then
         (vars.(idx) ^ "->")
       else
         failwith "D.to_string hit unknown variable!"
-  ) res |>
+  ) s |>
   (* Second pass to adjust subs. *)
   Str.global_substitute subs_re (
     fun m -> 
-      Printf.printf "%s" m;
       let idx = int_of_string (Str.matched_group 1 m) in
       if idx < Array.length vars then
         vars.(idx)
