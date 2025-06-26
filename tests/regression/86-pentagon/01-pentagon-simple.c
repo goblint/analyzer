c341544136aa88f77815434e2025968786242512
Alexander Mazur <alexandermazur@MacBook-Air-von-Alexander.local>
2025-06-26 17:13:44 +0200

    simple test

--- tests/regression/86-pentagon/01-pentagon-simple.c
+++ tests/regression/86-pentagon/01-pentagon-simple.c
@@ -1,16 +1,13 @@
 // PARAM: --set "ana.activated[+]" pentagon
 
-// Run with ./goblint tests/regression/86-pentagon/01-pentagon-simple.c --conf ./conf/empty.json
+// Run with ./goblint tests/regression/86-pentagon/01-pentagon-simple.c --conf ./conf/pentagon.json
 
 #include <goblint.h>
 
 void main(void) {
-  int x;
-  
-  if (x <= 0) {
-    x = -x;
-  }
-
-__goblint_check(x >= 0);
-
+  int x = rand(), y = x, z = x;
+  y = x - 1;
+  z = y - 1;
+  z = y - 1;
+  __goblint_check(z < x);
 }
