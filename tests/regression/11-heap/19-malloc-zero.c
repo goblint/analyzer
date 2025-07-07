1014dcda13a8254dc1e01d51bc6a4a0f50b246e0
Michael Schwarz <michael.schwarz93@gmail.com>
2025-07-07 10:28:03 +0200

    11/19: Add out of bounds access
    Co-Authored-By: Simmo Saan <simmo.saan@gmail.com>

--- tests/regression/11-heap/19-malloc-zero.c
+++ tests/regression/11-heap/19-malloc-zero.c
@@ -1,4 +1,4 @@
-// PARAM: --set sem.malloc.zero either
+// PARAM: --set sem.malloc.zero either --set ana.activated[+] memOutOfBounds 
 #include<pthread.h>
 #include<goblint.h>
 
@@ -8,5 +8,7 @@ int main(void){
   if(ptr == 0) {
     // Reachable
     __goblint_check(1);
+  } else {
+    *ptr = 1; //WARN
   }
 }
