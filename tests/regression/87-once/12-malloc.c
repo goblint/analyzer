3815e2eb5c6e6306973619d509362c934ba593f6
Michael Schwarz <michael.schwarz93@gmail.com>
2025-07-09 15:25:24 +0200

    And unsoundness due to malloc non-uniqueness

---
+++ tests/regression/87-once/12-malloc.c
@@ -0,0 +1,34 @@
+// PARAM: --set ana.activated[+] pthreadOnce --enable ana.int.interval
+#include <pthread.h>
+#include <stdio.h>
+#include <goblint.h>
+#include <stdlib.h>
+
+int g;
+pthread_once_t once = PTHREAD_ONCE_INIT;
+pthread_mutex_t mtx;
+
+void increment() {
+  g++;
+}
+
+int main(void) {
+  pthread_once_t* optr5;
+  pthread_once_t* optr;
+
+  for(int i = 0; i < 10; i++) {
+    optr = malloc(sizeof(pthread_once_t));
+    *optr = PTHREAD_ONCE_INIT;
+
+    if(i == 5) {
+      optr5 = optr;
+    }
+  }
+
+  pthread_once(optr, increment);
+  pthread_once(optr5, increment);
+
+  __goblint_check(g == 2); //TODO
+
+  return 0;
+}
