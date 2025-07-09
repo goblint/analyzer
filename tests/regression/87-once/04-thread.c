a8b0d15b2d559679b29bc1def8c8b09a760bceac
Michael Schwarz <michael.schwarz93@gmail.com>
2025-07-09 13:45:26 +0200

    87/04: Clarify and mark race as actual race

--- tests/regression/87-once/04-thread.c
+++ tests/regression/87-once/04-thread.c
@@ -10,8 +10,12 @@ pthread_once_t i_once = PTHREAD_ONCE_INIT;
 pthread_mutex_t mut;
 
 void *t_other(void* arg) {
-  g = 17; //RACE
+  g = 17; //RACE!
 
+  //TODO: This mutex is needed to avoid races between the two threads
+  // As fun() is executed only once, only one thread exists in any execution, but we cannot capture this
+  // [main, t_fun@04-thread.c:50:3-50:40, t_other@04-thread.c:27:13-27:59]
+  // [main, t_other@04-thread.c:27:13-27:59]
   pthread_mutex_lock(&mut);
   i = 7; //NORACE
   pthread_mutex_unlock(&mut);
@@ -25,7 +29,7 @@ void fun() {
   // Even though this is only called inside the once, the accesses in the new thread and the accesses here can happen in parallel
   // Checks that active is not passed to the created thread, but seen is passed
   pthread_t tid = pthread_create(&tid, NULL, t_other, NULL);
-  g = 42; //RACE
+  g = 42; //RACE!
 
   h = 8; //NORACE
   // Active onces get passed to the callee and back to the caller
