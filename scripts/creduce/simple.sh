1a8d3ce343e4a9ac945b556db7af806a7180467b
Philip Feniuk <p.feniuk@tum.de>
2025-07-22 16:57:39 +0200

    Add gcc to creduce script

--- scripts/creduce/simple.sh
+++ scripts/creduce/simple.sh
@@ -6,10 +6,12 @@
 
 GOBLINTDIR="/home/feniuk/University-Coding/analyzer-pentagon/goblint"
 INPUTFILE="/home/feniuk/University-Coding/bench/coreutils/cut_comb.minimized.c"
+OUTPUTFILE="fe8f7af3a.out"
 PARAMS="--set ana.activated[+] pentagon --enable allglobs --enable dbg.timing.enabled"
 
 $GOBLINTDIR $INPUTFILE $PARAMS -v &> out.txt;
 if [ $? -eq 2 ]; then
+    gcc $INPUTFILE -o $OUTPUTFILE  && rm $OUTPUTFILE &> /dev/null &&
     grep "Fatal error: exception IntDomain0.ArithmeticOnIntegerBot(\"Error int op 8\")" "out.txt" >/dev/null 2>&1
 else
     exit 5
