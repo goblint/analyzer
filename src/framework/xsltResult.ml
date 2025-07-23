bedfa49647d3a51741ba2f48b7272d6bbe484dcd
Simmo Saan <simmo.saan@gmail.com>
2025-07-23 20:53:30 +0300

    Extract cfg_task in direct g2html output

--- src/framework/xsltResult.ml
+++ src/framework/xsltResult.ml
@@ -224,6 +224,17 @@ struct
     close_out out;
     fname
 
+  let cfg_task fname fd loc: ProcessPool.task =
+    let base_dir2 = GobSys.mkdir_or_exists_absolute Fpath.(v "result2" / "cfgs") in
+    let c_file_name = Str.global_substitute (Str.regexp Filename.dir_sep) (fun _ -> "%2F") loc.file in
+    let svg_file_name = fd.svar.vname^".svg" in
+    let file_dir2 = GobSys.mkdir_or_exists_absolute Fpath.(base_dir2 / c_file_name) in
+    let fname2 = Fpath.(file_dir2 / svg_file_name) in
+    {
+      command = Filename.quote_command "dot" [Fpath.to_string fname; "-Tsvg"; "-o"; Fpath.to_string fname2];
+      cwd = None;
+    }
+
   let output table live gtable gtfxml (module FileCfg: MyCFG.FileCfg) =
     let file = FileCfg.file in
     match get_string "result" with
@@ -289,16 +300,7 @@ struct
           | GFun (fd,loc) ->
             (* ignore (Printf.printf "fun: %s\n" fd.svar.vname); *)
             let fname = write_dot (module FileCfg) ~live fd loc in
-            let base_dir2 = GobSys.mkdir_or_exists_absolute Fpath.(v "result2" / "cfgs") in
-            let c_file_name = Str.global_substitute (Str.regexp Filename.dir_sep) (fun _ -> "%2F") loc.file in
-            let svg_file_name = fd.svar.vname^".svg" in
-            let file_dir2 = GobSys.mkdir_or_exists_absolute Fpath.(base_dir2 / c_file_name) in
-            let fname2 = Fpath.(file_dir2 / svg_file_name) in
-            let task: ProcessPool.task = {
-              command = Filename.quote_command "dot" [Fpath.to_string fname; "-Tsvg"; "-o"; Fpath.to_string fname2];
-              cwd = None;
-            } in
-            task :: acc
+            cfg_task fname fd loc :: acc
           | _ -> acc
         ) []
       in
