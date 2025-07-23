f43c45811d2892470082cc771514a9725207409d
Alexander Mazur <alexandermazur@w192-5t-v4.eduroam.dynamic.rbg.tum.de>
2025-07-23 16:04:21 +0200

    Adapted regression test because of added functionality

--- tests/regression/88-pentagon/06-subs.c
+++ tests/regression/88-pentagon/06-subs.c
@@ -8,8 +8,11 @@ int main() {
   __goblint_check(y < x);     // SUCCESS
   __goblint_check(x - y > 0); // SUCCESS
 
-  z = x - y;
-  __goblint_check(z > 0);     // UNKNOWN (different from pentagon paper, section subtraction)
+  __goblint_assume(x <= 1000);
+  __goblint_assume(y <= 10000);
+  z = x - y + 4;
+  __goblint_check(z > 0);     // SUCCESS
+  __goblint_check(z > 4);     // SUCCESS
 
   x = rand(), y = x, z = x;
   __goblint_assume(x <= 11);
@@ -18,7 +21,7 @@ int main() {
   __goblint_assume(z <= 10);
 
   if (9*x + 10*y > 20*z - 1) {
-    __goblint_check(x >= 10); // UNKNOWN (we might want to implement this to be more precise)
+    __goblint_check(x >= 10); // UNKNOWN (we might want to implement this to be more precise, or just use the interval domain)
     __goblint_check(y == 1);  // UNKNOWN
     __goblint_check(x >= 2);  // SUCCESS
   }
