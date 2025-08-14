133b6e515d06d3b16644943b705e2bc3e56b0f78
Simmo Saan <simmo.saan@gmail.com>
2025-08-14 12:41:09 +0300

    Add cram test 39-signed-overflows/15-div-minus-1

--- tests/regression/39-signed-overflows/15-div-minus-1.c
+++ tests/regression/39-signed-overflows/15-div-minus-1.c
@@ -2,10 +2,11 @@
 #include <limits.h>
 
 int main() {
-    int bad = INT_MIN / -1; // WARN
+    int bad = INT_MIN / -1; // WARN (overflow)
     int x, y;
+    bad = x / y; // WARN (div by zero and overflow, distinguished in cram test)
     if (y != 0) {
-        bad = x / y; // WARN
+        bad = x / y; // WARN (overflow)
     }
     return 0;
 }
