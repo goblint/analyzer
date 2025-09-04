ece91c30005af28704ad8f41902353745b8d53dd
Simmo Saan <simmo.saan@gmail.com>
2025-09-04 13:39:41 +0300

    Convert remaining input YAML witnesses to invariant_set

--- tests/regression/56-witness/23-base-unassume-priv2.c
+++ tests/regression/56-witness/23-base-unassume-priv2.c
@@ -1,4 +1,4 @@
-// PARAM: --enable ana.int.interval --set witness.yaml.entry-types[*] location_invariant --set ana.activated[+] unassume --set witness.yaml.unassume 23-base-unassume-priv2.yml --set solvers.td3.side_widen always --enable ana.widen.tokens
+// PARAM: --enable ana.int.interval --set ana.activated[+] unassume --set witness.yaml.unassume 23-base-unassume-priv2.yml --set solvers.td3.side_widen always --enable ana.widen.tokens
 #include <pthread.h>
 #include <assert.h>
 
