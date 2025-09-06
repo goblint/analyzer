8691919e8149a6111e0982f2b5907ef122bd466d
Simmo Saan <simmo.saan@gmail.com>
2025-09-06 13:15:26 +0300

    Document Json_repr_compat

--- src/vendor/json-data-encoding/json_repr_compat.ml
+++ src/vendor/json-data-encoding/json_repr_compat.ml
@@ -1,5 +1,13 @@
+(* Copied literally from json-data-encoding's Json_repr.Yojson. *)
 module Yojson = struct
-  type value = Yojson.Safe.t
+  type value = Yojson.Safe.t (* Only this type is modified to refer to current version of Yojson. *)
+
+  (* The following functions need to be copied from Json_repr.Yojson.
+     The versions in Json_repr cannot be called because its interface has already fixed the value type.
+
+     However, we need them in their fully polymorphic form.
+     This allows the same implementations to be reused for Yojson 2 and 3 without conditional compilation.
+     With Yojson 3, the `Tuple and `Variant cases are simply never used. *)
 
   let view = function
     | `Intlit i -> `String i
--- src/vendor/json-data-encoding/json_repr_compat.mli
+++ src/vendor/json-data-encoding/json_repr_compat.mli
@@ -1,1 +1,5 @@
+(** {!Json_repr} compatibility. *)
+
 module Yojson : Json_repr.Repr with type value = Yojson.Safe.t
+(** A view over the {!Yojson.Safe.t} representation.
+    Unlike {!Json_repr.Yojson}, this directly uses {!Yojson.Safe.t} and, thus, is compatible both with Yojson 2 and 3. *)
