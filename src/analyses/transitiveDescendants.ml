163f8713b3aea5df3c0c687fae557801347b83f5
dabund24 <dev@dabund24.de>
2025-11-07 16:17:02 +0100

    initial version of transitive descendants analysis

---
+++ src/analyses/transitiveDescendants.ml
@@ -0,0 +1,39 @@
+open Analyses
+module TID = ThreadIdDomain.Thread
+
+(** flow-insensitive analysis mapping threads to may-sets of descendants *)
+module TransitiveDescendants = struct
+  include IdentityUnitContextsSpec
+  module D = Lattice.Unit
+
+  module V = struct
+    include TID
+    include StdV
+  end
+
+  module G = SetDomain.Make (TID)
+
+  let name () = "transitiveDescendants"
+  let startstate _ = D.bot ()
+  let exitstate _ = D.bot ()
+
+  let threadspawn man ~multiple lval f args fman =
+    let ask = Analyses.ask_of_man man in
+    let tid_lifted = ask.f Queries.CurrentThreadId in
+    match tid_lifted with
+    | `Top | `Bot -> ()
+    | `Lifted tid ->
+      let child_ask = Analyses.ask_of_man fman in
+      let child_tid_lifted = child_ask.f Queries.CurrentThreadId in
+      (match child_tid_lifted with
+       | `Top | `Bot -> ()
+       | `Lifted child_tid ->
+         (* contribute new child *)
+         let _ = man.sideg tid (G.singleton child_tid) in
+         (* transitive hull *)
+         let child_descendants = man.global child_tid in
+         man.sideg tid child_descendants)
+  ;;
+end
+
+let _ = MCP.register_analysis ~dep:[ "threadid" ] (module TransitiveDescendants : MCPSpec)
