f67adcb9fd7a029c7a304f5689c7c4d7a6e566cb
Simmo Saan <simmo.saan@gmail.com>
2025-11-14 14:18:53 +0200

    Disable mutexGhosts for single-threaded programs via autotuner

--- src/autoTune.ml
+++ src/autoTune.ml
@@ -214,7 +214,7 @@ let enableAnalyses reason description analyses =
 (*escape is also still enabled, because otherwise we get a warning*)
 (*does not consider dynamic calls!*)
 let notNeccessaryRaceAnalyses = ["race"; "symb_locks"; "region"]
-let notNeccessaryThreadAnalyses = notNeccessaryRaceAnalyses @ ["deadlock"; "maylocks"; "thread"; "threadid"; "threadJoins"; "threadreturn"; "mhp"; "pthreadMutexType"]
+let notNeccessaryThreadAnalyses = notNeccessaryRaceAnalyses @ ["deadlock"; "maylocks"; "thread"; "threadid"; "threadJoins"; "threadreturn"; "mhp"; "pthreadMutexType"; "mutexGhosts"]
 
 let hasSpec spec = List.mem spec (Svcomp.Specification.of_option ())
 
