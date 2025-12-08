1ccc286f0c473180cac11ff722ee52adcb0e869b
dabund24 <dev@dabund24.de>
2025-12-08 21:04:30 +0100

    more racefree test cases

---
+++ tests/regression/90-races_ancestor_locksets/05-maybe_create_racefree.c
@@ -0,0 +1,28 @@
+// PARAM: --set ana.activated[+] taintedCreationLockset --set ana.activated[+] threadJoins --set ana.activated[+] transitiveDescendants --set ana.activated[+] creationLockset
+#include <pthread.h>
+
+int global = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t id1, id2;
+
+void *t1(void *arg) {
+  pthread_mutex_lock(&mutex);
+  global++; // NORACE
+  pthread_mutex_unlock(&mutex);
+  return NULL;
+}
+
+void *t2(void *arg) { // t2 is protected by mutex locked in main thread
+  global++; // NORACE
+  return NULL;
+}
+
+int main(void) {
+  pthread_create(&id1, NULL, t1, NULL);
+  pthread_mutex_lock(&mutex);
+  int maybe;
+  if (maybe) {
+    pthread_create(&id2, NULL, t2, NULL);
+  }
+  return 0;
+}
