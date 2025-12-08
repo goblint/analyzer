8615223c3a04f16ce0b730f06924248eb27812dd
dabund24 <dev@dabund24.de>
2025-12-08 12:40:48 +0100

    ambiguous thread creation test cases

---
+++ tests/regression/90-races_ancestor_locksets/21-diamond_racing.c
@@ -0,0 +1,39 @@
+// PARAM: --set ana.activated[+] taintedCreationLockset --set ana.activated[+] threadJoins --set ana.activated[+] transitiveDescendants --set ana.activated[+] creationLockset
+#include <pthread.h>
+
+int global = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t id1, id2, id3, id4_1, id4_2;
+
+void *t1(void *arg) {
+  pthread_mutex_lock(&mutex);
+  global++; // RACE!
+  pthread_mutex_unlock(&mutex);
+  return NULL;
+}
+
+void *t4(void *arg) { // t4 is not protected by mutex, since it is created twice and the creation in t2 does not happen with mutex locked 
+  global++; // RACE!
+  return NULL;
+}
+
+void *t3(void *arg) {
+  pthread_mutex_lock(&mutex);
+  pthread_create(&id4_2, NULL, t4, NULL);
+  pthread_join(id4_2, NULL);
+  pthread_mutex_unlock(&mutex);
+  return NULL;
+}
+
+void *t2(void *arg) {
+  pthread_create(&id4_1, NULL, t4, NULL);
+  pthread_join(id4_1, NULL);
+  return NULL;
+}
+
+int main(void) {
+  pthread_create(&id1, NULL, t1, NULL);
+  pthread_create(&id2, NULL, t2, NULL);
+  pthread_create(&id3, NULL, t3, NULL);
+  return 0;
+}
