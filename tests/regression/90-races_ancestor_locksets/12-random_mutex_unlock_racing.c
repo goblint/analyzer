6e966b6219ffcf351dc78f00cb857817b126cb02
dabund24 <dev@dabund24.de>
2025-12-08 21:05:11 +0100

    more racing test cases

---
+++ tests/regression/90-races_ancestor_locksets/12-random_mutex_unlock_racing.c
@@ -0,0 +1,34 @@
+// PARAM: --set ana.activated[+] taintedCreationLockset --set ana.activated[+] threadJoins --set ana.activated[+] transitiveDescendants --set ana.activated[+] creationLockset
+// TODO broken, since locksets have more granular path sensitivity than creation locksets
+#include <pthread.h>
+
+int global = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER, mutex_alt = PTHREAD_MUTEX_INITIALIZER;
+pthread_t id1, id2;
+
+void *t1(void *arg) {
+  pthread_mutex_lock(&mutex);
+  global++; // RACE!
+  pthread_mutex_unlock(&mutex);
+  return NULL;
+}
+
+void *t2(void *arg) { // t2 is not protected by mutex locked in main thread, since an unlock may happen before joining
+  global++; // RACE!
+  return NULL;
+}
+
+int main(void) {
+  pthread_create(&id1, NULL, t1, NULL);
+  pthread_mutex_lock(&mutex);
+  pthread_create(&id2, NULL, t2, NULL);
+  pthread_mutex_lock(&mutex_alt);
+  pthread_mutex_t *mutex_ref = &mutex_alt;
+  int maybe;
+  if (maybe) { 
+    mutex_ref = &mutex;
+  }
+  pthread_mutex_unlock(mutex_ref);
+  pthread_join(id2, NULL);
+  return 0;
+}
