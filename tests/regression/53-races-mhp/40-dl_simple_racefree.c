4d5fee2115b0add2046656c72f724ed49e75c2d1
dabund24 <dev@dabund24.de>
2026-01-15 13:09:53 +0100

    first descendant lockset racefree tests

---
+++ tests/regression/53-races-mhp/40-dl_simple_racefree.c
@@ -0,0 +1,22 @@
+// PARAM: --set ana.activated[+] threadJoins --set ana.activated[+] threadDescendants --set ana.activated[+] mustlockHistory --set ana.activated[+] descendantLockset
+#include <pthread.h>
+
+int global = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t id1;
+
+void *t1(void *arg) {
+  pthread_mutex_lock(&mutex);
+  // everything from here must happen after unlock in main
+  pthread_mutex_unlock(&mutex);
+  global++; // NORACE
+  return NULL;
+}
+
+int main(void) {
+  pthread_mutex_lock(&mutex);
+  pthread_create(&id1, NULL, t1, NULL);
+  global++; // NORACE
+  pthread_mutex_unlock(&mutex);
+  return 0;
+}
