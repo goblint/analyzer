3985f35a93a88db11db66172b5fd2446a93f72a9
Simmo Saan <simmo.saan@gmail.com>
2026-01-15 17:20:56 +0200

    Fix IntDomain cast_to documentation
    from_ik is always provided by IntDomLifter, which is used for IntDomTuple.

--- src/cdomain/value/cdomains/intDomain_intf.ml
+++ src/cdomain/value/cdomains/intDomain_intf.ml
@@ -242,9 +242,8 @@ sig
   val div : ?no_ov:bool -> Cil.ikind ->  t -> t -> t
   val neg : ?no_ov:bool -> Cil.ikind ->  t -> t
   val cast_to : ?suppress_ovwarn:bool -> kind:castkind -> ?from_ik:Cil.ikind -> ?no_ov:bool -> Cil.ikind -> t -> t
-  (** Cast to {!Cil.ikind}.
+  (** Cast from [from_ik] (if known) to {!Cil.ikind}.
       The function is also called to handle overflow/wraparound after operations.
-      @param from_ik Only present for actual casts, missing for overflow/wraparound (where the type stays the same).
       @param no_ov If true, assume no overflow can occur. *)
 
   val join: Cil.ikind -> t ->  t -> t
@@ -304,9 +303,8 @@ sig
   val neg : ?no_ov:bool -> Cil.ikind ->  t -> t * overflow_info
 
   val cast_to : kind:castkind -> ?from_ik:Cil.ikind -> ?no_ov:bool -> Cil.ikind -> t -> t * overflow_info
-  (** Cast to {!Cil.ikind}.
+  (** Cast from [from_ik] (if known) to {!Cil.ikind}.
       The function is also called to handle overflow/wraparound after operations.
-      @param from_ik Only present for actual casts, missing for overflow/wraparound (where the type stays the same).
       @param no_ov If true, assume no overflow can occur. *)
 
   val of_int : Cil.ikind -> int_t -> t * overflow_info
