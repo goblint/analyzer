8f46eb38f1ac10b90db609631d48482f44186955
dabund24 <dev@dabund24.de>
2026-01-19 10:29:44 +0100

    regression tests for global descendant locksets

---
+++ tests/regression/53-races-mhp/42-dl_cl_simple_racefree.c
@@ -0,0 +1,31 @@
+// PARAM: --set ana.activated[+] threadJoins --set ana.activated[+] threadDescendants --set ana.activated[+] mustlockHistory --set ana.activated[+] descendantLockset --set "ana.activated[+]" creationLockset
+#include <pthread.h>
+
+int global = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t id1, id2;
+
+void *t1(void *arg) {
+  pthread_mutex_lock(&mutex);
+  // everything from here must happen after unlock in main
+  pthread_mutex_unlock(&mutex);
+  global++; // NORACE
+  return NULL;
+}
+
+void *t2(void *arg) { // t2 is joined into main before unlock happens
+  global++; // NORACE
+  return NULL;
+}
+
+int main(void) {
+  pthread_mutex_lock(&mutex);
+
+  pthread_create(&id1, NULL, t1, NULL);
+
+  pthread_create(&id2, NULL, t2, NULL);
+  pthread_join(id2, NULL);
+
+  pthread_mutex_unlock(&mutex);
+  return 0;
+}
---
+++ tests/regression/53-races-mhp/44-dl_cl_transitive_create_racefree.c
@@ -0,0 +1,35 @@
+// PARAM: --set ana.activated[+] threadJoins --set ana.activated[+] threadDescendants --set ana.activated[+] mustlockHistory --set ana.activated[+] descendantLockset --set "ana.activated[+]" creationLockset
+#include <pthread.h>
+
+int global = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t id1, id2, id3;
+
+void *t1(void *arg) {
+  pthread_mutex_lock(&mutex);
+  // everything from here must happen after unlock in main
+  pthread_mutex_unlock(&mutex);
+  global++; // NORACE
+  return NULL;
+}
+
+void *t3(void *arg) { // t3 is joined into main before unlock happens
+  global++; // NORACE
+  return NULL;
+}
+
+void *t2(void *arg) {
+  pthread_create(&id3, NULL, t3, NULL);
+}
+
+int main(void) {
+  pthread_mutex_lock(&mutex);
+
+  pthread_create(&id1, NULL, t1, NULL);
+
+  pthread_create(&id2, NULL, t2, NULL);
+  pthread_join(id3, NULL);
+
+  pthread_mutex_unlock(&mutex);
+  return 0;
+}
