bb16da6406354af64e12a64617871586858c217d
dabund24 <dev@dabund24.de>
2026-01-19 10:28:46 +0100

    global descendant locksets

--- src/analyses/descendantLockset.ml
+++ src/analyses/descendantLockset.ml
@@ -2,10 +2,23 @@ open Analyses
 module TID = ThreadIdDomain.Thread
 module TIDs = ConcDomain.ThreadSet
 module Lockset = LockDomain.MustLockset
+module TidToLocksetMapTop = MapDomain.MapBot (TID) (Lockset)
 
 module Spec = struct
   include IdentityUnitContextsSpec
   module D = MapDomain.MapBot (TID) (Lockset)
+
+  (** [{ t_0 |-> { t_d |-> L } }]
+
+      [{ t_d |-> L }] is the descendant lockset valid for the [V] value,
+      because [t_d] was created in [t_0] with the lockset being a superset of L.
+  *)
+  module G = MapDomain.MapBot (TID) (TidToLocksetMapTop)
+
+  module V = struct
+    include TID
+    include StdV
+  end
 
   let name () = "descendantLockset"
   let startstate _ = D.empty ()
@@ -20,17 +33,38 @@ module Spec = struct
     let must_ancestors_descendants = TIDs.filter (TID.must_be_ancestor tid) descendants in
     TIDs.add tid must_ancestors_descendants
 
+  let threadspawn_contribute_globals man tid must_ancestor_descendants =
+    let descendant_lockset = man.local in
+    let contribute_for_descendant t_d =
+      let creation_lockset = man.ask @@ Queries.CreationLockset t_d in
+      let to_contribute =
+        D.fold
+          (fun t_l l_dl acc ->
+             let l_cl = Queries.CL.find tid creation_lockset in
+             let l_inter = Lockset.inter l_cl l_dl in
+             TidToLocksetMapTop.add t_l l_inter acc)
+          descendant_lockset
+          (TidToLocksetMapTop.empty ())
+      in
+      man.sideg t_d (G.singleton tid to_contribute)
+    in
+    TIDs.iter contribute_for_descendant must_ancestor_descendants
+
+  let threadspawn_compute_local_contribution man tid must_ancestor_descendants =
+    let lockset = man.ask Queries.MustLockset in
+    TIDs.fold
+      (fun t_d -> D.join (D.singleton t_d lockset))
+      must_ancestor_descendants
+      man.local
+
   let threadspawn man ~multiple lval f args fman =
     let tid_lifted = man.ask Queries.CurrentThreadId in
     let child_tid_lifted = fman.ask Queries.CurrentThreadId in
     match tid_lifted, child_tid_lifted with
     | `Lifted tid, `Lifted child_tid when TID.must_be_ancestor tid child_tid ->
       let must_ancestor_descendants = must_ancestor_descendants_closure fman child_tid in
-      let lockset = man.ask Queries.MustLockset in
-      TIDs.fold
-        (fun t_d -> D.join (D.singleton t_d lockset))
-        must_ancestor_descendants
-        man.local
+      threadspawn_contribute_globals man tid must_ancestor_descendants;
+      threadspawn_compute_local_contribution man tid must_ancestor_descendants
     | _ -> man.local
 
   let get_must_ancestor_running_descendants man tid =
@@ -75,8 +109,8 @@ module Spec = struct
     | _ -> man.local
 
   module A = struct
-    (** ego tid * lock history * descendant lockset *)
-    include Printable.Prod3 (TID) (Queries.LH) (D)
+    (** ego tid * lock history * (local descendant lockset * global descendant lockset) *)
+    include Printable.Prod3 (TID) (Queries.LH) (Printable.Prod (D) (G))
 
     let happens_before (t1, dl1) (t2, lh2) =
       let locks_held_creating_t2 = D.find t2 dl1 in
@@ -95,8 +129,15 @@ module Spec = struct
              && (TID.equal t_lh t2 || TID.must_be_ancestor t_lh t2))
           relevant_lh2_threads)
 
-    let may_race (t1, lh1, dl1) (t2, lh2, dl2) =
-      not (happens_before (t1, dl1) (t2, lh2) || happens_before (t2, dl2) (t1, lh1))
+    let happens_before_global dlg1 (t2, lh2) =
+      G.exists (fun t dl -> happens_before (t, dl) (t2, lh2)) dlg1
+
+    let may_race (t1, lh1, (dl1, dlg1)) (t2, lh2, (dl2, dlg2)) =
+      not
+        (happens_before (t1, dl1) (t2, lh2)
+         || happens_before (t2, dl2) (t1, lh1)
+         || happens_before_global dlg1 (t2, lh2)
+         || happens_before_global dlg2 (t1, lh1))
 
     let should_print _ = false
   end
@@ -105,8 +146,8 @@ module Spec = struct
     let lh = man.ask Queries.MustlockHistory in
     let tid_lifted = man.ask Queries.CurrentThreadId in
     match tid_lifted with
-    | `Lifted tid -> tid, lh, man.local
-    | _ -> ThreadIdDomain.UnknownThread, Queries.LH.empty (), D.empty ()
+    | `Lifted tid -> tid, lh, (man.local, man.global tid)
+    | _ -> ThreadIdDomain.UnknownThread, Queries.LH.empty (), (D.empty (), G.empty ())
 end
 
 let _ =
