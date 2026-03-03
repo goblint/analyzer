a495aff4617a0a7d223928c09a5ed9d755f6799d
Ali Rasim Kocal <ali.rasim.kocal@tum.de>
2026-03-03 14:01:19 +0100

    minor fix for fixpoint test

--- reduce_fixpoint.sh
+++ reduce_fixpoint.sh
@@ -4,11 +4,12 @@ CONF="/home/arkocal/Workspace/goblint/fwd_rework/config.json"
 GOBLINT="/home/arkocal/Workspace/goblint/fwd_rework/goblint"
 
 # Run goblint with fwd solver
-"$GOBLINT" --conf "$CONF" --solver fwd input.c &> out.txt
+$GOBLINT --conf $CONF --solver fwd input.c > out.txt 2>&1
 
 if [ $? -eq 3 ]; then
     grep Fixpoint out.txt >/dev/null 2>&1
 else
+    echo "Not error 3"
     exit 5
 fi
 
