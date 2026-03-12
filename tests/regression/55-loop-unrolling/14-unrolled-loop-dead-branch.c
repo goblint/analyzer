faa85ec49a025c97b44b50c773b4481a02938c1d
Simmo Saan <simmo.saan@gmail.com>
2026-03-12 13:35:20 +0200

    Fix duplicate conflicting dead branch warnings due to loop unrolling

--- tests/regression/55-loop-unrolling/14-unrolled-loop-dead-branch.c
+++ tests/regression/55-loop-unrolling/14-unrolled-loop-dead-branch.c
@@ -1,7 +1,7 @@
 // PARAM: --enable ana.dead-code.branches --set exp.unrolling-factor 2
 int main() {
-  for (int i = 0; i < 2; i++) { // TODO NOWARN (dead branch)
-    if (i == 1) { // TODO NOWARN (dead branch)
+  for (int i = 0; i < 2; i++) { // NOWARN (dead branch)
+    if (i == 1) { // NOWARN (dead branch)
 
     }
   }
