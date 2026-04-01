e981c8ec43386db27fd7e57b86d2f8be7bf55f7f
Simmo Saan <simmo.saan@gmail.com>
2026-04-01 10:01:56 +0300

    Use IntDomain equal_to instead of eq for offset check in base

--- src/analyses/base.ml
+++ src/analyses/base.ml
@@ -305,9 +305,9 @@ struct
           begin match t with
             | Some t ->
               let f_offset_bytes = Cilfacade.fieldBytesOffsetOnly f in
-              let f_offset = IdxDom.of_int (Cilfacade.ptrdiff_ikind ()) (Z.of_int f_offset_bytes) in
-              begin match IdxDom.(to_bool (eq f_offset (neg n_offset))) with
-                | Some true -> `NoOffset
+              let f_offset = Z.of_int f_offset_bytes in
+              begin match IdxDom.(equal_to f_offset (neg n_offset)) with
+                | `Eq -> `NoOffset
                 | _ -> `Field (f, `Index (n_offset, `NoOffset))
               end
             | None -> `Field (f, `Index (n_offset, `NoOffset))
