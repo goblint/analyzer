80ae35f1ab2e4074927a8f0b09eac3ece26df655
Simmo Saan <simmo.saan@gmail.com>
2026-04-02 12:54:12 +0300

    Limit Goblint_memtrace signatures

--- src/goblint_memtrace.memtrace.ml
+++ src/goblint_memtrace.memtrace.ml
@@ -1,1 +1,1 @@
-include Memtrace
+let trace_if_requested = Memtrace.trace_if_requested
--- src/goblint_memtrace.no-memtrace.ml
+++ src/goblint_memtrace.no-memtrace.ml
@@ -1,4 +1,4 @@
 (** Dummy implementation when the optional memtrace dependency is not installed. *)
 
-let trace_if_requested ?context ?sampling_rate () =
+let trace_if_requested ?(context: string option) ?(sampling_rate: float option) () =
   ()
