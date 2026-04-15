241106bf772e606b1959a82718250cac05ec27f2
Michael Schwarz <michael.schwarz93@gmail.com>
2026-04-15 12:42:11 +0800

    Progress on `phaseGhost` detection

--- src/analyses/phaseGhost.ml
+++ src/analyses/phaseGhost.ml
@@ -4,9 +4,13 @@ open Analyses
 open GoblintCil
 
 module TID = ThreadIdDomain.Thread
-module ID = IntDomain.IntDomTuple
-
 module TIDs = ConcDomain.ThreadSet
+
+module Const =
+struct
+  include Lattice.Flat (IntOps.BigIntOps)
+  let name () = "ghost-constant"
+end
 
 module IncByOne =
 struct
@@ -16,10 +20,13 @@ end
 
 module Spec =
 struct
-  module D = Lattice.Unit
-  include IdentityUnitContextsSpec
+  include IdentitySpec
 
   let name () = "phaseGhost"
+
+  module D = MapDomain.MapBot (Basetype.Variables) (Const)
+  include ValueContexts (D)
+  module P = IdentityP (D)
 
   module V = VarinfoV
   module G =
@@ -31,8 +38,26 @@ struct
     let create_inc_by_one inc_by_one = (TIDs.bot (), inc_by_one)
   end
 
-  let startstate _ = ()
-  let exitstate _ = ()
+  let initial_ghost_values () =
+    List.fold_left (fun acc -> function
+        | GVar (v, initinfo, _) when YamlWitness.VarSet.mem v !(YamlWitness.ghostVars) ->
+          begin match initinfo.init with
+            | Some (SingleInit exp) ->
+              begin match Cil.getInteger (Cil.constFold true exp) with
+                | Some z -> D.add v (`Lifted z) acc
+                | None -> acc
+              end
+            | None when Cil.isIntegralType v.vtype ->
+              D.add v (`Lifted Z.zero) acc
+            | _ ->
+              acc
+          end
+        | _ ->
+          acc
+      ) (D.bot ()) !Cilfacade.current_file.globals
+
+  let startstate _ = initial_ghost_values ()
+  let exitstate _ = initial_ghost_values ()
 
   let tids_of_current_thread man =
     match man.ask Queries.CurrentThreadId with
@@ -45,35 +70,62 @@ struct
       | Lval lval' -> CilType.Lval.equal lval lval'
       | _ -> false
     in
-    let is_positive_constant e =
+    let is_one_constant e =
       match Cil.getInteger (Cil.constFold true e) with
-      | Some k -> Z.gt k Z.zero
+      | Some k -> Z.equal k Z.one
       | None -> false
     in
     match Cil.stripCasts rval with
     | BinOp (PlusA, e1, e2, _) ->
       if is_same_lval e1 then
-        is_positive_constant e2
+        is_one_constant e2
       else if is_same_lval e2 then
-        is_positive_constant e1
+        is_one_constant e1
       else
         false
     | _ ->
       false
 
-  let is_increment_by_one man lval rval =
-    if increment_constant lval rval then
-      true
-    else
-      let old_value = man.ask (Queries.EvalInt (Lval lval)) in
-      let new_value = man.ask (Queries.EvalInt rval) in
-      match old_value, new_value with
-      | `Lifted old_value, `Lifted new_value ->
-        let ik = Cilfacade.get_ikind_exp (Lval lval) in
-        let expected_new_value = ID.add old_value (ID.of_int ik Z.one) in
-        ID.equal expected_new_value new_value
-      | _ ->
-        false
+  (* This local constant folding intentionally disregards writes from other threads.
+     It is only for the phaseGhost checker itself. *)
+  (* This information must **not** be used to refine other analyses or returned by any query,
+     because it is unsound in the presence of other threads interfering. By the same token, it must
+     be not used to raise Deadcode in branch. *)
+  let rec eval_const state e =
+    match Cil.stripCasts e with
+    | Const _ ->
+      Cil.getInteger (Cil.constFold true e)
+    | Lval (Var var, NoOffset) when YamlWitness.VarSet.mem var !(YamlWitness.ghostVars) ->
+      begin match D.find_opt var state with
+        | Some (`Lifted z) -> Some z
+        | _ -> None
+      end
+    | UnOp (Neg, e, _) ->
+      Option.map Z.neg (eval_const state e)
+    | BinOp (PlusA, e1, e2, _)
+    | BinOp (IndexPI, e1, e2, _)
+    | BinOp (PlusPI, e1, e2, _) ->
+      Option.bind (eval_const state e1) (fun z1 ->
+          Option.map (Z.add z1) (eval_const state e2)
+        )
+    | BinOp (MinusA, e1, e2, _) ->
+      Option.bind (eval_const state e1) (fun z1 ->
+          Option.map (Z.sub z1) (eval_const state e2)
+        )
+    | BinOp (Mult, e1, e2, _) ->
+      Option.bind (eval_const state e1) (fun z1 ->
+          Option.map (Z.mul z1) (eval_const state e2)
+        )
+    | _ ->
+      None
+
+  let is_increment_by_one state lval rval =
+    increment_constant lval rval ||
+    match eval_const state (Lval lval), eval_const state rval with
+    | Some old_value, Some new_value ->
+      Z.equal new_value (Z.succ old_value)
+    | _ ->
+      false
 
   let event man e oman =
     match e with
@@ -90,13 +142,21 @@ struct
       man.local
 
   let assign man lval rval =
-    begin match lval with
+    if !AnalysisState.global_initialization then
+      man.local
+    else
+      match lval with
       | Var var, NoOffset when YamlWitness.VarSet.mem var !(YamlWitness.ghostVars) ->
-        man.sideg var (G.create_inc_by_one (is_increment_by_one man lval rval))
+        let inc_by_one = is_increment_by_one man.local lval rval in
+        let local =
+          match inc_by_one, eval_const man.local rval with
+          | true, Some z -> D.add var (`Lifted z) man.local
+          | _ -> D.add var (Const.top ()) man.local
+        in
+        man.sideg var (G.create_inc_by_one inc_by_one);
+        local
       | _ ->
-        ()
-    end;
-    man.local
+        man.local
 
   let query man (type a) (q: a Queries.t): a Queries.result =
     match q with
@@ -113,8 +173,7 @@ struct
            else
              M.warn_noloc ~category:Witness "phaseGhost: global %a is only accessed by unique thread %a, but is not only ever increased by one" CilType.Varinfo.pretty g TID.pretty tid
          | _ ->
-           M.warn_noloc ~category:Witness "phaseGhost: global %a is accessed by multiple unique threads: %a" CilType.Varinfo.pretty g TIDs.pretty tidset
-        )
+           M.warn_noloc ~category:Witness "phaseGhost: global %a is accessed by multiple unique threads: %a" CilType.Varinfo.pretty g TIDs.pretty tidset)
     | _ ->
       Queries.Result.top q
 end
