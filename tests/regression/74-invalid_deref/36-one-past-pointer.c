9625835ca18320537ed29f6e949eb2203081df30
Simmo Saan <simmo.saan@gmail.com>
2026-05-13 10:07:27 +0300

    Add memory initialization to 74-invalid_deref/36-one-past-pointer

--- tests/regression/74-invalid_deref/36-one-past-pointer.c
+++ tests/regression/74-invalid_deref/36-one-past-pointer.c
@@ -1,9 +1,15 @@
-// PARAM: --set ana.activated[+] memOutOfBounds --enable ana.int.interval
+// PARAM: --enable ana.sv-comp.functions --set ana.activated[+] memOutOfBounds --enable ana.int.interval
 #include <stdlib.h>
 #include <stdio.h>
 
+extern char __VERIFIER_nondet_char();
+
 int main(void) {
   char *buf = malloc(4);
+  buf[0] = __VERIFIER_nondet_char();
+  buf[1] = __VERIFIER_nondet_char();
+  buf[2] = __VERIFIER_nondet_char();
+  buf[3] = '\0';
   char *end;
 
   end = buf + 3; // NOWARN
