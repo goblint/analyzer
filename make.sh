bba5c1f9b07d52370a9f2f0d5b5437ce7c6cce10
Simmo Saan <simmo.saan@gmail.com>
2026-05-22 10:39:49 +0300

    Mention opam 2.2 lower bound

--- make.sh
+++ make.sh
@@ -74,7 +74,7 @@ rule() {
         opam upgrade -y $(opam list --pinned -s)
       }
     ;; setup)
-      echo "Make sure you have the following installed: opam >= 2.0.0, git, patch, m4, autoconf, libgmp-dev, libmpfr-dev, pkg-config"
+      echo "Make sure you have the following installed: opam >= 2.2.0, git, patch, m4, autoconf, libgmp-dev, libmpfr-dev, pkg-config"
       echo "For the --html output you also need: graphviz and python3-pygments (optional)"
       echo "For running the regression tests you also need: ruby, gem, curl, and the os gem"
       echo "For reference see ./Dockerfile or ./scripts/travis-ci.sh"
