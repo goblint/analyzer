de8b8b17129dc944725a2cdef4e380727717ab01
Nicolas Till Roth <nicolas.roth@tum.de>
2026-05-31 13:30:27 +0200

    Changed type t and all occurences in other functions. Adapted dim_add and dim_remove to suit the new datatype. To be reviewed by Leonie if merged into main.

--- src/cdomains/apron/subpoly/nico_explanation.md
+++ src/cdomains/apron/subpoly/nico_explanation.md
@@ -1,8 +1,9 @@
-### Notes for the analysis of the Microsoft Research Codebase on SubPolyhedra:
+## Notes for the analysis of the Microsoft Research Codebase on SubPolyhedra:
 
-## The Info field:
+### The Info field:
 contains the linear expression upon insertion of the slack variable. Does not get updated with gaussian elimination or row-echelon form but does get updated with renaming of variables (not sure if this happens) and substitution.
 
+### Forget_vars:
 When a variable x is 'forgotten' we perform the following steps:
 1.  Perform Gaussian Elimination to remove x from as many rows as possible.
     This leaves only one row with non-zero x column.
@@ -12,3 +13,51 @@ When a variable x is 'forgotten' we perform the following steps:
 
 Later when doing Simplex, whenever there are multiple slack variables in a row it treats them as additional constraints like {0 <= b_1 <= 10} to solve the system and find closer bounds. Slack variables may be consolidated when they are redundant without loss of precision. So in a row like
 b1 - b2 = 5 we can substitute b1 in the matrix with b2 + 5 and reduce the dimensionality of the problem.
+
+** Important: The intervals can be valid while variables that helped create this constraint are no longer present. **
+
+** Important: We need constants in the matrix for precision! Because we will have multiple slack variables per constraint we need some other way to identify them from program variables. Then we could have the last column always be the constant one. **
+
+** Important: The Matrix and the Intervals are Rational for maximum precision and because we need rationals for gaussian elimination. Therefore slack variables can have coefficients, these are not propagated into the intervals until the reduction step! **
+
+=> old comment from subpolycore, copied here to organize the code better:
+(********************
+    1 3 2   -1
+    0 5 4       -1
+
+    2 1 0   -2  1
+    0 5 4       -1
+
+
+    FINAL
+    2 1 0   -2   1
+
+
+    2 3 1  4 5 -1
+    0 2 5  2 5    -1
+    0 0 3  4 6        -1
+    2 0 0  5 1           -1
+    0 3 0  0 5               -1
+    4 0 0 10 2 -1
+
+    2 3 1  4 5 0 -1
+    0 2 5  2 5 0   -1
+    0 0 3  4 6 0       -1
+    0 0 0  5 1 0          -1
+    0 0 0  0 5 0              -1
+    0 0 0  0 0 0 -1         2
+    0 0 0  0 1 2                 -1
+
+
+    When inserting new constraints we should take care not to insert something linearly dependent on 
+    anything already in the constraints. => This is true
+    Thus there should be as many rows as there are variables. By this we should be able to derive 
+    the point where slack variables start. => This is not true
+
+    Need one function like normalize_affeq that puts constraints into row-echelon form 
+    and applies proper changes to intervals. (Normalize slacks to -1, thereby we essentially
+    get a protocol matrix which tells us which operations were carried our for normalization.)
+    Let's add the function to the domain for now and we can change it later if we realize that
+    we need to use it inside the core functionality =>This is not true
+
+***********************)
--- src/cdomains/apron/subpoly/subpolycore.apron.ml
+++ src/cdomains/apron/subpoly/subpolycore.apron.ml
@@ -22,69 +22,16 @@ module SubPoly (Var : Var) (I : IntervalSig) = struct
   module Matrix =
     AffineEqualityDomain.AffineEqualityMatrix
       (Vector)
-      (ListMatrix.ListMatrix)
+      (ListMatrix.ListMatrix) (*Question: do we actually use this, if we just use the Matrix and Vector implementations? *)
 
   (* Map keyed by variables. *)
   module VarMap = Map.Make(Var)
 
-  type affeq = Matrix.t [@@deriving eq, ord, hash]
-  type interval = I.t [@@deriving eq, ord, hash]
-  type interval_map = interval VarMap.t [@@deriving eq, ord]
-  type slackintervals = interval_map [@@deriving eq, ord]
-  (* QUESTION: Would the slack_expr have a constant? I thought we wanted to pull out constants into intervals?
-               Then it also wouldn't be so nested because the slack_expr can be the info rightaway.            *)
-  type slack_expr = {
-    terms: (Var.t * Mpqf.t) list;
-    const: Mpqf.t; (*Factor how much it was scaled*)
-  } [@@deriving eq, ord, hash]
-  
-  (* REFACTORING: I renamed info to intv and expr to info, as the slack_expr coinicides with the info terminology from the paper.*)
-  type slack = {
-    info: slack_expr;
-    intv: interval;
-  } [@@deriving eq, ord, hash]
-  type slack_map = slack VarMap.t [@@deriving eq, ord]
+  type affeq = Matrix.t [@@deriving eq, ord, hash] (*Our affine equality matrix.*)
+  type interval_map = I.t VarMap.t [@@deriving eq, ord] (*Map from Var to Interval*)
+  type info = (Var.t * int) list [@@deriving eq, ord, hash] (*similar to sparse vector, might acutally use sparse vector here? QUESTION*)
+  type info_map = info VarMap.t [@@deriving eq, ord] (*Map from Var to info (maybe sparse vector)*)
 
-(**
-1 3 2   -1
-0 5 4       -1
-
-2 1 0   -2  1
-0 5 4       -1
-
-
-FINAL
-2 1 0   -2   1
-
-
-2 3 1  4 5 -1
-0 2 5  2 5    -1
-0 0 3  4 6        -1
-2 0 0  5 1           -1
-0 3 0  0 5               -1
-4 0 0 10 2 -1
-
-2 3 1  4 5 0 -1
-0 2 5  2 5 0   -1
-0 0 3  4 6 0       -1
-0 0 0  5 1 0          -1
-0 0 0  0 5 0              -1
-0 0 0  0 0 0 -1         2
-0 0 0  0 1 2                 -1
-
-
-When inserting new constraints we should take care not to insert something linearly dependent on 
-anything already in the constraints.
-Thus there should be as many rows as there are variables. By this we should be able to derive 
-the point where slack variables start. 
-
-Need one function like normalize_affeq that puts constraints into row-echelon form 
-and applies proper changes to intervals. (Normalize slacks to -1, thereby we essentially
-get a protocol matrix which tells us which operations were carried our for normalization.)
-Let's add the function to the domain for now and we can change it later if we realize that
-we need to use it inside the core functionality
-
-*)
   let hash_interval_map (m: interval_map) =
     VarMap.fold (fun var interval acc ->
         Hashtbl.hash (Var.hash var, I.hash interval, acc)
@@ -92,15 +39,15 @@ we need to use it inside the core functionality
 
   let hash_slackintervals = hash_interval_map
 
-  let hash_slack_map (m: slack_map) =
-    VarMap.fold (fun var slack acc ->
-        Hashtbl.hash (Var.hash var, hash_slack slack, acc)
+  let hash_info_map (m: info_map) =
+    VarMap.fold (fun var info acc ->
+        Hashtbl.hash (Var.hash var, hash_info info, acc)
       ) m 0
 
   type t = {
     affeq: affeq; (*Affine Equalities stored as (sparse?) Matrix*)
-    intervals: slackintervals; (*Probably won't need these! Program variable intervals. QUESTION: Do we actually keep track of these?*)
-    slacks: slack_map;
+    intervals: interval_map; (*Map of slack vars to intervals*)
+    infos: info_map; (*Map of slack vars to info*)
   } [@@deriving eq, ord, hash]
 
 
@@ -108,55 +55,27 @@ we need to use it inside the core functionality
 
   let copy = Fun.id
 
-  let empty () = { affeq = Matrix.empty (); intervals = VarMap.empty; slacks = VarMap.empty }
+  let empty () = { affeq = Matrix.empty (); intervals = VarMap.empty; infos = VarMap.empty }
 
   let is_empty (t: t) =
-    Matrix.is_empty t.affeq && VarMap.is_empty t.intervals && VarMap.is_empty t.slacks
+    Matrix.is_empty t.affeq && VarMap.is_empty t.intervals && VarMap.is_empty t.infos
 
-  let set_interval (var: Var.t) (interval: interval) (t: t) =
-    { t with intervals = VarMap.add var interval t.intervals }
+  let set_info (var: Var.t) (info: info) (t : t)  =
+    {t with infos = VarMap.add var info t.infos}
 
-  let set_slack (var: Var.t) (slack: slack) (t: t) =
-    { t with slacks = VarMap.add var slack t.slacks }
+  let set_intv (var: Var.t) (intv: I.t) (t: t) = 
+    {t with intervals = VarMap.add var intv t.intervals}
+  let mem_info (var: Var.t) (t: t) = 
+    VarMap.mem var t.infos
 
-  let mem_slack (var: Var.t) (t: t) =
-    VarMap.mem var t.slacks
+  let mem_intv (var: Var.t) (t: t) =
+    VarMap.mem var t.intervals
 
   let add_affeq_row (row: CoeffVector.t) (t: t) =
     { t with affeq = Matrix.append_row t.affeq row }
 
   let forget_vars (vars: Var.t list) (t: t) = failwith "todo"
-    (**************
-    As far as I understand it, were we to normalize our state using integer arithmetic we would lose a lot of precision.
-    Therefore, we should do Matrix.reduce_col and 'invalidate' the slack variables created with the variable we forget.
-    If we can guarantee that the slack variable is >= 0, we can keep it in the matrix and still deduce information from it.
-    Example:
-    y >= x --> y - x - b1 = 0
-    x >= z --> x - z - b2 = 0
-    => forget x!
-    From:
-    x   y   z   b1  b2
-   -1   1   0  -1   0
-    1   0  -1   0  -1
-
-    To:
-    0   1  -1  -1  -1
-    => y - z - b1 - b1 = 0
-    If we know that b1 and b2 are positive we can still deduce that y >= z.
-
-    So forget_var should do the following:
-    Gaussian elimination to remove var from all but one constraint.
-    Remove last constraint containing var.
-    Keep the factors of slack variables and multiple slack variables
-    Invalidate/Anonymize the Intervals if we can guarantee that they are >= 0, which we can take care of during 
-    insertion and evaluation of expressions.
-    This might mean that we need constants in the linear expressions to guarantee slack vars are always >= 0
-    The slack variables always being >= 0 also enables us to evaluate bottom which is when a slack variable
-    is forced to be < 0. This is then unreachable.
-    I think that this constraint is also part of the LP reduction where we maximize with constraints and the constraint 
-    is that slacks are >= 0 (or lower <= beta <= upper with lower >= 0).
-
-    ***************)
+      (*TODO*)
 
   (* HELPER-FUNCTIONS FOR DIMENSIONAL OPERATIONS *)
   let shift_index_add (old_index : Var.t) (occ_cols : (int * int) list) : Var.t = 
@@ -170,31 +89,25 @@ we need to use it inside the core functionality
     in let new_index = (Var.to_int old_index) - k 
     in Var.to_t new_index)
 
-  let new_slacks_add slacks occ_cols  : slack VarMap.t =
-    VarMap.fold (fun var slack acc ->
+    (** Replacements for new_slack_add and new_slack_remove for the new datatype: *)
+  let new_infos_add (infos : info_map) occ_cols : info_map = 
+    VarMap.fold(fun var info acc ->
       let new_var = shift_index_add var occ_cols in
-      let new_slack = {
-        info = {
-          terms = List.rev (List.fold_left (fun acc (v, c) -> (shift_index_add v occ_cols, c) :: acc) [] slack.info.terms);
-          const = slack.info.const;
-        };
-        intv = slack.intv;
-      } in
-      VarMap.add new_var new_slack acc
-    ) slacks VarMap.empty
-
-  let new_slacks_remove slacks dim_list : slack VarMap.t =
-    VarMap.fold (fun var slack acc ->
+      let new_info = List.rev (List.fold_left (fun acc (v, c) -> (shift_index_add v occ_cols, c) :: acc) [] info) in
+      VarMap.add new_var new_info acc) infos VarMap.empty
+  let new_infos_remove (infos : info_map) dim_list : info_map = 
+    VarMap.fold (fun var info acc ->
       let new_var = shift_index_remove var dim_list in
-      let new_slack = {
-        info = {
-          terms = List.rev (List.fold_left (fun acc (v, c) -> (shift_index_remove v dim_list, c) :: acc) [] slack.info.terms);
-          const = slack.info.const;
-        };
-        intv = slack.intv;
-      } in
-      VarMap.add new_var new_slack acc
-    ) slacks VarMap.empty
+      let new_info = List.rev (List.fold_left (fun acc (v, c) -> (shift_index_remove v dim_list, c) :: acc) [] info) in
+      VarMap.add new_var new_info acc) infos VarMap.empty
+  let new_intervals_add (intervals : interval_map) occ_cols : interval_map = 
+    VarMap.fold( fun var interval acc ->
+      let new_var = shift_index_add var occ_cols in
+      VarMap.add new_var interval acc) intervals VarMap.empty
+  let new_intervals_remove (intervals : interval_map) dim_list : interval_map =
+    VarMap.fold( fun var interval acc ->
+      let new_var = shift_index_remove var dim_list in
+      VarMap.add new_var interval acc) intervals VarMap.empty
 
   let dim_add (ch: Apron.Dim.change) (t: t) = 
     let new_affeq = Matrix.dim_add ch t.affeq in
@@ -202,54 +115,43 @@ we need to use it inside the core functionality
     let grouped_indices = List.group Int.compare list in 
     let occ_cols = List.map (fun group -> ((List.hd group, List.length group))) grouped_indices in 
     (* Approach from listMatrix.ml: add_empty_columns; Example: cols_list = [1; 3; 3; 5] -> grouped_indices = [[1]; [3; 3]; [5]] -> occ_cols = [(1, 1); (3, 2); (5, 1)] *)
-    let new_slacks = new_slacks_add t.slacks occ_cols in 
-    {t with affeq = new_affeq; slacks = new_slacks}
+    let new_infos = new_infos_add t.infos occ_cols in 
+    let new_intervals = new_intervals_add t.intervals occ_cols in
+    {affeq = new_affeq; infos = new_infos; intervals = new_intervals}
 
   let dim_remove (ch: Apron.Dim.change) (t: t) = 
     let new_affeq = Matrix.dim_remove ch t.affeq in
     let new_t = failwith "forget_vars (List.map Var.to_t dim_list) t" in (* TODO: forget_vars is corretly in module D, but we need to use this here *)
     let dim_list = Array.to_list ch.dim in
     let dim_list = List.sort_uniq Int.compare dim_list in (* remove duplicates *)
-    let new_slacks = new_slacks_remove new_t.slacks dim_list in 
-    {t with affeq = new_affeq; slacks = new_slacks}
+    let new_infos = new_infos_remove new_t.infos dim_list in 
+    let new_intervals = new_intervals_remove new_t.intervals dim_list in
+    {affeq = new_affeq; infos = new_infos; intervals = new_intervals}
 
   let string_of_interval_map (m: interval_map) =
     VarMap.bindings m
     |> List.map (fun (var, interval) -> Var.string_of var ^ " -> " ^ I.show interval)
     |> String.concat "; "
 
-  let string_of_slack_expr (e: slack_expr) =
-    let term_str =
-      match e.terms with
+  let string_of_info (e: info) =
+      match e with
       | [] -> ""
       | terms ->
         terms
-        |> List.map (fun (v, c) -> Mpqf.to_string c ^ "*" ^ Var.string_of v)
+        |> List.map (fun (v, c) -> Int.to_string c ^ "*" ^ Var.string_of v)
         |> String.concat " + "
-    in
-    let const_str =
-      if Mpqf.compare e.const Mpqf.zero = 0 then
-        ""
-      else if term_str = "" then
-        Mpqf.to_string e.const
-      else
-        " + " ^ Mpqf.to_string e.const
-    in
-    term_str ^ const_str
-  (*changed info and intv below to match renaming of type t.*)
-  let string_of_slack (s: slack) =
-    I.show s.intv ^ "  (" ^ string_of_slack_expr s.info ^ ")"
-
-  let string_of_slack_map (m: slack_map) =
-    VarMap.bindings m
-    |> List.map (fun (var, slack) ->
-        Var.string_of var ^ " -> " ^ string_of_slack slack)
-    |> String.concat "; "
+  let string_of_infos (infos: info_map) = 
+    VarMap.bindings infos
+      |> List.map (fun (var, info) -> Var.string_of var ^ " -> " ^ string_of_info info)
+      |> String.concat "; "
+  
+  let string_of_interval (s: I.t) (i : info)=
+    I.show s ^ "  (" ^ string_of_info i ^ ")"
 
   let string_of (t: t) =
     "{ affeq = " ^ Matrix.show t.affeq
     ^ "; intervals = [" ^ string_of_interval_map t.intervals ^ "]"
-    ^ "; slacks = [" ^ string_of_slack_map t.slacks ^ "] }"
+    ^ "; slacks = [" ^ string_of_infos t.infos ^ "] }"
 
   let meet (a: t) (b: t) =
     if equal a b then a else empty ()
