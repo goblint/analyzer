9141a68b239dba436e0a0f7f07bb46715d404158
Simmo Saan <simmo.saan@gmail.com>
2026-07-15 14:52:23 +0300

    Add Vg.Either experiment

--- src/framework/vg.ml
+++ src/framework/vg.ml
@@ -1,5 +1,10 @@
 open Analyses
 
+module type S0 =
+sig
+
+end
+
 module type S =
 sig
   module V0: SpecSysVar
@@ -21,6 +26,69 @@ struct
 
   let global man = man.global
   let sideg man = man.sideg
+end
+
+
+module Either (VG1: S) (VG2: S):
+sig
+  module V: SpecSysVar
+  module G: Lattice.S
+  module Left: S with module V0 = VG1.V0 and module G0 = VG1.G0 and module V = V and module G = G
+  module Right: S with module V0 = VG2.V0 and module G0 = VG2.G0 and module V = V and module G = G
+end =
+struct
+  module V =
+  struct
+    include Printable.Either (VG1.V) (VG2.V)
+    include StdV
+  end
+  module G = Lattice.Lift2 (VG1.G) (VG2.G)
+
+  module Left: S with module V0 = VG1.V0 and module G0 = VG1.G0 and module V = V and module G = G =
+  struct
+    module V0 = VG1.V0
+    module G0 = VG1.G0
+    module V = V
+    module G = G
+
+    let man' man: (_, VG1.G.t, _, VG1.V.t) man =
+        {
+          man with
+          global = (fun x -> match man.global (`Left x) with
+            | `Bot -> VG1.G.bot ()
+            | `Lifted1 x -> x
+            | _ -> assert false);
+          sideg = (fun g x -> man.sideg (`Left g) (`Lifted1 x));
+        }
+
+    let global man g =
+      VG1.global (man' man) g
+    let sideg man g x =
+      VG1.sideg (man' man) g x
+  end
+
+  module Right: S with module V0 = VG2.V0 and module G0 = VG2.G0 and module V = V and module G = G =
+  struct
+    module V0 = VG2.V0
+    module G0 = VG2.G0
+    module V = V
+    module G = G
+
+    let man' man: (_, VG2.G.t, _, VG2.V.t) man =
+        {
+          man with
+          global = (fun x -> match man.global (`Right x) with
+            | `Bot -> VG2.G.bot ()
+            | `Lifted2 x -> x
+            | _ -> assert false);
+          sideg = (fun g x -> man.sideg (`Right g) (`Lifted2 x));
+        }
+
+    let global man g =
+      VG2.global (man' man) g
+    let sideg man g x =
+      VG2.sideg (man' man) g x
+  end
 end
 
 
@@ -64,6 +132,24 @@ struct
     | `Bot -> Seq.empty
     | `Lifted2 x -> V0Set.to_seq x
     | _ -> assert false
+end
+
+module Make32 (V: SpecSysVar) (G: Lattice.S): S2 with module V0 = V and module G0 = G =
+struct
+  module V0 = V
+  module G0 = G
+  module V0Set = SetDomain.Make (V)
+  include Either (Make (V) (G)) (Make (UnitV) (V0Set))
+
+  let global = Left.global
+
+  let sideg man g x =
+    Left.sideg man g x;
+    Right.sideg man () (V0Set.singleton g)
+
+  let all_globals man =
+    Right.global man ()
+    |> V0Set.to_seq
 end
 
 module Make3 (V: SpecSysVar) (G: Lattice.S): S2 with module V0 = V and module G0 = G =
