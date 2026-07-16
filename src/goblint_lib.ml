1a8486abbf95308eea9b00a25c6022e8b2cfe63f
Michael Schwarz <michael.schwarz93@gmail.com>
2026-07-16 17:28:21 +0200

    Remove unused StackTrace Analysis

--- src/goblint_lib.ml
+++ src/goblint_lib.ml
@@ -179,7 +179,6 @@ module LoopfreeCallstring = LoopfreeCallstring
 module Uninit = Uninit
 module Expsplit = Expsplit
 module BranchSet = BranchSet
-module StackTrace = StackTrace
 
 (** {2 Helper}
 
