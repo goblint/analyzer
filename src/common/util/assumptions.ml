278409a3df2fd51eeaa84a9b4197fd3132322cca
Simmo Saan <simmo.saan@gmail.com>
2026-09-04 16:12:45 +0300

    Document Assumptions module

--- src/common/util/assumptions.ml
+++ src/common/util/assumptions.ml
@@ -1,8 +1,12 @@
+(** Assumptions used by the analysis. *)
+
+(** Declare used assumption at current (or provided) location. *)
 let add ?loc ?tags fmt =
   GoblintCil.Pretty.gprintf (fun doc ->
       Messages.info ?loc ~category:Assumption ?tags "%a" GoblintCil.Pretty.insert doc;
       Messages.msg_final Info ~category:Assumption ?tags "%a" GoblintCil.Pretty.insert doc;
     ) fmt
 
+(** Declare used assumption without location. *)
 let add_noloc ?tags fmt =
   Messages.msg_final Info ~category:Assumption ?tags fmt
