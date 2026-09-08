24b18445f4396fdf5678db8954586b6f61d9acb6
Simmo Saan <simmo.saan@gmail.com>
2026-09-08 15:53:31 +0300

    Add optional final argument to Assumptions.add

--- src/common/util/assumptions.ml
+++ src/common/util/assumptions.ml
@@ -1,10 +1,14 @@
 (** Assumptions used by the analysis. *)
 
 (** Declare used assumption at current (or provided) location. *)
-let add ?loc ?tags fmt =
+let add ?loc ?tags ?final fmt =
   GoblintCil.Pretty.gprintf (fun doc ->
       Messages.info ?loc ~category:Assumption ?tags "%a" GoblintCil.Pretty.insert doc;
-      Messages.msg_final Info ~category:Assumption ?tags "%a" GoblintCil.Pretty.insert doc;
+      match final with
+      | None -> (* use name message for final *)
+        Messages.msg_final Info ~category:Assumption ?tags "%a" GoblintCil.Pretty.insert doc
+      | Some final -> (* use different unformatted message for final *)
+        Messages.msg_final Info ~category:Assumption ?tags "%s" final
     ) fmt
 
 (** Declare used assumption without location. *)
