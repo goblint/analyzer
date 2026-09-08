2c3d3e99aab15d1dbcac0f5b71f0c4a1241afabc
Simmo Saan <simmo.saan@gmail.com>
2026-09-08 12:08:09 +0300

    Remove ikind argument from IndexDomain.of_congruence
    Should be the default ptrdiff_t.

--- src/cdomain/value/cdomains/intDomain_intf.ml
+++ src/cdomain/value/cdomains/intDomain_intf.ml
@@ -340,7 +340,7 @@ sig
 
   val of_interval: ?suppress_ovwarn:bool -> int_t * int_t -> t
 
-  val of_congruence: Cil.ikind -> int_t * int_t -> t
+  val of_congruence: int_t * int_t -> t
 
   val of_bitfield: Cil.ikind -> int_t * int_t -> t
   val to_bitfield: Cil.ikind -> t -> int_t * int_t
