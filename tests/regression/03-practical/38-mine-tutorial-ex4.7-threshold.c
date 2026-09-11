875516b76acc1fe34a25aec5e6d1ca4b4fe02fe8
Simmo Saan <simmo.saan@gmail.com>
2026-09-11 14:32:17 +0300

    Find widening thresholds from reset conditions

--- tests/regression/03-practical/38-mine-tutorial-ex4.7-threshold.c
+++ tests/regression/03-practical/38-mine-tutorial-ex4.7-threshold.c
@@ -6,7 +6,7 @@ int main() {
   int x = 0;
   while (__VERIFIER_nondet_bool() == 0) {
     __goblint_check(0 <= x);
-    __goblint_check(x <= 40); // TODO
+    __goblint_check(x <= 40);
     if (__VERIFIER_nondet_bool() == 0) {
       x++;
       if (x > 40)
