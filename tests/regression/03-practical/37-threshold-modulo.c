186acb017c327428b157f9da7b35b8d4edfa058c
bernalalexis-try <292251307+bernalalexis-try@users.noreply.github.com>
2026-09-21 11:46:40 +0000

    Use __VERIFIER_nondet_bool in threshold-modulo test and add second example from #2135

--- tests/regression/03-practical/37-threshold-modulo.c
+++ tests/regression/03-practical/37-threshold-modulo.c
@@ -1,12 +1,12 @@
-// PARAM: --enable ana.int.interval --enable ana.int.interval_threshold_widening --set ana.int.interval_threshold_widening_constants comparisons --enable exp.no-narrow
+// PARAM: --enable ana.sv-comp.functions --enable ana.int.interval --enable ana.int.interval_threshold_widening --set ana.int.interval_threshold_widening_constants comparisons --enable exp.no-narrow
 #include <goblint.h>
 
-int nondet();
+extern _Bool __VERIFIER_nondet_bool();
 
 int main() {
   int x = 0;
 
-  while (nondet()) {
+  while (__VERIFIER_nondet_bool()) {
     x = (x + 1) % 10;
   }
 
@@ -15,11 +15,24 @@ int main() {
 
   int y = 0;
 
-  while (nondet()) {
+  while (__VERIFIER_nondet_bool()) {
     y = (y - 1) % 10;
   }
 
   __goblint_check(y >= -9);
+
+  // Second example from #2135: the counter is reset by a modulo guard rather
+  // than by a modulo assignment.
+  int z = 1;
+
+  while (__VERIFIER_nondet_bool()) {
+    if (z % 10 == 0)
+      z = 1;
+    else
+      z++;
+  }
+
+  __goblint_check(z <= 10);
 
   return 0;
 }
