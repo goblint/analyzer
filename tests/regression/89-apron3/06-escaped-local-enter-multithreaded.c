573bea657e4d6158be8c225a18e5fe92bd712286
Simmo Saan <simmo.saan@gmail.com>
2026-09-21 10:27:50 +0300

    Add SKIP to Apron tests from PR #2100
    These fail in marshal CI otherwise.

--- tests/regression/89-apron3/06-escaped-local-enter-multithreaded.c
+++ tests/regression/89-apron3/06-escaped-local-enter-multithreaded.c
@@ -1,4 +1,4 @@
-// PARAM: --set ana.activated[+] apron --enable ana.sv-comp.functions
+// SKIP PARAM: --set ana.activated[+] apron --enable ana.sv-comp.functions
 #include <goblint.h>
 #include <pthread.h>
 
